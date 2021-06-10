cd7833723eb0d1709e80e8539eb2ad70ce20ffa6
Phil Fried <phil@estuary.dev>
2021-06-10 15:01:15 -0400

    Verify kinesis partition key hashing consistincy

--- go/kinesis/shard_range_test.go
+++ go/kinesis/shard_range_test.go
@@ -14,6 +14,47 @@ import (
 // max u128
 const maxKinesisHash = "340282366920938463463374607431768211455"
 
+// Ideally, our behavior will match theirs exactly, although technically our results will be correct
+// even if it is different. If our behavior doesn't match, then there could potentially be
+// pathalogical edge cases where records from partially overlapping kinesis shards get hashed
+// disproportionately into a flow capture shard. The AWS docs don't document the particulars around
+// hashing partition keys, other than the fact that they use md5. This test exists to verify that
+// the given partition keys hash into the same kinesis shard that kinsis itself determines. Kinesis
+// doesn't expose the actual hashed values, only the unhashed partition key and the shard id that it
+// went into. So this was setup by manually creating a kinesis stream with 2 shards, adding records
+// with the given partition keys and seeing which shard they went into. This can, and possibly
+// should, be automated later, but for now it seems to give a level of confidence that's high
+// enough.
+func TestHashPartitionKeyMatchesKinesisHashing(t *testing.T) {
+	// The hash key range of the first of 2 kinesis shards.
+	var shard0Range, err = parseKinesisShardRange("0", "170141183460469231731687303715884105727")
+	require.NoError(t, err)
+
+	var testCases = []struct {
+		// The partition key that was provided in put-record
+		partitionKey string
+		// Whether the record was put into shard 0. False means it was put into shard 1
+		included bool
+	}{
+		{"canary", true},
+		{"fooo", false},
+		{"barticus", false},
+		{"snapple", false},
+		{"joseph", false},
+		{"jessica", false},
+		{"jeebus", true},
+		{"daffy", true},
+		{"fartition", true},
+		{"pancakes", false},
+		{"waffles", false},
+		{"crepes", false},
+	}
+	for _, tc := range testCases {
+		var keyHash = hashPartitionKey(&tc.partitionKey)
+		require.Equalf(t, tc.included, shard0Range.Includes(keyHash), "TC: %#v", tc)
+	}
+}
+
 func TestShardRangeTranslation(t *testing.T) {
 	var result, err = parseKinesisShardRange("0", maxKinesisHash)
 	require.NoError(t, err)
