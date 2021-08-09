234be87cc347992f776ae29a61b044b2a63f946b
Alex Burkhart <alex@estuary.dev>
2021-08-09 16:02:00 -0400

    Setup tracing

--- source-kafka/src/lib.rs
+++ source-kafka/src/lib.rs
@@ -1,5 +1,19 @@
+use tracing::info;
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::EnvFilter;
+
 pub mod connector;
 
 pub fn run(cmd: connector::Command) {
-    println!("You have selected {:?}", cmd);
+    setup_tracing();
+
+    info!("You have selected {:?}", cmd);
+}
+
+fn setup_tracing() {
+    tracing_subscriber::fmt()
+        .with_span_events(FmtSpan::ENTER | FmtSpan::EXIT)
+        .with_writer(std::io::stderr)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
 }
