01031c9c03f2586c0e3e8dfe806ae15729f1acc9
Will Donnelly <wgd@willdonnelly.net>
2021-08-25 19:22:14 -0700

    Refactored change streaming

--- source-postgres/main.go
+++ source-postgres/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"log"
 	"os"
 	"time"
 
@@ -91,24 +92,138 @@ func doRead(args airbyte.ReadCmd) error {
 		return errors.Wrap(err, "unable to establish replication connection")
 	}
 
-	if err := streamSnapshotEvents(ctx, conn, "public", "babynames", emitChangeRecord); err != nil {
-		return errors.Wrap(err, "error streaming snapshot events")
+	snapshot, err := SnapshotTable(ctx, conn, "public", "babynames")
+	if err != nil {
+		return errors.Wrap(err, "error creating table snapshot stream")
 	}
+	log.Printf("Created table snapshot stream at LSN=%q", snapshot.TransactionLSN())
+	if err := snapshot.Process(ctx, emitChangeRecord); err != nil {
+		return errors.Wrap(err, "error processing snapshot events")
+	}
+	log.Printf("Table snapshot processing complete")
 
 	// TODO: Emit a state update as soon as the snapshot is complete, don't
 	// wait for the first COMMIT message to occur. Or modify the logic in
 	// `streamReplicationEvents` to emit eagerly before doing anything else.
-
-	if err := streamReplicationEvents(ctx, replConn, SLOT_NAME, PUBLICATION_NAME, 0, emitChangeRecord); err != nil {
-		return errors.Wrap(err, "error streaming replication events")
+	sysident, err := pglogrepl.IdentifySystem(ctx, replConn)
+	if err != nil {
+		return errors.Wrap(err, "unable to get current LSN from database")
 	}
+	stream, err := StartReplication(ctx, replConn, SLOT_NAME, PUBLICATION_NAME, sysident.XLogPos)
+	if err != nil {
+		return errors.Wrap(err, "unable to start replication stream")
+	}
+
+	log.Printf("Beginning stream processing")
+	if err := stream.Process(ctx, emitChangeRecord); err != nil {
+		return errors.Wrap(err, "error processing replication events")
+	}
+	log.Printf("Stream processing completed (this shouldn't happen)")
+
+	// What's the data type for the state encoding which can represent
+	// things like "The connector was previously running on tables 'foo'
+	// and 'bar' through to <LSN>, and now it's been relaunched and there
+	// is a new table 'baz' which needs to be snapshot-scanned" and then
+	// a bit later "The table 'baz' has been snapshot-scanned and we are
+	// now doing a catchup stream of the replication events until we reach
+	// the same point in the logs"?
+	//
+	// Or rather, our state encoding largely just needs to represent the
+	// checkpoints in that process. Since there's no way to *resume* the
+	// snapshot scanning part, that should be treated as atomic. So we
+	// need to keep track of each table separately, and there are two
+	// states that a table can be in:
+	//   + Catchup(<LSN>) - Until we catch up with <LSN> in the replication
+	//     stream we will discard all events for this table.
+	//   + Active() - Just emit records for this table.
+	//
+	// What happens if the connector is shut down, a table is removed from
+	// the catalog, it's re-run for a while, and then the table is re-added?
+	// There's no hope of doing something better than re-scanning, so that
+	// implies that if a table vanishes from the catalog while being mentioned
+	// in the resume state we should erase it from future resume state outputs.
+
+	// There are two viable approaches here: either suspend log processing
+	// entirely using context cancellation, or else cause it to block on
+	// a semaphore *inside* of the handler until we're ready to resume.
+
+	// Okay, so the idea is that we're going to start up by opening a
+	// transaction for the full table scan and figure out what tables
+	// will need said full scan. These can be immediately scanned and
+	// the corresponding records emitted to stdout.
+	//
+	// Then we begin streaming processing from our resume cursor, and
+	// what we need to do is filter out any change events from our new
+	// tables until such a time as the change LSNs are greater than
+	// the transaction LSN in which we read the table initially.
+	//
+	// Now, what are the semantics if the connector crashes or gets
+	// killed prior to finishing the table scan and/or catch-up
+	// streaming? As I understand it, Flow/Gazette will bundle
+	// together data records and state updates into a "Checkpoint"
+	// of some sort, so what are the consistency guarantees there?
+	//
+	// I'm especially worried about the following scenario:
+	//   1. Connector launches, reads all table contents, begins catch-up
+	//      streaming (that is, event processing where the current LSN is
+	//      still less than the "start emitting events for the new table"
+	//      threshold), and emits at least one state update.
+	//   2. Connector gets killed
+	//   3. Some rows get deleted from that table
+	//   4. Connector is re-launched
+	//
+	// If we're not careful this could result in some deletions that will
+	// never be noticed. But if the checkpointing behavior relies on there
+	// being state updates then we might be able to provide better guarantees
+	// by suppressing any state updates until the catch-up streaming ends.
+	//
+	// Of course there's the possibility of a reinforcing loop there, if the
+	// catch-up streaming process consistently takes longer than the "kill and
+	// restart connector" threshold then the amount of data to be caught up on
+	// will just keep growing larger and larger each time it runs.
+	//
+	// Which brings us back to the "chunked" table scan option, which would
+	// require us to interleave table scans with replication event handling,
+	// but would in exchange allow us to say "when doing stream processing,
+	// ignore events on <table> for keys between <key> and <key> until you
+	// reach sequence number <LSN>".
+
+	// But that's more complex than currently required. To start with, let's
+	// simply do the "initial table scan" case. But specifically let's have
+	// an LSN per table
 
 	return nil
 }
 
-func emitChangeRecord(namespace, table string, eventType string, fields map[string]interface{}) error {
-	fields["_type"] = eventType
-	if eventType == "Delete" {
+func emitChangeRecord(event string, lsn pglogrepl.LSN, ns, table string, fields map[string]interface{}) error {
+	log.Printf("%s(LSN=%q, Table=%s.%s)", event, lsn, ns, table)
+
+	if event == "Commit" {
+		// TODO(wgd): What exactly are the semantics of transaction committing in the
+		// logical replication WAL? Should we be buffering Insert/Update/Delete operations
+		// and only emitting them as part of the Commit?
+		rawState, err := json.Marshal(ResumeState{
+			ResumeLSN: lsn,
+		})
+		if err != nil {
+			return errors.Wrap(err, "error encoding state message")
+		}
+		if err := json.NewEncoder(os.Stdout).Encode(airbyte.Message{
+			Type:  airbyte.MessageTypeState,
+			State: &airbyte.State{Data: json.RawMessage(rawState)},
+		}); err != nil {
+			return errors.Wrap(err, "error writing state message")
+		}
+		return nil
+	}
+
+	if event == "Begin" {
+		log.Printf("Ignoring BEGIN at LSN=%q", pglogrepl.LSN(lsn))
+		return nil
+	}
+
+	fields["_type"] = event
+	if event == "Delete" {
 		fields["_deleted"] = true
 	}
 
@@ -119,8 +234,8 @@ func emitChangeRecord(namespace, table string, eventType string, fields map[stri
 	if err := json.NewEncoder(os.Stdout).Encode(airbyte.Message{
 		Type: airbyte.MessageTypeRecord,
 		Record: &airbyte.Record{
+			Namespace: ns,
 			Stream:    table,
-			Namespace: namespace,
 			EmittedAt: time.Now().Unix(),
 			Data:      json.RawMessage(rawData),
 		},
--- source-postgres/snapshot.go
+++ source-postgres/snapshot.go
@@ -5,13 +5,55 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/jackc/pglogrepl"
 	"github.com/jackc/pgx/v4"
 	"github.com/pkg/errors"
 )
 
-func streamSnapshotEvents(ctx context.Context, conn *pgx.Conn, namespace, table string, handler ChangeEventHandler) error {
+type TableSnapshotStream struct {
+	conn        *pgx.Conn
+	transaction pgx.Tx
+	namespace   string
+	table       string
+	txLSN       pglogrepl.LSN
+}
+
+func SnapshotTable(ctx context.Context, conn *pgx.Conn, namespace, table string) (*TableSnapshotStream, error) {
+	transaction, err := conn.Begin(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to begin transaction")
+	}
+
+	// TODO(wgd): Using `pg_current_wal_lsn()` isn't technically correct, ideally
+	// there would be some query we could execute to get an LSN corresponding to
+	// the current transaction. But this will do until it becomes a problem or I
+	// can find something better.
+	//
+	// I'll note that this is why Netflix's "DBLog" uses a "Watermark Table"
+	// with a UUID for syncing up SELECT results with WAL entries, and say no
+	// more about it for now.
+	var txLSN pglogrepl.LSN
+	if err := transaction.QueryRow(ctx, `SELECT * FROM pg_current_wal_lsn();`).Scan(&txLSN); err != nil {
+		return nil, errors.Wrap(err, "unable to query transaction LSN")
+	}
+	log.Printf("Transaction LSN: %q", txLSN)
+
+	return &TableSnapshotStream{
+		conn:        conn,
+		transaction: transaction,
+		namespace:   namespace,
+		table:       table,
+		txLSN:       txLSN,
+	}, nil
+}
+
+func (s *TableSnapshotStream) TransactionLSN() pglogrepl.LSN {
+	return s.txLSN
+}
+
+func (s *TableSnapshotStream) Process(ctx context.Context, handler ChangeEventHandler) error {
 	log.Printf("streamSnapshotEvents()")
-	rows, err := conn.Query(ctx, fmt.Sprintf(`SELECT * FROM %s.%s;`, namespace, table))
+	rows, err := s.conn.Query(ctx, fmt.Sprintf(`SELECT * FROM %s.%s;`, s.namespace, s.table))
 	if err != nil {
 		return errors.Wrap(err, "unable to execute query")
 	}
@@ -27,9 +69,13 @@ func streamSnapshotEvents(ctx context.Context, conn *pgx.Conn, namespace, table 
 			colName := string(cols[idx].Name)
 			fields[colName] = val
 		}
-		if err := handler(namespace, table, "Insert", fields); err != nil {
+		if err := handler("Insert", s.txLSN, s.namespace, s.table, fields); err != nil {
 			return errors.Wrap(err, "error handling change event")
 		}
 	}
 	return nil
+}
+
+func (s *TableSnapshotStream) Close(ctx context.Context) error {
+	return s.transaction.Rollback(ctx)
 }
