c0acb0cd1f195c47ae5e31bb58e5ec48fbf06dd4
Johnny Graettinger <johnny531@gmail.com>
2021-09-08 15:37:05 +0000

    materializations: factor Postgres, Snowflake, and Webhook from Flow
    Add `materialize-boilerplate` and `testsupport` for implementing and
    testing materialization connectors.
    
    Add postgres, snowflake, and webhooks -- each extracted from the Flow
    repo -- as connectors managed and built by this repository.
    
    Issue #173

--- go.mod
+++ go.mod
@@ -4,13 +4,22 @@ go 1.16
 
 require (
 	cloud.google.com/go/storage v1.16.0
+	github.com/alecthomas/jsonschema v0.0.0-20210818095345-1014919a589c
 	github.com/aws/aws-sdk-go v1.40.29
-	github.com/estuary/protocols v0.0.0-20210825031721-68a8bbd0c136
+	github.com/bradleyjkemp/cupaloy v2.3.0+incompatible
+	github.com/estuary/protocols v0.0.0-20210907222139-aadf3b609d9e
+	github.com/gogo/protobuf v1.3.2
+	github.com/google/uuid v1.3.0
+	github.com/jackc/pgconn v1.8.0
+	github.com/jackc/pgx/v4 v4.10.1
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/snowflakedb/gosnowflake v1.6.1
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac
 	google.golang.org/api v0.54.0
+	google.golang.org/grpc v1.40.0
 )
 
 replace go.gazette.dev/core => github.com/jgraettinger/gazette v0.0.0-20210726192503-178f10d4ba3d
---
+++ materialize-postgres/sqlgen_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/estuary/connectors/testsupport"
+	sqlDriver "github.com/estuary/protocols/materialize/sql"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSQLGeneration(t *testing.T) {
+	var built = testsupport.BuildCatalog(t, "flow.yaml")
+	require.Empty(t, built.Errors)
+
+	var gen = sqlDriver.PostgresSQLGenerator()
+	var spec = &built.Materializations[0]
+	var table = sqlDriver.TableForMaterialization("test_table", "", &gen.IdentifierQuotes, spec.Bindings[0])
+
+	keyCreate, keyInsert, keyJoin, err := buildSQL(&gen, 123, table, spec.Bindings[0].FieldSelection)
+	require.NoError(t, err)
+
+	require.Equal(t, `
+		CREATE TEMPORARY TABLE flow_load_key_tmp_123 (
+			key1 BIGINT NOT NULL, key2 BOOLEAN NOT NULL
+		) ON COMMIT DELETE ROWS
+		;`, keyCreate)
+
+	require.Equal(t, `
+		INSERT INTO flow_load_key_tmp_123 (
+			key1, key2
+		) VALUES (
+			$1, $2
+		);`, keyInsert)
+
+	// Note the intentional missing semicolon, as this is a subquery.
+	require.Equal(t, `
+		SELECT 123, l.flow_document
+			FROM test_table AS l
+			JOIN flow_load_key_tmp_123 AS r
+			ON l.key1 = r.key1 AND l.key2 = r.key2
+		`, keyJoin)
+}
