665c13e16ec7abdd38f3b42b135917d787123f85
Zach Brown <zach@estuary.dev>
2021-10-06 21:56:34 +0000

    WIP: commit of BigQuery materialization

---
+++ materialize-bigquery/fence.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	pf "github.com/estuary/protocols/flow"
+	"github.com/sirupsen/logrus"
+)
+
+// StdFence is an installed barrier in a shared checkpoints table which prevents
+// other sessions from committing transactions under the fenced ID --
+// and prevents this Fence from committing where another session has in turn
+// fenced this instance off. This implementation of the Fence is for
+// *sql.DB compatable databases.
+type fence struct {
+	// checkpoint associated with this Fence.
+	checkpoint []byte
+	// fence is the current value of the monotonically increasing integer used to identify unique
+	// instances of transactions rpcs.
+	fence int64
+	// Full name of the fenced materialization.
+	materialization pf.Materialization
+	// [keyBegin, keyEnd) identify the range of keys covered by this Fence.
+	keyBegin uint32
+	keyEnd   uint32
+}
+
+// LogEntry returns a log.Entry with pre-set fields that identify the Shard ID and Fence
+func (f *fence) LogEntry() *logrus.Entry {
+	return logrus.WithFields(logrus.Fields{
+		"materialization": f.materialization,
+		"keyBegin":        f.keyBegin,
+		"keyEnd":          f.keyEnd,
+		"fence":           f.fence,
+	})
+}
+
+// Checkpoint returns the current checkpoint for the fence
+func (f *fence) Checkpoint() []byte {
+	return f.checkpoint
+}
+
+// SetCheckpoint sets the current checkpoint
+func (f *fence) SetCheckpoint(checkpoint []byte) {
+	f.checkpoint = checkpoint
+}
