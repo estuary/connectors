aa4df01670ce45e63a506b5df97572c87ab142aa
Will Donnelly <will@estuary.dev>
2021-11-16 17:03:57 -0600

    source-postgres: refactor table snapshot code
    After the watermarks rewrite the entire "table snapshot" abstraction
    is no longer necessary, and the whole thing can basically be inlined
    into the `scanTableChunk` function.

--- source-postgres/snapshot.go
+++ source-postgres/snapshot.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
-	"github.com/jackc/pglogrepl"
 	"github.com/jackc/pgx/v4"
 	"github.com/sirupsen/logrus"
 )
@@ -20,17 +19,57 @@ import (
 // `resumeKey` if non-nil. This is the entrypoint to the database-specific portion of the
 // backfill process.
 func scanTableChunk(ctx context.Context, conn *pgx.Conn, streamID string, keyColumns []string, resumeKey []byte) ([]*changeEvent, error) {
-	var snapshot, err = snapshotDatabase(ctx, conn)
-	if err != nil {
-		return nil, fmt.Errorf("error creating database snapshot: %w", err)
-	}
-	defer snapshot.Close(ctx)
+	logrus.WithFields(logrus.Fields{
+		"streamID":   streamID,
+		"keyColumns": keyColumns,
+		"resumeKey":  base64.StdEncoding.EncodeToString(resumeKey),
+	}).Debug("scanning table chunk")
 
-	events, err := snapshot.Table(streamID, keyColumns).ScanChunk(ctx, resumeKey)
-	if err != nil {
-		return nil, fmt.Errorf("error scanning table: %w", err)
-	}
+	// Split "public.foo" tableID into "public" schema and "foo" table name
+	var parts = strings.SplitN(streamID, ".", 2)
+	var schemaName, tableName = parts[0], parts[1]
 
+	// Build and execute a query to fetch the next `snapshotChunkSize` rows from the database
+	var query = buildScanQuery(resumeKey == nil, keyColumns, schemaName, tableName)
+	var args []interface{}
+	if resumeKey != nil {
+		var err error
+		args, err = unpackTuple(resumeKey)
+		if err != nil {
+			return nil, fmt.Errorf("error unpacking encoded tuple: %w", err)
+		}
+		if len(args) != len(keyColumns) {
+			return nil, fmt.Errorf("expected %d primary-key values but got %d", len(keyColumns), len(args))
+		}
+	}
+	logrus.WithField("query", query).WithField("args", args).Debug("executing query")
+	rows, err := conn.Query(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("unable to execute query %q: %w", query, err)
+	}
+	defer rows.Close()
+
+	// Process the results into `changeEvent` structs and return them
+	var cols = rows.FieldDescriptions()
+	var events []*changeEvent
+	for rows.Next() {
+		// Scan the row values and copy into the equivalent map
+		var vals, err = rows.Values()
+		if err != nil {
+			return nil, fmt.Errorf("unable to get row values: %w", err)
+		}
+		var fields = make(map[string]interface{})
+		for idx := range cols {
+			fields[string(cols[idx].Name)] = vals[idx]
+		}
+
+		events = append(events, &changeEvent{
+			Type:      "Insert",
+			Namespace: schemaName,
+			Table:     tableName,
+			Fields:    fields,
+		})
+	}
 	return events, nil
 }
 
@@ -58,93 +97,15 @@ func writeWatermark(ctx context.Context, conn *pgx.Conn, table, slot string) (st
 	return wm, nil
 }
 
-// A databaseSnapshot represents a long-running read transaction which
-// can be used to query the contents of various tables.
-type databaseSnapshot struct {
-	Transaction pgx.Tx
-	TxLSN       pglogrepl.LSN
-}
-
-// A tableSnapshot represents a consistent view of a single table in
-// the database. A TableSnapshot cannot be closed because it's really
-// just a struct combining a DatabaseSnapshot with a specific table
-// name and scanning key.
-type tableSnapshot struct {
-	DB         *databaseSnapshot
-	SchemaName string
-	TableName  string
-	KeyColumns []string
-
-	scanFromQuery string
-}
-
 // snapshotChunkSize controls how many rows will be read from the database in a
 // single query. In normal use it acts like a constant, it's just a variable here
 // so that it can be lowered in tests to exercise chunking behavior more easily.
 var snapshotChunkSize = 4096
 
-func snapshotDatabase(ctx context.Context, conn *pgx.Conn) (*databaseSnapshot, error) {
-	var transaction, err = conn.BeginTx(ctx, pgx.TxOptions{
-		// We could probably get away with `RepeatableRead` isolation here, but
-		// I see no reason not to err on the side of getting the strongest
-		// guarantees that we can, especially since the full scan is not the
-		// normal mode of operation and happens just once per table.
-		IsoLevel:   pgx.Serializable,
-		AccessMode: pgx.ReadOnly,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("unable to begin transaction: %w", err)
-	}
-	var snapshot = &databaseSnapshot{
-		Transaction: transaction,
-	}
-	// TODO(wgd): Empirically this function doesn't have a stable value during our read
-	// transaction, and I don't know what ordering guarantees we might actually have.
-	// Even though this is what Debezium does I remain unconvinced that it actually works
-	// in all circumstances. Revisit this after writing some sort of concurrent-changes
-	// stress test which could reveal problems.
-	if err := transaction.QueryRow(ctx, `SELECT * FROM pg_current_wal_lsn();`).Scan(&snapshot.TxLSN); err != nil {
-		snapshot.Close(ctx)
-		return nil, fmt.Errorf("unable to query transaction LSN: %w", err)
-	}
-	return snapshot, nil
-}
-
-func (s *databaseSnapshot) TransactionLSN() pglogrepl.LSN {
-	return s.TxLSN
-}
-
-func (s *databaseSnapshot) Table(tableID string, keyColumns []string) *tableSnapshot {
-	// Split "public.foo" tableID into "public" schema and "foo" table name
-	var parts = strings.SplitN(tableID, ".", 2)
-	var schemaName, tableName = parts[0], parts[1]
-
-	return &tableSnapshot{
-		DB:         s,
-		SchemaName: schemaName,
-		TableName:  tableName,
-		KeyColumns: keyColumns,
-	}
-}
-
-func (s *databaseSnapshot) Close(ctx context.Context) error {
-	return s.Transaction.Rollback(ctx)
-}
-
-func (s *tableSnapshot) TransactionLSN() pglogrepl.LSN {
-	return s.DB.TransactionLSN()
-}
-
-func (s *tableSnapshot) buildScanQuery(start bool) string {
-	// We cache the query used in start=false cases (ScanFrom) to avoid
-	// redundantly building it over and over for no reason.
-	if !start && s.scanFromQuery != "" {
-		return s.scanFromQuery
-	}
-
+func buildScanQuery(start bool, keyColumns []string, schemaName, tableName string) string {
 	// Construct strings like `(foo, bar, baz)` and `($1, $2, $3)` for use in the query
 	var pkey, args string
-	for idx, colName := range s.KeyColumns {
+	for idx, colName := range keyColumns {
 		if idx > 0 {
 			pkey += ", "
 			args += ", "
@@ -155,77 +116,11 @@ func (s *tableSnapshot) buildScanQuery(start bool) string {
 
 	// Construct the query itself
 	var query = new(strings.Builder)
-	fmt.Fprintf(query, "SELECT * FROM %s.%s", s.SchemaName, s.TableName)
+	fmt.Fprintf(query, "SELECT * FROM %s.%s", schemaName, tableName)
 	if !start {
 		fmt.Fprintf(query, " WHERE (%s) > (%s)", pkey, args)
 	}
 	fmt.Fprintf(query, " ORDER BY (%s)", pkey)
 	fmt.Fprintf(query, " LIMIT %d;", snapshotChunkSize)
-
-	// Cache where applicable and return
-	if !start {
-		s.scanFromQuery = query.String()
-		return s.scanFromQuery
-	}
 	return query.String()
-}
-
-// ScanChunk requests a chunk of rows from the table snapshot, transforms each row into
-// an `Insert` change event and gives it to the provided handler. If `resumeKey` is nil
-// the very first chunk of the table is scanned, otherwise it must contain the final
-// key from a previous chunk, and this chunk will begin immediately after that.
-func (s *tableSnapshot) ScanChunk(ctx context.Context, resumeKey []byte) ([]*changeEvent, error) {
-	var logFields = logrus.Fields{
-		"namespace":  s.SchemaName,
-		"table":      s.TableName,
-		"keyColumns": s.KeyColumns,
-		"resumeKey":  base64.StdEncoding.EncodeToString(resumeKey),
-		"txLSN":      s.TransactionLSN(),
-	}
-	logrus.WithFields(logFields).Debug("scanning table chunk")
-
-	var query = s.buildScanQuery(resumeKey == nil)
-	var args []interface{}
-	if resumeKey != nil {
-		var err error
-		args, err = unpackTuple(resumeKey)
-		if err != nil {
-			return nil, fmt.Errorf("error unpacking encoded tuple: %w", err)
-		}
-		if len(args) != len(s.KeyColumns) {
-			return nil, fmt.Errorf("expected %d primary-key values but got %d", len(s.KeyColumns), len(args))
-		}
-	}
-
-	logrus.WithField("query", query).WithField("args", args).Debug("executing query")
-	rows, err := s.DB.Transaction.Query(ctx, query, args...)
-	if err != nil {
-		return nil, fmt.Errorf("unable to execute query %q: %w", query, err)
-	}
-	defer rows.Close()
-	return s.processResults(ctx, rows)
-}
-
-func (s *tableSnapshot) processResults(ctx context.Context, rows pgx.Rows) ([]*changeEvent, error) {
-	var cols = rows.FieldDescriptions()
-	var events []*changeEvent
-	for rows.Next() {
-		// Scan the row values and copy into the equivalent map
-		var vals, err = rows.Values()
-		if err != nil {
-			return nil, fmt.Errorf("unable to get row values: %w", err)
-		}
-		var fields = make(map[string]interface{})
-		for idx := range cols {
-			fields[string(cols[idx].Name)] = vals[idx]
-		}
-
-		events = append(events, &changeEvent{
-			Type:      "Insert",
-			Namespace: s.SchemaName,
-			Table:     s.TableName,
-			Fields:    fields,
-		})
-	}
-	return events, nil
 }
