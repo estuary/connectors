94b6485af9c6b081e52aa7fc32acecbe04710360
Will Donnelly <will@estuary.dev>
2021-12-13 13:31:07 -0600

    Add `sqlcapture` package
    This package contains a lightly refactored version of the generic
    capture/discovery logic from `source-postgres`, with database-
    specific behaviors factored out via interfaces.
    
    This commit doesn't modify source-postgres in any way, it just
    adds new logic based heavily on the source-postgres originals.
    The next commit in this series contains the actual Postgres
    changes.

---
+++ sqlcapture/interface.go
@@ -0,0 +1,138 @@
+package sqlcapture
+
+import (
+	"context"
+
+	"github.com/alecthomas/jsonschema"
+)
+
+// ChangeOp encodes a change operation type.
+// It's compatible with Debezium's change event representation.
+// TODO(johnny): Factor into a shared package.
+type ChangeOp string
+
+const (
+	// InsertOp is an INSERT operation.
+	InsertOp ChangeOp = "c"
+	// UpdateOp is an UPDATE operation.
+	UpdateOp ChangeOp = "u"
+	// DeleteOp is a DELETE operation.
+	DeleteOp ChangeOp = "d"
+	// FlushOp is an internal-only ChangeOp which flushes a completed
+	// transaction, but is not actually serialized.
+	FlushOp ChangeOp = "x"
+)
+
+// SourceCommon is common source metadata for data capture events.
+// It's a subset of the corresponding Debezium message definition.
+// Our design goal is a high signal-to-noise representation which can be
+// trivially converted to a Debezium equivalent if desired. See:
+// 	https://debezium.io/documentation/reference/stable/connectors/postgresql.html#postgresql-create-events
+// TODO(johnny): Factor into a shared package.
+type SourceCommon struct {
+	Millis   int64  `json:"ts_ms,omitempty" jsonschema:"description=Unix timestamp (in millis) at which this event was recorded by the database."`
+	Schema   string `json:"schema" jsonschema:"description=Database schema (namespace) of the event."`
+	Snapshot bool   `json:"snapshot,omitempty" jsonschema:"description=Snapshot is true if the record was produced from an initial table backfill and unset if produced from the replication log."`
+	Table    string `json:"table" jsonschema:"description=Database table of the event."`
+
+	// Implementor's note: `snapshot` is a mildly contentious name, because it
+	// could imply involvement of a database snapshot that is not, in fact, used.
+	// A better term might be "backfill", but this is an established term in the
+	// CDC ecosystem and it's water under the bridge now.
+
+	// Fields which are part of the generalized Debezium representation
+	// but are not included here:
+	// * `version` string of the connector.
+	// * `connector` is the connector name.
+	// * `database` is the logical database name.
+	// * `name` string of the capture. Debezium connectors use strings like
+	//   "PostgreSQL_server", and it's expected that this string composes with
+	//   `schema` and `table` to produce an identifier under which an Avro schema is
+	//   registered within a schema registry, like "PostgreSQL_server.inventory.customers.Value".
+	//
+	// All of `version`, `connector`, and `database` are defined by the catalog specification
+	// and are available through logs. They seem noisy and low-signal here.
+}
+
+// SourceMetadata is source-specific metadata about data capture events.
+type SourceMetadata interface {
+	Common() SourceCommon
+	Cursor() string // TODO(wgd): Maybe json.RawMessage?
+}
+
+// ChangeEvent represents either an Insert/Update/Delete operation on a specific
+// row in the database, or a Commit event which indicates that the database is at
+// a consistent point from which we could restart in the future.
+type ChangeEvent struct {
+	Operation ChangeOp
+	Source    SourceMetadata
+	Before    map[string]interface{}
+	After     map[string]interface{}
+}
+
+// KeyFields returns suitable fields for extracting the event primary key.
+func (e *ChangeEvent) KeyFields() map[string]interface{} {
+	if e.Operation == DeleteOp {
+		return e.Before
+	}
+	return e.After
+}
+
+// Database represents the operations which must be performed on a specific database
+// during the course of a capture in order to perform discovery, backfill preexisting
+// data, and process replicated change events.
+type Database interface {
+	// TODO(wgd): Document specific methods
+	Connect(ctx context.Context) error
+	// TODO(wgd): Document specific methods
+	Close(ctx context.Context) error
+	// TODO(wgd): Document specific methods
+	StartReplication(ctx context.Context, startCursor string) (ReplicationStream, error)
+	// TODO(wgd): Document specific methods
+	WriteWatermark(ctx context.Context, watermark string) error
+	// TODO(wgd): Document specific methods
+	WatermarksTable() string
+	// TODO(wgd): Document specific methods
+	ScanTableChunk(ctx context.Context, schema, table string, keyColumns []string, resumeKey []interface{}) ([]ChangeEvent, error)
+	// TODO(wgd): Document specific methods
+	DiscoverTables(ctx context.Context) (map[string]TableInfo, error)
+	// TODO(wgd): Document specific methods
+	TranslateDBToJSONType(typeName string) (*jsonschema.Type, error)
+	// TODO(wgd): Document specific methods
+	TranslateRecordField(val interface{}) (interface{}, error)
+	// TODO(wgd): Document specific methods
+	DefaultSchema(ctx context.Context) (string, error)
+	// Returns an empty instance of the source-specific metadata (used for JSON schema generation).
+	EmptySourceMetadata() SourceMetadata
+}
+
+// ReplicationStream represents the process of receiving change events
+// from a database, managing keepalives and status updates, and translating
+// these changes into a stream of ChangeEvents.
+type ReplicationStream interface {
+	Events() <-chan ChangeEvent
+	Commit(ctx context.Context, cursor string) error
+	Close(ctx context.Context) error
+}
+
+// TableInfo holds metadata about a specific table in the database, and
+// is used during discovery to automatically generate catalog information.
+type TableInfo struct {
+	Name       string       // The PostgreSQL table name.
+	Schema     string       // The PostgreSQL schema (a namespace, in normal parlance) which contains the table.
+	Columns    []ColumnInfo // Information about each column of the table.
+	PrimaryKey []string     // An ordered list of the column names which together form the table's primary key.
+}
+
+// ColumnInfo holds metadata about a specific column of some table in the
+// database, and is used during discovery to automatically generate catalog
+// information.
+type ColumnInfo struct {
+	Name        string  // The name of the column.
+	Index       int     // The ordinal position of this column in a row.
+	TableName   string  // The name of the table to which this column belongs.
+	TableSchema string  // The schema of the table to which this column belongs.
+	IsNullable  bool    // True if the column can contain nulls.
+	DataType    string  // The PostgreSQL type name of this column.
+	Description *string // Stored PostgreSQL description of the column, if any.
+}
