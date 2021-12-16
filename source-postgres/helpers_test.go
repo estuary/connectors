bdf73fa37740c719a699fcaff8339a78f6d917fb
Will Donnelly <will@estuary.dev>
2021-12-16 12:04:04 -0600

    Factor out generic source-postgres tests into sqlcapture/tests
    Most of the source-postgres test suite is generally applicable to
    all SQL database captures, so this will allow MySQL and others to
    be supported with less connector-specific code.
    
    There are still a few PostgreSQL specific tests, for instance the
    TestSlotLSNAdvances and TestReplicaIdentity tests require using
    PostgreSQL-specific queries and concepts, but I think that I've
    struck an acceptable balance here.

--- source-postgres/helpers_test.go
+++ source-postgres/helpers_test.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/csv"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"strconv"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/estuary/connectors/sqlcapture"
-	"github.com/estuary/protocols/airbyte"
-	jsonpatch "github.com/evanphx/json-patch/v5"
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	"github.com/sirupsen/logrus"
@@ -38,6 +31,7 @@ var (
 var (
 	TestDefaultConfig Config
 	TestDatabase      *pgx.Conn
+	TestBackend       *postgresTestBackend
 )
 
 func TestMain(m *testing.M) {
@@ -88,6 +82,7 @@ func TestMain(m *testing.M) {
 	}
 	defer conn.Close(ctx)
 	TestDatabase = conn
+	TestBackend = &postgresTestBackend{conn: conn, cfg: TestDefaultConfig}
 
 	var exitCode = m.Run()
 	if err := replConn.Exec(ctx, fmt.Sprintf(`DROP_REPLICATION_SLOT %s;`, *TestReplicationSlot)).Close(); err != nil {
@@ -96,110 +91,42 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-// createTestTable is a test helper for creating a new database table and returning the
+type postgresTestBackend struct {
+	conn *pgx.Conn
+	cfg  Config
+}
+
+// CreateTable is a test helper for creating a new database table and returning the
 // name of the new table. The table is named "test_<testName>", or "test_<testName>_<suffix>"
 // if the suffix is non-empty.
-func createTestTable(ctx context.Context, t *testing.T, suffix string, tableDef string) string {
+func (tb *postgresTestBackend) CreateTable(ctx context.Context, t *testing.T, suffix string, tableDef string) string {
 	t.Helper()
 
 	var tableName = "test_" + strings.TrimPrefix(t.Name(), "Test")
 	if suffix != "" {
 		tableName += "_" + suffix
 	}
-	tableName = strings.ReplaceAll(tableName, "/", "_")
-	tableName = strings.ReplaceAll(tableName, "=", "_")
+	for _, str := range []string{"/", "=", "(", ")"} {
+		tableName = strings.ReplaceAll(tableName, str, "_")
+	}
 
 	logrus.WithFields(logrus.Fields{"table": tableName, "cols": tableDef}).Debug("creating test table")
-	dbQueryInternal(ctx, t, fmt.Sprintf(`DROP TABLE IF EXISTS %s;`, tableName))
-	dbQueryInternal(ctx, t, fmt.Sprintf(`CREATE TABLE %s%s;`, tableName, tableDef))
+	tb.Query(ctx, t, fmt.Sprintf(`DROP TABLE IF EXISTS %s;`, tableName))
+	tb.Query(ctx, t, fmt.Sprintf(`CREATE TABLE %s%s;`, tableName, tableDef))
 	t.Cleanup(func() {
 		logrus.WithField("table", tableName).Debug("destroying test table")
-		dbQueryInternal(ctx, t, fmt.Sprintf(`DROP TABLE %s;`, tableName))
+		tb.Query(ctx, t, fmt.Sprintf(`DROP TABLE %s;`, tableName))
 	})
 	return tableName
 }
 
-// shortTestContext is a test helper which creates a time-bounded context for running test logic
-func shortTestContext(t *testing.T) context.Context {
-	return longTestContext(t, 10*time.Second)
-}
-
-// longTestContext is a test helper which creates a time-bounded context for running test logic
-func longTestContext(t *testing.T, timeout time.Duration) context.Context {
-	var ctx, cancel = context.WithTimeout(context.Background(), timeout)
-	t.Cleanup(cancel)
-	return ctx
-}
-
-// testCatalog is a test helper for constructing a ConfiguredCatalog from stream names
-func testCatalog(streams ...string) airbyte.ConfiguredCatalog {
-	var catalog = airbyte.ConfiguredCatalog{}
-	for _, s := range streams {
-		catalog.Streams = append(catalog.Streams, airbyte.ConfiguredStream{
-			Stream: airbyte.Stream{Name: s, Namespace: "public"},
-		})
-	}
-	return catalog
-}
-
-// dbLoadCSV is a test helper which opens a CSV file and inserts its contents
-// into the specified database table. It supports strings and integers, however
-// integers are autodetected as "any element which can be parsed as an integer",
-// so the source dataset needs to be clean. For test data this should be fine.
-func dbLoadCSV(ctx context.Context, t *testing.T, table string, filename string, limit int) {
-	t.Helper()
-	logrus.WithFields(logrus.Fields{"table": table, "file": filename}).Info("loading csv")
-	var file, err = os.Open("testdata/" + filename)
-	if err != nil {
-		t.Fatalf("unable to open CSV file: %q", "testdata/"+filename)
-	}
-	defer file.Close()
-
-	var dataset [][]interface{}
-	var r = csv.NewReader(file)
-	// If `limit` is positive, load at most `limit` rows
-	for count := 0; count < limit || limit <= 0; count++ {
-		var row, err = r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			t.Fatalf("error reading from CSV: %v", err)
-		}
-
-		var datarow []interface{}
-		for _, elemStr := range row {
-			// Convert elements to numbers where possible
-			var numeric, err = strconv.ParseFloat(elemStr, 64)
-			if err == nil {
-				datarow = append(datarow, numeric)
-			} else {
-				datarow = append(datarow, elemStr)
-			}
-		}
-		dataset = append(dataset, datarow)
-
-		// Insert in chunks of 64 rows at a time
-		if len(dataset) >= 64 {
-			dbInsert(ctx, t, table, dataset)
-			dataset = nil
-		}
-	}
-	// Perform one final insert for the remaining rows
-	if len(dataset) > 0 {
-		dbInsert(ctx, t, table, dataset)
-	}
-}
-
-// dbInsert is a test helper for inserting multiple rows into TestDatabase
-// as a single transaction.
-func dbInsert(ctx context.Context, t *testing.T, table string, rows [][]interface{}) {
+func (tb *postgresTestBackend) Insert(ctx context.Context, t *testing.T, table string, rows [][]interface{}) {
 	t.Helper()
 
 	if len(rows) < 1 {
 		t.Fatalf("must insert at least one row")
 	}
-	var tx, err = TestDatabase.BeginTx(ctx, pgx.TxOptions{})
+	var tx, err = tb.conn.BeginTx(ctx, pgx.TxOptions{})
 	if err != nil {
 		t.Fatalf("unable to begin transaction: %v", err)
 	}
@@ -221,23 +148,20 @@ func dbInsert(ctx context.Context, t *testing.T, table string, rows [][]interfac
 	}
 }
 
-func argsTuple(argc int) string {
-	var tuple = "($1"
-	for idx := 1; idx < argc; idx++ {
-		tuple += fmt.Sprintf(",$%d", idx+1)
-	}
-	return tuple + ")"
+func (tb *postgresTestBackend) Update(ctx context.Context, t *testing.T, table string, whereCol string, whereVal interface{}, setCol string, setVal interface{}) {
+	t.Helper()
+	tb.Query(ctx, t, fmt.Sprintf("UPDATE %s SET %s = $1 WHERE %s = $2;", table, setCol, whereCol), setVal, whereVal)
 }
 
-// dbQuery is a test helper for executing arbitrary queries against TestDatabase
-func dbQuery(ctx context.Context, t *testing.T, query string, args ...interface{}) {
+func (tb *postgresTestBackend) Delete(ctx context.Context, t *testing.T, table string, whereCol string, whereVal interface{}) {
+	t.Helper()
+	tb.Query(ctx, t, fmt.Sprintf("DELETE FROM %s WHERE %s = $1;", table, whereCol), whereVal)
+}
+
+func (tb *postgresTestBackend) Query(ctx context.Context, t *testing.T, query string, args ...interface{}) {
 	t.Helper()
 	logrus.WithFields(logrus.Fields{"query": query, "args": args}).Debug("executing query")
-	dbQueryInternal(ctx, t, query, args...)
-}
-
-func dbQueryInternal(ctx context.Context, t *testing.T, query string, args ...interface{}) {
-	var rows, err = TestDatabase.Query(ctx, query, args...)
+	var rows, err = tb.conn.Query(ctx, query, args...)
 	if err != nil {
 		t.Fatalf("unable to execute query: %v", err)
 	}
@@ -251,209 +175,15 @@ func dbQueryInternal(ctx context.Context, t *testing.T, query string, args ...in
 	}
 }
 
-// verifiedCapture is a test helper which performs a database capture and automatically
-// verifies the result against a golden snapshot. It returns a list of all states
-// emitted during the capture, and updates the `state` argument to the final one.
-func verifiedCapture(ctx context.Context, t *testing.T, db sqlcapture.Database, catalog *airbyte.ConfiguredCatalog, state *sqlcapture.PersistentState, suffix string) []sqlcapture.PersistentState {
-	t.Helper()
-	var result, states = performCapture(ctx, t, db, catalog, state)
-	verifySnapshot(t, suffix, result)
-	return states
+func (tb *postgresTestBackend) GetDatabase() sqlcapture.Database {
+	var cfg = tb.cfg
+	return &postgresDatabase{config: &cfg}
 }
 
-// performCapture runs a new capture instance with the specified configuration, catalog,
-// and state. The resulting messages are stored into a buffer, and returned as a string
-// holding all emitted records, plus a list of all state updates. The records string is
-// sanitized of "nondeterministic" data like timestamps and LSNs which will vary across
-// test runs, and so can be fed directly into verifySnapshot.
-//
-// As a side effect the input state is modified to the final result state.
-func performCapture(ctx context.Context, t *testing.T, db sqlcapture.Database, catalog *airbyte.ConfiguredCatalog, state *sqlcapture.PersistentState) (string, []sqlcapture.PersistentState) {
-	t.Helper()
-
-	// Use a JSON round-trip to deep-copy the state, so that the act of running a
-	// capture can't modify the passed-in state argument, and thus we can treat
-	// the sequence of states as having value semantics within tests.
-	var bs, err = json.Marshal(state)
-	if err != nil {
-		t.Fatal(err)
+func argsTuple(argc int) string {
+	var tuple = "($1"
+	for idx := 1; idx < argc; idx++ {
+		tuple += fmt.Sprintf(",$%d", idx+1)
 	}
-	var cleanState = new(sqlcapture.PersistentState)
-	if err := json.Unmarshal(bs, cleanState); err != nil {
-		t.Fatal(err)
-	}
-
-	var buf = new(CaptureOutputBuffer)
-	if err := sqlcapture.RunCapture(ctx, db, catalog, cleanState, buf); err != nil {
-		t.Fatal(err)
-	}
-
-	var result, states = buf.Output()
-	if len(states) > 0 {
-		*state = states[len(states)-1]
-	}
-	return result, states
-}
-
-// A CaptureOutputBuffer receives the stream of output messages from a
-// Capture instance, recording State updates in one list and Records
-// in another.
-type CaptureOutputBuffer struct {
-	States    []sqlcapture.PersistentState
-	Snapshot  strings.Builder
-	lastState string
-}
-
-func (buf *CaptureOutputBuffer) Encode(v interface{}) error {
-	var msg, ok = v.(airbyte.Message)
-	if !ok {
-		return fmt.Errorf("output message is not an airbyte.Message: %#v", v)
-	}
-
-	// Accumulate State updates in one list
-	if msg.Type == airbyte.MessageTypeState {
-		return buf.bufferState(msg)
-	}
-	if msg.Type == airbyte.MessageTypeRecord {
-		return buf.bufferRecord(msg)
-	}
-	if msg.Type == airbyte.MessageTypeLog {
-		return nil // Ignore log messages when validating test output
-	}
-	return fmt.Errorf("unhandled message type: %#v", msg.Type)
-}
-
-func (buf *CaptureOutputBuffer) bufferState(msg airbyte.Message) error {
-	// Parse state data and store a copy for later resume testing. Note
-	// that because we're unmarshalling each state update from JSON we
-	// can rely on the states being independent and not sharing any
-	// pointer-identity in their 'Streams' map or `ScanRanges` lists.
-	var originalState sqlcapture.PersistentState
-	if err := json.Unmarshal(msg.State.Data, &originalState); err != nil {
-		return fmt.Errorf("error unmarshaling to PersistentState: %w", err)
-	}
-
-	// Sanitize state by rewriting the LSN to a constant, then encode
-	// back into new bytes.
-	var cleanState = sqlcapture.PersistentState{Cursor: "REDACTED", Streams: originalState.Streams}
-	var bs, err = json.Marshal(cleanState)
-	if err != nil {
-		return fmt.Errorf("error encoding cleaned state: %w", err)
-	}
-
-	// Suppress identical (after sanitizing LSN) successive state updates.
-	// This improves test stability in the presence of unexpected 'Commit'
-	// events (typically on other tables not subject to the current test).
-	if string(bs) == buf.lastState {
-		return nil
-	}
-	buf.lastState = string(bs)
-
-	// Buffer the original state for later resuming, and append the sanitized
-	// state to the output buffer.
-	buf.States = append(buf.States, originalState)
-	return buf.bufferMessage(airbyte.Message{
-		Type:  airbyte.MessageTypeState,
-		State: &airbyte.State{Data: json.RawMessage(bs)},
-	})
-}
-
-func (buf *CaptureOutputBuffer) bufferRecord(msg airbyte.Message) error {
-	buf.lastState = ""
-
-	// Blank fields which are not reproducible across tests.
-	var err error
-	if msg.Record.Data, err = jsonpatch.MergePatch(msg.Record.Data, []byte(`{
-		"_meta": {
-			"source": {
-				"loc": null,
-				"ts_ms": null
-			}
-		}
-	}`)); err != nil {
-		return err
-	}
-
-	return buf.bufferMessage(airbyte.Message{
-		Type: airbyte.MessageTypeRecord,
-		Record: &airbyte.Record{
-			Namespace: msg.Record.Namespace,
-			Stream:    msg.Record.Stream,
-			EmittedAt: 1234, // Replaced because non-reproducible
-			Data:      msg.Record.Data,
-		},
-	})
-}
-
-func (buf *CaptureOutputBuffer) bufferMessage(msg airbyte.Message) error {
-	var bs, err = json.Marshal(msg)
-	if err != nil {
-		return fmt.Errorf("error encoding sanitized message: %w", err)
-	}
-	buf.Snapshot.Write(bs)
-	buf.Snapshot.WriteByte('\n')
-	return nil
-}
-
-func (buf *CaptureOutputBuffer) Output() (string, []sqlcapture.PersistentState) {
-	return buf.Snapshot.String(), buf.States
-}
-
-// verifySnapshot loads snapshot content from a file and compares with the
-// actual result of a test. The snapshot filename is derived automatically
-// from the current test name, with an optional suffix in case a single
-// test needs multiple snapshots. In the event of a mismatch, a ".new"
-// file is written for ease of comparison/updating.
-func verifySnapshot(t *testing.T, suffix string, actual string) {
-	t.Helper()
-
-	var snapshotDir = "testdata"
-	var snapshotFile = snapshotDir + "/" + t.Name()
-	if suffix != "" {
-		snapshotFile += "_" + suffix
-	}
-	snapshotFile += ".snapshot"
-
-	var snapBytes, err = os.ReadFile(snapshotFile)
-	// Nonexistent snapshots aren't an error, because when adding a
-	// new test we'd like it to produce a "snapshot.new" file for us
-	// and the empty string won't match the expected result anyway.
-	if err != nil && !os.IsNotExist(err) {
-		t.Fatalf("error reading snapshot %q: %v", snapshotFile, err)
-	}
-
-	if actual == string(snapBytes) {
-		return
-	}
-
-	// Locate the first non-matching line and log it
-	var actualLines = strings.Split(actual, "\n")
-	var snapshotLines = strings.Split(string(snapBytes), "\n")
-	for idx := 0; idx < len(snapshotLines) || idx < len(actualLines); idx++ {
-		var x, y string
-		if idx < len(snapshotLines) {
-			x = snapshotLines[idx]
-		}
-		if idx < len(actualLines) {
-			y = actualLines[idx]
-		}
-		if x != y {
-			t.Errorf("snapshot %q mismatch at line %d", snapshotFile, idx)
-			t.Errorf("old: %s", x)
-			t.Errorf("new: %s", y)
-			break
-		}
-	}
-	if !t.Failed() {
-		t.Errorf("snapshot %q mismatch at undetermined line", snapshotFile)
-	}
-
-	// Replicate cupaloy's use of an environment variable to update snapshots in-place.
-	var newSnapshotFile = snapshotFile
-	if os.Getenv("UPDATE_SNAPSHOTS") != "1" {
-		newSnapshotFile += ".new"
-	}
-	if err := os.WriteFile(newSnapshotFile, []byte(actual), 0644); err != nil {
-		t.Errorf("error writing new snapshot file %q: %v", newSnapshotFile, err)
-	}
+	return tuple + ")"
 }
