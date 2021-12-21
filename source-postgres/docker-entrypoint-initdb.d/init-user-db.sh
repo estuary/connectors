9c4b40642246bc678c8e553e520c4cff8ad8d0b8
Travis Jenkins <travis@estuary.dev>
2021-12-21 14:50:46 -0500

    Update default password. Add comments.

--- source-postgres/docker-entrypoint-initdb.d/init-user-db.sh
+++ source-postgres/docker-entrypoint-initdb.d/init-user-db.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+# The alter system command at the end is redundant due command line settings. 
+#  Included as an example of how to set this without using the command line. 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER flow_capture WITH PASSWORD 'secret' REPLICATION;
+    CREATE USER flow_capture WITH PASSWORD 'flow_capture' REPLICATION;
     GRANT SELECT ON ALL TABLES IN SCHEMA public TO flow_capture;
     GRANT SELECT ON ALL TABLES IN SCHEMA information_schema, pg_catalog TO flow_capture;
     CREATE TABLE IF NOT EXISTS public.flow_watermarks (slot TEXT PRIMARY KEY, watermark TEXT);
     GRANT ALL PRIVILEGES ON TABLE public.flow_watermarks TO flow_capture;
     CREATE PUBLICATION flow_publication FOR ALL TABLES;
-    ALTER SYSTEM SET wal_level = logical;
+    ALTER SYSTEM SET wal_level = logical; 
 EOSQL
