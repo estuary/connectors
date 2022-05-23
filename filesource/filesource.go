9e78d3e632adf3f0f3876e7e8dcbeb0e2f8f170d
Johnny Graettinger <johnny@estuary.dev>
2022-05-23 22:12:00 +0000

    remove stale `parser` implementation (now in estaury/flow)

--- filesource/filesource.go
+++ filesource/filesource.go
@@ -12,7 +12,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/estuary/connectors/parser"
+	"github.com/estuary/flow/go/parser"
 	"github.com/estuary/flow/go/protocols/airbyte"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
