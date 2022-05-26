ed7a75ee37d044d282091d62a7134a9b575fddb4
Mahdi Dibaiee <mdibaiee@pm.me>
2022-05-26 10:10:38 +0100

    materialize-firebolt: fix prefix without trailing slash causing issues

--- materialize-firebolt/spec.go
+++ materialize-firebolt/spec.go
@@ -12,6 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	pf "github.com/estuary/flow/go/protocols/flow"
 	proto "github.com/gogo/protobuf/proto"
+	"strings"
 )
 
 // LoadSpec loads existing spec from S3 and create a map of it by table name
@@ -23,7 +24,8 @@ func LoadSpec(cfg config, materialization string) (map[string]*pf.Materializatio
 	sess := session.Must(session.NewSession(&awsConfig))
 	downloader := s3manager.NewDownloader(sess)
 	buf := aws.NewWriteAtBuffer([]byte{})
-	existingSpecKey := fmt.Sprintf("%s%s.flow.materialization_spec", cfg.S3Prefix, materialization)
+
+	existingSpecKey := strings.TrimLeft(fmt.Sprintf("%s/%s.flow.materialization_spec", cfg.S3Prefix, materialization), "/")
 
 	var existing pf.MaterializationSpec
 
@@ -68,7 +70,7 @@ func WriteSpec(cfg config, materialization *pf.MaterializationSpec, version stri
 	if err != nil {
 		return fmt.Errorf("marshalling materialization spec: %w", err)
 	}
-	specKey := fmt.Sprintf("%s%s.flow.materialization_spec", cfg.S3Prefix, materialization.Materialization)
+	specKey := strings.TrimLeft(fmt.Sprintf("%s/%s.flow.materialization_spec", cfg.S3Prefix, materialization.Materialization), "/")
 
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Bucket: &cfg.S3Bucket,
@@ -93,7 +95,7 @@ func CleanSpec(cfg config, materialization string) error {
 	}
 	sess := session.Must(session.NewSession(&awsConfig))
 	svc := s3.New(sess)
-	specKey := fmt.Sprintf("%s%s.flow.materialization_spec", cfg.S3Prefix, materialization)
+	specKey := strings.TrimLeft(fmt.Sprintf("%s/%s.flow.materialization_spec", cfg.S3Prefix, materialization), "/")
 	_, err := svc.DeleteObject(&s3.DeleteObjectInput{
 		Bucket: &cfg.S3Bucket,
 		Key:    &specKey,
