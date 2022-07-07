460acbec707e7cf7a8bf549be2663f08924e789d
Alex Burkhart <alex@estuary.dev>
2022-07-07 18:46:00 -0400

    Pass --network=host inside connector e2e tests

--- tests/materialize/flowctl/build-and-activate.sh
+++ tests/materialize/flowctl/build-and-activate.sh
@@ -8,9 +8,11 @@ flowctl api build \
   --directory "${TEST_DIR}"/builds \
   --build-id "${BUILD_ID}" \
   --source "file://${TEST_DIR}/${CATALOG}" \
+  --network host \
   --log.level debug
 
 flowctl api activate \
   --build-id "${BUILD_ID}" \
+  --network host \
   --log.level debug \
   --all
