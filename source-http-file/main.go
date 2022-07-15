a38e8e5a7365aa262ff181119cc3a31457d8e128
Mahdi Dibaiee <mdibaiee@pm.me>
2022-07-15 21:06:36 +0100

    source-http-file: remove modificationTime config

--- source-http-file/main.go
+++ source-http-file/main.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -16,29 +15,20 @@ import (
 	"github.com/estuary/flow/go/parser"
 )
 
-type modificationTime struct {
-	Type string `json:"type"`
-	Key  string `json:"key"`
-}
-
 type credentials struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
 }
 
 type config struct {
-	URL              string           `json:"url"`
-	ModificationTime modificationTime `json:"modificationTime"`
-	Credentials      *credentials     `json:"credentials"`
-	Parser           *parser.Config   `json:"parser"`
+	URL         string         `json:"url"`
+	Credentials *credentials   `json:"credentials"`
+	Parser      *parser.Config `json:"parser"`
 }
 
 func (c *config) Validate() error {
 	if c.URL == "" {
 		return fmt.Errorf("url is required")
-	}
-	if c.ModificationTime.Type == "" || c.ModificationTime.Key == "" {
-		return fmt.Errorf("modification time type and key are required")
 	}
 	return nil
 }
@@ -53,8 +43,9 @@ func (c *config) DiscoverRoot() string {
 }
 
 func (c *config) FilesAreMonotonic() bool {
-	// FIXME / Question: we have only one file, so I suppose it's monotonic
-	return true
+	// Conceptually, if the source files are monotonic, the same file will not be visited again, and only
+	// ascending file names are visited, so we set this to false.
+	return false
 }
 
 func (c *config) ParserConfig() *parser.Config {
@@ -74,23 +65,15 @@ type httpSource struct {
 	fileName  string
 }
 
-func ModTime(typ string, key string, resp *http.Response) (time.Time, error) {
+func ModTime(resp *http.Response) (time.Time, error) {
 	const LastModifiedHeaderLayout = "Mon, 02 Jan 2006 15:04:05 GMT"
 
-	if typ == "header" {
-		var value = resp.Header.Get(key)
-		if value == "" {
-			return time.Now(), fmt.Errorf("Could not find header %s in response of HEAD request", key)
-		}
-
-		if key == "Last-Modified" {
-			return time.Parse(LastModifiedHeaderLayout, value)
-		} else {
-			return time.Parse(time.RFC3339, value)
-		}
-	} else {
-		return time.Now(), fmt.Errorf("Unsupported Modification Time Type %s", typ)
+	var value = resp.Header.Get("Last-Modified")
+	if value == "" {
+		return time.Now(), fmt.Errorf("Could not find Last-Modified header in response of HEAD request")
 	}
+
+	return time.Parse(LastModifiedHeaderLayout, value)
 }
 
 func newHttpSource(ctx context.Context, cfg *config) (*httpSource, error) {
@@ -107,7 +90,7 @@ func newHttpSource(ctx context.Context, cfg *config) (*httpSource, error) {
 		return nil, fmt.Errorf("could not fetch HEAD %s: %w", cfg.URL, err)
 	}
 
-	modTime, err := ModTime(strings.ToLower(cfg.ModificationTime.Type), cfg.ModificationTime.Key, resp)
+	modTime, err := ModTime(resp)
 	if err != nil {
 		return nil, fmt.Errorf("processing modification time: %w", err)
 	}
@@ -164,60 +147,36 @@ func main() {
 		},
 		ConfigSchema: func(parserSchema json.RawMessage) json.RawMessage {
 			return json.RawMessage(`{
-		"$schema": "http://json-schema.org/draft-07/schema#",
-		"title":   "HTTP File Source",
-		"type":    "object",
-		"required": [
-			"url",
-      "modificationTime"
-		],
-		"properties": {
-			"url": {
-				"type":        "string",
-				"title":       "HTTP File URL",
-				"description": "A valid HTTP url for downloading the source file."
-			},
-      "modificationTime": {
-        "type": "object",
-        "title": "Modification Time",
-        "description": "Specify how the last modification time of the file can be determined from the HTTP response. For example the 'Last-Modified' key in header.",
-        "required": ["type", "key"],
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title":   "HTTP File Source",
+        "type":    "object",
+        "required": ["url"],
         "properties": {
-          "type": {
+          "url": {
             "type":        "string",
-            "title":       "Modification Time Type",
-            "enum": ["Header"],
-            "description": "Where in the HTTP response is the last modification time of the file available.",
-            "default": "Header"
+            "title":       "HTTP File URL",
+            "description": "A valid HTTP url for downloading the source file."
           },
-          "key": {
-            "type":        "string",
-            "title":       "Modification Time Key",
-            "description": "What is the modification time's key.",
-            "default": "Last-Modified"
-          }
+          "credentials": {
+            "title": "Credentials",
+            "oneOf": [{
+              "type": "object",
+              "title": "Basic Authentication",
+              "properties": {
+                "user": {
+                  "type": "string",
+                  "secret": true
+                },
+                "password": {
+                  "type": "string",
+                  "secret": true
+                }
+              }
+            }]
+          },
+          "parser": ` + string(parserSchema) + `
         }
-      },
-      "credentials": {
-        "title": "Credentials",
-        "oneOf": [{
-          "type": "object",
-          "title": "Basic Authentication",
-          "properties": {
-            "user": {
-              "type": "string",
-              "secret": true
-            },
-            "password": {
-              "type": "string",
-              "secret": true
-            }
-          }
-        }]
-      },
-			"parser": ` + string(parserSchema) + `
-		}
-    }`)
+      }`)
 		},
 		DocumentationURL: "https://go.estuary.dev/source-http-file",
 	}
