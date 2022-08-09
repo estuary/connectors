53f2241f5f34187859e709c09e4c3d7aee134e78
Mahdi Dibaiee <mdibaiee@pm.me>
2022-08-09 15:11:18 +0100

    source-firestore: periodic scan + listen on changes

--- source-firestore/main.go
+++ source-firestore/main.go
@@ -18,18 +18,29 @@ import (
 )
 
 type State struct {
-	// Maps each collection to the latest DocumentSnapshot that we have received
-	Snapshots map[string]interface{}
+	// Last time a full table scan was run, we use this to know if we need to do a full scan
+	// to reach eventual consistency
+	LastScan string
 }
 
 type config struct {
 	// Service account JSON key to use as Application Default Credentials
 	CredentialsJSON string `json:"googleCredentials" jsonschema:"title=Credentials,description=Google Cloud Service Account JSON credentials." jsonschema_extras:"secret=true,multiline=true"`
+
+	// How frequently should we scan all collections to ensure consistency
+	ScanInterval string `json:"scan_interval" jsonschema:"title=Scan Interval,description=How frequently should all collections be scanned to ensure consistency. See https://pkg.go.dev/time#ParseDuration for supported values.,default=12h"`
 }
 
 func (c *config) Validate() error {
 	if c.CredentialsJSON == "" {
 		return fmt.Errorf("googleCredentials is required")
+	}
+	if c.ScanInterval != "" {
+		var _, err = time.ParseDuration(c.ScanInterval)
+
+		if err != nil {
+			return fmt.Errorf("parsing scan interval failed: %w", err)
+		}
 	}
 	return nil
 }
@@ -167,6 +178,13 @@ func doDiscover(args airbyte.DiscoverCmd) error {
 	})
 }
 
+// struct sent over channel to encoder
+type encDocument struct {
+	doc        []byte
+	streamName string
+	state      []byte
+}
+
 func doRead(args airbyte.ReadCmd) error {
 	var cfg config
 	var state State
@@ -182,9 +200,29 @@ func doRead(args airbyte.ReadCmd) error {
 		}
 	}
 
-	if state.Snapshots == nil {
-		state.Snapshots = map[string]interface{}{}
+	var lastScan = time.UnixMilli(0)
+	if state.LastScan != "" {
+		var err error
+		lastScan, err = time.Parse(time.RFC3339, state.LastScan)
+		if err != nil {
+			return fmt.Errorf("parsing last scan checkpoint failed: %w", err)
+		}
 	}
+
+	var scanInterval, _ = time.ParseDuration("12h")
+	if cfg.ScanInterval != "" {
+		var err error
+		scanInterval, err = time.ParseDuration(cfg.ScanInterval)
+
+		if err != nil {
+			return fmt.Errorf("parsing scan interval failed: %w", err)
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"last_scan":     lastScan,
+		"scan_interval": scanInterval,
+	}).Info("state check for scan")
 
 	ctx := context.Background()
 	sa := option.WithCredentialsJSON([]byte(cfg.CredentialsJSON))
@@ -199,74 +237,175 @@ func doRead(args airbyte.ReadCmd) error {
 	}
 	defer client.Close()
 
+	// The channel where documents will be sent to from different streams
+	// this is here to ensure we write documents sequentially and avoid scrambled outputs
+	var docsCh = make(chan encDocument)
+
+	var eg = new(errgroup.Group)
+
+	for _, stream := range catalog.Streams {
+		var streamName = stream.Stream.Name
+
+		var collection = client.Collection(streamName)
+		eg.Go(func() error {
+			return fullScan(ctx, lastScan, scanInterval, collection, docsCh, catalog.Tail)
+		})
+
+		if catalog.Tail {
+			eg.Go(func() error {
+				return listenForChanges(ctx, collection, docsCh)
+			})
+		}
+	}
+
+	eg.Go(func() error {
+		return encodeDocs(docsCh)
+	})
+
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+
+	close(docsCh)
+
+	return nil
+}
+
+func encodeDocs(docsCh chan encDocument) error {
 	var enc = airbyte.NewStdoutEncoder()
+
 	for {
-		var changes = false
-		var now = time.Now()
-		for _, stream := range catalog.Streams {
-			var streamName = stream.Stream.Name
-			var streamState = state.Snapshots[streamName]
-			var streamCursor string
-			if len(stream.CursorField) > 0 {
-				streamCursor = stream.CursorField[0]
-			} else {
-				return fmt.Errorf("Cursor is mandatory for all bindings. Binding %s does not have a cursorField.", streamName)
+		encDoc := <-docsCh
+
+		if len(encDoc.doc) != 0 {
+			log.WithFields(log.Fields{
+				"doc":        string(encDoc.doc),
+				"streamName": encDoc.streamName,
+			}).Info("encoding doc")
+
+			if err := enc.Encode(&airbyte.Message{
+				Type: airbyte.MessageTypeRecord,
+				Record: &airbyte.Record{
+					Stream:    encDoc.streamName,
+					EmittedAt: time.Now().UTC().UnixNano() / int64(time.Millisecond),
+					Data:      encDoc.doc,
+				},
+			}); err != nil {
+				return err
 			}
-
-			var collection = client.Collection(streamName)
-			var query = collection.Query.OrderBy(streamCursor, firestore.Asc)
-			if streamState != "" {
-				query = query.StartAfter(streamState)
+			if err := enc.Encode(airbyte.Message{
+				Type:  airbyte.MessageTypeState,
+				State: &airbyte.State{Data: []byte("{}"), Merge: true},
+			}); err != nil {
+				return err
 			}
-			var docs = query.Documents(ctx)
+		} else if len(encDoc.state) != 0 {
+			if err := enc.Encode(airbyte.Message{
+				Type:  airbyte.MessageTypeState,
+				State: &airbyte.State{Data: encDoc.state},
+			}); err != nil {
+				return err
+			}
+		}
+	}
+}
 
-			for {
-				var doc, err = docs.Next()
-				if err == iterator.Done {
-					break
-				} else if err != nil {
-					return err
-				}
+func listenForChanges(ctx context.Context, collection *firestore.CollectionRef, docsCh chan encDocument) error {
+	var query = collection.Query
+	var snapshotsIterator = query.Snapshots(ctx)
 
+	log.WithFields(log.Fields{
+		"collection": collection.ID,
+	}).Info("listening for changes on collection")
+
+	for {
+		var snapshot, err = snapshotsIterator.Next()
+
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		for _, change := range snapshot.Changes {
+			if change.Kind == firestore.DocumentAdded || change.Kind == firestore.DocumentModified {
+				log.WithFields(log.Fields{
+					"kind": change.Kind,
+					"id":   change.Doc.Ref.ID,
+				}).Info("received change")
+
+				var doc = change.Doc
 				var data = doc.Data()
 				data[firestore.DocumentID] = doc.Ref.ID
 				docJson, err := json.Marshal(data)
 				if err != nil {
 					return err
 				}
-				if err = enc.Encode(&airbyte.Message{
-					Type: airbyte.MessageTypeRecord,
-					Record: &airbyte.Record{
-						Stream:    streamName,
-						EmittedAt: now.UTC().UnixNano() / int64(time.Millisecond),
-						Data:      docJson,
-					},
-				}); err != nil {
-					return err
+				log.WithFields(log.Fields{
+					"doc": string(docJson),
+					"id":  change.Doc.Ref.ID,
+				}).Info("received change doc")
+				docsCh <- encDocument{
+					streamName: collection.ID,
+					doc:        docJson,
 				}
-
-				state.Snapshots[streamName] = data[streamCursor]
-				changes = true
 			}
 		}
-
-		if changes {
-			newStateJson, err := json.Marshal(state)
-			if err != nil {
-				return err
-			}
-			if err = enc.Encode(airbyte.Message{
-				Type:  airbyte.MessageTypeState,
-				State: &airbyte.State{Data: newStateJson},
-			}); err != nil {
-				return err
-			}
-		}
-
-		if !catalog.Tail {
-			return nil
-		}
-
-		now = <-time.After(time.Millisecond * 500)
 	}
+
+	return nil
+}
+
+func fullScan(ctx context.Context, lastScan time.Time, scanInterval time.Duration, collection *firestore.CollectionRef, docsCh chan encDocument, tail bool) error {
+	var nextScan = time.Until(lastScan.Add(scanInterval))
+	// Wait until next scan
+	time.Sleep(nextScan)
+
+	log.WithFields(log.Fields{
+		"collection": collection.ID,
+	}).Info("full scan on collection")
+
+	var query = collection.Query
+	var docsIterator = query.Documents(ctx)
+
+	var scanTime, _ = time.Now().MarshalText()
+	for {
+		var doc, err = docsIterator.Next()
+
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		var data = doc.Data()
+		data[firestore.DocumentID] = doc.Ref.ID
+		docJson, err := json.Marshal(data)
+		if err != nil {
+			return err
+		}
+		docsCh <- encDocument{
+			streamName: collection.ID,
+			doc:        docJson,
+		}
+	}
+
+	var newState, err = json.Marshal(State{
+		LastScan: string(scanTime),
+	})
+	if err != nil {
+		return fmt.Errorf("marshalling state: %w", err)
+	}
+	docsCh <- encDocument{
+		state: newState,
+	}
+
+	// If we are not tailing, we close the channel after the full scan
+	// to allow the encoding routine to finish so the connector can exit
+	if !tail {
+		close(docsCh)
+		return nil
+	}
+
+	return fullScan(ctx, time.Now(), scanInterval, collection, docsCh, tail)
 }
