2b3b6c52ae8f550943867b37da932d032051bf85
Mahdi Dibaiee <mdibaiee@pm.me>
2022-08-19 16:43:03 +0100

    source-firestore: use flow protocol rpc, support nested docs

---
+++ source-firestore/helpers.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const DEFAULT_SCAN_INTERVAL string = "12h"
+const SCAN_INTERVAL_NEVER string = "never"
+const PATH_FIELD = "__path__"
+
+// Nested collections are referred to by their parent document in Firestore
+// e.g. parent_collection/my_doc_id123/nested_collection
+// however in resource spec, we use wildcard /*/ to refer to all nested_collections under parent_collection
+// this function matches collection paths with resource paths
+func collectionMatchesResourcePath(collectionPath string, resourcePath string) bool {
+	return collectionToResourcePath(collectionPath) == resourcePath
+}
+
+// Searches in list of bindings for one that matches the collection path
+func findBindingForCollectionPath(collectionPath string, bindings []resource) int {
+	for index, binding := range bindings {
+		if collectionMatchesResourcePath(collectionPath, binding.Path) {
+			return index
+		}
+	}
+
+	return -1
+}
+
+// Our resource paths are formatted with parent_collection/*/nested_collection
+// in order to query documents for such collections we use Firestore's Collection Groups
+// which operate on the name of the last collection. Note that this name might not be unique
+// so another check is necessary to make sure we do not mix documents from nested collections
+// that have the same ID, but different parent collections
+func getLastCollectionGroupID(resourcePath string) string {
+	var pieces = strings.Split(resourcePath, "/")
+	return pieces[len(pieces)-1]
+}
+
+func isRootCollection(resourcePath string) bool {
+	return !strings.Contains(resourcePath, "/")
+}
+
+// Firestore's original collection path is very verbose, and it includes the parent document ID
+// This function creates a path for a collection which only refers to its parent collections, not documents
+// e.g. projects/hello-flow-mahdi/databases/(default)/documents/group/OLgLVvZnykvFR4ZyqUuS/group
+// becomes group/*/group
+func collectionToResourcePath(collection string) string {
+	// the prefix up to the first "/documents" is unnecessary, so we remove that
+	var parts = strings.SplitN(collection, "/documents/", 2)
+	if len(parts) < 2 {
+		panic(fmt.Sprintf("collection path does not match expectations: %s", collection))
+	}
+	var after = parts[1]
+
+	// we now need to get rid of document references, which appear after every collection name
+	var pieces = strings.Split(after, "/")
+	var cleanedPath = ""
+
+	for i, piece := range pieces {
+		if i%2 == 0 {
+			cleanedPath = cleanedPath + "/*/" + piece
+		}
+	}
+
+	return strings.Trim(cleanedPath, "/*/")
+}
--- source-firestore/main.go
+++ source-firestore/main.go
@@ -4,27 +4,20 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"
 	"time"
 
-	firestore "cloud.google.com/go/firestore"
-	firebase "firebase.google.com/go"
 	schemagen "github.com/estuary/connectors/go-schema-gen"
-	"github.com/estuary/flow/go/protocols/airbyte"
-	log "github.com/sirupsen/logrus"
-	"golang.org/x/sync/errgroup"
-	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"
+	boilerplate "github.com/estuary/connectors/source-boilerplate"
+	pc "github.com/estuary/flow/go/protocols/capture"
 )
 
-type State struct {
-	// Last time a full table scan was run, we use this to know if we need to do a full scan
-	// to reach eventual consistency
-	LastScan time.Time
+type resource struct {
+	Path string `json:"path" jsonschema:"title=Path to Collection,description=Supports parent/*/nested to capture all nested collections of parent's children"`
 }
 
-const DEFAULT_SCAN_INTERVAL string = "12h"
-const SCAN_INTERVAL_NEVER string = "never"
+func (r resource) Validate() error {
+	return nil
+}
 
 type config struct {
 	// Service account JSON key to use as Application Default Credentials
@@ -38,7 +31,7 @@ func (c *config) Validate() error {
 	if c.CredentialsJSON == "" {
 		return fmt.Errorf("googleCredentials is required")
 	}
-	if c.ScanInterval != "" {
+	if c.ScanInterval != "" && c.ScanInterval != SCAN_INTERVAL_NEVER {
 		var _, err = time.ParseDuration(c.ScanInterval)
 
 		if err != nil {
@@ -48,356 +41,25 @@ func (c *config) Validate() error {
 	return nil
 }
 
-func (c *State) Validate() error {
-	return nil
-}
+type driver struct{}
 
-func main() {
+func (driver) Spec(ctx context.Context, req *pc.SpecRequest) (*pc.SpecResponse, error) {
 	var endpointSchema, err = schemagen.GenerateSchema("Google Firestore", &config{}).MarshalJSON()
 	if err != nil {
 		fmt.Println(fmt.Errorf("generating endpoint schema: %w", err))
 	}
-
-	var spec = airbyte.Spec{
-		SupportsIncremental:           true,
-		DocumentationURL:              "https://go.estuary.dev/source-firestore",
-		SupportedDestinationSyncModes: airbyte.AllDestinationSyncModes,
-		ConnectionSpecification:       json.RawMessage(endpointSchema),
+	resourceSchema, err := schemagen.GenerateSchema("Firestore Resource Spec", &resource{}).MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("generating resource schema: %w", err)
 	}
 
-	airbyte.RunMain(spec, doCheck, doDiscover, doRead)
+	return &pc.SpecResponse{
+		EndpointSpecSchemaJson: json.RawMessage(endpointSchema),
+		ResourceSpecSchemaJson: json.RawMessage(resourceSchema),
+		DocumentationUrl:       "https://go.estuary.dev/source-firestore",
+	}, nil
 }
 
-func doCheck(args airbyte.CheckCmd) error {
-	var result = &airbyte.ConnectionStatus{
-		Status: airbyte.StatusSucceeded,
-	}
-
-	var cfg config
-	if err := args.ConfigFile.Parse(&cfg); err != nil {
-		result.Status = airbyte.StatusFailed
-		result.Message = err.Error()
-	}
-
-	ctx := context.Background()
-	sa := option.WithCredentialsJSON([]byte(cfg.CredentialsJSON))
-	app, err := firebase.NewApp(ctx, nil, sa)
-	if err != nil {
-		result.Status = airbyte.StatusFailed
-		result.Message = err.Error()
-	}
-
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		result.Status = airbyte.StatusFailed
-		result.Message = err.Error()
-	}
-	defer client.Close()
-
-	return airbyte.NewStdoutEncoder().Encode(airbyte.Message{
-		Type:             airbyte.MessageTypeConnectionStatus,
-		ConnectionStatus: result,
-	})
-}
-
-const PATH_FIELD = "__path__"
-
-// Firestore's original collection path is very verbose, and it includes the parent document ID
-// This function creates a path for a collection which only refers to its parent collections, not documents
-// e.g. projects/hello-flow-mahdi/databases/(default)/documents/group/OLgLVvZnykvFR4ZyqUuS/group
-// becomes group/*/group
-func collectionPath(collection string) string {
-	// the prefix up to the first "/documents" is unnecessary, so we remove that
-	var parts = strings.SplitN(collection, "/documents/", 2)
-	if len(parts) < 2 {
-		panic(fmt.Sprintf("collection path does not match expectations: %s", collection))
-	}
-	var after = parts[1]
-
-	// we now need to get rid of document references, which appear after every collection name
-	var pieces = strings.Split(after, "/")
-	var cleanedPath = ""
-
-	for i, piece := range pieces {
-		if i%2 == 0 {
-			cleanedPath = cleanedPath + "/*/" + piece
-		}
-	}
-
-	return strings.Trim(cleanedPath, "/*/")
-}
-
-func doDiscover(args airbyte.DiscoverCmd) error {
-	var cfg config
-	if err := args.ConfigFile.Parse(&cfg); err != nil {
-		return err
-	}
-
-	ctx := context.Background()
-	sa := option.WithCredentialsJSON([]byte(cfg.CredentialsJSON))
-	app, err := firebase.NewApp(ctx, nil, sa)
-	if err != nil {
-		return err
-	}
-
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
-	var catalog = new(airbyte.Catalog)
-	err = discoverCollections(ctx, client, catalog)
-	if err != nil {
-		return err
-	}
-
-	var encoder = airbyte.NewStdoutEncoder()
-	return encoder.Encode(airbyte.Message{
-		Type:    airbyte.MessageTypeCatalog,
-		Catalog: catalog,
-	})
-}
-
-type EncoderRequestType int8
-
-const (
-	EncodeDocument EncoderRequestType = 0
-	StateUpdate    EncoderRequestType = 1
-)
-
-// struct sent over channel to encoder
-type encDocument struct {
-	requestType EncoderRequestType
-	doc         []byte
-	streamName  string
-	state       []byte
-}
-
-func doRead(args airbyte.ReadCmd) error {
-	var cfg config
-	var state State
-	var catalog airbyte.ConfiguredCatalog
-
-	if err := args.ConfigFile.Parse(&cfg); err != nil {
-		return err
-	} else if err := args.CatalogFile.Parse(&catalog); err != nil {
-		return err
-	} else if args.StateFile != "" {
-		if err := args.StateFile.Parse(&state); err != nil {
-			return err
-		}
-	}
-
-	var lastScan = state.LastScan
-
-	var scanInterval, _ = time.ParseDuration(DEFAULT_SCAN_INTERVAL)
-	if cfg.ScanInterval != "" && cfg.ScanInterval != SCAN_INTERVAL_NEVER {
-		var err error
-		scanInterval, err = time.ParseDuration(cfg.ScanInterval)
-
-		if err != nil {
-			return fmt.Errorf("parsing scan interval failed: %w", err)
-		}
-	}
-
-	log.WithFields(log.Fields{
-		"last_scan":     lastScan,
-		"scan_interval": scanInterval,
-	}).Info("state check for scan")
-
-	ctx := context.Background()
-	sa := option.WithCredentialsJSON([]byte(cfg.CredentialsJSON))
-	app, err := firebase.NewApp(ctx, nil, sa)
-	if err != nil {
-		return err
-	}
-
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
-	// The channel where documents will be sent to from different streams
-	// this is here to ensure we write documents sequentially and avoid scrambled outputs
-	var docsCh = make(chan encDocument)
-
-	var eg = new(errgroup.Group)
-
-	// TODO: support nested collections which are in the form of <collection>/<collection>/<collection>/...
-	for _, stream := range catalog.Streams {
-		var streamName = stream.Stream.Name
-
-		var collection = client.Collection(streamName)
-		if cfg.ScanInterval != SCAN_INTERVAL_NEVER {
-			eg.Go(func() error {
-				return fullScan(ctx, lastScan, scanInterval, collection, docsCh, catalog.Tail)
-			})
-		}
-
-		if catalog.Tail {
-			eg.Go(func() error {
-				return listenForChanges(ctx, collection, docsCh)
-			})
-		}
-	}
-
-	eg.Go(func() error {
-		return encodeDocs(docsCh)
-	})
-
-	if err := eg.Wait(); err != nil {
-		return err
-	}
-
-	close(docsCh)
-
-	return nil
-}
-
-func encodeDocs(docsCh chan encDocument) error {
-	var enc = airbyte.NewStdoutEncoder()
-
-	for {
-		encDoc := <-docsCh
-
-		if encDoc.requestType == EncodeDocument {
-			log.WithFields(log.Fields{
-				"doc":        string(encDoc.doc),
-				"streamName": encDoc.streamName,
-			}).Info("encoding doc")
-
-			if err := enc.Encode(&airbyte.Message{
-				Type: airbyte.MessageTypeRecord,
-				Record: &airbyte.Record{
-					Stream:    encDoc.streamName,
-					EmittedAt: time.Now().UTC().UnixNano() / int64(time.Millisecond),
-					Data:      encDoc.doc,
-				},
-			}); err != nil {
-				return err
-			}
-			if err := enc.Encode(airbyte.Message{
-				Type:  airbyte.MessageTypeState,
-				State: &airbyte.State{Data: []byte("{}"), Merge: true},
-			}); err != nil {
-				return err
-			}
-		} else if encDoc.requestType == StateUpdate {
-			if err := enc.Encode(airbyte.Message{
-				Type:  airbyte.MessageTypeState,
-				State: &airbyte.State{Data: encDoc.state},
-			}); err != nil {
-				return err
-			}
-		} else {
-			panic(fmt.Sprintf("unknown EncoderRequestType %v", encDoc.requestType))
-		}
-	}
-}
-
-func listenForChanges(ctx context.Context, collection *firestore.CollectionRef, docsCh chan encDocument) error {
-	var query = collection.Query
-	var snapshotsIterator = query.Snapshots(ctx)
-
-	log.WithFields(log.Fields{
-		"collection": collection.Path,
-	}).Info("listening for changes on collection")
-
-	for {
-		var snapshot, err = snapshotsIterator.Next()
-
-		if err == iterator.Done {
-			break
-		} else if err != nil {
-			return err
-		}
-
-		for _, change := range snapshot.Changes {
-			if change.Kind == firestore.DocumentAdded || change.Kind == firestore.DocumentModified {
-				log.WithFields(log.Fields{
-					"kind": change.Kind,
-					"id":   change.Doc.Ref.ID,
-				}).Info("received change")
-
-				var doc = change.Doc
-				var data = doc.Data()
-				data[firestore.DocumentID] = doc.Ref.ID
-				data[PATH_FIELD] = doc.Ref.Path
-				docJson, err := json.Marshal(data)
-				if err != nil {
-					return err
-				}
-				log.WithFields(log.Fields{
-					"doc": string(docJson),
-					"id":  change.Doc.Ref.ID,
-				}).Info("received change doc")
-				docsCh <- encDocument{
-					requestType: EncodeDocument,
-					streamName:  collectionPath(collection.Path),
-					doc:         docJson,
-				}
-			}
-		}
-	}
-
-	return nil
-}
-
-func fullScan(ctx context.Context, lastScan time.Time, scanInterval time.Duration, collection *firestore.CollectionRef, docsCh chan encDocument, tail bool) error {
-	var nextScan = time.Until(lastScan.Add(scanInterval))
-	// Wait until next scan
-	time.Sleep(nextScan)
-
-	log.WithFields(log.Fields{
-		"collection": collection.Path,
-	}).Info("full scan on collection")
-
-	var query = collection.Query
-	var docsIterator = query.Documents(ctx)
-
-	var scanTime = time.Now()
-	for {
-		var doc, err = docsIterator.Next()
-
-		if err == iterator.Done {
-			break
-		} else if err != nil {
-			return err
-		}
-
-		var data = doc.Data()
-		data[firestore.DocumentID] = doc.Ref.ID
-		data[PATH_FIELD] = doc.Ref.Path
-		docJson, err := json.Marshal(data)
-		if err != nil {
-			return err
-		}
-		docsCh <- encDocument{
-			requestType: EncodeDocument,
-			streamName:  collectionPath(collection.Path),
-			doc:         docJson,
-		}
-	}
-
-	var newState, err = json.Marshal(State{
-		LastScan: scanTime,
-	})
-	if err != nil {
-		return fmt.Errorf("marshalling state: %w", err)
-	}
-	docsCh <- encDocument{
-		requestType: StateUpdate,
-		state:       newState,
-	}
-
-	// If we are not tailing, we close the channel after the full scan
-	// to allow the encoding routine to finish so the connector can exit
-	if !tail {
-		close(docsCh)
-		return nil
-	}
-
-	return fullScan(ctx, scanTime, scanInterval, collection, docsCh, tail)
+func main() {
+	boilerplate.RunMain(new(driver))
 }
