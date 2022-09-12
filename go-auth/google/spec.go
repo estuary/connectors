0532fa83dfe6f0367b488fde298d4ff7e66b29c1
Will Baker <wbaker@gmail.com>
2022-09-12 15:53:00 -0300

    format string as a const

--- go-auth/google/spec.go
+++ go-auth/google/spec.go
@@ -8,15 +8,12 @@ import (
 	pf "github.com/estuary/flow/go/protocols/flow"
 )
 
-func Spec(scopes ...string) *pf.OAuth2Spec {
-	authUrlTemplate := fmt.Sprintf(
-		"https://accounts.google.com/o/oauth2/auth?access_type=offline&prompt=consent&client_id={{ client_id }}&redirect_uri={{ redirect_uri }}&response_type=code&scope=%s&state={{ state }}",
-		strings.Join(scopes, " "),
-	)
+const AUTH_URL_TEMPLATE_FORMAT_STR = "https://accounts.google.com/o/oauth2/auth?access_type=offline&prompt=consent&client_id={{ client_id }}&redirect_uri={{ redirect_uri }}&response_type=code&scope=%s&state={{ state }}"
 
+func Spec(scopes ...string) *pf.OAuth2Spec {
 	return &pf.OAuth2Spec{
 		Provider:                   "google",
-		AuthUrlTemplate:            authUrlTemplate,
+		AuthUrlTemplate:            fmt.Sprintf(AUTH_URL_TEMPLATE_FORMAT_STR, strings.Join(scopes, " ")),
 		AccessTokenUrlTemplate:     "https://oauth2.googleapis.com/token",
 		AccessTokenBody:            "{\"grant_type\": \"authorization_code\", \"client_id\": \"{{ client_id }}\", \"client_secret\": \"{{ client_secret }}\", \"redirect_uri\": \"{{ redirect_uri }}\", \"code\": \"{{ code }}\"}",
 		AccessTokenResponseMapJson: json.RawMessage("{\"refresh_token\": \"/refresh_token\"}"),
