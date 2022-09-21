883cde15f7faefaf415166b27c556a5ab8884236
Johnny Graettinger <johnny@estuary.dev>
2022-09-21 00:59:46 +0000

    materialize-sql: new package for various SQL materializations
    This is a deep re-working of our existing `sql` package in the Flow
    repo, which leans much more heavily onto Go's template package.
    
    It's an earlier release candidate and we're leaving it side-by-side with
    the existing connectors for now.

---
+++ materialize-sql/endpoint.go
@@ -0,0 +1,167 @@
+package sql
+
+import (
+	"context"
+	"database/sql"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"text/template"
+
+	pf "github.com/estuary/flow/go/protocols/flow"
+	pm "github.com/estuary/flow/go/protocols/materialize"
+	"github.com/sirupsen/logrus"
+)
+
+type Client interface {
+	// FetchSpecAndVersion retrieves the materialization from Table `specs`,
+	// or returns sql.ErrNoRows if no such spec exists.
+	FetchSpecAndVersion(ctx context.Context, specs Table, materialization pf.Materialization) (specB64, version string, _ error)
+	ExecStatements(ctx context.Context, statements []string) error
+	InstallFence(ctx context.Context, checkpoints Table, fence Fence) (Fence, error)
+}
+
+// Resource is a driver-provided type which represents the SQL resource
+// (for example, a table) bound to by a binding.
+type Resource interface {
+	// Validate returns an error if the Resource is malformed.
+	Validate() error
+	// Path returns the fully qualified name of the resource, as '.'-separated components.
+	Path() TablePath
+	// DeltaUpdates is true if the resource should be materialized using delta updates.
+	DeltaUpdates() bool
+}
+
+// Fence is an installed barrier in a shared checkpoints table which prevents
+// other sessions from committing transactions under the fenced ID,
+// and prevents this Fence from committing where another session has in turn
+// fenced this instance off.
+type Fence struct {
+	// TablePath of the checkpoints table in which this Fence lives.
+	TablePath TablePath
+	// Full name of the fenced Materialization.
+	Materialization pf.Materialization
+	// [KeyBegin, KeyEnd) identify the range of keys covered by this Fence.
+	KeyBegin uint32
+	KeyEnd   uint32
+	// Fence is the current value of the monotonically increasing integer used
+	// to order and isolate instances of the Transactions RPCs.
+	// If fencing is not supported, Fence is zero.
+	Fence int64
+	// Checkpoint associated with this Fence.
+	// A zero-length Checkpoint indicates that the Endpoint is unable to supply
+	// a Checkpoint and that the recovery-log Checkpoint should be used instead.
+	Checkpoint []byte
+}
+
+// Endpoint is a driver description of the SQL endpoint being driven.
+type Endpoint struct {
+	// Config is an implementation-specific type for the Endpoint configuration.
+	Config interface{}
+	// Dialect of the Endpoint.
+	Dialect
+	// MetaSpecs is the specification meta-table of the Endpoint.
+	MetaSpecs TableShape
+	// MetaCheckpoints is the checkpoints meta-table of the Endpoint.
+	// It's optional, and won't be created or used if it's nil.
+	MetaCheckpoints *TableShape
+	// Client must either be a standard *sql.DB, or an implementation of MinClient.
+	// Fencing is not used with implementations of MinClient and the endpoint
+	// must implement the post-commit acknowledge flow for end-to-end exactly
+	// once semantics.
+	// See "Implementation Pattern: Recovery Log with Idempotent Apply" in the Driver.Transactions gRPC.
+	Client Client
+	// CreateTableTemplate evaluates a Table into an endpoint statement which creates it.
+	CreateTableTemplate *template.Template
+
+	// NewResource returns an uninitialized or partially-initialized Resource
+	// which will be parsed into and validated from a resource configuration.
+	NewResource func(*Endpoint) Resource
+	// NewTransactor returns a Transactor ready for pm.RunTransactions.
+	NewTransactor func(ctx context.Context, _ *Endpoint, _ Fence, bindings []Table) (pm.Transactor, error)
+}
+
+// loadSpec loads the named MaterializationSpec and its version that's stored within the Endpoint, if any.
+func loadSpec(ctx context.Context, endpoint *Endpoint, materialization pf.Materialization) (*pf.MaterializationSpec, string, error) {
+	var (
+		err              error
+		metaSpecs        Table
+		spec             = new(pf.MaterializationSpec)
+		specB64, version string
+	)
+
+	if metaSpecs, err = ResolveTable(endpoint.MetaSpecs, endpoint.Dialect); err != nil {
+		return nil, "", fmt.Errorf("resolving specifications table: %w", err)
+	}
+	specB64, version, err = endpoint.Client.FetchSpecAndVersion(ctx, metaSpecs, materialization)
+
+	if err == sql.ErrNoRows {
+		return nil, "", nil
+	} else if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"table": endpoint.MetaSpecs.Path,
+			"err":   err,
+		}).Info("failed to query materialization spec (the table may not be initialized?)")
+		return nil, "", nil
+	} else if specBytes, err := base64.StdEncoding.DecodeString(specB64); err != nil {
+		return nil, version, fmt.Errorf("base64.Decode: %w", err)
+	} else if err = spec.Unmarshal(specBytes); err != nil {
+		return nil, version, fmt.Errorf("spec.Unmarshal: %w", err)
+	} else if err = spec.Validate(); err != nil {
+		return nil, version, fmt.Errorf("validating spec: %w", err)
+	}
+
+	return spec, version, nil
+}
+
+// resolveResourceToExistingBinding identifies an existing binding of `loadedSpec`
+// which matches the parsed `resourceSpec`. It further identifies field constraints
+// of the resolved binding, and performs initial validation that the resource is
+// well-formed and is a valid transition for an existing binding (if present).
+func resolveResourceToExistingBinding(
+	endpoint *Endpoint,
+	resourceSpec json.RawMessage,
+	collection *pf.CollectionSpec,
+	loadedSpec *pf.MaterializationSpec,
+) (
+	constraints map[string]*pm.Constraint,
+	loadedBinding *pf.MaterializationSpec_Binding,
+	resource Resource,
+	err error,
+) {
+	resource = endpoint.NewResource(endpoint)
+
+	if err = pf.UnmarshalStrict(resourceSpec, resource); err != nil {
+		err = fmt.Errorf("resource binding for collection %q: %w", &collection.Collection, err)
+	} else if loadedBinding, err = findBinding(resource.Path(), collection.Collection, loadedSpec); err != nil {
+	} else if loadedBinding != nil && loadedBinding.DeltaUpdates && !resource.DeltaUpdates() {
+		// We allow a binding to switch from standard => delta updates but not the other way.
+		// This is because a standard materialization is trivially a valid delta-updates
+		// materialization, but a delta-updates table may have multiple instances of a given
+		// key and can no longer be loaded correctly by a standard materialization.
+		err = fmt.Errorf("cannot disable delta-updates binding of collection %s", collection.Collection)
+	} else if loadedBinding != nil {
+		constraints = ValidateMatchesExisting(loadedBinding, collection)
+	} else {
+		constraints = ValidateNewSQLProjections(resource, collection)
+	}
+
+	return
+}
+
+func findBinding(path TablePath, collection pf.Collection, spec *pf.MaterializationSpec) (*pf.MaterializationSpec_Binding, error) {
+	if spec == nil {
+		return nil, nil // Binding is trivially not found.
+	}
+	for i := range spec.Bindings {
+		var b = spec.Bindings[i]
+
+		if b.Collection.Collection == collection && path.equals(b.ResourcePath) {
+			return b, nil
+		} else if path.equals(b.ResourcePath) {
+			return nil, fmt.Errorf("cannot materialize %s to table %s because the table is already materializing collection %s",
+				path, &b.Collection.Collection, collection)
+		}
+	}
+	return nil, nil
+}
---
+++ materialize-sql/std_sql.go
@@ -0,0 +1,254 @@
+package sql
+
+import (
+	"context"
+	"database/sql"
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	pf "github.com/estuary/flow/go/protocols/flow"
+	"github.com/sirupsen/logrus"
+)
+
+// StdFetchSpecAndVersion is a convenience for Client implementations which
+// use Go's standard `sql.DB` type under the hood.
+func StdFetchSpecAndVersion(ctx context.Context, db *sql.DB, specs Table, materialization pf.Materialization) (specB64, version string, err error) {
+	// Fail-fast: surface a connection issue.
+	if err = db.PingContext(ctx); err != nil {
+		err = fmt.Errorf("connecting to DB: %w", err)
+		return
+	}
+	err = db.QueryRowContext(
+		ctx,
+		fmt.Sprintf(
+			"SELECT version, spec FROM %s WHERE materialization = %s;",
+			specs.Identifier,
+			specs.Keys[0].Placeholder,
+		),
+		materialization.String(),
+	).Scan(&version, &specB64)
+
+	return
+}
+
+// StdSQLExecStatements is a convenience for Client implementations which
+// use Go's standard `sql.DB` type under the hood.
+func StdSQLExecStatements(ctx context.Context, db *sql.DB, statements []string) error {
+	// Obtain a verified connection to the database.
+	// We don't explicitly wrap `statements` in a transaction, as not all
+	// databases support transactional DDL statements, but we do run them
+	// through a single connection. This allows a driver to explicitly run
+	// `BEGIN;` and `COMMIT;` statements around a transactional operation.
+	var conn, err = db.Conn(ctx)
+	if err == nil {
+		err = conn.PingContext(ctx)
+	}
+	if err != nil {
+		return fmt.Errorf("connecting to DB: %w", err)
+	}
+
+	for _, statement := range statements {
+		if _, err := conn.ExecContext(ctx, statement); err != nil {
+			return fmt.Errorf("executing statement (%s): %w", statement, err)
+		}
+		logrus.WithField("sql", statement).Debug("executed statement")
+	}
+	return conn.Close() // Release to pool.
+}
+
+// StdInstallFence is a convenience for Client implementations which
+// use Go's standard `sql.DB` type under the hood.
+func StdInstallFence(ctx context.Context, db *sql.DB, checkpoints Table, fence Fence) (Fence, error) {
+	var txn, err = db.BeginTx(ctx, nil)
+	if err != nil {
+		return Fence{}, fmt.Errorf("db.BeginTx: %w", err)
+	}
+	defer func() {
+		if txn != nil {
+			_ = txn.Rollback()
+		}
+	}()
+
+	// Increment the fence value of _any_ checkpoint which overlaps our key range.
+	if _, err = txn.Exec(
+		fmt.Sprintf(`
+			UPDATE %s
+				SET fence=fence+1
+				WHERE materialization=%s
+				AND key_end>=%s
+				AND key_begin<=%s
+			;
+			`,
+			checkpoints.Identifier,
+			checkpoints.Keys[0].Placeholder,
+			checkpoints.Keys[1].Placeholder,
+			checkpoints.Keys[2].Placeholder,
+		),
+		fence.Materialization,
+		fence.KeyBegin,
+		fence.KeyEnd,
+	); err != nil {
+		return Fence{}, fmt.Errorf("incrementing fence: %w", err)
+	}
+
+	// Read the checkpoint with the narrowest [key_begin, key_end] which fully overlaps our range.
+	var readBegin, readEnd uint32
+	var checkpointB64 string
+
+	if err = txn.QueryRow(
+		fmt.Sprintf(`
+			SELECT fence, key_begin, key_end, checkpoint
+				FROM %s
+				WHERE materialization=%s
+				AND key_begin<=%s
+				AND key_end>=%s
+				ORDER BY key_end - key_begin ASC
+				LIMIT 1
+			;
+			`,
+			checkpoints.Identifier,
+			checkpoints.Keys[0].Placeholder,
+			checkpoints.Keys[1].Placeholder,
+			checkpoints.Keys[2].Placeholder,
+		),
+		fence.Materialization,
+		fence.KeyBegin,
+		fence.KeyEnd,
+	).Scan(&fence.Fence, &readBegin, &readEnd, &checkpointB64); err == sql.ErrNoRows {
+		// Set an invalid range, which compares as unequal to trigger an insertion below.
+		readBegin, readEnd = 1, 0
+	} else if err != nil {
+		return Fence{}, fmt.Errorf("scanning fence and checkpoint: %w", err)
+	} else if fence.Checkpoint, err = base64.StdEncoding.DecodeString(checkpointB64); err != nil {
+		return Fence{}, fmt.Errorf("base64.Decode(checkpoint): %w", err)
+	}
+
+	// If a checkpoint for this exact range doesn't exist then insert it now.
+	if readBegin == fence.KeyBegin && readEnd == fence.KeyEnd {
+		// Exists; no-op.
+	} else if _, err = txn.Exec(
+		fmt.Sprintf(
+			"INSERT INTO %s (materialization, key_begin, key_end, fence, checkpoint) VALUES (%s, %s, %s, %s, %s);",
+			checkpoints.Identifier,
+			checkpoints.Keys[0].Placeholder,
+			checkpoints.Keys[1].Placeholder,
+			checkpoints.Keys[2].Placeholder,
+			checkpoints.Values[0].Placeholder,
+			checkpoints.Values[1].Placeholder,
+		),
+		fence.Materialization,
+		fence.KeyBegin,
+		fence.KeyEnd,
+		fence.Fence,
+		base64.StdEncoding.EncodeToString(fence.Checkpoint),
+	); err != nil {
+		return Fence{}, fmt.Errorf("inserting fence: %w", err)
+	}
+
+	err = txn.Commit()
+	txn = nil // Disable deferred rollback.
+
+	if err != nil {
+		return Fence{}, fmt.Errorf("txn.Commit: %w", err)
+	}
+	return fence, nil
+}
+
+// StdUpdateFence updates a Fence within the checkpoints Table.
+// It's a convenience for Client implementations which use Go's standard `sql.DB` type under the hood.
+func StdUpdateFence(ctx context.Context, txn *sql.Tx, checkpoints Table, fence Fence) error {
+	var result, err = txn.ExecContext(ctx,
+		fmt.Sprintf(
+			"UPDATE %s SET checkpoint=%s WHERE materialization=%s AND key_begin=%s AND key_end=%s AND fence=%s;",
+			checkpoints.Identifier,
+			checkpoints.Values[1].Placeholder,
+			checkpoints.Keys[0].Placeholder,
+			checkpoints.Keys[1].Placeholder,
+			checkpoints.Keys[2].Placeholder,
+			checkpoints.Values[0].Placeholder,
+		),
+		fence.Materialization,
+		fence.KeyBegin,
+		fence.KeyEnd,
+		fence.Fence,
+		base64.StdEncoding.EncodeToString(fence.Checkpoint),
+	)
+
+	if err != nil {
+		return fmt.Errorf("updating fence: %w", err)
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("fetching fence update rows: %w", err)
+	} else if rows != 1 {
+		return fmt.Errorf("this transactions session was fenced off by another")
+	}
+	return nil
+}
+
+// StdDumpTable returns a debug representation of the contents of a table.
+// It's a convenience for Client implementations which use Go's standard `sql.DB` type under the hood.
+func StdDumpTable(ctx context.Context, db *sql.DB, table Table) (string, error) {
+	var b strings.Builder
+
+	var keys []string
+	var all []string
+
+	for _, key := range table.Keys {
+		keys = append(keys, key.Identifier)
+		all = append(all, key.Identifier)
+	}
+	for _, val := range table.Values {
+		all = append(all, val.Identifier)
+	}
+
+	var sql = fmt.Sprintf("select %s from %s order by %s asc;",
+		strings.Join(all, ","),
+		table.Identifier,
+		strings.Join(keys, ","))
+
+	rows, err := db.Query(sql)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+
+	b.WriteString(strings.Join(all, ", "))
+
+	for rows.Next() {
+		var data = make([]anyColumn, len(table.Columns()))
+		var ptrs = make([]interface{}, len(table.Columns()))
+		for i := range data {
+			ptrs[i] = &data[i]
+		}
+		if err = rows.Scan(ptrs...); err != nil {
+			return "", err
+		}
+		b.WriteString("\n")
+		for i, v := range ptrs {
+			if i > 0 {
+				b.WriteString(", ")
+			}
+			var val = v.(*anyColumn)
+			b.WriteString(val.String())
+		}
+	}
+	return b.String(), nil
+}
+
+type anyColumn string
+
+func (col *anyColumn) Scan(i interface{}) error {
+	var sval string
+	if b, ok := i.([]byte); ok {
+		sval = string(b)
+	} else {
+		sval = fmt.Sprint(i)
+	}
+	*col = anyColumn(sval)
+	return nil
+}
+func (col anyColumn) String() string {
+	return string(col)
+}
