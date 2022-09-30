6adeef590dc2c0f734337855cff1e22ecb4ea318
Will Baker <wbaker@gmail.com>
2022-09-30 13:54:16 -0300

    improve end-to-end test compatibility

--- tests/run.sh
+++ tests/run.sh
@@ -10,6 +10,8 @@
 # -m turns on job management, required for our use of `fg` below.
 set -em
 
+command -v flowctl-admin >/dev/null 2>&1 || { echo >&2 "flowctl-admin must be available via PATH, aborting."; exit 1; }
+
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 cd "$ROOT_DIR"
 
@@ -49,16 +51,12 @@ export TESTDIR=$(realpath ${TESTDIR})
 export BROKER_ADDRESS=unix://localhost${TESTDIR}/gazette.sock
 export CONSUMER_ADDRESS=unix://localhost${TESTDIR}/consumer.sock
 
-# Always use the latest development package to verify the mutual integration
-# of connectors and the Flow runtime.
-curl -L --proto '=https' --tlsv1.2 -sSf "https://github.com/estuary/flow/releases/download/dev/flow-x86-linux.tar.gz" | tar -zx -C ${TESTDIR}
-
 # Start an empty local data plane within our TESTDIR as a background job.
 # --poll so that connectors are polled rather than continuously tailed.
 # --sigterm to verify we cleanly tear down the test catalog (otherwise it hangs).
 # --tempdir to use our known TESTDIR rather than creating a new temporary directory.
 # --unix-sockets to create UDS socket files in TESTDIR in well-known locations.
-${TESTDIR}/flowctl-admin temp-data-plane \
+flowctl-admin temp-data-plane \
     --log.level info \
     --network=host \
     --poll \
@@ -92,7 +90,7 @@ trap "kill -s SIGTERM ${DATA_PLANE_PID} && wait ${DATA_PLANE_PID} && ./tests/${C
 export ID_TYPE="${ID_TYPE:-integer}"
 
 # Verify discover works
-${TESTDIR}/flowctl-admin api discover --image="${CONNECTOR_IMAGE}" --network=host --log.level=debug --config=<(echo ${CONNECTOR_CONFIG}) > ${TESTDIR}/discover_output.json || bail "Discover failed."
+flowctl-admin api discover --image="${CONNECTOR_IMAGE}" --network=host --log.level=debug --config=<(echo ${CONNECTOR_CONFIG}) > ${TESTDIR}/discover_output.json || bail "Discover failed."
 cat ${TESTDIR}/discover_output.json | jq ".bindings[] | select(.recommendedName == \"${TEST_STREAM}\") | .documentSchema" > ${TESTDIR}/bindings.json
 
 if [[ -f "tests/${CONNECTOR}/bindings.json" ]]; then
@@ -103,16 +101,16 @@ fi
 cat tests/template.flow.yaml | envsubst > "${CATALOG_SOURCE}"
 
 # Build the catalog.
-${TESTDIR}/flowctl-admin api build --directory ${TESTDIR}/builds --build-id test-build-id --source ${CATALOG_SOURCE} --ts-package --network=host || bail "Build failed."
+flowctl-admin api build --directory ${TESTDIR}/builds --build-id test-build-id --source ${CATALOG_SOURCE} --ts-package --network=host || bail "Build failed."
 
 # Activate the catalog.
-${TESTDIR}/flowctl-admin api activate --build-id test-build-id --all --network=host --log.level info || bail "Activate failed."
+flowctl-admin api activate --build-id test-build-id --all --network=host --log.level info || bail "Activate failed."
 # Wait for a data-flow pass to finish.
-${TESTDIR}/flowctl-admin api await --build-id test-build-id --log.level info || bail "Await failed."
+flowctl-admin api await --build-id test-build-id --log.level info || bail "Await failed."
 # Read out materialization results.
 sqlite3 -header "${OUTPUT_DB}" "select id, canary from test_results;" > "${ACTUAL}"
 # Clean up the activated catalog.
-${TESTDIR}/flowctl-admin api delete --build-id test-build-id --all --log.level info || bail "Delete failed."
+flowctl-admin api delete --build-id test-build-id --all --log.level info || bail "Delete failed."
 
 # Verify actual vs expected results. `diff` will exit 1 if files are different
 diff --suppress-common-lines --side-by-side "${ACTUAL}" "tests/${CONNECTOR}/expected.txt" || bail "Test Failed"
