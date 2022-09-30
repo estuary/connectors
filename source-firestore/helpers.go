dc3216961c5fc8a2060d3a293d291c4ccf006b08
Will Donnelly <will@estuary.dev>
2022-09-30 15:40:08 -0500

    source-firestore: Addressing PR feedback

--- source-firestore/helpers.go
+++ source-firestore/helpers.go
@@ -8,25 +8,6 @@ import (
 type collectionGroupID = string
 type resourcePath = string
 
-// Nested collections are referred to by their parent document in Firestore
-// e.g. parent_collection/my_doc_id123/nested_collection
-// however in resource spec, we use wildcard /*/ to refer to all nested_collections under parent_collection
-// this function matches collection paths with resource paths
-func collectionMatchesResourcePath(collectionPath string, resourcePath string) bool {
-	return collectionToResourcePath(collectionPath) == resourcePath
-}
-
-// Searches in list of bindings for one that matches the collection path
-func findBindingForCollectionPath(collectionPath string, bindings []resource) int {
-	for index, binding := range bindings {
-		if collectionMatchesResourcePath(collectionPath, binding.Path) {
-			return index
-		}
-	}
-
-	return -1
-}
-
 // Our resource paths are formatted with parent_collection/*/nested_collection
 // in order to query documents for such collections we use Firestore's Collection Groups
 // which operate on the name of the last collection. Note that this name might not be unique
@@ -37,14 +18,11 @@ func getLastCollectionGroupID(path resourcePath) collectionGroupID {
 	return pieces[len(pieces)-1]
 }
 
-func isRootCollection(resourcePath string) bool {
-	return !strings.Contains(resourcePath, "/")
-}
-
-// Firestore's original collection path is very verbose, and it includes the parent document ID
-// This function creates a path for a collection which only refers to its parent collections, not documents
-// e.g. projects/hello-flow-mahdi/databases/(default)/documents/group/OLgLVvZnykvFR4ZyqUuS/group
-// becomes group/*/group
+// Transforms a collection path to a "resource path" which abstracts over
+// document IDs and omits the database path prefix. For example:
+//
+//	"projects/blissful-jet-364120/databases/(default)/documents/users/OLgLVvZnykvFR4ZyqUuS/messages"
+//	=> "users/*/messages"
 func collectionToResourcePath(collection string) resourcePath {
 	// the prefix up to the first "/documents" is unnecessary, so we remove that
 	var parts = strings.SplitN(collection, "/documents/", 2)
@@ -66,9 +44,11 @@ func collectionToResourcePath(collection string) resourcePath {
 	return strings.Trim(cleanedPath, "/*/")
 }
 
-func documentToResourcePath(documentPath string) string {
-	if idx := strings.LastIndex(documentPath, "/"); idx >= 0 {
-		return collectionToResourcePath(documentPath[:idx])
-	}
-	return documentPath
+// Transforms a document path to a "resource path" which abstracts over document
+// IDs and omits the database path prefix. For example:
+//
+//	"projects/blissful-jet-364120/databases/(default)/documents/users/OLgLVvZnykvFR4ZyqUuS/messages/136434"
+//	=> "users/*/messages"
+func documentToResourcePath(documentPath string) resourcePath {
+	return collectionToResourcePath(documentPath)
 }
