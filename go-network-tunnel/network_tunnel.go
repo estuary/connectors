a0c8dd6400c7699004977006f5704b782255af51
Mahdi Dibaiee <mdibaiee@pm.me>
2022-10-05 17:40:58 +0100

    postgres-rc + tunnel: first working version!

--- go-network-tunnel/network_tunnel.go
+++ go-network-tunnel/network_tunnel.go
@@ -21,19 +21,21 @@ type SshConfig struct {
 }
 
 type SshTunnel struct {
-	Config *SshConfig
-	ctx    context.Context
-	Cmd    *exec.Cmd
-	cancel context.CancelFunc
+	Config      *SshConfig
+	ctx         context.Context
+	Cmd         *exec.Cmd
+	tmpFileName string
+	cancel      context.CancelFunc
 }
 
 func (c *SshConfig) CreateTunnel() SshTunnel {
 	var ctx, cancel = context.WithCancel(context.Background())
 	return SshTunnel{
-		Config: c,
-		ctx:    ctx,
-		cancel: cancel,
-		Cmd:    nil,
+		Config:      c,
+		ctx:         ctx,
+		cancel:      cancel,
+		tmpFileName: "",
+		Cmd:         nil,
 	}
 }
 
@@ -43,10 +45,11 @@ func (t SshTunnel) Start() error {
 		return errors.New("This tunnel has already been started.")
 	}
 
-	var tmpKeyFile, err = os.CreateTemp("flow-network-tunnel", "")
+	var tmpKeyFile, err = os.CreateTemp("", "flow-network-tunnel")
 	if err != nil {
 		return fmt.Errorf("creating temporary file for private key: %w", err)
 	}
+	t.tmpFileName = tmpKeyFile.Name()
 
 	if _, err := tmpKeyFile.Write(t.Config.PrivateKey); err != nil {
 		return fmt.Errorf("writing key to temporary file: %w", err)
@@ -62,6 +65,7 @@ func (t SshTunnel) Start() error {
 	t.Cmd = exec.CommandContext(
 		t.ctx,
 		"flow-network-tunnel",
+		"ssh",
 		"--ssh-endpoint", t.Config.SshEndpoint,
 		"--private-key", tmpKeyFile.Name(),
 		"--forward-host", t.Config.ForwardHost,
@@ -74,7 +78,20 @@ func (t SshTunnel) Start() error {
 		return fmt.Errorf("could not get stdout pipe for network-tunnel: %w", err)
 	}
 
-	t.Cmd.Start()
+	stderr, err := t.Cmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("could not get stderr pipe for network-tunnel: %w", err)
+	}
+	go func() {
+		_, err = io.Copy(os.Stderr, stderr)
+		if err != nil {
+			log.WithField("error", err).Info("error copying stderr of tunnel")
+		}
+	}()
+
+	if err := t.Cmd.Start(); err != nil {
+		return fmt.Errorf("starting network-tunnel: %w", err)
+	}
 	// we want to read "READY" from stdout of tunnel
 	var readyBuf = make([]byte, 5)
 	var ready = []byte("READY")
@@ -91,4 +108,8 @@ func (t SshTunnel) Start() error {
 
 func (t SshTunnel) Stop() {
 	t.cancel()
+	// cleanup key file
+	if t.tmpFileName != "" {
+		os.Remove(t.tmpFileName)
+	}
 }
