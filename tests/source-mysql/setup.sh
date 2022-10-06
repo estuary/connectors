b8108746ea7dc6edd3d7a44aef0fc22cf7a72328
Mahdi Dibaiee <mdibaiee@pm.me>
2022-10-06 16:15:57 +0100

    fix source-mysql and source-postgres tests

--- tests/source-mysql/setup.sh
+++ tests/source-mysql/setup.sh
@@ -4,10 +4,17 @@ set -e
 export TEST_STREAM="estuary_test_$(shuf -zer -n6 {a..z} | tr -d '\0')"
 export RESOURCE="{ \"namespace\": \"test\", \"stream\": \"${TEST_STREAM}\" }"
 
+export MYSQL_HOST="${MYSQL_HOST:=source-mysql-db-1.flow-test}"
+export MYSQL_PORT="${MYSQL_PORT:=3306}"
+export MYSQL_USER="${MYSQL_USER:=flow}"
+export MYSQL_PASSWORD="${MYSQL_PASSWORD:=flow}"
+
+docker compose -f source-mysql/docker-compose.yaml up --detach
+
 config_json_template='{
     "address": "$MYSQL_HOST:$MYSQL_PORT",
     "user": "$MYSQL_USER",
-    "password": "$MYSQL_PWD"
+    "password": "$MYSQL_PASSWORD"
 }'
 
 export CONNECTOR_CONFIG="$(echo "$config_json_template" | envsubst | jq -c)"
@@ -17,10 +24,12 @@ root_dir="$(git rev-parse --show-toplevel)"
 
 function sql {
     echo "mysql> " $@
-    echo "$@" | mysql $MYSQL_DATABASE --user=$MYSQL_USER --local-infile=1
+    echo "$@" | docker exec -i \
+      source-mysql-db-1 \
+      mysql $MYSQL_DATABASE --user=$MYSQL_USER --local-infile=1
 }
 
 sql "DROP TABLE IF EXISTS test.${TEST_STREAM};"
 sql "CREATE TABLE test.${TEST_STREAM} (id INTEGER PRIMARY KEY, canary TEXT);"
 sql "SET GLOBAL local_infile=1;"
-sql "LOAD DATA LOCAL INFILE '${root_dir}/tests/files/b.csv' INTO TABLE ${TEST_STREAM} FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;"
+sql "LOAD DATA LOCAL INFILE '/b.csv' INTO TABLE ${TEST_STREAM} FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;"
--- tests/source-postgres/setup.sh
+++ tests/source-postgres/setup.sh
@@ -4,6 +4,14 @@ set -e
 export TEST_STREAM="estuary_test_$(shuf -zer -n6 {a..z} | tr -d '\0')"
 export RESOURCE="{ stream: ${TEST_STREAM} }"
 
+export PGHOST="${PGHOST:=source-postgres-postgres-1.flow-test}"
+export PGPORT="${PGPORT:=5432}"
+export PGDATABASE="${PGDATABASE:=flow}"
+export PGPASSWORD="${PGPASSWORD:=flow}"
+export PGUSER="${PGUSER:=flow}"
+
+docker compose -f source-postgres/docker-compose.yaml up --detach
+
 config_json_template='{
    "address":  "$PGHOST:$PGPORT",
    "database": "$PGDATABASE",
@@ -18,9 +26,15 @@ root_dir="$(git rev-parse --show-toplevel)"
 
 function sql {
     echo "psql> " $@
-    psql -c "$@"
+
+    docker exec \
+      -e PGUSER=$PGUSER \
+      -e PGPASSWORD=$PGPASSWORD \
+      -e PGDATABASE=$PGDATABASE \
+      source-postgres-postgres-1 \
+      psql -c "$@"
 }
 
 sql "DROP TABLE IF EXISTS ${TEST_STREAM};"
 sql "CREATE TABLE ${TEST_STREAM} (id INTEGER PRIMARY KEY, canary TEXT);"
-sql "\\copy ${TEST_STREAM} FROM '${root_dir}/tests/files/b.csv' DELIMITER ',' CSV HEADER;"
+sql "\\copy ${TEST_STREAM} FROM '/b.csv' DELIMITER ',' CSV HEADER;"
