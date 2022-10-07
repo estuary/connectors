cad9e18d7365f3c9a5a3953e6f1d7b694edf4868
Mahdi Dibaiee <mdibaiee@pm.me>
2022-10-07 11:57:09 +0100

    fix source-kafka tests

--- tests/source-kafka/cleanup.sh
+++ tests/source-kafka/cleanup.sh
@@ -4,5 +4,9 @@ set -e
 root_dir="$(git rev-parse --show-toplevel)"
 kafkactl_config="$root_dir/tests/source-kafka/kafkactl.yaml"
 
+function kctl() {
+  docker run -i --network flow-test --mount "type=bind,src=$kafkactl_config,target=/kafkactl.yaml" deviceinsight/kafkactl --config-file=/kafkactl.yaml $@
+}
+
 # Remove the test topic
-kafkactl --config-file=$kafkactl_config delete topic $TEST_STREAM
+kctl delete topic $TEST_STREAM
--- tests/source-kafka/setup.sh
+++ tests/source-kafka/setup.sh
@@ -7,7 +7,7 @@ export RESOURCE="{ stream: ${TEST_STREAM} }"
 # Because Flow uses network=host, the port exposed to Flow is different than the
 # one we use when running `docker exec` below.
 export CONNECTOR_CONFIG='{
-  "bootstrap_servers": ["localhost:9092"],
+  "bootstrap_servers": ["infra-kafka-1.flow-test:9092"],
   "authentication": {
     "mechanism": "SCRAM-SHA-256",
     "username": "alice",
@@ -20,6 +20,10 @@ root_dir="$(git rev-parse --show-toplevel)"
 kafkactl_config="$root_dir/tests/source-kafka/kafkactl.yaml"
 TOTAL_PARTITIONS=4
 
+function kctl() {
+  docker run -i --network flow-test --mount "type=bind,src=$kafkactl_config,target=/kafkactl.yaml" deviceinsight/kafkactl --config-file=/kafkactl.yaml $@
+}
+
 if [ -z $(which kafkactl) ]; then
   echo "kafkactl is required"
   exit 1
@@ -27,12 +31,12 @@ fi
 
 # Ensure we can connect to a broker.
 for i in $(seq 1 10); do
-  if [ -n "$(kafkactl --config-file=$kafkactl_config get topics)" ]; then
+  if [ -n "$(kctl get topics)" ]; then
     break
   else
     if [ $i -ge 10 ]; then
       echo "Can't connect to Kafka. Is the kafkactl config correct?"
-      kafkactl --config-file=$kafkactl_config config view
+      kctl config view
       exit 1
     fi
     sleep 1
@@ -40,11 +44,11 @@ for i in $(seq 1 10); do
 done
 
 # Create the topic with n partitions
-kafkactl --config-file=$kafkactl_config create topic $TEST_STREAM --partitions $TOTAL_PARTITIONS
+kctl create topic $TEST_STREAM --partitions $TOTAL_PARTITIONS
 
 # Seed the topic with documents
 for i in $(seq 1 $TOTAL_PARTITIONS); do
   cat $root_dir/tests/files/d.jsonl \
   | jq -cs "map(select(.id % $TOTAL_PARTITIONS == $i - 1)) | .[]" \
-  | kafkactl --config-file=$kafkactl_config produce $TEST_STREAM
+  | kctl produce $TEST_STREAM
 done
