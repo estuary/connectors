e5dd3953b00e707527ab59bf0f835a37ece3d32d
Mahdi Dibaiee <mdibaiee@pm.me>
2022-10-07 11:39:05 +0100

    fix materialize-s3-parquet tests

--- tests/materialize/materialize-s3-parquet/setup.sh
+++ tests/materialize/materialize-s3-parquet/setup.sh
@@ -2,8 +2,8 @@
 set -e
 
 export LOCALSTACK_CONTAINER_NAME=localstack
-# with virtual host based addressing https://github.com/localstack/localstack/pull/3690/files
-export LOCALSTACK_S3_ENDPOINT=http://s3.localhost.localstack.cloud:4566
+export LOCALSTACK_S3_ENDPOINT=http://${LOCALSTACK_CONTAINER_NAME}.flow-test:4566
+export LOCALSTACK_S3_LOCAL_ENDPOINT=http://localhost:4566
 
 # Dummy configs for awscli to access localstack.
 export AWS_ACCESS_KEY_ID=test_key
@@ -19,13 +19,14 @@ function startLocalStack() {
       --rm \
       --user 0 \
       --network "flow-test" \
+      --publish 4566:4566 \
       --name="${LOCALSTACK_CONTAINER_NAME}" \
       --env "SERVICES=s3" \
       localstack/localstack
 
     for i in {1..20}; do
         # Wait until the local stack is ready for serving.
-        if aws s3 ls --endpoint-url "${LOCALSTACK_S3_ENDPOINT}";  then
+        if aws s3 ls --endpoint-url "${LOCALSTACK_S3_LOCAL_ENDPOINT}";  then
             echo "localstack started successfully."
             return 0
         fi
@@ -38,7 +39,7 @@ function startLocalStack() {
 }
 startLocalStack || bail "failed to start localstack."
 
-aws s3 mb "s3://${TEST_BUCKET}" --endpoint "${LOCALSTACK_S3_ENDPOINT}"
+aws s3 mb "s3://${TEST_BUCKET}" --endpoint "${LOCALSTACK_S3_LOCAL_ENDPOINT}"
 
 config_json_template='{
     "bucket": "${TEST_BUCKET}",
