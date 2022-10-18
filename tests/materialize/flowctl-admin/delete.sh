b2d78954e11bb9d5181a32d70055d3f460e216b6
Will Baker <wbaker@gmail.com>
2022-10-18 10:20:25 -0300

    build: fix materialize run script

--- tests/materialize/flowctl-admin/delete.sh
+++ tests/materialize/flowctl-admin/delete.sh
@@ -6,6 +6,6 @@ echo "deleting the testing catalog via delete.sh"
 
 flowctl-admin api delete \
   --build-id ${BUILD_ID} \
-  --network host \
+  --network "flow-test" \
   --log.level info \
   --all
