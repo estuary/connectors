e48c894708a74ee6763874ea624601ba4ed37990
Will Donnelly <will@estuary.dev>
2022-11-01 13:36:05 -0500

    source-mysql: Fix inconsistencies between MySQL and MariaDB
    These tweaks should make it possible to capture from MariaDB v10.3
    and up, however the automated test suite fails in a few cases:
    
      - The 'datetime' datatype round trip test fails because MariaDB
        truncates fractional seconds instead of rounding.
      - The 'json' datatype round trip test fails because MariaDB has
        JSON as an alias for the LONGTEXT column type, so discovery
        and captures will just treat that as a string.
      - The `TestBinlogExpirySanityCheck` test fails on older versions
        of MariaDB (but not latest) because the `binlog_expire_logs_seconds`
        system variable doesn't exist. However the sanity check itself
        should still work on those DB versions, it will simply fall
        back to looking at `expire_logs_days` like it should.
    
    I don't believe it's worth trying to make the automated test suite
    pass cleanly on MariaDB, because I couldn't find a not-terrible
    way of setting things up so that CI will automatically test it.
    
    And so long as we're just spot-checking against MariaDB manually, it
    seems fine to just ignore the failure of those two/three test cases.

--- source-mysql/docker-entrypoint-initdb.d/init-user-db.sh
+++ source-mysql/docker-entrypoint-initdb.d/init-user-db.sh
@@ -8,9 +8,8 @@ echo "======================================="
 mysql --user="root" --password="flow" --database="test" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS flow;
     CREATE TABLE IF NOT EXISTS flow.watermarks (slot INTEGER PRIMARY KEY, watermark TEXT);
-    CREATE USER IF NOT EXISTS flow_capture IDENTIFIED BY 'secret' COMMENT 'User account for Flow MySQL data capture';
+    CREATE USER IF NOT EXISTS flow_capture IDENTIFIED BY 'secret';
     GRANT REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'flow_capture';
     GRANT SELECT ON *.* TO 'flow_capture';
     GRANT INSERT, UPDATE, DELETE ON flow.watermarks TO 'flow_capture';
-    SET PERSIST binlog_row_metadata = 'FULL';
 EOSQL
