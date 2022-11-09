e9eb57a4fb8e9c215deb9d7f077cc7af9f3ad9a8
Will Baker <wbaker@gmail.com>
2022-11-09 09:24:01 -0400

    source-{gcs,s3}: disable schema inference

--- source-gcs/main.go
+++ source-gcs/main.go
@@ -196,7 +196,8 @@ func main() {
 		DocumentationURL: "https://go.estuary.dev/source-gcs",
 		// Set the delta to 30 seconds in the past, to guard against new files appearing with a
 		// timestamp that's equal to the `MinBound` in the state.
-		TimeHorizonDelta: time.Second * -30,
+		TimeHorizonDelta:    time.Second * -30,
+		SkipSchemaInference: true,
 	}
 
 	src.Main()
