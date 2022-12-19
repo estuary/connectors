a76b76847cf8f06a8d1215ac25ab374f261976e5
Mahdi Dibaiee <mdibaiee@pm.me>
2022-12-19 14:50:22 +0000

    go-auth: update oauth templates to mustache

--- go-auth/google/spec.go
+++ go-auth/google/spec.go
@@ -8,14 +8,14 @@ import (
 	pf "github.com/estuary/flow/go/protocols/flow"
 )
 
-const AUTH_URL_TEMPLATE_FORMAT_STR = "https://accounts.google.com/o/oauth2/auth?access_type=offline&prompt=consent&client_id={{ client_id }}&redirect_uri={{ redirect_uri }}&response_type=code&scope=%s&state={{ state }}"
+const AUTH_URL_TEMPLATE_FORMAT_STR = "https://accounts.google.com/o/oauth2/auth?access_type=offline&prompt=consent&client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}&response_type=code&scope={{#urlencode}}%s{{/urlencode}}&state={{#urlencode}}{{{ state }}}{{/urlencode}}"
 
 func Spec(scopes ...string) *pf.OAuth2Spec {
 	return &pf.OAuth2Spec{
 		Provider:                   "google",
 		AuthUrlTemplate:            fmt.Sprintf(AUTH_URL_TEMPLATE_FORMAT_STR, strings.Join(scopes, " ")),
 		AccessTokenUrlTemplate:     "https://oauth2.googleapis.com/token",
-		AccessTokenBody:            "{\"grant_type\": \"authorization_code\", \"client_id\": \"{{ client_id }}\", \"client_secret\": \"{{ client_secret }}\", \"redirect_uri\": \"{{ redirect_uri }}\", \"code\": \"{{ code }}\"}",
+		AccessTokenBody:            "{\"grant_type\": \"authorization_code\", \"client_id\": \"{{{ client_id }}}\", \"client_secret\": \"{{{ client_secret }}}\", \"redirect_uri\": \"{{{ redirect_uri }}}\", \"code\": \"{{{ code }}}\"}",
 		AccessTokenResponseMapJson: json.RawMessage("{\"refresh_token\": \"/refresh_token\"}"),
 	}
 }
