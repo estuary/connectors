5d4056860716fc70aeb1ab9eb4d7befb958027d0
Will Baker <wbaker@gmail.com>
2022-12-21 11:44:34 -0500

    source-alpaca: additional advanced configuration options
    Adds some additional knobs to fine-tune the connector behavior for advanced use cases. Disabling
    backfill is a useful companion to disabling real-time streaming to compare collection data produced
    exclusively from streaming vs. the historical API. Setting backfill intervals can be helpful for
    mitigating timeouts when collection large amounts of data.

--- source-alpaca/main_test.go
+++ source-alpaca/main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 	"regexp"
 	"testing"
@@ -64,6 +65,91 @@ func TestDiscover(t *testing.T) {
 
 	capture := captureSpec(t, map[string]string{}, startDate, endDate, nil)
 	capture.VerifyDiscover(context.Background(), t, nil...)
+}
+
+func TestConfigValidate(t *testing.T) {
+	valid := config{
+		ApiKey:    "something",
+		ApiSecret: "something",
+		Feed:      "iex",
+		Symbols:   "AAPL,MSFT",
+		StartDate: time.Now(),
+	}
+
+	stopBeforeStart := valid
+	stopBeforeStart.StartDate = time.Now()
+	stopBeforeStart.Advanced.StopDate = stopBeforeStart.StartDate.Add(-1 * time.Minute)
+
+	missingStart := valid
+	missingStart.StartDate = time.Time{}
+
+	wrongFeed := valid
+	wrongFeed.Feed = "otherThing"
+
+	minBackfillBiggerThanMaxBackfill := valid
+	minBackfillBiggerThanMaxBackfill.Advanced.MaxBackfillInterval = time.Duration(1 * time.Minute)
+	minBackfillBiggerThanMaxBackfill.Advanced.MinBackfillInterval = time.Duration(2 * time.Minute)
+
+	backfillAndRealTimeDisabled := valid
+	backfillAndRealTimeDisabled.Advanced.DisableBackfill = true
+	backfillAndRealTimeDisabled.Advanced.DisableRealTime = true
+
+	tests := []struct {
+		name   string
+		config config
+		want   error
+	}{
+		{
+			name:   "valid",
+			config: valid,
+			want:   nil,
+		},
+		{
+			name:   "stop date before start date",
+			config: stopBeforeStart,
+			want:   fmt.Errorf("stop_date %s cannot be before start_date %s", stopBeforeStart.Advanced.StopDate, stopBeforeStart.StartDate),
+		},
+		{
+			name:   "missing start date",
+			config: missingStart,
+			want:   fmt.Errorf("must provide a value for start_date"),
+		},
+		{
+			name:   "invalid feed",
+			config: wrongFeed,
+			want:   fmt.Errorf("feed must be iex or sip, got %s", wrongFeed.Feed),
+		},
+		{
+			name:   "min backfill duration too large",
+			config: minBackfillBiggerThanMaxBackfill,
+			want:   fmt.Errorf("min_backfill_interval of %s cannot be greater than max_backfill_interval of %s", minBackfillBiggerThanMaxBackfill.Advanced.MinBackfillInterval, minBackfillBiggerThanMaxBackfill.Advanced.MaxBackfillInterval),
+		},
+		{
+			name:   "backfill and real time both disabled",
+			config: backfillAndRealTimeDisabled,
+			want:   fmt.Errorf("cannot disable both real time and backfill data collection"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.want, tt.config.Validate())
+		})
+	}
+
+	t.Run("default intervals are set correctly", func(t *testing.T) {
+		conf := &config{
+			ApiKey:    "something",
+			ApiSecret: "something",
+			Feed:      "iex",
+			Symbols:   "AAPL,MSFT",
+			StartDate: time.Now(),
+		}
+
+		require.Equal(t, nil, conf.Validate())
+		require.Equal(t, defaultMaxBackfillInterval, conf.Advanced.MaxBackfillInterval)
+		require.Equal(t, defaultMinBackfillInterval, conf.Advanced.MinBackfillInterval)
+	})
 }
 
 // The remaining 2 tests are end-to-end tests that will interact with the live Alpaca API and assert
@@ -165,8 +251,8 @@ func captureSpec(t testing.TB, bMappings bindingsMapping, start, end time.Time, 
 	endpointSpec := &config{
 		ApiKey:    apiKey,
 		ApiSecret: apiSecret,
-		Feed:      "iex",  // Will be over-ridden by individual bindings.
-		Symbols:   "AAPL", // Will be over-ridden by individual bindings.
+		Feed:      "iex",
+		Symbols:   "AAPL", // Will be over-ridden by individual resource bindings.
 		StartDate: start,
 		Advanced: advancedConfig{
 			IsFreePlan:      true,
@@ -189,7 +275,6 @@ func makeBindings(t testing.TB, bMappings bindingsMapping, start, end time.Time)
 	for name, symbols := range bMappings {
 		spec := resource{
 			Name:      name,
-			Feed:      "iex",
 			Symbols:   symbols,
 			startDate: start,
 		}
