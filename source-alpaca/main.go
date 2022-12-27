d88c14e4644cb387053275f415a5fdf29a669a40
Will Baker <wbaker@gmail.com>
2022-12-27 16:18:54 -0600

    source-alpaca: remove extraneous multiline spec annotations
    I had originally annotated most of the schema fields as `multiline=true` think that this was
    referring to the length of the _description_ rather than the input box. But it's only for making the
    input box multiline, so only the symbols list needs it.

--- source-alpaca/main.go
+++ source-alpaca/main.go
@@ -33,9 +33,9 @@ func (r *resource) Validate() error {
 type config struct {
 	ApiKeyID     string         `json:"api_key_id" jsonschema:"title=Alpaca API Key ID,description=Your Alpaca API key ID." jsonschema_extras:"secret=true"`
 	ApiSecretKey string         `json:"api_secret_key" jsonschema:"title=Alpaca API Secret Key,description=Your Alpaca API Secret key." jsonschema_extras:"secret=true"`
-	Feed         string         `json:"feed" jsonschema:"title=Feed,description=The feed to pull market data from.,enum=iex,enum=sip" jsonschema_extras:"multiline=true"`
+	Feed         string         `json:"feed" jsonschema:"title=Feed,description=The feed to pull market data from.,enum=iex,enum=sip"`
 	Symbols      string         `json:"symbols" jsonschema:"title=Symbols,description=Comma separated list of symbols to monitor." jsonschema_extras:"multiline=true"`
-	StartDate    time.Time      `json:"start_date" jsonschema:"title=Start Date,description=Get trades starting at this date. Has no effect if changed after the capture has started." jsonschema_extras:"multiline=true"`
+	StartDate    time.Time      `json:"start_date" jsonschema:"title=Start Date,description=Get trades starting at this date. Has no effect if changed after the capture has started."`
 	Advanced     advancedConfig `json:"advanced,omitempty" jsonschema:"title=Advanced Options,description=Options for advanced users. You should not typically need to modify these." jsonschema_extra:"advanced=true"`
 
 	effectiveMaxBackfillInterval time.Duration
@@ -52,12 +52,12 @@ func (c *config) GetSymbols() []string {
 }
 
 type advancedConfig struct {
-	IsFreePlan          bool      `json:"is_free_plan,omitempty" jsonschema:"title=Free Plan,description=Set this if you are using a free plan. Delays data by 15 minutes." jsonschema_extras:"multiline=true"`
-	StopDate            time.Time `json:"stop_date,omitempty" jsonschema:"title=Stop Date,description=Stop backfilling historical data at this date." jsonschema_extras:"multiline=true"`
-	DisableRealTime     bool      `json:"disable_real_time,omitempty" jsonschema:"title=Disable Real-Time Streaming,description=Disables real-time streaming via the websocket API. Data will only be collected via the backfill mechanism." jsonschema_extras:"multiline=true"`
-	DisableBackfill     bool      `json:"disable_backfill,omitempty" jsonschema:"title=Disable Historical Data Backfill,description=Disables historical data backfill via the historical data API. Data will only be collected via streaming." jsonschema_extras:"multiline=true"`
-	MaxBackfillInterval string    `json:"max_backfill_interval,omitempty" jsonschema:"title=Maximum Backfill Interval,description=The largest time interval that will be requested for backfills. Using smaller intervals may be useful when tracking many symbols. Must be a valid Go duration string." jsonschema_extras:"multiline=true"`
-	MinBackfillInterval string    `json:"min_backfill_interval,omitempty" jsonschema:"title=Minimum Backfill Interval,description=The smallest time interval that will be requested for backfills after the initial backfill is complete. Must be a valid Go duration string." jsonschema_extras:"multiline=true"`
+	IsFreePlan          bool      `json:"is_free_plan,omitempty" jsonschema:"title=Free Plan,description=Set this if you are using a free plan. Delays data by 15 minutes."`
+	StopDate            time.Time `json:"stop_date,omitempty" jsonschema:"title=Stop Date,description=Stop backfilling historical data at this date."`
+	DisableRealTime     bool      `json:"disable_real_time,omitempty" jsonschema:"title=Disable Real-Time Streaming,description=Disables real-time streaming via the websocket API. Data will only be collected via the backfill mechanism."`
+	DisableBackfill     bool      `json:"disable_backfill,omitempty" jsonschema:"title=Disable Historical Data Backfill,description=Disables historical data backfill via the historical data API. Data will only be collected via streaming."`
+	MaxBackfillInterval string    `json:"max_backfill_interval,omitempty" jsonschema:"title=Maximum Backfill Interval,description=The largest time interval that will be requested for backfills. Using smaller intervals may be useful when tracking many symbols. Must be a valid Go duration string."`
+	MinBackfillInterval string    `json:"min_backfill_interval,omitempty" jsonschema:"title=Minimum Backfill Interval,description=The smallest time interval that will be requested for backfills after the initial backfill is complete. Must be a valid Go duration string."`
 }
 
 func (c *config) Validate() error {
