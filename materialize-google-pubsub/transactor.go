02c064b721ddee128e1f9070c0f906100ad1ef36
Will Baker <wbaker@gmail.com>
2023-01-11 19:52:54 -0500

    materialize-google-pubsub: limit buffered publish calls
    Configures the PubSub client to block on publish calls if MaxOutstandingMessages is reached.
    
    If the store iterator can be read from faster than messages can be published (very likely) and the
    transaction has many messages, memory usage increases proportionally with the original unbounded
    never-blocking publish configuration. This sets the PubSub client to instead block on calls to
    publish if the number of queued messages reaches MaxOutstandingMessages.
    
    The default batch size is 100 messages and the default MaxOutstandingMessages is 1000, so this would
    represent 10 queued batches per binding.

--- materialize-google-pubsub/transactor.go
+++ materialize-google-pubsub/transactor.go
@@ -40,6 +40,8 @@ func (t *transactor) Store(it *pm.StoreIterator) (pm.StartCommitFunc, error) {
 			msg.Attributes = map[string]string{IDENTIFIER_ATTRIBUTE_KEY: binding.identifier}
 		}
 
+		// Blocks if the maximum number of messages are queue'd, since
+		// topic.PublishSettings.FlowControlSettings.LimitExceededBehavior = pubsub.FlowControlBlock
 		res := binding.topic.Publish(ctx, msg)
 
 		errGroup.Go(func() error {
