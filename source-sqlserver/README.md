ee0cc58ebb8045a8ffa63611f3e7f984c13e9528
Will Donnelly <will@estuary.dev>
2023-01-30 11:58:44 -0600

    source-sqlserver: Implement the connector
    This is a pretty large commit because it implements all of the
    database-specific interactions which were previously stubbed out
    with `panic()` calls, and essentially all of them are necessary
    before tests start passing.
    
    With these changes `source-sqlserver` can pass the entire generic
    test suite (the test snapshot updates have been split into a
    separate commit for clarity), however I feel like the codebase
    is still a bit rough and needs more testing beyond that.

--- source-sqlserver/README.md
+++ source-sqlserver/README.md
@@ -14,3 +14,30 @@ TODO(wgd): Describe the details of this connector in more depth once it's writte
     -------------------------------------------------------------------------------------------------------------------------------- ----------- ----- ------ ------ ----------- --------------- ----------- ----------- ----------------------- ------- ----------- ---------------- ---- -------- ------- -------- ----------------------- ----------- ----------- ----------- ----------- ----------- ----------- ------
     foobar                                                                                                                             631673298 U          1      0           0               0           0           0 2023-01-05 20:02:30.960       0           0                0 U           1       3        0 2023-01-05 20:02:30.960           0           0           0           0           0           0      0
 
+setup process:
+
+    $ docker-compose -f ./source-sqlserver/docker-compose.yaml exec db /opt/mssql-tools/bin/sqlcmd -U sa -P gf6w6dkD
+    > CREATE DATABASE test;
+    > GO
+    > USE test;
+    > GO
+    > EXEC sys.sp_cdc_enable_db;
+    > GO
+
+doing cdc from a table:
+
+    $ docker-compose -f ./source-sqlserver/docker-compose.yaml exec db /opt/mssql-tools/bin/sqlcmd -U sa -P gf6w6dkD
+    > USE test;
+    > GO
+    > CREATE TABLE foobar (id INTEGER PRIMARY KEY, data TEXT);
+    > GO
+    > EXEC sys.sp_cdc_help_change_data_capture;
+    > GO
+    > EXEC sys.sp_cdc_enable_table @source_schema = N'dbo', @source_name = N'foobar', @role_name = N'sa', @capture_instance = N'dbo_foobar';
+    > GO
+
+checking if cdc is enabled:
+
+    > SELECT tables.name, schemas.name, is_tracked_by_cdc FROM sys.tables JOIN sys.schemas ON tables.schema_id = schemas.schema_id;
+    > GO
+
--- source-sqlserver/backfill.go
+++ source-sqlserver/backfill.go
@@ -2,18 +2,170 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/estuary/connectors/sqlcapture"
+	"github.com/estuary/flow/go/protocols/fdb/tuple"
+	log "github.com/sirupsen/logrus"
 )
 
-// ShouldBackfill returns true if a given table's contents should be backfilled.
+// ShouldBackfill returns true if a given table's contents should be backfilled, given
+// that we intend to capture that table.
 func (db *sqlserverDatabase) ShouldBackfill(streamID string) bool {
-	// TODO(wgd): Implement backfills
-	return false
+	if db.config.Advanced.SkipBackfills != "" {
+		// This repeated splitting is a little inefficient, but this check is done at
+		// most once per table during connector startup and isn't really worth caching.
+		for _, skipStreamID := range strings.Split(db.config.Advanced.SkipBackfills, ",") {
+			if streamID == strings.ToLower(skipStreamID) {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 // ScanTableChunk fetches a chunk of rows from the specified table, resuming from the `resumeAfter` row key if non-nil.
 func (db *sqlserverDatabase) ScanTableChunk(ctx context.Context, info *sqlcapture.DiscoveryInfo, keyColumns []string, resumeAfter []byte) ([]*sqlcapture.ChangeEvent, error) {
-	// TODO(wgd): Implement backfills
-	panic("NOT YET IMPLEMENTED: ScanTableChunk")
+	var schema, table = info.Schema, info.Name
+	var streamID = sqlcapture.JoinStreamID(schema, table)
+
+	// Decode the resumeAfter key if non-nil
+	var err error
+	var resumeKey []interface{}
+	if resumeAfter != nil {
+		resumeKey, err = sqlcapture.UnpackTuple(resumeAfter, decodeKeyFDB)
+		if err != nil {
+			return nil, fmt.Errorf("error unpacking resume key for %q: %w", streamID, err)
+		}
+		if len(resumeKey) != len(keyColumns) {
+			return nil, fmt.Errorf("expected %d resume-key values but got %d", len(keyColumns), len(resumeKey))
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"stream":     streamID,
+		"keyColumns": keyColumns,
+		"resumeKey":  resumeKey,
+	}).Debug("scanning table chunk")
+
+	var columnTypes = make(map[string]interface{})
+	for name, column := range info.Columns {
+		columnTypes[name] = column.DataType
+	}
+
+	var query = db.buildScanQuery(resumeKey == nil, keyColumns, columnTypes, schema, table)
+
+	log.WithFields(log.Fields{"query": query, "args": resumeKey}).Debug("executing query")
+	rows, err := db.conn.QueryContext(ctx, query, resumeKey...)
+	if err != nil {
+		return nil, fmt.Errorf("unable to execute query %q: %w", query, err)
+	}
+	defer rows.Close()
+
+	// Set up the necessary slices for generic scanning over query rows
+	cnames, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	var vals = make([]any, len(cnames))
+	var vptrs = make([]any, len(vals))
+	for idx := range vals {
+		vptrs[idx] = &vals[idx]
+	}
+
+	// Iterate over the result set appending change events to the list
+	var events []*sqlcapture.ChangeEvent
+	for rows.Next() {
+		if err := rows.Scan(vptrs...); err != nil {
+			return nil, fmt.Errorf("error scanning result row: %w", err)
+		}
+		var fields = make(map[string]interface{})
+		for idx, name := range cnames {
+			fields[name] = vals[idx]
+		}
+
+		var rowKey, err = sqlcapture.EncodeRowKey(keyColumns, fields, columnTypes, encodeKeyFDB)
+		if err != nil {
+			return nil, fmt.Errorf("error encoding row key for %q: %w", streamID, err)
+		}
+		if err := db.translateRecordFields(columnTypes, fields); err != nil {
+			return nil, fmt.Errorf("error backfilling table %q: %w", table, err)
+		}
+
+		log.WithField("fields", fields).Trace("got row")
+		events = append(events, &sqlcapture.ChangeEvent{
+			Operation: sqlcapture.InsertOp,
+			RowKey:    rowKey,
+			Source: &sqlserverSourceInfo{
+				SourceCommon: sqlcapture.SourceCommon{
+					Schema:   schema,
+					Snapshot: true,
+					Table:    table,
+				},
+			},
+			Before: nil,
+			After:  fields,
+		})
+	}
+	return events, nil
+}
+
+func (db *sqlserverDatabase) buildScanQuery(start bool, keyColumns []string, columnTypes map[string]interface{}, schemaName, tableName string) string {
+	var pkey []string
+	var args []string
+	for idx, colName := range keyColumns {
+		pkey = append(pkey, colName)
+		args = append(args, fmt.Sprintf("@p%d", idx+1))
+	}
+
+	var query = new(strings.Builder)
+	fmt.Fprintf(query, "SELECT * FROM %s.%s", schemaName, tableName)
+	if !start {
+		for i := range pkey {
+			if i == 0 {
+				fmt.Fprintf(query, " WHERE (")
+			} else {
+				fmt.Fprintf(query, ") OR (")
+			}
+
+			for j := 0; j < i; j++ {
+				fmt.Fprintf(query, "%s = %s AND ", pkey[j], args[j])
+			}
+			fmt.Fprintf(query, "%s > %s", pkey[i], args[i])
+		}
+		fmt.Fprintf(query, ")")
+	}
+	fmt.Fprintf(query, " ORDER BY %s", strings.Join(pkey, ", "))
+	fmt.Fprintf(query, " OFFSET 0 ROWS FETCH FIRST %d ROWS ONLY;", db.config.Advanced.BackfillChunkSize)
+	return query.String()
+}
+
+func encodeKeyFDB(key, ktype interface{}) (tuple.TupleElement, error) {
+	return key, nil
+}
+
+func decodeKeyFDB(t tuple.TupleElement) (interface{}, error) {
+	return t, nil
+}
+
+func (db *sqlserverDatabase) translateRecordFields(columnTypes map[string]interface{}, f map[string]interface{}) error {
+	if columnTypes == nil {
+		return fmt.Errorf("unknown column types")
+	}
+	if f == nil {
+		return nil
+	}
+	for id, val := range f {
+		var translated, err = db.translateRecordField(columnTypes[id], val)
+		if err != nil {
+			return fmt.Errorf("error translating field %q value %v: %w", id, val, err)
+		}
+		f[id] = translated
+	}
+	return nil
+}
+
+func (db *sqlserverDatabase) translateRecordField(columnType interface{}, val interface{}) (interface{}, error) {
+	return val, nil
 }
