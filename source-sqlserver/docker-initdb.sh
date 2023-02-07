26baf675b82dca11f3573f0c36a832485ad34275
Will Donnelly <will@estuary.dev>
2023-02-07 12:33:42 -0600

    source-sqlserver: Automatically enable CDC on test database
    Previously the dockerized test database would have CDC disabled
    by default and require manual action to enable, which meant that
    it wouldn't work with the CI build setup from the previous commit.
    
    This fixes that by mounting a custom entrypoint script which
    checks for a sentinel file indicating "this database has been
    initialized", and if not found it performs a series of setup
    commands and then touches the sentinel file.

---
+++ source-sqlserver/docker-initdb.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+/opt/mssql/bin/sqlservr &
+DBPID="$!"
+
+if [ ! -e /var/opt/mssql/initdb-performed ]; then
+  echo "Launching database initialzation process"
+  sleep 5
+  echo "Initializing database"
+  echo "
+CREATE DATABASE test;
+GO
+USE test;
+GO
+EXEC sys.sp_cdc_enable_db;
+GO
+CREATE TABLE dummy_table (id INTEGER PRIMARY KEY, data TEXT);
+GO
+EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'dummy_table', @role_name = 'sa';
+GO
+  " | /opt/mssql-tools/bin/sqlcmd -U sa -P gf6w6dkD
+  echo "Database initialzation complete"
+  touch /var/opt/mssql/initdb-performed
+else
+  echo "Database already initialized, skipping setup"
+fi
+
+wait ${DBPID}
