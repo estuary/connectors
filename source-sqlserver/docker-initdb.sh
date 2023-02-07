860480487975ade8fa59900a3b7ab0b60a605cdc
Will Donnelly <will@estuary.dev>
2023-02-07 17:07:56 -0600

    source-sqlserver: Tweak test database init for CI
    Looks like it just needed to sleep for longer -- when running on
    CI we were trying to execute the database setup commands before
    the database was done initializing. Doubling the delay should
    fix that so our custom setup doesn't run until after the database
    itself is up and running.
    
    Since the "wait for setup" delay has gone from 5s->10s we also
    bump the Docker "healthcheck" delay from 10s to 15s, just to make
    sure everything has plenty of time to complete.

--- source-sqlserver/docker-initdb.sh
+++ source-sqlserver/docker-initdb.sh
@@ -3,9 +3,9 @@
 DBPID="$!"
 
 if [ ! -e /var/opt/mssql/initdb-performed ]; then
-  echo "Launching database initialzation process"
-  sleep 5
-  echo "Initializing database"
+  echo "[initdb] Waiting to initialize database..."
+  sleep 10
+  echo "[initdb] Initializing database..."
   echo "
 CREATE DATABASE test;
 GO
@@ -18,10 +18,10 @@ GO
 EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'dummy_table', @role_name = 'sa';
 GO
   " | /opt/mssql-tools/bin/sqlcmd -U sa -P gf6w6dkD
-  echo "Database initialzation complete"
+  echo "[initdb] Database initialzation complete!"
   touch /var/opt/mssql/initdb-performed
 else
-  echo "Database already initialized, skipping setup"
+  echo "[initdb] Database previously initialized"
 fi
 
 wait ${DBPID}
