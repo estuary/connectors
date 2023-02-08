86077deb8e071716d3ca1764f584f99120b3b947
Will Donnelly <will@estuary.dev>
2023-02-07 20:39:08 -0600

    source-sqlserver: Tweaks to get CI builds passing

--- source-sqlserver/docker-initdb.sh
+++ source-sqlserver/docker-initdb.sh
@@ -17,6 +17,8 @@ CREATE TABLE dummy_table (id INTEGER PRIMARY KEY, data TEXT);
 GO
 EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'dummy_table', @role_name = 'sa';
 GO
+INSERT INTO dummy_table VALUES (0, 'hello'), (1, 'world');
+GO
   " | /opt/mssql-tools/bin/sqlcmd -U sa -P gf6w6dkD
   echo "[initdb] Database initialzation complete!"
   touch /var/opt/mssql/initdb-performed
