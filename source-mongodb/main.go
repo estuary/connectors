9749fe9f15358f9b4615e74745a5fb277ae82539
Mahdi Dibaiee <mdibaiee@pm.me>
2023-02-16 15:40:15 +0000

    source-mongodb: pull change stream

--- source-mongodb/main.go
+++ source-mongodb/main.go
@@ -270,13 +270,6 @@ func (d *driver) Pull(stream pc.Driver_PullServer) error {
 		return fmt.Errorf("parsing config json: %w", err)
 	}
 
-	//var prevState captureState
-	//if open.Open.DriverCheckpointJson != nil {
-	//if err := pf.UnmarshalStrict(open.Open.DriverCheckpointJson, &prevState); err != nil {
-	//return fmt.Errorf("parsing state checkpoint: %w", err)
-	//}
-	//}
-
 	var ctx = context.Background()
 
 	client, err := d.Connect(ctx, cfg)
@@ -304,15 +297,70 @@ func (d *driver) Pull(stream pc.Driver_PullServer) error {
 	for idx, binding := range open.Open.Capture.Bindings {
 		var res resource
 		if err := pf.UnmarshalStrict(binding.ResourceSpecJson, &res); err != nil {
-			return fmt.Errorf("error parsing resource config: %w", err)
+			return fmt.Errorf("parsing resource config: %w", err)
 		}
 
 		eg.Go(func() error { return c.BackfillCollection(ctx, client, uint32(idx), res) })
+		eg.Go(func() error { return c.ChangeStream(ctx, client, uint32(idx), res) })
 	}
 
 	if err := eg.Wait(); err != nil && !errors.Is(err, io.EOF) {
 		return err
 	}
+
+	return nil
+}
+
+type changeEvent struct{
+	OperationType string `bson:"operationType"`
+	FullDocument bson.M `bson:"fullDocument"`
+}
+
+func (c *capture) ChangeStream(ctx context.Context, client *mongo.Client, binding uint32, res resource) error {
+	var db = client.Database(res.Database)
+
+	var collection = db.Collection(res.Collection)
+
+	log.Debug("listening on changes on collection ", res.Collection)
+	var eventFilter = bson.D{{"$match", bson.D{{"$or",
+	bson.A{
+		// bson.D{{"operationType", "delete"}}, TODO: how do we handle deletion
+		// events
+		bson.D{{"operationType", "insert"}},
+		bson.D{{"operationType", "update"}},
+	}}}}}
+	cursor, err := collection.Watch(ctx, mongo.Pipeline{eventFilter}, options.ChangeStream().SetFullDocument(options.UpdateLookup))
+	if err != nil {
+		return fmt.Errorf("change stream on collection %s: %w", res.Collection, err)
+	}
+
+	for cursor.Next(ctx) {
+		var ev changeEvent
+		if err = cursor.Decode(&ev); err != nil {
+			return fmt.Errorf("decoding document in collection %s: %w", res.Collection, err)
+		}
+
+		var doc = sanitizeDocument(ev.FullDocument)
+
+		js, err := json.Marshal(doc)
+		if err != nil {
+			return fmt.Errorf("encoding document in collection %s as json: %w", res.Collection, err)
+		}
+
+		if err = c.Output.Documents(binding, js); err != nil {
+			return fmt.Errorf("output documents failed: %w", err)
+		}
+
+		if err = c.Output.Checkpoint([]byte("{}"), true); err != nil {
+			return fmt.Errorf("output checkpoint failed: %w", err)
+		}
+	}
+
+	if err := cursor.Err(); err != nil {
+		return fmt.Errorf("cursor error for change stream %s: %w", res.Collection, err)
+	}
+
+	defer cursor.Close(ctx)
 
 	return nil
 }
@@ -324,24 +372,24 @@ func (c *capture) BackfillCollection(ctx context.Context, client *mongo.Client, 
 
 	var collection = db.Collection(res.Collection)
 
+	log.Debug("finding documents in collection ", res.Collection)
 	cursor, err := collection.Find(ctx, bson.D{})
 	if err != nil {
 		return fmt.Errorf("could not run find query on collection %s: %w", res.Collection, err)
 	}
-	log.Debug("finding documents in collection ", res.Collection)
 
 	defer cursor.Close(ctx)
 
 	var i = 0
 	for cursor.Next(ctx) {
 		i += 1
-		var decoded bson.M
-		if err = cursor.Decode(&decoded); err != nil {
+		var doc bson.M
+		if err = cursor.Decode(&doc); err != nil {
 			return fmt.Errorf("decoding document in collection %s: %w", res.Collection, err)
 		}
-		decoded = sanitizeDocument(decoded)
+		doc = sanitizeDocument(doc)
 
-		js, err := json.Marshal(decoded)
+		js, err := json.Marshal(doc)
 		if err != nil {
 			return fmt.Errorf("encoding document in collection %s as json: %w", res.Collection, err)
 		}
@@ -359,6 +407,10 @@ func (c *capture) BackfillCollection(ctx context.Context, client *mongo.Client, 
 
 	if err = c.Output.Checkpoint([]byte("{}"), true); err != nil {
 		return fmt.Errorf("sending checkpoint failed: %w", err)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return fmt.Errorf("cursor error for backfill %s: %w", res.Collection, err)
 	}
 
 	return nil
