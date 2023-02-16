6b7a869162e5df429d7ef19c40c7d243ab360a4f
Mahdi Dibaiee <mdibaiee@pm.me>
2023-02-16 13:23:13 +0000

    source-mongodb: basic backfill

--- source-mongodb/main.go
+++ source-mongodb/main.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-  "net/url"
+	"math"
+	"errors"
+	"io"
+	"net/url"
 
 	schemagen "github.com/estuary/connectors/go-schema-gen"
 	boilerplate "github.com/estuary/connectors/source-boilerplate"
 	pc "github.com/estuary/flow/go/protocols/capture"
 	pf "github.com/estuary/flow/go/protocols/flow"
-  //log "github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/invopop/jsonschema"
 
-  "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -22,7 +26,7 @@ import (
 
 
 type resource struct {
-  Database   string `json:"database" jsonschema=title=Database name"`
+	Database   string `json:"database" jsonschema=title=Database name"`
 	Collection string `json:"collection" jsonschema:"title=Collection name"`
 }
 
@@ -57,7 +61,7 @@ func (c *config) Validate() error {
 		{"address", c.Address},
 		{"user", c.User},
 		{"password", c.Password},
-    {"database", c.Database},
+		{"database", c.Database},
 	}
 	for _, req := range requiredProperties {
 		if req[1] == "" {
@@ -91,18 +95,18 @@ func (c *config) ToURI() string {
 type driver struct{}
 
 func (d *driver) Connect(ctx context.Context, cfg config) (*mongo.Client, error) {
-  // Create a new client and connect to the server
+	// Create a new client and connect to the server
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.ToURI()))
 	if err != nil {
-    return nil, err
+		return nil, err
 	}
 
 	// Ping the primary
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-    return nil, err
+		return nil, err
 	}
 
-  return client, nil
+	return client, nil
 }
 
 func (driver) Spec(ctx context.Context, req *pc.SpecRequest) (*pc.SpecResponse, error) {
@@ -138,8 +142,8 @@ func generateMinimalSchema() json.RawMessage {
 		Extras: map[string]interface{}{
 			"properties": map[string]*jsonschema.Schema{
 				idProperty: &jsonschema.Schema{
-          Type: "string",
-        },
+					Type: "string",
+				},
 			},
 			"x-infer-schema": true,
 		},
@@ -160,38 +164,38 @@ func (d *driver) Discover(ctx context.Context, req *pc.DiscoverRequest) (*pc.Dis
 		return nil, fmt.Errorf("parsing config json: %w", err)
 	}
 
-  var client, err = d.Connect(ctx, cfg)
-  if err != nil {
-    return nil, fmt.Errorf("connecting to database: %w", err)
-  }
+	var client, err = d.Connect(ctx, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("connecting to database: %w", err)
+	}
 
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
-      panic(err)
+			panic(err)
 		}
 	}()
 
-  var db = client.Database(cfg.Database)
+	var db = client.Database(cfg.Database)
 
-  collections, err := db.ListCollectionSpecifications(ctx, bson.D{})
-  if err != nil {
-    return nil, fmt.Errorf("listing collections: %w", err)
-  }
+	collections, err := db.ListCollectionSpecifications(ctx, bson.D{})
+	if err != nil {
+		return nil, fmt.Errorf("listing collections: %w", err)
+	}
 
-  var bindings = []*pc.DiscoverResponse_Binding{}
-  for _, collection := range collections {
-    resourceJSON, err := json.Marshal(resource{Database: db.Name(), Collection: collection.Name})
-    if err != nil {
-      return nil, fmt.Errorf("serializing resource json: %w", err)
-    }
+	var bindings = []*pc.DiscoverResponse_Binding{}
+	for _, collection := range collections {
+		resourceJSON, err := json.Marshal(resource{Database: db.Name(), Collection: collection.Name})
+		if err != nil {
+			return nil, fmt.Errorf("serializing resource json: %w", err)
+		}
 
-    bindings = append(bindings, &pc.DiscoverResponse_Binding{
-      RecommendedName: pf.Collection(fmt.Sprintf("%s/%s", db.Name(), collection.Name)),
-      ResourceSpecJson: resourceJSON,
-      DocumentSchemaJson: minimalSchema,
-      KeyPtrs: []string{"_id"},
-    })
-  }
+		bindings = append(bindings, &pc.DiscoverResponse_Binding{
+			RecommendedName: pf.Collection(fmt.Sprintf("%s/%s", db.Name(), collection.Name)),
+			ResourceSpecJson: resourceJSON,
+			DocumentSchemaJson: minimalSchema,
+			KeyPtrs: []string{"/" + idProperty},
+		})
+	}
 
 	return &pc.DiscoverResponse{Bindings: bindings}, nil
 }
@@ -203,40 +207,40 @@ func (d *driver) Validate(ctx context.Context, req *pc.ValidateRequest) (*pc.Val
 		return nil, fmt.Errorf("parsing config json: %w", err)
 	}
 
-  var client, err = d.Connect(ctx, cfg)
-  if err != nil {
-    return nil, fmt.Errorf("connecting to database: %w", err)
-  }
+	var client, err = d.Connect(ctx, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("connecting to database: %w", err)
+	}
 
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
-      panic(err)
+			panic(err)
 		}
 	}()
 
-  var bindings = []*pc.ValidateResponse_Binding{}
+	var bindings = []*pc.ValidateResponse_Binding{}
 
-  for _, binding := range req.Bindings {
+	for _, binding := range req.Bindings {
 		var res resource
 		if err := pf.UnmarshalStrict(binding.ResourceSpecJson, &res); err != nil {
 			return nil, fmt.Errorf("error parsing resource config: %w", err)
 		}
 
-    var db = client.Database(res.Database)
+		var db = client.Database(res.Database)
 
-    collections, err := db.ListCollectionNames(ctx, bson.D{{"name", res.Collection}})
-    if err != nil {
-      return nil, fmt.Errorf("listing collections in database %s: %w", db.Name(), err)
-    }
+		collections, err := db.ListCollectionNames(ctx, bson.D{{"name", res.Collection}})
+		if err != nil {
+			return nil, fmt.Errorf("listing collections in database %s: %w", db.Name(), err)
+		}
 
-    if !SliceContains(res.Collection, collections) {
-      return nil, fmt.Errorf("could not find collection %s in database %s", res.Collection, db.Name())
-    }
+		if !SliceContains(res.Collection, collections) {
+			return nil, fmt.Errorf("could not find collection %s in database %s", res.Collection, db.Name())
+		}
 
-    bindings = append(bindings, &pc.ValidateResponse_Binding{
-      ResourcePath: []string{res.Database, res.Collection},
-    })
-  }
+		bindings = append(bindings, &pc.ValidateResponse_Binding{
+			ResourcePath: []string{res.Database, res.Collection},
+		})
+	}
 
 	return &pc.ValidateResponse{Bindings: bindings}, nil
 }
@@ -266,27 +270,117 @@ func (d *driver) Pull(stream pc.Driver_PullServer) error {
 		return fmt.Errorf("parsing config json: %w", err)
 	}
 
-  var ctx = context.Background()
+	//var prevState captureState
+	//if open.Open.DriverCheckpointJson != nil {
+	//if err := pf.UnmarshalStrict(open.Open.DriverCheckpointJson, &prevState); err != nil {
+	//return fmt.Errorf("parsing state checkpoint: %w", err)
+	//}
+	//}
 
-  client, err := d.Connect(ctx, cfg)
-  if err != nil {
-    return fmt.Errorf("connecting to database: %w", err)
-  }
+	var ctx = context.Background()
+
+	client, err := d.Connect(ctx, cfg)
+	if err != nil {
+		return fmt.Errorf("connecting to database: %w", err)
+	}
+	log.Info("connected to database")
 
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
-      panic(err)
+			panic(err)
 		}
 	}()
 
-	for _, binding := range open.Open.Capture.Bindings {
+	var c = capture{
+		Output: &boilerplate.PullOutput{Stream: stream},
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	if err := c.Output.Ready(); err != nil {
+		return err
+	}
+
+	for idx, binding := range open.Open.Capture.Bindings {
 		var res resource
 		if err := pf.UnmarshalStrict(binding.ResourceSpecJson, &res); err != nil {
 			return fmt.Errorf("error parsing resource config: %w", err)
 		}
+
+		eg.Go(func() error { return c.BackfillCollection(ctx, client, uint32(idx), res) })
 	}
 
-  return nil
+	if err := eg.Wait(); err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
+	return nil
+}
+
+const CHECKPOINT_EVERY = 100
+
+func (c *capture) BackfillCollection(ctx context.Context, client *mongo.Client, binding uint32, res resource) error {
+	var db = client.Database(res.Database)
+
+	var collection = db.Collection(res.Collection)
+
+	cursor, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		return fmt.Errorf("could not run find query on collection %s: %w", res.Collection, err)
+	}
+	log.Debug("finding documents in collection ", res.Collection)
+
+	defer cursor.Close(ctx)
+
+	var i = 0
+	for cursor.Next(ctx) {
+		i += 1
+		var decoded bson.M
+		if err = cursor.Decode(&decoded); err != nil {
+			return fmt.Errorf("decoding document in collection %s: %w", res.Collection, err)
+		}
+		decoded = sanitizeDocument(decoded)
+
+		js, err := json.Marshal(decoded)
+		if err != nil {
+			return fmt.Errorf("encoding document in collection %s as json: %w", res.Collection, err)
+		}
+
+		if err = c.Output.Documents(binding, js); err != nil {
+			return fmt.Errorf("output documents failed: %w", err)
+		}
+
+		if i % CHECKPOINT_EVERY == 0 {
+			if err = c.Output.Checkpoint([]byte("{}"), true); err != nil {
+				return fmt.Errorf("output checkpoint failed: %w", err)
+			}
+		}
+	}
+
+	if err = c.Output.Checkpoint([]byte("{}"), true); err != nil {
+		return fmt.Errorf("sending checkpoint failed: %w", err)
+	}
+
+	return nil
+}
+
+func sanitizeDocument(doc map[string]interface{}) map[string]interface{} {
+	for key, value := range doc {
+		switch v := value.(type) {
+		case float64:
+			if math.IsNaN(v) {
+				doc[key] = "NaN"
+			}
+		case map[string]interface{}:
+			doc[key] = sanitizeDocument(v)
+		}
+	}
+
+	return doc
+}
+
+type capture struct{
+	Output   *boilerplate.PullOutput
 }
 
 func main() {
