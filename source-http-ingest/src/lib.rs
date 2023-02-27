1a390301b47734e6a2e6c8af8b790904151a8726
Phil <phil@estuary.dev>
2023-02-27 17:01:03 -0500

    intorduce source-http-ingest connector
    Introduces the source-http-ingest connector, which allows users to
    ingest data from webhooks, a swagger UI, or any other HTTP client.
    This is the first connector to take advantage of Flow's inbound
    connector networking. The README has more details on the connector
    itself, which I won't repeat here.
    
    In terms of implementation, this is also the first capture connector
    to use the new JSON protocol. I tried to factor out some functions that
    could be re-used with future Rust-based connectors, but stopped short of
    actually putting them in a separate crate, since that would seem premature.

---
+++ source-http-ingest/src/lib.rs
@@ -0,0 +1,275 @@
+pub mod server;
+pub mod transactor;
+
+use anyhow::Context;
+
+use connector_protocol::{
+    capture::{
+        ApplyBinding, DiscoveredBinding, Request, Response, ValidateBinding, ValidatedBinding,
+    },
+    CollectionSpec, RawValue,
+};
+use schemars::{schema::RootSchema, JsonSchema};
+use serde::{Deserialize, Serialize};
+use tokio::io::{self, AsyncBufReadExt};
+
+#[derive(Serialize, Deserialize, JsonSchema, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct EndpointConfig {
+    /// Optional bearer token to authenticate webhook requests.
+    ///
+    /// WARNING: If this is empty or unset, then anyone who knows the URL of the connector
+    /// will be able to write data to your collections.
+    #[serde(default)]
+    require_auth_token: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct ResourceConfig {
+    /// The URL path to use for adding documents to this binding. Defaults to the name of the collection.
+    #[serde(default)]
+    pub path: Option<String>,
+    /// Set the /_meta/webhookId from the given HTTP header in each request.
+    ///
+    /// If not set, then a random id will be generated automatically. If set, then each request will be required
+    /// to have the header, and the header value will be used as the value of `/_meta/webhookId`.
+    #[serde(default)]
+    pub id_from_header: Option<String>,
+}
+
+pub struct Binding {
+    pub collection: CollectionSpec,
+    pub resource_path: Vec<String>,
+    pub resource_config: ResourceConfig,
+}
+
+pub struct HttpIngestConnector {}
+
+fn schema_for<T: JsonSchema>() -> RootSchema {
+    schemars::gen::SchemaSettings::draft2019_09()
+        .into_generator()
+        .into_root_schema_for::<T>()
+}
+
+pub async fn run_connector(
+    mut stdin: io::BufReader<io::Stdin>,
+    stdout: io::Stdout,
+) -> Result<(), anyhow::Error> {
+    let req = read_capture_request(&mut stdin)
+        .await
+        .context("reading request")?;
+    let result = match req {
+        Request::Spec {} => do_spec(stdout).await,
+        Request::Discover { config } => do_discover(config, stdout).await,
+        Request::Validate {
+            config, bindings, ..
+        } => do_validate(config, bindings, stdout).await,
+        Request::Apply { .. } => do_apply(stdout).await,
+        Request::Open {
+            config, bindings, ..
+        } => do_pull(config, bindings, stdin, stdout).await,
+        Request::Acknowledge {} => Err(anyhow::anyhow!("unexpected 'Acknowledge' request")),
+    };
+
+    if let Err(err) = result.as_ref() {
+        tracing::error!(error = %err, "operation failed");
+    } else {
+        tracing::debug!("connector run successful");
+    }
+    result
+}
+
+async fn do_pull(
+    endpoint_config: RawValue,
+    bindings: Vec<ApplyBinding>,
+    stdin: io::BufReader<io::Stdin>,
+    mut stdout: io::Stdout,
+) -> anyhow::Result<()> {
+    let config = serde_json::from_str::<EndpointConfig>(endpoint_config.get())
+        .context("deserializing endpoint config")?;
+    let mut typed_bindings = Vec::with_capacity(bindings.len());
+    for ApplyBinding {
+        collection,
+        resource_config,
+        resource_path,
+    } in bindings
+    {
+        let resource_config = serde_json::from_str::<ResourceConfig>(resource_config.get())
+            .context("deserializing resource config")?;
+        typed_bindings.push(Binding {
+            collection,
+            resource_path,
+            resource_config,
+        });
+    }
+    write_capture_response(
+        Response::Opened {
+            explicit_acknowledgements: true,
+        },
+        &mut stdout,
+    )
+    .await?;
+
+    server::run_server(config, typed_bindings, stdin, stdout).await
+}
+
+async fn do_apply(mut stdout: io::Stdout) -> anyhow::Result<()> {
+    // There's nothing to apply
+    write_capture_response(
+        Response::Applied {
+            action_description: String::new(),
+        },
+        &mut stdout,
+    )
+    .await
+}
+
+async fn do_spec(mut stdout: io::Stdout) -> anyhow::Result<()> {
+    let config_schema = to_raw_value(&schema_for::<EndpointConfig>())?;
+    let resource_config_schema = to_raw_value(&schema_for::<ResourceConfig>())?;
+    let response = Response::Spec {
+        documentation_url: "https://go.estuary.dev/http-ingest".to_string(),
+        config_schema,
+        resource_config_schema,
+        oauth2: None,
+    };
+    write_capture_response(response, &mut stdout).await
+}
+
+async fn do_discover(config: RawValue, mut stdout: io::Stdout) -> anyhow::Result<()> {
+    // make sure we can parse the config, just as an extra sanity check
+    let _ =
+        serde_json::from_str::<EndpointConfig>(config.get()).context("parsing endpoint config")?;
+    let bindings = vec![discovered_webhook_collection()];
+    let response = Response::Discovered { bindings };
+    write_capture_response(response, &mut stdout).await
+}
+
+async fn do_validate(
+    config: RawValue,
+    bindings: Vec<ValidateBinding>,
+    mut stdout: io::Stdout,
+) -> anyhow::Result<()> {
+    let config = serde_json::from_str::<EndpointConfig>(config.get())
+        .context("deserializing endpoint config")?;
+    let mut output = Vec::with_capacity(bindings.len());
+    let mut typed_bindings = Vec::with_capacity(bindings.len());
+    for ValidateBinding {
+        collection,
+        resource_config,
+    } in bindings
+    {
+        let resource_config = serde_json::from_str::<ResourceConfig>(resource_config.get())
+            .context("deserializing resource config")?;
+
+        let resource_path = if let Some(path) = resource_config.path.as_ref() {
+            vec![server::ensure_slash_prefix(path.as_str().trim())]
+        } else {
+            vec![collection.name.clone()]
+        };
+
+        output.push(ValidatedBinding {
+            resource_path: resource_path.clone(),
+        });
+        typed_bindings.push(Binding {
+            collection,
+            resource_path,
+            resource_config,
+        });
+    }
+
+    // Check to make sure we can successfully create an openapi spec
+    server::openapi_spec(&config, &typed_bindings)
+        .context("cannot create openapi spec from bindings")?;
+    let response = Response::Validated { bindings: output };
+    write_capture_response(response, &mut stdout).await
+}
+
+pub async fn read_capture_request(stdin: &mut io::BufReader<io::Stdin>) -> anyhow::Result<Request> {
+    let mut buf = String::with_capacity(4096);
+    stdin
+        .read_line(&mut buf)
+        .await
+        .context("reading next request line")?;
+    if buf.trim().is_empty() {
+        anyhow::bail!("unexpected EOF reading request from stdin");
+    }
+    let deser = serde_json::from_str(&buf).context("deserializing request")?;
+    Ok(deser)
+}
+
+/// Writes the response to stdout, and waits to a flush to complete. The flush ensures that the complete
+/// response will be written, even if the runtime is immediately shutdown after this call returns.
+pub async fn write_capture_response(
+    response: Response,
+    stdout: &mut io::Stdout,
+) -> anyhow::Result<()> {
+    use tokio::io::AsyncWriteExt;
+
+    let resp = serde_json::to_vec(&response).context("serializing response")?;
+    stdout.write_all(&resp).await.context("writing response")?;
+    stdout
+        .write_u8(b'\n')
+        .await
+        .context("writing response newline")?;
+    stdout.flush().await?;
+    Ok(())
+}
+
+pub fn to_raw_value(val: &impl serde::Serialize) -> anyhow::Result<RawValue> {
+    let ser = serde_json::to_string(val)?;
+    Ok(RawValue(serde_json::value::RawValue::from_string(ser)?))
+}
+
+fn discovered_webhook_collection() -> DiscoveredBinding {
+    DiscoveredBinding {
+        recommended_name: "webhook-data".to_string(),
+        resource_config: to_raw_value(&ResourceConfig::default()).unwrap(),
+        document_schema: to_raw_value(&serde_json::json!({
+            "type": "object",
+            "properties": {
+                "_meta": {
+                    "type": "object",
+                    "properties": {
+                        "webhookId": {
+                            "type": "string",
+                            "description": "id of the webhook request"
+                        },
+                        "headers": {
+                            "type": "object",
+                            "description": "any HTTP headers that were sent with the request. Headers that are known to be sensitive will not be included here",
+                            "additionalProperties": { "type": "string" }
+                        },
+                        "receivedAt": {
+                            "type": "string",
+                            "format": "date-time",
+                            "description": "timestamp of when the request was received"
+                        }
+                    },
+                    "required": ["webhookId", "receivedAt"]
+                }
+            },
+            "required": ["_meta"]
+        }))
+        .unwrap(),
+        key: vec!["/_meta/webhookId".to_string()],
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn endpoint_config_schema() {
+        let schema = schema_for::<EndpointConfig>();
+        insta::assert_json_snapshot!(schema);
+    }
+
+    #[test]
+    fn resource_config_schema() {
+        let schema = schema_for::<ResourceConfig>();
+        insta::assert_json_snapshot!(schema);
+    }
+}
---
+++ source-http-ingest/src/server.rs
@@ -0,0 +1,419 @@
+use crate::{transactor::Transactor, Binding, EndpointConfig};
+
+use anyhow::Context;
+use axum::{
+    extract::{Json, Path, State},
+    routing, Router,
+};
+use http::status::StatusCode;
+use utoipa::openapi::{self, schema, security, OpenApi, OpenApiBuilder};
+use utoipa_swagger_ui::SwaggerUi;
+
+use std::{collections::HashMap, sync::Arc};
+use tokio::io;
+use tokio::sync::Mutex;
+
+pub async fn run_server(
+    endpoint_config: EndpointConfig,
+    bindings: Vec<Binding>,
+    stdin: io::BufReader<io::Stdin>,
+    stdout: io::Stdout,
+) -> anyhow::Result<()> {
+    let openapi_spec =
+        openapi_spec(&endpoint_config, &bindings).context("creating openapi spec")?;
+
+    let handler = Handler::try_new(stdin, stdout, endpoint_config, bindings)?;
+
+    let router = Router::new()
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", openapi_spec))
+        // The root path redirects to the swagger ui, so that a user who clicks a link to just
+        // the hostname will be redirected to a more useful page.
+        .route(
+            "/",
+            routing::get(|| async { axum::response::Redirect::permanent("/swagger-ui/") }),
+        )
+        // There's just one route that handles all bindings. The path will be provided as an argument.
+        .route("/*path", routing::post(handle_webhook).put(handle_webhook))
+        .with_state(Arc::new(handler));
+
+    let address = std::net::SocketAddr::from((std::net::Ipv4Addr::UNSPECIFIED, listen_on_port()));
+    tracing::info!(%address, "listening for connections");
+    axum::Server::bind(&address)
+        .serve(router.into_make_service())
+        .await
+        .map_err(anyhow::Error::from)
+}
+
+// If you turn on debug logging, you get a pretty decent request handling log from just this attribute :)
+#[tracing::instrument(
+    level = "debug",
+    skip(handler, request_headers, body),
+    fields(response_status)
+)]
+async fn handle_webhook(
+    State(handler): State<Arc<Handler>>,
+    request_headers: axum::http::header::HeaderMap,
+    Path(path): Path<String>,
+    Json(body): Json<serde_json::Value>,
+) -> (StatusCode, Json<serde_json::Value>) {
+    let resp = match handler.handle_webhook(path, request_headers, body).await {
+        Ok(resp) => resp,
+        Err(err) => {
+            tracing::error!(error = %err, "failed to handle request");
+            let body = serde_json::json!({ "error": err.to_string() });
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(body))
+        }
+    };
+    tracing::Span::current().record("response_status", resp.0.as_str());
+    resp
+}
+
+struct CollectionHandler {
+    binding_index: u32,
+    validator: doc::Validator,
+    id_header: Option<String>,
+}
+
+fn required_string_header<'a>(
+    headers: &'a axum::http::HeaderMap,
+    key: &str,
+) -> anyhow::Result<&'a str> {
+    headers
+        .get(key)
+        .ok_or_else(|| anyhow::anyhow!("missing required header: {}", key))
+        .and_then(|value| {
+            value.to_str().map_err(|_| {
+                anyhow::anyhow!(
+                    "invalid '{}' header value contains non-ascii characters",
+                    key
+                )
+            })
+        })
+}
+
+fn err_response(status: StatusCode, err: anyhow::Error) -> (StatusCode, Json<serde_json::Value>) {
+    let json = serde_json::json!({ "error": err.to_string() });
+    (status, Json(json))
+}
+
+impl CollectionHandler {
+    fn prepare_document(
+        &self,
+        mut body: serde_json::Value,
+        request_headers: axum::http::header::HeaderMap,
+    ) -> anyhow::Result<serde_json::Value> {
+        let id = if let Some(header_key) = self.id_header.as_ref() {
+            // If the config specified a header to use as the id, then require it to be present.
+            required_string_header(&request_headers, header_key.as_str())?.to_owned()
+        } else {
+            // we're supposed to generate a random uuid.
+            uuid::Uuid::new_v4().to_string()
+        };
+
+        let id_pointer = doc::Pointer::from_str("/_meta/webhookId");
+        if let Some(loc) = id_pointer.create_value(&mut body) {
+            *loc = serde_json::Value::String(id);
+        } else {
+            anyhow::bail!("invalid document, cannot create _meta object");
+        }
+
+        for (maybe_key, value) in request_headers {
+            // Are both the key and value ascii strings? If not, then skip this header.
+            let Some(key) = maybe_key else {
+                continue;
+            };
+            let Ok(value_str) = value.to_str() else {
+                continue;
+            };
+
+            // Filter out any sensitive headers or useless headers. Note that we don't do
+            // json pointer escaping on the header names. We _could_, but it hardly seems
+            // worth the extra code complexity, given that these character rarely appear in
+            // header keys, and the consequence if they do are that we'll either ignore the
+            // header (due to `ptr.create_value` returning `None`), or else the document will
+            // fail validation (due to a header value being an object or array).
+            if !REDACT_HEADERS.contains(&key) {
+                let ptr = doc::Pointer::from_str(&format!("/_meta/headers/{}", key.as_str()));
+                // If we're unable to create the /_meta/headers object, then don't consider it an error.
+                // These headers are nice to have, but are not necessarily requried.
+                if let Some(loc) = ptr.create_value(&mut body) {
+                    *loc = serde_json::Value::String(value_str.to_owned());
+                }
+            }
+        }
+
+        let ts_pointer = doc::Pointer::from_str("/_meta/receivedAt");
+        if let Some(loc) = ts_pointer.create_value(&mut body) {
+            let ts = time::OffsetDateTime::now_utc()
+                .format(&time::format_description::well_known::Rfc3339)
+                .unwrap();
+            *loc = serde_json::Value::String(ts);
+        }
+
+        Ok(body)
+    }
+}
+
+struct Handler {
+    /// If an Authorization header is required, this will be the full expected value of that header,
+    /// including the "Bearer " prefix.
+    require_auth_header: Option<String>,
+    /// Map of http url path (without the leading slash) to the handler for that collection.
+    handlers_by_path: Mutex<HashMap<String, CollectionHandler>>,
+    io: Transactor,
+}
+
+impl Handler {
+    fn try_new(
+        stdin: io::BufReader<io::Stdin>,
+        stdout: io::Stdout,
+        endpoint_config: EndpointConfig,
+        bindings: Vec<Binding>,
+    ) -> anyhow::Result<Handler> {
+        let mut collections_by_path = HashMap::with_capacity(bindings.len());
+
+        for (i, binding) in bindings.into_iter().enumerate() {
+            let binding_index = i as u32;
+
+            let Binding {
+                mut resource_path,
+                mut collection,
+                resource_config,
+            } = binding;
+            let url_path = resource_path.pop().unwrap();
+            tracing::info!(%url_path, collection = %collection.name, "binding http url path to collection");
+
+            // In documentation and configuration, we always represent the path with the leading /.
+            // But the axum `Path` extractor always strips out the leading /, so we strip it here
+            // to make matching easier.
+            let path = if let Some(without_slash) = url_path.strip_prefix('/') {
+                without_slash.to_owned()
+            } else {
+                url_path
+            };
+
+            let schema_json = collection.write_schema.take().unwrap_or_else(|| {
+                collection
+                    .schema
+                    .take()
+                    .expect("collection must have schema if write_schema is missing")
+            });
+            let schema_value =
+                serde_json::from_str::<serde_json::Value>(schema_json.get()).unwrap();
+            let uri = url::Url::parse("http://not.areal.host/").unwrap();
+            let schema = json::schema::build::build_schema(uri, &schema_value)?;
+            let validator = doc::Validator::new(schema)?;
+
+            collections_by_path.insert(
+                path,
+                CollectionHandler {
+                    validator,
+                    binding_index,
+                    id_header: resource_config.id_from_header,
+                },
+            );
+        }
+
+        // Pre-format the expected auth header, to make checking requests easier.
+        let require_auth_header = endpoint_config
+            .require_auth_token
+            .map(|token| format!("Bearer {token}"));
+        if require_auth_header.is_none() {
+            tracing::warn!("authentication is disabled, allowing writes from anyone");
+        }
+
+        let io = Transactor::start(stdin, stdout);
+
+        Ok(Handler {
+            handlers_by_path: Mutex::new(collections_by_path),
+            io,
+            require_auth_header,
+        })
+    }
+
+    async fn handle_webhook(
+        &self,
+        collection_path: String,
+        request_headers: axum::http::header::HeaderMap,
+        body: serde_json::Value,
+    ) -> anyhow::Result<(StatusCode, Json<serde_json::Value>)> {
+        if let Some(expected_header) = self.require_auth_header.as_ref() {
+            match required_string_header(&request_headers, "Authorization") {
+                Ok(auth) if auth == expected_header => { /* request is authorized */ }
+                Ok(_) => {
+                    return Ok(err_response(
+                        StatusCode::FORBIDDEN,
+                        anyhow::anyhow!("invalid authorization token"),
+                    ));
+                }
+                Err(err) => {
+                    return Ok(err_response(StatusCode::UNAUTHORIZED, err));
+                }
+            }
+        }
+
+        let mut handlers_guard = self.handlers_by_path.lock().await;
+
+        let Some(collection) = handlers_guard.get_mut(collection_path.as_str()) else {
+            return Ok((StatusCode::NOT_FOUND, Json(serde_json::json!({"error": "not found"}))));
+        };
+
+        let enhanced_doc = match collection.prepare_document(body, request_headers) {
+            Ok(doc) => doc,
+            Err(err) => return Ok(err_response(StatusCode::BAD_REQUEST, err)),
+        };
+
+        let validation_result = collection.validator.validate(None, &enhanced_doc)?;
+        if let Err(validation_err) = validation_result.ok() {
+            return Ok((
+                StatusCode::BAD_REQUEST,
+                Json(
+                    serde_json::json!({"error": "request body failed validation", "validationError": validation_err }),
+                ),
+            ));
+        }
+        let txn = vec![(collection.binding_index, enhanced_doc)];
+        std::mem::drop(handlers_guard);
+
+        self.io.publish(txn).await?;
+        Ok((StatusCode::OK, Json(serde_json::json!({"published": 1 }))))
+    }
+}
+
+const JSON_CONTENT_TYPE: &str = "application/json";
+
+pub fn openapi_spec<'a>(
+    endpoint_config: &EndpointConfig,
+    bindings: &[Binding],
+) -> anyhow::Result<OpenApi> {
+    let failure_schema = schema::ObjectBuilder::new()
+        .property(
+            "error",
+            schema::Object::with_type(schema::SchemaType::String),
+        )
+        .build();
+    let success_schema = schema::ObjectBuilder::new()
+        .property(
+            "published",
+            schema::Object::with_type(schema::SchemaType::Integer),
+        )
+        .build();
+    let mut components = schema::ComponentsBuilder::new()
+        .response(
+            "successResponse",
+            openapi::ResponseBuilder::new().content(
+                JSON_CONTENT_TYPE,
+                openapi::content::ContentBuilder::new()
+                    .example(Some(serde_json::json!({"published": 1})))
+                    .schema(success_schema)
+                    .build(),
+            ),
+        )
+        .response(
+            "failureResponse",
+            openapi::ResponseBuilder::new().content(
+                JSON_CONTENT_TYPE,
+                openapi::content::ContentBuilder::new()
+                    .example(Some(
+                        serde_json::json!({"error": "missing required header 'X-Webhook-Id'"}),
+                    ))
+                    .schema(failure_schema)
+                    .build(),
+            ),
+        );
+    if endpoint_config.require_auth_token.is_some() {
+        let sec = security::SecurityScheme::Http(
+            security::HttpBuilder::new()
+                .scheme(security::HttpAuthScheme::Bearer)
+                .build(),
+        );
+        components = components.security_scheme("bearerAuth", sec);
+    }
+
+    let mut paths = openapi::PathsBuilder::new();
+
+    for binding in bindings.iter() {
+        let url_path = ensure_slash_prefix(binding.resource_path[0].as_str());
+
+        let raw_schema = binding.collection.schema.as_ref().unwrap();
+        let openapi_schema = serde_json::from_str::<openapi::Schema>(raw_schema.get())
+            .context("deserializing collection schema")?;
+
+        let content = openapi::content::ContentBuilder::new()
+            .schema(openapi_schema)
+            .build();
+        let request_body = openapi::request_body::RequestBodyBuilder::new()
+            .content(JSON_CONTENT_TYPE, content)
+            .description(Some(format!(
+                "a JSON object conforming to the write schema of the collection '{}'",
+                binding.collection.name
+            )))
+            .required(Some(openapi::Required::True)) // lol IDK why
+            .build();
+
+        let responses = openapi::ResponsesBuilder::new()
+            .response("200", openapi::Ref::from_response_name("successResponse"))
+            .response("400", openapi::Ref::from_response_name("failureResponse"))
+            .build();
+        let mut op_builder = openapi::path::OperationBuilder::new()
+            .request_body(Some(request_body))
+            .responses(responses)
+            .description(Some("append a document to the flow collection"));
+        if endpoint_config.require_auth_token.is_some() {
+            op_builder = op_builder.security(security::SecurityRequirement::new(
+                "bearerAuth",
+                Option::<String>::None,
+            ));
+        }
+        let operation = op_builder.build();
+        let path_item = openapi::path::PathItemBuilder::new()
+            .operation(openapi::PathItemType::Post, operation.clone())
+            .operation(openapi::PathItemType::Put, operation.clone())
+            .build();
+        paths = paths.path(url_path, path_item);
+    }
+
+    let spec = OpenApiBuilder::new()
+        .info(
+            openapi::InfoBuilder::new()
+                .title("Flow HTTP ingest")
+                .version(env!("CARGO_PKG_VERSION"))
+                .build(),
+        )
+        .components(Some(components.build()))
+        .paths(paths.build())
+        .build();
+    Ok(spec)
+}
+
+pub fn ensure_slash_prefix(path: &str) -> String {
+    if path.starts_with('/') {
+        path.to_owned()
+    } else {
+        format!("/{path}")
+    }
+}
+
+fn listen_on_port() -> u16 {
+    if let Ok(port_str) = std::env::var("SOURCE_HTTP_INGEST_PORT") {
+        port_str
+            .parse()
+            .expect("invalid SOURCE_HTTP_INGEST_PORT value")
+    } else {
+        8080
+    }
+}
+
+lazy_static::lazy_static! {
+    /// These are headers that should be excluded from the document that's output
+    /// into the Flow collection.
+    static ref REDACT_HEADERS: std::collections::HashSet<axum::http::HeaderName> = {
+        let headers = [
+            "authorization",
+            "cookie",
+            "content-type",
+            "accept",
+        ];
+        headers.iter().map(|s| axum::http::HeaderName::from_static(*s)).collect()
+    };
+}
