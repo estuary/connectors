4d0594f855fdff9becc96080be4fdef7c4ab8fe0
Will Donnelly <will@estuary.dev>
2023-03-01 17:35:19 -0600

    source-mysql: Clarify control vs capture user in tests
    This makes MySQL test setup around control vs capture accounts
    consistent with the previous Postgres changes.

--- source-mysql/docker-entrypoint-initdb.d/init-user-db.sh
+++ source-mysql/docker-entrypoint-initdb.d/init-user-db.sh
@@ -5,11 +5,15 @@ echo "======================================="
 echo "Initializing Database for Flow Captures"
 echo "======================================="
 
-mysql --user="root" --password="flow" --database="test" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS flow;
-    CREATE TABLE IF NOT EXISTS flow.watermarks (slot INTEGER PRIMARY KEY, watermark TEXT);
-    CREATE USER IF NOT EXISTS flow_capture IDENTIFIED BY 'secret';
+mysql --user="root" --password="secret1234" --database="mysql" <<-EOSQL
+    CREATE USER IF NOT EXISTS flow_capture IDENTIFIED BY 'secret1234';
+
+    CREATE DATABASE IF NOT EXISTS test;
+
     GRANT REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'flow_capture';
     GRANT SELECT ON *.* TO 'flow_capture';
+
+    CREATE DATABASE IF NOT EXISTS flow;
+    CREATE TABLE IF NOT EXISTS flow.watermarks (slot INTEGER PRIMARY KEY, watermark TEXT);
     GRANT INSERT, UPDATE, DELETE ON flow.watermarks TO 'flow_capture';
 EOSQL
