fe5348acfa836a791e62f607c1e0f03feea67198
Will Baker <wbaker@gmail.com>
2023-03-02 12:55:56 -0800

    materialize-postgres: convert to json protocol
    Converts materialize-postgres to use the Flow JSON protcol.

--- materialize-postgres/sqlgen_test.go
+++ materialize-postgres/sqlgen_test.go
@@ -2,40 +2,40 @@ package main
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"strings"
 	"testing"
 	"text/template"
 
 	"github.com/bradleyjkemp/cupaloy"
-	sqlDriver "github.com/estuary/connectors/materialize-sql"
+	"github.com/estuary/connectors/go/protocol"
+	sqlDriver "github.com/estuary/connectors/materialize-sql-json"
 	"github.com/estuary/connectors/testsupport"
-	"github.com/estuary/flow/go/protocols/catalog"
-	pf "github.com/estuary/flow/go/protocols/flow"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSQLGeneration(t *testing.T) {
-	var spec *pf.MaterializationSpec
+	var bindings []protocol.ApplyBinding
 	require.NoError(t, testsupport.CatalogExtract(t, "testdata/flow.yaml",
 		func(db *sql.DB) error {
 			var err error
-			spec, err = catalog.LoadMaterialization(db, "test/sqlite")
+			bindings, err = protocol.BindingsFromCatalog(db, "test/sqlite")
 			return err
 		}))
 
 	// TODO(whb): These keys are manually set as "nullable" for now to test the query generation
 	// with nullable keys. Once flow supports nullable keys natively, this should be cleaned up.
-	spec.Bindings[0].Collection.Projections[7].Inference.Exists = pf.Inference_MAY
-	spec.Bindings[0].Collection.Projections[7].Inference.Types = append(spec.Bindings[0].Collection.Projections[7].Inference.Types, "null")
-	spec.Bindings[1].Collection.Projections[2].Inference.Exists = pf.Inference_MAY
-	spec.Bindings[1].Collection.Projections[2].Inference.Types = append(spec.Bindings[1].Collection.Projections[2].Inference.Types, "null")
+	bindings[0].Collection.Projections[7].Inference.Exists = protocol.MayExist
+	bindings[0].Collection.Projections[7].Inference.Types = append(bindings[0].Collection.Projections[7].Inference.Types, "null")
+	bindings[1].Collection.Projections[2].Inference.Exists = protocol.MayExist
+	bindings[1].Collection.Projections[2].Inference.Types = append(bindings[1].Collection.Projections[2].Inference.Types, "null")
 
-	var shape1 = sqlDriver.BuildTableShape(spec, 0, tableConfig{
+	var shape1 = sqlDriver.BuildTableShape("test/sqlite", bindings, 0, tableConfig{
 		Schema: "a-schema",
 		Table:  "target_table",
 		Delta:  false,
 	})
-	var shape2 = sqlDriver.BuildTableShape(spec, 1, tableConfig{
+	var shape2 = sqlDriver.BuildTableShape("test/sqlite", bindings, 1, tableConfig{
 		Schema: "",
 		Table:  "Delta Updates",
 		Delta:  true,
@@ -71,9 +71,9 @@ func TestSQLGeneration(t *testing.T) {
 
 	var fence = sqlDriver.Fence{
 		TablePath:       sqlDriver.TablePath{"path", "To", "checkpoints"},
-		Checkpoint:      []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		Checkpoint:      base64.StdEncoding.EncodeToString([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}),
 		Fence:           123,
-		Materialization: pf.Materialization("some/Materialization"),
+		Materialization: "some/Materialization",
 		KeyBegin:        0x00112233,
 		KeyEnd:          0xffeeddcc,
 	}
@@ -90,11 +90,11 @@ func TestSQLGeneration(t *testing.T) {
 
 func TestDateTimeColumn(t *testing.T) {
 	var mapped, err = pgDialect.MapType(&sqlDriver.Projection{
-		Projection: pf.Projection{
-			Inference: pf.Inference{
+		Projection: &protocol.Projection{
+			Inference: protocol.Inference{
 				Types:   []string{"string"},
-				String_: &pf.Inference_String{Format: "date-time"},
-				Exists:  pf.Inference_MUST,
+				String_: &protocol.StringInference{Format: "date-time"},
+				Exists:  protocol.MustExist,
 			},
 		},
 	})
