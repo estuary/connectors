a3e11f41784d715ad15cbf68c44de89267387135
Will Donnelly <will@estuary.dev>
2023-03-02 17:03:56 -0600

    source-postgres: Implement prerequisite checks
    Also moves the automatic creation of the watermarks table,
    replication slot, and publication into those checks, rather
    than having them take place at various points partway through
    capture startup. Doing it this way means that we can attempt
    to autocreate them during validation (while the user is still
    setting up the capture in the UI) rather than having the first
    failure occur after they leave the capture setup UI and the
    capture just starts failing repeatedly.
    
    There's a new automated test which I've been using to exercise
    these checks, however it doesn't actually cover most of the
    failure cases because many of those require deliberately
    misconfigured database config or user permissions setups,
    and those are tricky to automate.

--- source-postgres/prerequisites.go
+++ source-postgres/prerequisites.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/estuary/connectors/sqlcapture"
+	"github.com/sirupsen/logrus"
 )
 
 func (db *postgresDatabase) SetupPrerequisites(ctx context.Context) []error {
@@ -22,31 +26,107 @@ func (db *postgresDatabase) SetupPrerequisites(ctx context.Context) []error {
 }
 
 func (db *postgresDatabase) prerequisiteLogicalReplication(ctx context.Context) error {
-	// TODO: Verify that 'wal_level = LOGICAL'
+	var level string
+	if err := db.conn.QueryRow(ctx, `SHOW wal_level;`).Scan(&level); err != nil {
+		return fmt.Errorf("unable to query 'wal_level' system variable: %w", err)
+	} else if level != "logical" {
+		return fmt.Errorf("logical replication isn't enabled: current wal_level = %q", level)
+	}
 	return nil
 }
 
 func (db *postgresDatabase) prerequisiteReplicationUser(ctx context.Context) error {
-	// TODO: Verify that the capture user has REPLICATION permission
+	var query = fmt.Sprintf(`SELECT rolreplication FROM pg_catalog.pg_roles WHERE rolname = '%s'`, db.config.User)
+	var replication bool
+	if err := db.conn.QueryRow(ctx, query).Scan(&replication); err != nil {
+		return fmt.Errorf("error querying REPLICATION role for user %q: %w", db.config.User, err)
+	}
+	if !replication {
+		return fmt.Errorf("user %q must have the REPLICATION role", db.config.User)
+	}
 	return nil
 }
 
 func (db *postgresDatabase) prerequisiteReplicationSlot(ctx context.Context) error {
-	// TODO: Verify that the replication slot exists
+	// If the replication slot already exists then we're satisfied
+	var slotName = db.config.Advanced.SlotName
+	var logEntry = logrus.WithField("slot", slotName)
+	var count int
+	if err := db.conn.QueryRow(ctx, fmt.Sprintf(`SELECT COUNT(*) FROM pg_catalog.pg_replication_slots WHERE slot_name = '%s' AND slot_type = 'logical';`, slotName)).Scan(&count); err != nil {
+		return fmt.Errorf("error querying replication slots: %w", err)
+	}
+	if count == 1 {
+		logEntry.Debug("replication slot exists")
+		return nil
+	}
+
+	// Otherwise try and create it
+	logEntry.Info("attempting to create replication slot")
+	if _, err := db.conn.Exec(ctx, fmt.Sprintf(`SELECT pg_create_logical_replication_slot('%s', 'pgoutput');`, slotName)); err != nil {
+		return fmt.Errorf("replication slot %q doesn't exist and couldn't be created", slotName)
+	}
+
+	logEntry.Info("created replication slot")
 	return nil
 }
 
 func (db *postgresDatabase) prerequisitePublication(ctx context.Context) error {
-	// TODO: Verify that the publication exists
+	var pubName = db.config.Advanced.PublicationName
+	var logEntry = logrus.WithField("publication", pubName)
+
+	var count int
+	if err := db.conn.QueryRow(ctx, fmt.Sprintf(`SELECT COUNT(*) FROM pg_catalog.pg_publication WHERE pubname = '%s';`, pubName)).Scan(&count); err != nil {
+		return fmt.Errorf("error querying publications: %w", err)
+	}
+	if count == 1 {
+		logEntry.Debug("publication exists")
+		return nil
+	}
+
+	logEntry.Info("attempting to create publication")
+	// TODO(wgd): We would like to stop using 'FOR ALL TABLES' at some point in the future
+	// (see https://github.com/estuary/connectors/issues/460), but this whole prerequisite-
+	// checking branch has gotten complicated enough already, so for now we'll keep doing
+	// this the same as before.
+	if _, err := db.conn.Exec(ctx, fmt.Sprintf(`CREATE PUBLICATION %s FOR ALL TABLES;`, pubName)); err != nil {
+		return fmt.Errorf("publication %q doesn't exist and couldn't be created", pubName)
+	}
+
 	return nil
 }
 
 func (db *postgresDatabase) prerequisiteWatermarksTable(ctx context.Context) error {
-	// TODO: Verify that the watermarks table exists and is writable
-	return nil
+	var table = db.config.Advanced.WatermarksTable
+	var logEntry = logrus.WithField("table", table)
+
+	// If we can successfully write a watermark then we're satisfied here
+	if err := db.WriteWatermark(ctx, "existence-check"); err == nil {
+		logEntry.Debug("watermarks table already exists")
+		return nil
+	}
+
+	// If we can create the watermarks table and then write a watermark, that also works
+	logEntry.Info("watermarks table doesn't exist, attempting to create it")
+	var _, err = db.conn.Exec(ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (slot TEXT PRIMARY KEY, watermark TEXT);", table))
+	if err == nil {
+		if err := db.WriteWatermark(ctx, "existence-check"); err == nil {
+			logEntry.Info("successfully created watermarks table")
+			return nil
+		}
+	} else {
+		logEntry.WithField("err", err).Error("failed to create watermarks table")
+	}
+
+	// Otherwise this is a failure
+	return fmt.Errorf("user %q cannot write to the watermarks table %q", db.config.User, table)
 }
 
 func (db *postgresDatabase) SetupTablePrerequisites(ctx context.Context, schema, table string) error {
-	// TODO(wgd): Verify that the table is present in the publication
+	var rows, err = db.conn.Query(ctx, fmt.Sprintf(`SELECT * FROM "%s"."%s" LIMIT 0;`, schema, table))
+	rows.Close()
+	if err != nil {
+		var streamID = sqlcapture.JoinStreamID(schema, table)
+		return fmt.Errorf("user %q cannot read from table %q", db.config.User, streamID)
+	}
 	return nil
 }
