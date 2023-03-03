31a882db8735f45be92f463e914f9df14df23f3d
Will Donnelly <will@estuary.dev>
2023-03-03 13:04:48 -0600

    source-{mysql,postgres,sqlserver}: Improved 'TestPrerequisites'

--- source-sqlserver/prerequisites_test.go
+++ source-sqlserver/prerequisites_test.go
@@ -16,9 +16,9 @@ func TestPrerequisites(t *testing.T) {
 	var tableB = tb.CreateTable(ctx, t, "bbb", "(id INTEGER PRIMARY KEY, data TEXT)")
 	var tableC = strings.ReplaceAll(tableA, "aaa", "ccc")
 	tb.Insert(ctx, t, tableA, [][]any{{0, "hello"}, {1, "world"}})
-	var cs = tb.CaptureSpec(t, tableA, tableB, tableC)
 
-	t.Run("validate", func(t *testing.T) {
+	t.Run("validateAB", func(t *testing.T) {
+		var cs = tb.CaptureSpec(t, tableA, tableB)
 		_, err := cs.Validate(ctx, t)
 		if err != nil {
 			cupaloy.SnapshotT(t, err.Error())
@@ -26,7 +26,21 @@ func TestPrerequisites(t *testing.T) {
 			cupaloy.SnapshotT(t, "no error")
 		}
 	})
-	t.Run("capture", func(t *testing.T) {
+	t.Run("validateABC-fails", func(t *testing.T) {
+		var cs = tb.CaptureSpec(t, tableA, tableB, tableC)
+		_, err := cs.Validate(ctx, t)
+		if err != nil {
+			cupaloy.SnapshotT(t, err.Error())
+		} else {
+			cupaloy.SnapshotT(t, "no error")
+		}
+	})
+	t.Run("captureAB", func(t *testing.T) {
+		var cs = tb.CaptureSpec(t, tableA, tableB)
+		tests.VerifiedCapture(ctx, t, cs)
+	})
+	t.Run("captureABC-fails", func(t *testing.T) {
+		var cs = tb.CaptureSpec(t, tableA, tableB, tableC)
 		tests.VerifiedCapture(ctx, t, cs)
 	})
 }
