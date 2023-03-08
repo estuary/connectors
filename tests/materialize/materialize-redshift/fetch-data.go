cacafb60c0f56671b10d42082e605cec052fef1e
Will Baker <wbaker@gmail.com>
2023-03-08 14:44:21 -0800

    materialize-redshift: end-to-end tests
    These end-to-end tests are not really practical to run in CI: It costs hundreds of dollars a month
    to keep a cluster running, and the timing differences from run to run would surely make them flaky.
    But they are handy for running locally to verify that changes haven't broken anything, so they're
    worth checking in.

---
+++ tests/materialize/materialize-redshift/fetch-data.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	stdsql "database/sql"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func main() {
+	address, ok := os.LookupEnv("REDSHIFT_ADDRESS")
+	if !ok {
+		log.Fatal("missing REDSHIFT_ADDRESS environment variable")
+	}
+
+	user, ok := os.LookupEnv("REDSHIFT_USER")
+	if !ok {
+		log.Fatal("missing REDSHIFT_USER environment variable")
+	}
+
+	password, ok := os.LookupEnv("REDSHIFT_PASSWORD")
+	if !ok {
+		log.Fatal("missing REDSHIFT_PASSWORD environment variable")
+	}
+
+	database, ok := os.LookupEnv("REDSHIFT_DATABASE")
+	if !ok {
+		log.Fatal("missing REDSHIFT_DATABASE environment variable")
+	}
+
+	tables := os.Args[1:]
+	if len(tables) != 1 {
+		log.Fatal("must provide table name as an argument")
+	}
+
+	uri := url.URL{
+		Scheme: "postgres",
+		Host:   address,
+		User:   url.UserPassword(user, password),
+		Path:   "/" + database,
+	}
+
+	db, err := stdsql.Open("pgx", uri.String())
+	if err != nil {
+		log.Fatal(fmt.Errorf("connecting to db: %w", err))
+	}
+	defer db.Close()
+
+	rows, err := db.Query(fmt.Sprintf("SELECT * FROM %s ORDER BY ID", tables[0]))
+	if err != nil {
+		log.Fatal(fmt.Errorf("running query: %w", err))
+	}
+	defer rows.Close()
+
+	cols, err := rows.Columns()
+	if err != nil {
+		log.Fatal(fmt.Errorf("reading columns: %w", err))
+	}
+	row := make([]interface{}, len(cols))
+
+	out := json.NewEncoder(os.Stdout)
+	for rows.Next() {
+		for idx := range row {
+			row[idx] = &row[idx]
+		}
+
+		if err := rows.Scan(row...); err != nil {
+			log.Fatal(fmt.Errorf("scanning query result: %w", err))
+		}
+
+		jsonlRow := make(map[string]interface{})
+		for idx, col := range cols {
+			jsonlRow[col] = row[idx]
+		}
+		delete(jsonlRow, "flow_document")
+
+		if err := out.Encode(jsonlRow); err != nil {
+			log.Fatal(fmt.Errorf("encoding row result: %w", err))
+		}
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(fmt.Errorf("error from returned rows: %w", err))
+	}
+}
