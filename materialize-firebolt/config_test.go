a86ddd31e05d15c88966b9dadc1bf4e4a7b891c6
Will Baker <wbaker@gmail.com>
2023-03-24 12:03:48 -0700

    materialize-firebolt

--- materialize-firebolt/config_test.go
+++ materialize-firebolt/config_test.go
@@ -6,14 +6,13 @@ import (
 	"testing"
 
 	"github.com/bradleyjkemp/cupaloy"
-	pf "github.com/estuary/flow/go/protocols/flow"
 	pm "github.com/estuary/flow/go/protocols/materialize"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDriverSpec(t *testing.T) {
 	var drv = driver{}
-	var resp, err1 = drv.Spec(context.Background(), &pm.SpecRequest{EndpointType: pf.EndpointType_FLOW_SINK})
+	var resp, err1 = drv.Spec(context.Background(), &pm.Request_Spec{})
 	require.NoError(t, err1)
 	var formatted, err2 = json.MarshalIndent(resp, "", "  ")
 	require.NoError(t, err2)
