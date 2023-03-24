273e31e519762b99ea15aeb74711a1fd84fc533a
Will Baker <wbaker@gmail.com>
2023-03-24 11:18:59 -0700

    materialize-mongodb

--- materialize-mongodb/config_test.go
+++ materialize-mongodb/config_test.go
@@ -6,18 +6,17 @@ import (
 	"testing"
 
 	"github.com/bradleyjkemp/cupaloy"
-	pf "github.com/estuary/flow/go/protocols/flow"
 	pm "github.com/estuary/flow/go/protocols/materialize"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSpecification(t *testing.T) { 
-	var resp, err = (driver{}). 
-		Spec(context.Background(), &pm.SpecRequest{EndpointType: pf.EndpointType_AIRBYTE_SOURCE}) 
-	require.NoError(t, err) 
+func TestSpecification(t *testing.T) {
+	var resp, err = (driver{}).
+		Spec(context.Background(), &pm.Request_Spec{})
+	require.NoError(t, err)
 
-	formatted, err := json.MarshalIndent(resp, "", "  ") 
-	require.NoError(t, err) 
+	formatted, err := json.MarshalIndent(resp, "", "  ")
+	require.NoError(t, err)
 
-	cupaloy.SnapshotT(t, formatted) 
-} 
+	cupaloy.SnapshotT(t, formatted)
+}
