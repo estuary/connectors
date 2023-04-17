e425e5cd2d4b53ba3d25d57a2998e48d7a00c94b
Will Donnelly <will@estuary.dev>
2023-04-17 18:57:46 -0500

    source-{mysql,postgres,sqlserver}: Add TestSecondaryIndexDiscovery
    This is a new test which excercises discovery behavior in various
    cases where secondary indices are present. Currently these test
    snapshots reflect the old behavior, which is to ignore secondary
    indices entirely.
    
    But this should be a good starting point for implementing secondary
    index discovery on each database.

--- source-sqlserver/discovery_test.go
+++ source-sqlserver/discovery_test.go
@@ -8,50 +8,8 @@ import (
 	"testing"
 )
 
-func TestDiscoveryComplex(t *testing.T) {
-	var tb, ctx = postgresTestBackend(t), context.Background()
-
-	const uniqueString = "cheap_oxygenation"
-	var tableName = tb.CreateTable(ctx, t, uniqueString, `(
-		k1             INTEGER NOT NULL,
-		foo            TEXT,
-		real_          REAL NOT NULL,
-		"Bounded Text" VARCHAR(255),
-		k2             TEXT,
-		doc            JSON,
-		"doc/bin"      JSONB NOT NULL,
-		PRIMARY KEY(k2, k1)
-	)`)
-	tb.Query(ctx, t, fmt.Sprintf("COMMENT ON COLUMN %s.foo IS 'This is a text field!';", tableName))
-	tb.Query(ctx, t, fmt.Sprintf("COMMENT ON COLUMN %s.k1 IS 'I think this is a key ?';", tableName))
-
-	tb.CaptureSpec(ctx, t).VerifyDiscover(ctx, t, regexp.MustCompile(regexp.QuoteMeta(uniqueString)))
-}
-
-func TestDiscoveryCapitalization(t *testing.T) {
-	var tb, ctx = postgresTestBackend(t), context.Background()
-	var tablePrefix = strings.TrimPrefix(t.Name(), "Test")
-	var tableA = tablePrefix + "_AaAaA"                  // Name contains capital letters and will be quoted
-	var tableB = tablePrefix + "_BbBbB"                  // Name contains capital letters and will not be quoted
-	var tableC = strings.ToLower(tablePrefix + "_CcCcC") // Name is all lowercase and will be quoted
-
-	var cleanup = func() {
-		tb.Query(ctx, t, fmt.Sprintf(`DROP TABLE IF EXISTS "%s"."%s";`, testSchemaName, tableA))
-		tb.Query(ctx, t, fmt.Sprintf(`DROP TABLE IF EXISTS "%s".%s;`, testSchemaName, tableB))
-		tb.Query(ctx, t, fmt.Sprintf(`DROP TABLE IF EXISTS "%s"."%s";`, testSchemaName, tableC))
-	}
-	cleanup()
-	t.Cleanup(cleanup)
-
-	tb.Query(ctx, t, fmt.Sprintf(`CREATE TABLE "%s"."%s" (id INTEGER PRIMARY KEY, data TEXT);`, testSchemaName, tableA))
-	tb.Query(ctx, t, fmt.Sprintf(`CREATE TABLE "%s".%s (id INTEGER PRIMARY KEY, data TEXT);`, testSchemaName, tableB))
-	tb.Query(ctx, t, fmt.Sprintf(`CREATE TABLE "%s"."%s" (id INTEGER PRIMARY KEY, data TEXT);`, testSchemaName, tableC))
-
-	tb.CaptureSpec(ctx, t).VerifyDiscover(ctx, t, regexp.MustCompile(fmt.Sprintf(`(?i:%s)`, regexp.QuoteMeta(tablePrefix))))
-}
-
 func TestSecondaryIndexDiscovery(t *testing.T) {
-	var tb, ctx = postgresTestBackend(t), context.Background()
+	var tb, ctx = sqlserverTestBackend(t), context.Background()
 
 	t.Run("pk_and_index", func(t *testing.T) {
 		var uniqueString = "g14228"
