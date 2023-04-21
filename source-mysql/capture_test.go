edd22c469a73b5b22910b27491226988b51b7c35
Will Baker <wbaker@gmail.com>
2023-04-21 16:32:44 -0400

    source-mysql: quote table name identifiers
    Similar to quoting key column names, only a limited set of characters is allowed for MySQL when
    referring to tables without quotes. This change allows for a wider variety of table names by always
    quoting the schema & table identifiers.

--- source-mysql/capture_test.go
+++ source-mysql/capture_test.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"regexp"
 	"testing"
 
+	"github.com/estuary/connectors/sqlcapture"
 	"github.com/estuary/connectors/sqlcapture/tests"
+	"github.com/estuary/flow/go/protocols/flow"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTrickyColumnNames(t *testing.T) {
@@ -31,4 +36,36 @@ func TestTrickyColumnNames(t *testing.T) {
 	tb.Insert(ctx, t, tableA, [][]interface{}{{5, "eee"}, {6, "fff"}})
 	tb.Insert(ctx, t, tableB, [][]interface{}{{7, "ggg"}, {8, "hhh"}})
 	t.Run("replication", func(t *testing.T) { tests.VerifiedCapture(ctx, t, cs) })
+}
+
+func TestTrickyTableNames(t *testing.T) {
+	var tb, ctx = mysqlTestBackend(t), context.Background()
+	tb.Query(ctx, t, fmt.Sprintf("DROP TABLE IF EXISTS `%s`.`UsErS!@#$`", testSchemaName))
+	tb.Query(ctx, t, fmt.Sprintf("CREATE TABLE `%s`.`UsErS!@#$` (id INTEGER PRIMARY KEY, data TEXT NOT NULL)", testSchemaName))
+	var cs = tb.CaptureSpec(ctx, t)
+	t.Run("Discover", func(t *testing.T) {
+		cs.VerifyDiscover(ctx, t, regexp.MustCompile(`(?i:users)`))
+	})
+	var resourceSpecJSON, err = json.Marshal(sqlcapture.Resource{
+		Namespace: testSchemaName,
+		Stream:    "UsErS!@#$",
+	})
+	require.NoError(t, err)
+	cs.Bindings = []*flow.CaptureSpec_Binding{{
+		Collection:         flow.CollectionSpec{Name: flow.Collection("acmeCo/test/users____")},
+		ResourceConfigJson: resourceSpecJSON,
+		ResourcePath:       []string{testSchemaName, "UsErS!@#$"},
+	}}
+	t.Run("Validate", func(t *testing.T) {
+		var _, err = cs.Validate(ctx, t)
+		require.NoError(t, err)
+	})
+	t.Run("Capture", func(t *testing.T) {
+		tb.Query(ctx, t, fmt.Sprintf("INSERT INTO `%s`.`UsErS!@#$` VALUES (1, 'Alice'), (2, 'Bob')", testSchemaName))
+		tests.VerifiedCapture(ctx, t, cs)
+		t.Run("Replication", func(t *testing.T) {
+			tb.Query(ctx, t, fmt.Sprintf("INSERT INTO `%s`.`UsErS!@#$` VALUES (3, 'Carol'), (4, 'Dave')", testSchemaName))
+			tests.VerifiedCapture(ctx, t, cs)
+		})
+	})
 }
