ccfdc5cdfd03fd3eb6f00bf26f912723cde82097
Will Baker <wbaker@gmail.com>
2023-04-21 15:38:38 -0400

    sqlcapture: handle problematic table names
    Flow collections have naming requirements that are more strict than typical database table names.
    Attempted discovery on a database containing unusually named tables currently results in an error
    due to the recommend collection names. This change adds some simple sanitization to recommended
    collections names to allow for these tables to be discovered without error.
    
    There is still a potential problem if multiple tables get sanitized into the same flow collection
    name, for example tables named "name!" and "name?" will both get sanitized as "name_". Currently
    this edge case is not handled as it is likely rare.

---
+++ sqlcapture/discovery_test.go
@@ -0,0 +1,75 @@
+package sqlcapture
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRecommendedCatalogName(t *testing.T) {
+	tests := []struct {
+		schema string
+		table  string
+		want   string
+	}{
+		{
+			schema: "something",
+			table:  "typical",
+			want:   "something.typical",
+		},
+		{
+			schema: "Capital",
+			table:  "LetterS",
+			want:   "capital.letters",
+		},
+		{
+			schema: "for$bidden",
+			table:  "char%s",
+			want:   "for_bidden.char_s",
+		},
+		{
+			schema: "schema",
+			table:  "/table",
+			want:   "schema._table",
+		},
+		{
+			schema: "path-like",
+			table:  "/../this",
+			want:   "path-like._.._this",
+		},
+		{
+			schema: "@",
+			table:  "!",
+			want:   "_._",
+		},
+		{
+			schema: ".",
+			table:  "/",
+			want:   ".._",
+		},
+		{
+			schema: ".",
+			table:  "", // This should not be possible, but even if it were it would not be a problem as a collection name.
+			want:   "..",
+		},
+		{
+			schema: "/",
+			table:  "./",
+			want:   "_.._",
+		},
+		{
+			schema: "multiple////",
+			table:  "slashes",
+			want:   "multiple____.slashes",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.schema+"."+tt.table, func(t *testing.T) {
+			got := recommendedCatalogName(tt.schema, tt.table)
+			require.Equal(t, path.Clean(got), got)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
