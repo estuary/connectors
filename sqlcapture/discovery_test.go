eb3b757b5e3ebabd1a9773dbe76facbc8ee989fd
Will Baker <wbaker@gmail.com>
2023-04-27 11:45:25 -0400

    source-postgres: prerequisite check for minimum required database version
    Adds a check to SetupPrerequisites to verify that the captured database is of the minimum supported
    version.
    
    This check is implemented in a conservative way, so it would be more accurate to describe it as a
    check that if the database version can conclusively be determined NOT to meet the minimum required,
    we'll error out of the Validate RPC. It's done this way to account for unforeseen database variants
    (managed versions in particular) which may be just fine to capture from but not report correct
    version information. If we can't say for sure that the database is running on too old of a version
    we want to at least give it a try, rather than enforcing a requirement that a compatible database
    version be strictly available.
    
    This same strategy is applied for subsequent commits for `source-mysql` and `source-sqlserver` for
    their new minimum version prerequisite check.

--- sqlcapture/discovery_test.go
+++ sqlcapture/discovery_test.go
@@ -101,3 +101,156 @@ func TestKeyRoundTrip(t *testing.T) {
 		})
 	}
 }
+
+func TestParseVersion(t *testing.T) {
+	tests := []struct {
+		version   string
+		major     int
+		minor     int
+		shouldErr bool
+	}{
+		{
+			version:   "invalid",
+			major:     0,
+			minor:     0,
+			shouldErr: true,
+		},
+		{
+			version:   "",
+			major:     0,
+			minor:     0,
+			shouldErr: true,
+		},
+		{
+			version:   "10",
+			major:     0,
+			minor:     0,
+			shouldErr: true,
+		},
+		{
+			version:   "10.0",
+			major:     10,
+			minor:     0,
+			shouldErr: false,
+		},
+		{
+			version:   "11.0 (Debian 11.0-1.pgdg90+2)",
+			major:     11,
+			minor:     0,
+			shouldErr: false,
+		},
+		{
+			version:   "v11.0 (Debian 11.0-1.pgdg90+2)",
+			major:     11,
+			minor:     0,
+			shouldErr: false,
+		},
+		{
+			version:   "10.3.38-MariaDB-1:10.3.38+maria~ubu2004-log",
+			major:     10,
+			minor:     3,
+			shouldErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.version, func(t *testing.T) {
+			major, minor, err := ParseVersion(tt.version)
+			if tt.shouldErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.Equal(t, tt.major, major)
+			require.Equal(t, tt.minor, minor)
+		})
+	}
+}
+
+func TestValidVersion(t *testing.T) {
+	tests := []struct {
+		name               string
+		major, minor       int
+		reqMajor, reqMinor int
+		valid              bool
+	}{
+		{
+			name:     "equal major and minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 2,
+			reqMinor: 3,
+			valid:    true,
+		},
+		{
+			name:     "bigger major, smaller minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 1,
+			reqMinor: 4,
+			valid:    true,
+		},
+		{
+			name:     "smaller major, bigger minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 3,
+			reqMinor: 2,
+			valid:    false,
+		},
+		{
+			name:     "bigger major, bigger minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 1,
+			reqMinor: 2,
+			valid:    true,
+		},
+		{
+			name:     "smaller major, smaller minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 3,
+			reqMinor: 4,
+			valid:    false,
+		},
+		{
+			name:     "bigger major, equal minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 1,
+			reqMinor: 3,
+			valid:    true,
+		},
+		{
+			name:     "smaller major, equal minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 3,
+			reqMinor: 3,
+			valid:    false,
+		},
+		{
+			name:     "equal major, bigger minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 2,
+			reqMinor: 2,
+			valid:    true,
+		},
+		{
+			name:     "equal major, smaller minor",
+			major:    2,
+			minor:    3,
+			reqMajor: 2,
+			reqMinor: 4,
+			valid:    false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.valid, ValidVersion(tt.major, tt.minor, tt.reqMajor, tt.reqMinor))
+		})
+	}
+}
