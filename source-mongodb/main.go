3a8e644757148f627a78f5ec48366a8136450327
Mahdi Dibaiee <mdibaiee@pm.me>
2023-05-08 21:04:01 +0100

    mongodb: fix jsonschema description

--- source-mongodb/main.go
+++ source-mongodb/main.go
@@ -31,7 +31,7 @@ func (r resource) Validate() error {
 
 // config represents the endpoint configuration for mongodb
 type config struct {
-	Address  string `json:"address" jsonschema:"title=Address,description=The connection URI for your database without the username and password. For example mongodb://my-mongo.test?authSource=admin." jsonschema_extras:"order=0"`
+	Address  string `json:"address" jsonschema:"title=Address" jsonschema_description:"The connection URI for your database without the username and password. For example mongodb://my-mongo.test?authSource=admin." jsonschema_extras:"order=0"`
 	User     string `json:"user" jsonschema:"title=User,description=Database user to connect as." jsonschema_extras:"order=1"`
 	Password string `json:"password" jsonschema:"title=Password,description=Password for the specified database user." jsonschema_extras:"secret=true,order=2"`
 	Database string `json:"database" jsonschema:"title=Database,description=Name of the database to capture from." jsonschema_extras:"order=3"`
