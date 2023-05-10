292cf56692261974520a38d3b6d0aff51a4e923f
Will Donnelly <will@estuary.dev>
2023-05-10 17:49:59 -0500

    source-mysql: Ignore /* comments like this */
    Also adds a test because this regex is getting long enough
    to need one even if each individual case is fairly simple.

---
+++ source-mysql/replication_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestIgnoreQueries(t *testing.T) {
+	var cases = map[string]bool{
+		`# This is a comment`:          true,
+		`/* This is also a comment */`: true,
+		`BEGIN`:                        true,
+		`COMMIT`:                       true,
+
+		`CREATE USER IF NOT EXISTS flow_capture IDENTIFIED BY 'secret1234'`: true,
+
+		`CREATE DATABASE IF NOT EXISTS test`:     false,
+		`INSERT INTO foobar VALUES (1, 'hello')`: false,
+		`DROP TABLE foobar`:                      false,
+	}
+	for input, expect := range cases {
+		if ignoreQueriesRe.MatchString(input) != expect {
+			t.Errorf("ignore result mismath for %q (expected %v)", input, expect)
+		}
+	}
+}
