c1856f803d0042330151c6947b6516a35ee47366
Will Baker <wbaker@gmail.com>
2023-05-25 17:24:38 -0400

    materialize-pinecone: simplify transactor

--- materialize-pinecone/transactor.go
+++ materialize-pinecone/transactor.go
@@ -17,24 +17,13 @@ var (
 	concurrentWorkers = 5
 )
 
-type upserter interface {
-	Upsert(context.Context, client.PineconeUpsertRequest) error
-}
-
-type embedder interface {
-	CreateEmbeddings(context.Context, []string) ([]client.Embedding, error)
-}
-
 type transactor struct {
-	pineconeClient upserter
-	openAiClient   embedder
+	pineconeClient *client.PineconeClient
+	openAiClient   *client.OpenAiClient
 	bindings       []binding
 
-	// Per-transaction variables reset by init.
-	group           *errgroup.Group
-	groupCtx        context.Context
-	upserts         chan upsertDoc
-	flushEmbeddings chan struct{}
+	group    *errgroup.Group
+	groupCtx context.Context
 }
 
 type binding struct {
@@ -49,10 +38,9 @@ type binding struct {
 }
 
 type upsertDoc struct {
-	input     string
-	key       string
-	namespace string
-	metadata  map[string]interface{}
+	input    string
+	key      string
+	metadata map[string]interface{}
 }
 
 func (t *transactor) Load(it *pm.LoadIterator, loaded func(int, json.RawMessage) error) error {
@@ -65,7 +53,10 @@ func (t *transactor) Load(it *pm.LoadIterator, loaded func(int, json.RawMessage)
 func (t *transactor) Store(it *pm.StoreIterator) (pm.StartCommitFunc, error) {
 	ctx := it.Context()
 
-	t.init(ctx)
+	t.group, t.groupCtx = errgroup.WithContext(ctx)
+	t.group.SetLimit(concurrentWorkers)
+
+	batches := make(map[string][]upsertDoc)
 
 	for it.Next() {
 		b := t.bindings[it.Binding]
@@ -86,112 +77,72 @@ func (t *transactor) Store(it *pm.StoreIterator) (pm.StartCommitFunc, error) {
 			}
 		}
 
-		if err := t.sendDoc(ctx, upsertDoc{
-			input:     input,
-			key:       base64.RawURLEncoding.EncodeToString(it.PackedKey),
-			namespace: t.bindings[it.Binding].namespace,
-			metadata:  metadata,
-		}); err != nil {
-			return nil, err
+		namespace := t.bindings[it.Binding].namespace
+
+		batches[namespace] = append(batches[namespace], upsertDoc{
+			input:    input,
+			key:      base64.RawURLEncoding.EncodeToString(it.PackedKey),
+			metadata: metadata,
+		})
+
+		if len(batches[namespace]) >= batchSize {
+			if err := t.sendBatch(namespace, batches[namespace]); err != nil {
+				return nil, fmt.Errorf("sending batch of documents: %w", err)
+			}
+			batches[namespace] = nil
 		}
 	}
 
-	return nil, t.flushStores()
+	// Flush remaining partial batches.
+	for namespace, batch := range batches {
+		if len(batch) > 0 {
+			if err := t.sendBatch(namespace, batch); err != nil {
+				return nil, fmt.Errorf("flushing documents batch: %w", err)
+			}
+		}
+	}
+
+	return nil, t.group.Wait()
 }
 
-func (t *transactor) init(ctx context.Context) {
-	t.upserts = make(chan upsertDoc)
-	t.flushEmbeddings = make(chan struct{})
-
-	t.group, t.groupCtx = errgroup.WithContext(ctx)
-	t.group.Go(func() error {
-		return t.embeddingsWorker(t.groupCtx, t.flushEmbeddings, t.upserts)
-	})
-}
-
-func (t *transactor) sendDoc(ctx context.Context, u upsertDoc) error {
+func (t *transactor) sendBatch(namespace string, batch []upsertDoc) error {
 	select {
-	case <-ctx.Done():
-		return ctx.Err()
 	case <-t.groupCtx.Done():
 		return t.group.Wait()
-	case t.upserts <- u:
-		return nil
-	}
-}
-
-func (t *transactor) flushStores() error {
-	close(t.flushEmbeddings)
-	return t.group.Wait()
-}
-
-func (t *transactor) embeddingsWorker(ctx context.Context, flush <-chan struct{}, in <-chan upsertDoc) error {
-	workers, workerCtx := errgroup.WithContext(ctx)
-	workers.SetLimit(concurrentWorkers)
-
-	batches := make(map[string][]upsertDoc)
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-workerCtx.Done():
-			return workers.Wait()
-		case <-flush:
-			for nn, batch := range batches {
-				if len(batch) > 0 {
-					t.flushEmbeddingsBatch(workerCtx, workers, batch, nn)
-				}
+	default:
+		t.group.Go(func() error {
+			input := make([]string, 0, len(batch))
+			for _, r := range batch {
+				input = append(input, r.input)
 			}
-			return workers.Wait()
-		case next := <-in:
-			batches[next.namespace] = append(batches[next.namespace], next)
 
-			if len(batches[next.namespace]) >= batchSize {
-				// Copy for the goroutine we are going to run.
-				bCopy := append([]upsertDoc(nil), batches[next.namespace]...)
-				nn := next.namespace
-
-				// Re-initialize the main loop batch buffer.
-				batches[next.namespace] = batches[next.namespace][:0]
-
-				t.flushEmbeddingsBatch(workerCtx, workers, bCopy, nn)
+			embeddings, err := t.openAiClient.CreateEmbeddings(t.groupCtx, input)
+			if err != nil {
+				return fmt.Errorf("openAI creating embeddings: %w", err)
 			}
-		}
+
+			upsert := client.PineconeUpsertRequest{
+				Vectors:   []client.Vector{},
+				Namespace: namespace,
+			}
+
+			for _, e := range embeddings {
+				thisMeta := batch[e.Index]
+				upsert.Vectors = append(upsert.Vectors, client.Vector{
+					Id:       thisMeta.key,
+					Values:   e.Embedding,
+					Metadata: thisMeta.metadata,
+				})
+			}
+
+			if err := t.pineconeClient.Upsert(t.groupCtx, upsert); err != nil {
+				return fmt.Errorf("pinecone upserting batch: %w", err)
+			}
+			return nil
+		})
 	}
-}
 
-func (t *transactor) flushEmbeddingsBatch(ctx context.Context, workers *errgroup.Group, batch []upsertDoc, namespace string) {
-	input := make([]string, 0, len(batch))
-	for _, r := range batch {
-		input = append(input, r.input)
-	}
-
-	workers.Go(func() error {
-		embeddings, err := t.openAiClient.CreateEmbeddings(ctx, input)
-		if err != nil {
-			return fmt.Errorf("openAI creating embeddings: %w", err)
-		}
-
-		upsert := client.PineconeUpsertRequest{
-			Vectors:   []client.Vector{},
-			Namespace: namespace,
-		}
-
-		for _, e := range embeddings {
-			thisMeta := batch[e.Index]
-			upsert.Vectors = append(upsert.Vectors, client.Vector{
-				Id:       thisMeta.key,
-				Values:   e.Embedding,
-				Metadata: thisMeta.metadata,
-			})
-		}
-
-		if err := t.pineconeClient.Upsert(ctx, upsert); err != nil {
-			return fmt.Errorf("pinecone upserting batch: %w", err)
-		}
-		return nil
-	})
+	return nil
 }
 
 func (t *transactor) Destroy() {}
