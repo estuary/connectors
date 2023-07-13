68d0842ed220e2a5a475ce2c8a6888368a4d9f8d
Will Donnelly <will@estuary.dev>
2023-07-13 11:00:16 -0500

    source-postgres: Snapshots updates for 'xid' property

--- source-postgres/main_test.go
+++ source-postgres/main_test.go
@@ -104,6 +104,7 @@ func (tb *testBackend) CaptureSpec(ctx context.Context, t testing.TB, streamMatc
 	sanitizers[`"loc":[11111111,11111111,11111111]`] = regexp.MustCompile(`"loc":\[(-1|[0-9]+),[0-9]+,[0-9]+\]`)
 	sanitizers[`"cursor":"0/1111111"`] = regexp.MustCompile(`"cursor":"0/[0-9A-F]+"`)
 	sanitizers[`"ts_ms":1111111111111`] = regexp.MustCompile(`"ts_ms":[0-9]+`)
+	sanitizers[`"xid":111111`] = regexp.MustCompile(`"xid":[0-9]+`)
 
 	var cfg = tb.config
 	var cs = &st.CaptureSpec{
