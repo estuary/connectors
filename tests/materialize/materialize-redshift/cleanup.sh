58713e19a36f6a2ed1f22ffe038f87c130069ac7
Will Baker <wbaker@gmail.com>
2023-07-14 14:07:03 -0400

    materialize-redshift: fix end-to-end tests

--- tests/materialize/materialize-redshift/cleanup.sh
+++ tests/materialize/materialize-redshift/cleanup.sh
@@ -4,5 +4,21 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# TODO(johnny): This mostly works because deleting bindings also drops the tables.
-# But, this script should nuke it from orbit and ensure we get back to a clean state.
+function dropTable() {
+    echo "drop table if exists \"$1\";" | psql postgres://${REDSHIFT_USER}:${REDSHIFT_PASSWORD}@${REDSHIFT_ADDRESS}/${REDSHIFT_DATABASE}
+}
+
+echo "--- Running Cleanup ---"
+
+# Remove materialized tables.
+dropTable "Simple"
+dropTable "duplicate_keys_standard"
+dropTable "duplicate_keys_delta"
+dropTable "duplicate_keys_delta_exclude_flow_doc"
+dropTable "Multiple_Types"
+dropTable "Formatted_Strings"
+
+# Remove the persisted materialization spec & checkpoint for this test materialization so subsequent
+# runs start from scratch.
+echo "delete from FLOW_CHECKPOINTS_V1 where MATERIALIZATION='tests/materialize-redshift/materialize';" | psql postgres://${REDSHIFT_USER}:${REDSHIFT_PASSWORD}@${REDSHIFT_ADDRESS}/${REDSHIFT_DATABASE}
+echo "delete from FLOW_MATERIALIZATIONS_V2 where MATERIALIZATION='tests/materialize-redshift/materialize';" | psql postgres://${REDSHIFT_USER}:${REDSHIFT_PASSWORD}@${REDSHIFT_ADDRESS}/${REDSHIFT_DATABASE}
