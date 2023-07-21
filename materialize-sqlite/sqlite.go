e696fa140623484c914b402f519eaa5039bbb802
Will Baker <wbaker@gmail.com>
2023-07-21 19:13:10 -0400

    materialize-sql: do not apply table alterations for dry runs

--- materialize-sqlite/sqlite.go
+++ materialize-sqlite/sqlite.go
@@ -98,11 +98,11 @@ func NewSQLiteDriver() *sql.Driver {
 // or newly nullable. We may revisit this in the future if we want SQLite materialization to update
 // on the fly, but for now they can just be restarted which will re-create their tables in
 // accordance with the collection schema.
-func (c client) AddColumnToTable(ctx context.Context, tableIdentifier string, columnIdentifier string, columnDDL string) (string, error) {
+func (c client) AddColumnToTable(ctx context.Context, dryRun bool, tableIdentifier string, columnIdentifier string, columnDDL string) (string, error) {
 	return "", nil
 }
 
-func (c client) DropNotNullForColumn(ctx context.Context, tableIdentifier string, columnIdentifier string) (string, error) {
+func (c client) DropNotNullForColumn(ctx context.Context, dryRun bool, tableIdentifier string, columnIdentifier string) (string, error) {
 	return "", nil
 }
 
