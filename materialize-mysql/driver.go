9f541109eb2c5ec5ae04a334740e01df36a84263
Mahdi Dibaiee <mdibaiee@pm.me>
2023-07-28 12:29:09 +0100

    materialize-mysql: first version of MySQL materialization

---
+++ materialize-mysql/driver.go
@@ -0,0 +1,538 @@
+package main
+
+import (
+	"context"
+	stdsql "database/sql"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net"
+	"net/url"
+	"strings"
+	"text/template"
+	"time"
+
+	networkTunnel "github.com/estuary/connectors/go/network-tunnel"
+	"github.com/estuary/connectors/go/pkg/slices"
+	mysql "github.com/go-sql-driver/mysql"
+	boilerplate "github.com/estuary/connectors/materialize-boilerplate"
+	sql "github.com/estuary/connectors/materialize-sql"
+	pf "github.com/estuary/flow/go/protocols/flow"
+	pm "github.com/estuary/flow/go/protocols/materialize"
+	log "github.com/sirupsen/logrus"
+	"go.gazette.dev/core/consumer/protocol"
+)
+
+const (
+	// These are coarse limits on the amount of memory that will be used to buffer insert/update
+	// batches for load and store operations. In the future it may be interesting to investigate
+	// using the Postgres copy protocol in the future for bulk loading data tables instead of with
+	// DML statements.
+	storeBatchSizeLimit = 4096                    // Assuming store records average ~2kB then 4k * 2kB = 8MB
+	loadBatchSizeLimit  = storeBatchSizeLimit * 5 // Load records are keys-only so allow for more in a batch
+)
+
+type sshForwarding struct {
+	SshEndpoint string `json:"sshEndpoint" jsonschema:"title=SSH Endpoint,description=Endpoint of the remote SSH server that supports tunneling (in the form of ssh://user@hostname[:port])" jsonschema_extras:"pattern=^ssh://.+@.+$"`
+	PrivateKey  string `json:"privateKey" jsonschema:"title=SSH Private Key,description=Private key to connect to the remote SSH server." jsonschema_extras:"secret=true,multiline=true"`
+}
+
+type tunnelConfig struct {
+	SshForwarding *sshForwarding `json:"sshForwarding,omitempty" jsonschema:"title=SSH Forwarding"`
+}
+
+// config represents the endpoint configuration for mysql.
+type config struct {
+	Address  string         `json:"address" jsonschema:"title=Address,description=Host and port of the database (in the form of host[:port]). Port 3306 is used as the default if no specific port is provided." jsonschema_extras:"order=0"`
+	User     string         `json:"user" jsonschema:"title=User,description=Database user to connect as." jsonschema_extras:"order=1"`
+	Password string         `json:"password" jsonschema:"title=Password,description=Password for the specified database user." jsonschema_extras:"secret=true,order=2"`
+	Database string         `json:"database,omitempty" jsonschema:"title=Database,description=Name of the logical database to materialize to." jsonschema_extras:"order=3"`
+	Advanced advancedConfig `json:"advanced,omitempty" jsonschema:"title=Advanced Options,description=Options for advanced users. You should not typically need to modify these." jsonschema_extras:"advanced=true"`
+
+	NetworkTunnel *tunnelConfig `json:"networkTunnel,omitempty" jsonschema:"title=Network Tunnel,description=Connect to your system through an SSH server that acts as a bastion host for your network."`
+}
+
+type advancedConfig struct {
+	SSLMode string `json:"sslmode,omitempty" jsonschema:"title=SSL Mode,description=Overrides SSL connection behavior by setting the 'sslmode' parameter.,enum=disable,enum=allow,enum=prefer,enum=require,enum=verify-ca,enum=verify-full"`
+}
+
+// Validate the configuration.
+func (c *config) Validate() error {
+	var requiredProperties = [][]string{
+		{"address", c.Address},
+		{"user", c.User},
+		{"password", c.Password},
+	}
+	for _, req := range requiredProperties {
+		if req[1] == "" {
+			return fmt.Errorf("missing '%s'", req[0])
+		}
+	}
+
+	if c.Advanced.SSLMode != "" {
+		if !slices.Contains([]string{"disable", "allow", "prefer", "require", "verify-ca", "verify-full"}, c.Advanced.SSLMode) {
+			return fmt.Errorf("invalid 'sslmode' configuration: unknown setting %q", c.Advanced.SSLMode)
+		}
+	}
+
+	return nil
+}
+
+// ToURI converts the Config to a DSN string.
+func (c *config) ToURI() string {
+	var address = c.Address
+	// If SSH Tunnel is configured, we are going to create a tunnel from localhost:3306
+	// to address through the bastion server, so we use the tunnel's address
+	if c.NetworkTunnel != nil && c.NetworkTunnel.SshForwarding != nil && c.NetworkTunnel.SshForwarding.SshEndpoint != "" {
+		address = "localhost:3306"
+	}
+
+	// If the user did not specify a port (or no network tunnel is being used), default to port
+	// 3306. mysql ends up doing this anyway, but we do it here to make it more explicit and stable in
+	// case that underlying behavior changes in the future.
+	if !strings.Contains(address, ":") {
+		address = address + ":3306"
+	}
+
+	var uri = url.URL{
+		Scheme: "mysql",
+		Host:   fmt.Sprintf("tcp(%s)", address),
+		User:   url.UserPassword(c.User, c.Password),
+	}
+	if c.Database != "" {
+		uri.Path = "/" + c.Database
+	}
+	var params = make(url.Values)
+	if c.Advanced.SSLMode != "" {
+		params.Set("sslmode", c.Advanced.SSLMode)
+	}
+	params.Set("time_zone", "'+00:00'")
+	if len(params) > 0 {
+		uri.RawQuery = params.Encode()
+	}
+
+	// MySQL driver expects a uri without the scheme
+	return strings.TrimPrefix(uri.String(), "mysql://")
+}
+
+type tableConfig struct {
+	Table         string `json:"table" jsonschema:"title=Table,description=Name of the database table" jsonschema_extras:"x-collection-name=true"`
+	AdditionalSql string `json:"additional_table_create_sql,omitempty" jsonschema:"title=Additional Table Create SQL,description=Additional SQL statement(s) to be run in the same transaction that creates the table." jsonschema_extras:"multiline=true"`
+	Delta         bool   `json:"delta_updates,omitempty" jsonschema:"default=false,title=Delta Update,description=Should updates to this table be done via delta updates. Default is false."`
+}
+
+func newTableConfig(ep *sql.Endpoint) sql.Resource {
+	return &tableConfig{}
+}
+
+// Validate the resource configuration.
+func (r tableConfig) Validate() error {
+	if r.Table == "" {
+		return fmt.Errorf("missing table")
+	}
+	return nil
+}
+
+func (c tableConfig) Path() sql.TablePath {
+	return []string{c.Table}
+}
+
+func (c tableConfig) GetAdditionalSql() string {
+	return c.AdditionalSql
+}
+
+func (c tableConfig) DeltaUpdates() bool {
+	return c.Delta
+}
+
+func newPostgresDriver() *sql.Driver {
+	return &sql.Driver{
+		DocumentationURL: "https://go.estuary.dev/materialize-mysql",
+		EndpointSpecType: new(config),
+		ResourceSpecType: new(tableConfig),
+		NewEndpoint: func(ctx context.Context, raw json.RawMessage, tenant string) (*sql.Endpoint, error) {
+			var cfg = new(config)
+			if err := pf.UnmarshalStrict(raw, cfg); err != nil {
+				return nil, fmt.Errorf("parsing endpoint configuration: %w", err)
+			}
+
+			log.WithFields(log.Fields{
+				"database": cfg.Database,
+				"address":  cfg.Address,
+				"user":     cfg.User,
+			}).Info("opening database")
+
+			var metaBase sql.TablePath
+			var metaSpecs, metaCheckpoints = sql.MetaTables(metaBase)
+
+			// If SSH Endpoint is configured, then try to start a tunnel before establishing connections
+			if cfg.NetworkTunnel != nil && cfg.NetworkTunnel.SshForwarding != nil && cfg.NetworkTunnel.SshForwarding.SshEndpoint != "" {
+				host, port, err := net.SplitHostPort(cfg.Address)
+				if err != nil {
+					return nil, fmt.Errorf("splitting address to host and port: %w", err)
+				}
+
+				var sshConfig = &networkTunnel.SshConfig{
+					SshEndpoint: cfg.NetworkTunnel.SshForwarding.SshEndpoint,
+					PrivateKey:  []byte(cfg.NetworkTunnel.SshForwarding.PrivateKey),
+					ForwardHost: host,
+					ForwardPort: port,
+					LocalPort:   "3306",
+				}
+				var tunnel = sshConfig.CreateTunnel()
+
+				// FIXME/question: do we need to shut down the tunnel manually if it is a child process?
+				// at the moment tunnel.Stop is not being called anywhere, but if the connector shuts down, the child process also shuts down.
+				if err := tunnel.Start(); err != nil {
+					return nil, fmt.Errorf("error starting network tunnel: %w", err)
+				}
+			}
+
+			return &sql.Endpoint{
+				Config:              cfg,
+				Dialect:             mysqlDialect,
+				MetaSpecs:           &metaSpecs,
+				MetaCheckpoints:     &metaCheckpoints,
+				Client:              client{uri: cfg.ToURI()},
+				CreateTableTemplate: tplCreateTargetTable,
+				NewResource:         newTableConfig,
+				NewTransactor:       newTransactor,
+				CheckPrerequisites:  prereqs,
+				Tenant:              tenant,
+			}, nil
+		},
+	}
+}
+
+func prereqs(ctx context.Context, ep *sql.Endpoint) *sql.PrereqErr {
+	cfg := ep.Config.(*config)
+	errs := &sql.PrereqErr{}
+
+	// Use a reasonable timeout for this connection test. It is not uncommon for a misconfigured
+	// connection (wrong host, wrong port, etc.) to hang for several minutes on Ping and we want to
+	// bail out well before then.
+	ctx, cancel := context.WithTimeout(ctx, 20*time.Second)
+	defer cancel()
+
+	if db, err := stdsql.Open("mysql", cfg.ToURI()); err != nil {
+		errs.Err(err)
+	} else if err := db.PingContext(ctx); err != nil {
+		// Provide a more user-friendly representation of some common error causes.
+		var mysqlErr *mysql.MySQLError
+		var netConnErr *net.DNSError
+		var netOpErr *net.OpError
+
+		if errors.As(err, &mysqlErr) {
+			// See MySQL error reference: https://dev.mysql.com/doc/mysql-errors/5.7/en/error-reference-introduction.html
+			switch mysqlErr.Number {
+			case 1045:
+				err = fmt.Errorf("incorrect username or password (%d): %s", mysqlErr.Number, mysqlErr.Message)
+      case 1049:
+				err = fmt.Errorf("database %q cannot be accessed, it might not exist or you do not have permission to access it (%d): %s", cfg.Database, mysqlErr.Number, mysqlErr.Message)
+			case 1044:
+				err = fmt.Errorf("database %q cannot be accessed, it might not exist or you do not have permission to access it (%d): %s", cfg.Database, mysqlErr.Number, mysqlErr.Message)
+			}
+		} else if errors.As(err, &netConnErr) {
+			if netConnErr.IsNotFound {
+				err = fmt.Errorf("host at address %q cannot be found", cfg.Address)
+			}
+		} else if errors.As(err, &netOpErr) {
+			if netOpErr.Timeout() {
+				err = fmt.Errorf("connection to host at address %q timed out (incorrect host or port?)", cfg.Address)
+			}
+		}
+
+		errs.Err(err)
+	} else {
+		db.Close()
+	}
+
+	return errs
+}
+
+// client implements the sql.Client interface.
+type client struct {
+	uri string
+}
+
+func (c client) AddColumnToTable(ctx context.Context, dryRun bool, tableIdentifier string, columnIdentifier string, columnDDL string) (string, error) {
+	query := fmt.Sprintf(
+		"ALTER TABLE %s ADD COLUMN IF NOT EXISTS %s %s;",
+		tableIdentifier,
+		columnIdentifier,
+		columnDDL,
+	)
+
+	if !dryRun {
+		if err := c.withDB(func(db *stdsql.DB) error { return sql.StdSQLExecStatements(ctx, db, []string{query}) }); err != nil {
+			return "", err
+		}
+	}
+
+	return query, nil
+}
+
+func (c client) DropNotNullForColumn(ctx context.Context, dryRun bool, tableIdentifier string, columnIdentifier string) (string, error) {
+	query := fmt.Sprintf(
+		"ALTER TABLE %s ALTER COLUMN %s DROP NOT NULL;",
+		tableIdentifier,
+		columnIdentifier,
+	)
+
+	if !dryRun {
+		if err := c.withDB(func(db *stdsql.DB) error { return sql.StdSQLExecStatements(ctx, db, []string{query}) }); err != nil {
+			return "", err
+		}
+	}
+
+	return query, nil
+}
+
+func (c client) FetchSpecAndVersion(ctx context.Context, specs sql.Table, materialization pf.Materialization) (specB64, version string, err error) {
+	err = c.withDB(func(db *stdsql.DB) error {
+		specB64, version, err = sql.StdFetchSpecAndVersion(ctx, db, specs, materialization)
+		return err
+	})
+	return
+}
+
+// ExecStatements is used for the DDL statements of ApplyUpsert and ApplyDelete. Postgres supports
+// transactional DDL statements, so the statements are wrapped in a transaction.
+func (c client) ExecStatements(ctx context.Context, statements []string) error {
+	statements = append(append([]string{"begin;"}, statements...), "commit;")
+	return c.withDB(func(db *stdsql.DB) error { return sql.StdSQLExecStatements(ctx, db, statements) })
+}
+
+func (c client) InstallFence(ctx context.Context, checkpoints sql.Table, fence sql.Fence) (sql.Fence, error) {
+	var err = c.withDB(func(db *stdsql.DB) error {
+		var err error
+		fence, err = sql.StdInstallFence(ctx, db, checkpoints, fence, base64.StdEncoding.DecodeString)
+		return err
+	})
+	return fence, err
+}
+
+func (c client) withDB(fn func(*stdsql.DB) error) error {
+	var db, err = stdsql.Open("mysql", c.uri)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	return fn(db)
+}
+
+type transactor struct {
+	// Variables exclusively used by Load.
+	load struct {
+		conn     *stdsql.Conn
+		unionSQL string
+	}
+	// Variables exclusively used by Store.
+	store struct {
+		conn  *stdsql.Conn
+		fence sql.Fence
+	}
+	bindings []*binding
+}
+
+func newTransactor(
+	ctx context.Context,
+	ep *sql.Endpoint,
+	fence sql.Fence,
+	bindings []sql.Table,
+) (_ pm.Transactor, err error) {
+	var d = &transactor{}
+	d.store.fence = fence
+
+	var cfg = ep.Config.(*config)
+	// Establish connections.
+	if db, err := stdsql.Open("mysql", cfg.ToURI()); err != nil {
+		return nil, fmt.Errorf("load sql.Open: %w", err)
+	} else if d.load.conn, err = db.Conn(ctx); err != nil {
+		return nil, fmt.Errorf("load db.Conn: %w", err)
+	}
+	if db, err := stdsql.Open("mysql", cfg.ToURI()); err != nil {
+		return nil, fmt.Errorf("store sql.Open: %w", err)
+	} else if d.store.conn, err = db.Conn(ctx); err != nil {
+		return nil, fmt.Errorf("store db.Conn: %w", err)
+	}
+
+	for _, binding := range bindings {
+		if err = d.addBinding(ctx, binding); err != nil {
+			return nil, fmt.Errorf("addBinding of %s: %w", binding.Path, err)
+		}
+	}
+
+	// Build a query which unions the results of each load subquery.
+	var subqueries []string
+	for _, b := range d.bindings {
+		subqueries = append(subqueries, b.loadQuerySQL)
+	}
+	d.load.unionSQL = strings.Join(subqueries, "\nUNION ALL\n") + ";"
+
+	return d, nil
+}
+
+type binding struct {
+	target             sql.Table
+	createLoadTableSQL string
+	loadInsertSQL      string
+	storeUpdateSQL     string
+	storeInsertSQL     string
+	loadQuerySQL       string
+}
+
+func (t *transactor) addBinding(ctx context.Context, target sql.Table) error {
+	var b = &binding{target: target}
+
+	for _, m := range []struct {
+		sql *string
+		tpl *template.Template
+	}{
+		{&b.createLoadTableSQL, tplCreateLoadTable},
+		{&b.loadInsertSQL, tplLoadInsert},
+		{&b.storeInsertSQL, tplStoreInsert},
+		{&b.storeUpdateSQL, tplStoreUpdate},
+		{&b.loadQuerySQL, tplLoadQuery},
+	} {
+		var err error
+		if *m.sql, err = sql.RenderTableTemplate(target, m.tpl); err != nil {
+			return err
+		}
+	}
+
+	t.bindings = append(t.bindings, b)
+
+	// Create a binding-scoped temporary table for staged keys to load.
+	if _, err := t.load.conn.ExecContext(ctx, b.createLoadTableSQL); err != nil {
+		return fmt.Errorf("Exec(%s): %w", b.createLoadTableSQL, err)
+	}
+
+	return nil
+}
+
+func (d *transactor) Load(it *pm.LoadIterator, loaded func(int, json.RawMessage) error) error {
+	var ctx = it.Context()
+
+	// Use a read-only "load" transaction, which will automatically
+	// truncate the temporary key staging tables on commit.
+  var txn, err = d.load.conn.BeginTx(ctx, &stdsql.TxOptions{ReadOnly: true})
+	if err != nil {
+		return fmt.Errorf("DB.BeginTx: %w", err)
+	}
+	defer txn.Rollback()
+
+	for it.Next() {
+		var b = d.bindings[it.Binding]
+
+		if converted, err := b.target.ConvertKey(it.Key); err != nil {
+			return fmt.Errorf("converting Load key: %w", err)
+		} else {
+			txn.ExecContext(ctx, b.loadInsertSQL, converted...)
+		}
+	}
+
+	if it.Err() != nil {
+		return it.Err()
+	}
+
+	// Issue a union join of the target tables and their (now staged) load keys,
+	// and send results to the |loaded| callback.
+	rows, err := txn.QueryContext(ctx, d.load.unionSQL)
+	if err != nil {
+		return fmt.Errorf("querying Load documents: %w", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var binding int
+		var document json.RawMessage
+
+		if err = rows.Scan(&binding, &document); err != nil {
+			return fmt.Errorf("scanning Load document: %w", err)
+		} else if err = loaded(binding, json.RawMessage(document)); err != nil {
+			return err
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return fmt.Errorf("querying Loads: %w", err)
+	} else if err = txn.Commit(); err != nil {
+		return fmt.Errorf("commiting Load transaction: %w", err)
+	}
+
+	return nil
+}
+
+func (d *transactor) Store(it *pm.StoreIterator) (_ pm.StartCommitFunc, err error) {
+	ctx := it.Context()
+	txn, err := d.store.conn.BeginTx(ctx, &stdsql.TxOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("DB.BeginTx: %w", err)
+	}
+	defer func() {
+		if err != nil {
+			txn.Rollback()
+		}
+	}()
+
+	for it.Next() {
+		var b = d.bindings[it.Binding]
+
+		if converted, err := b.target.ConvertAll(it.Key, it.Values, it.RawJSON); err != nil {
+			return nil, fmt.Errorf("converting store parameters: %w", err)
+		} else if it.Exists {
+      // MySQL does not support indexed placeholders, and in case of the update
+      // query, the WHERE statement which includes the key comes last in the
+      // query, and as such we need to put the key as the last argument to the
+      // query
+      converted = append(converted[1:], converted[0])
+      if _, err := txn.ExecContext(ctx, b.storeUpdateSQL, converted...); err != nil {
+        return nil, fmt.Errorf("store update: %w", err)
+      }
+		} else {
+      if _, err := txn.ExecContext(ctx, b.storeInsertSQL, converted...); err != nil {
+        return nil, fmt.Errorf("store insert: %w", err)
+      }
+		}
+	}
+
+	return func(ctx context.Context, runtimeCheckpoint *protocol.Checkpoint, runtimeAckCh <-chan struct{}) (*pf.ConnectorState, pf.OpFuture) {
+		return nil, pf.RunAsyncOperation(func() error {
+			defer txn.Rollback()
+
+			var err error
+			if d.store.fence.Checkpoint, err = runtimeCheckpoint.Marshal(); err != nil {
+				return fmt.Errorf("marshalling checkpoint: %w", err)
+			}
+
+			var fenceUpdate strings.Builder
+			if err := tplUpdateFence.Execute(&fenceUpdate, d.store.fence); err != nil {
+				return fmt.Errorf("evaluating fence template: %w", err)
+			}
+
+			if results, err := txn.ExecContext(ctx, fenceUpdate.String()); err != nil {
+				return fmt.Errorf("updating flow checkpoint: %w", err)
+			} else if rowsAffected, err := results.RowsAffected(); err != nil {
+				return fmt.Errorf("updating flow checkpoint (rows affected): %w", err)
+			} else if rowsAffected < 1 {
+				return fmt.Errorf("This instance was fenced off by another")
+			}
+
+			if err := txn.Commit(); err != nil {
+				return fmt.Errorf("committing Store transaction: %w", err)
+			}
+
+			return nil
+		})
+	}, nil
+}
+
+func (d *transactor) Destroy() {
+	d.load.conn.Close()
+	d.store.conn.Close()
+}
+
+func main() {
+	boilerplate.RunMain(newPostgresDriver())
+}
---
+++ materialize-mysql/driver_test.go
@@ -0,0 +1,112 @@
+//go:build !nodb
+
+package main
+
+import (
+	"context"
+	stdsql "database/sql"
+	"fmt"
+	"strings"
+	"testing"
+
+	sql "github.com/estuary/connectors/materialize-sql"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFencingCases(t *testing.T) {
+	var ctx = context.Background()
+	var client = client{uri: "flow:flow@tcp(localhost:3306)/flow"}
+	sql.RunFenceTestCases(t,
+		sql.FenceSnapshotPath,
+		client,
+		[]string{"temp_test_fencing_checkpoints"},
+		mysqlDialect,
+		tplCreateTargetTable,
+		func(table sql.Table, fence sql.Fence) error {
+			var err = client.withDB(func(db *stdsql.DB) error {
+				// Option 1: Update using template.
+				var fenceUpdate strings.Builder
+				if err := tplUpdateFence.Execute(&fenceUpdate, fence); err != nil {
+					return fmt.Errorf("evaluating fence template: %w", err)
+				}
+				var _, err = db.Exec(fenceUpdate.String())
+
+				return err
+			})
+			return err
+		},
+		func(table sql.Table) (out string, err error) {
+			err = client.withDB(func(db *stdsql.DB) error {
+				out, err = sql.StdDumpTable(ctx, db, table)
+				return err
+			})
+			return
+		},
+	)
+}
+
+func TestPrereqs(t *testing.T) {
+	cfg := config{
+		Address:  "localhost:3306",
+		User:     "flow",
+		Password: "flow",
+		Database: "flow",
+	}
+
+	tests := []struct {
+		name string
+		cfg  func(config) *config
+		want []string
+	}{
+		{
+			name: "valid",
+			cfg:  func(cfg config) *config { return &cfg },
+			want: nil,
+		},
+		{
+			name: "wrong username",
+			cfg: func(cfg config) *config {
+				cfg.User = "wrong" + cfg.User
+				return &cfg
+			},
+			want: []string{fmt.Sprintf("incorrect username or password (1045): Access denied for user 'wrongflow'")},
+		},
+		{
+			name: "wrong password",
+			cfg: func(cfg config) *config {
+				cfg.Password = "wrong" + cfg.Password
+				return &cfg
+			},
+			want: []string{fmt.Sprintf("incorrect username or password (1045): Access denied for user 'flow'")},
+		},
+		{
+			name: "wrong database",
+			cfg: func(cfg config) *config {
+				cfg.Database = "wrong" + cfg.Database
+				return &cfg
+			},
+			want: []string{fmt.Sprintf("database \"wrongflow\" cannot be accessed, it might not exist or you do not have permission to access it (")},
+		},
+		{
+			name: "wrong address",
+			cfg: func(cfg config) *config {
+				cfg.Address = "wrong." + cfg.Address
+				return &cfg
+			},
+			want: []string{fmt.Sprintf("host at address %q cannot be found", "wrong."+cfg.Address)},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+      var actual = prereqs(context.Background(), &sql.Endpoint{
+				Config: tt.cfg(cfg),
+				Tenant: "tenant",
+			}).Unwrap()
+
+      for i := 0; i < len(tt.want); i++ {
+        require.ErrorContains(t, actual[i], tt.want[i])
+      }
+		})
+	}
+}
