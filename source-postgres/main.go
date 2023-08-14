ea2770ff5d9a431a3ddee06793e9a89be342d186
Will Donnelly <will@estuary.dev>
2023-08-14 13:44:03 -0500

    source-{mysql,postgres,sqlserver}: Backfill chunk size 50,000
    Subsequent to https://github.com/estuary/connectors/pull/898
    which removed the large in-memory buffer of backfill results
    (and thus made connector memory usage independent of backfill
    chunk size) and some Slack discussion about why we still don't
    want to set this to 1M or something absurd, we decided to bump
    this up to "something greater than 32,768 but less than 65,536"
    
    Despite the fact that we've been using powers of two for this in
    the past there's no actual technical requirement or performance
    related reason that it be a power of two, and a sum of powers such
    as `48*1024 = 49,152` isn't as instantly recognizable anyway, so
    I figure we might as well make it look nice to less technical
    users and have instead increased the default to 50,000 here.

--- source-postgres/main.go
+++ source-postgres/main.go
@@ -102,7 +102,7 @@ type advancedConfig struct {
 	SlotName          string `json:"slotName,omitempty" jsonschema:"default=flow_slot,description=The name of the PostgreSQL replication slot to replicate from."`
 	WatermarksTable   string `json:"watermarksTable,omitempty" jsonschema:"default=public.flow_watermarks,description=The name of the table used for watermark writes during backfills. Must be fully-qualified in '<schema>.<table>' form."`
 	SkipBackfills     string `json:"skip_backfills,omitempty" jsonschema:"title=Skip Backfills,description=A comma-separated list of fully-qualified table names which should not be backfilled."`
-	BackfillChunkSize int    `json:"backfill_chunk_size,omitempty" jsonschema:"title=Backfill Chunk Size,default=32768,description=The number of rows which should be fetched from the database in a single backfill query."`
+	BackfillChunkSize int    `json:"backfill_chunk_size,omitempty" jsonschema:"title=Backfill Chunk Size,default=50000,description=The number of rows which should be fetched from the database in a single backfill query."`
 	SSLMode           string `json:"sslmode,omitempty" jsonschema:"title=SSL Mode,description=Overrides SSL connection behavior by setting the 'sslmode' parameter.,enum=disable,enum=allow,enum=prefer,enum=require,enum=verify-ca,enum=verify-full"`
 }
 
@@ -152,7 +152,7 @@ func (c *Config) SetDefaults() {
 		c.Advanced.WatermarksTable = "public.flow_watermarks"
 	}
 	if c.Advanced.BackfillChunkSize <= 0 {
-		c.Advanced.BackfillChunkSize = 32768
+		c.Advanced.BackfillChunkSize = 50000
 	}
 
 	// The address config property should accept a host or host:port
