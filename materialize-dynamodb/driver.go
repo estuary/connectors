2381dc80288e3c38553829e99bd1769f6791d39a
Will Baker <wbaker@gmail.com>
2023-08-28 09:50:57 -0400

    materialize-dynamodb: new connector
    New connector for the DynamoDB endpoint.

---
+++ materialize-dynamodb/driver.go
@@ -0,0 +1,441 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	schemagen "github.com/estuary/connectors/go/schema-gen"
+	boilerplate "github.com/estuary/connectors/materialize-boilerplate"
+	"github.com/estuary/connectors/materialize-boilerplate/validate"
+	pf "github.com/estuary/flow/go/protocols/flow"
+	pm "github.com/estuary/flow/go/protocols/materialize"
+	log "github.com/sirupsen/logrus"
+)
+
+type config struct {
+	AWSAccessKeyID     string `json:"awsAccessKeyId" jsonschema:"title=Access Key ID,description=AWS Access Key ID for materializing to DynamoDB." jsonschema_extras:"order=1"`
+	AWSSecretAccessKey string `json:"awsSecretAccessKey" jsonschema:"title=Secret Access Key,description=AWS Secret Access Key for materializing to DynamoDB." jsonschema_extras:"secret=true,order=2"`
+	Region             string `json:"region" jsonschema:"title=Region,description=Region of the materialized tables." jsonschema_extras:"order=3"`
+
+	Advanced advancedConfig `json:"advanced,omitempty" jsonschema:"title=Advanced Options,description=Options for advanced users. You should not typically need to modify these." jsonschema_extra:"advanced=true"`
+}
+
+type advancedConfig struct {
+	Endpoint string `json:"endpoint,omitempty" jsonschema:"title=AWS Endpoint,description=The AWS endpoint URI to connect to. Use if you're materializing to a compatible API that isn't provided by AWS."`
+}
+
+func (c *config) Validate() error {
+	var requiredProperties = [][]string{
+		{"awsAccessKeyId", c.AWSAccessKeyID},
+		{"awsSecretAccessKey", c.AWSSecretAccessKey},
+		{"region", c.Region},
+	}
+	for _, req := range requiredProperties {
+		if req[1] == "" {
+			return fmt.Errorf("missing '%s'", req[0])
+		}
+	}
+
+	return nil
+}
+
+type resource struct {
+	Table        string `json:"table" jsonschema:"title=Table Name,description=The name of the table to be materialized to." jsonschema_extras:"x-collection-name=true"`
+	DeltaUpdates bool   `json:"delta_updates,omitempty" jsonschema:"title=Delta updates,default=false"`
+}
+
+func (r *resource) Validate() error {
+	var requiredProperties = [][]string{
+		{"table", r.Table},
+	}
+	for _, req := range requiredProperties {
+		if req[1] == "" {
+			return fmt.Errorf("missing '%s'", req[0])
+		}
+	}
+
+	if _, err := normalizeTableName(r.Table); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var (
+	tableNameSanitizer = regexp.MustCompile(`[^\.\-_0-9a-zA-Z]`)
+	maxTableNameLength = 255
+	minTableNameLength = 3
+)
+
+// For table naming requirements, see
+// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.NamingRules
+func normalizeTableName(t string) (string, error) {
+	cleaned := tableNameSanitizer.ReplaceAllString(t, "_")
+
+	// The cleaned string is now guaranteed to be only ASCII characters.
+	if len(cleaned) < minTableNameLength {
+		return "", fmt.Errorf(
+			"table name '%s' is invalid: must contain at least %d alphanumeric, dash ('-'), dot ('.'), or underscore ('_') characters",
+			t,
+			minTableNameLength,
+		)
+	}
+
+	if len(cleaned) > maxTableNameLength {
+		cleaned = cleaned[:maxTableNameLength]
+	}
+
+	return cleaned, nil
+}
+
+func (c *config) client(ctx context.Context) (*client, error) {
+	opts := []func(*awsConfig.LoadOptions) error{
+		awsConfig.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(c.AWSAccessKeyID, c.AWSSecretAccessKey, ""),
+		),
+		awsConfig.WithRegion(c.Region),
+	}
+
+	if c.Advanced.Endpoint != "" {
+		customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+			return aws.Endpoint{URL: c.Advanced.Endpoint}, nil
+		})
+
+		opts = append(opts, awsConfig.WithEndpointResolverWithOptions(customResolver))
+	}
+
+	awsCfg, err := awsConfig.LoadDefaultConfig(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("creating aws config: %w", err)
+	}
+
+	return &client{
+		db: dynamodb.NewFromConfig(awsCfg, func(opts *dynamodb.Options) {
+			// Bump up the number of retry maximum attempts from the default of 3. The maximum retry
+			// duration is 20 seconds, so this gives us around 5 minutes of retrying retryable
+			// errors before giving up and crashing the connector.
+			//
+			// Ref: https://aws.github.io/aws-sdk-go-v2/docs/configuring-sdk/retries-timeouts/
+			opts.RetryMaxAttempts = 15
+		}),
+	}, nil
+}
+
+type client struct {
+	db *dynamodb.Client
+}
+
+func Driver() driver {
+	return driver{}
+}
+
+type driver struct{}
+
+var _ boilerplate.Connector = &driver{}
+
+func (d driver) Spec(ctx context.Context, req *pm.Request_Spec) (*pm.Response_Spec, error) {
+	if err := req.Validate(); err != nil {
+		return nil, fmt.Errorf("validating request: %w", err)
+	}
+
+	es := schemagen.GenerateSchema("Materialize DynamoDB Spec", &config{})
+	endpointSchema, err := es.MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("generating endpoint schema: %w", err)
+	}
+
+	resourceSchema, err := schemagen.GenerateSchema("DynamoDB Table", &resource{}).MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("generating resource schema: %w", err)
+	}
+
+	return &pm.Response_Spec{
+		ConfigSchemaJson:         json.RawMessage(endpointSchema),
+		ResourceConfigSchemaJson: json.RawMessage(resourceSchema),
+		DocumentationUrl:         "https://go.estuary.dev/materialize-dynamodb",
+	}, nil
+}
+
+func (d driver) Validate(ctx context.Context, req *pm.Request_Validate) (*pm.Response_Validated, error) {
+	if err := req.Validate(); err != nil {
+		return nil, fmt.Errorf("validating request: %w", err)
+	}
+
+	cfg, err := resolveEndpointConfig(req.ConfigJson)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := cfg.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	storedSpec, err := getSpec(ctx, client, req.Name.String())
+	if err != nil && err != errMetaTableNotFound {
+		return nil, err
+	}
+
+	var bindings = []*pm.Response_Validated_Binding{}
+	for i, binding := range req.Bindings {
+		res, err := resolveResourceConfig(binding.ResourceConfigJson)
+		if err != nil {
+			return nil, fmt.Errorf("building resource for binding %v: %w", i, err)
+		}
+
+		// The primary key for a DynamoDB table is the partition key, and an optional sort key. For
+		// now we only support materializing collections with at most 2 collection keys, to map to
+		// this table structure.
+		if len(binding.Collection.Key) > 2 {
+			return nil, fmt.Errorf(
+				"cannot materialize collection '%s' because it has more than 2 keys (has %d keys)'",
+				binding.Collection.Name.String(),
+				len(binding.Collection.Key),
+			)
+		}
+
+		tableName, err := normalizeTableName(res.Table)
+		if err != nil {
+			return nil, err
+		}
+
+		constraints, err := ddbValidator.ValidateBinding(
+			[]string{tableName},
+			res.DeltaUpdates,
+			binding.Collection,
+			storedSpec,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		bindings = append(bindings, &pm.Response_Validated_Binding{
+			Constraints:  constraints,
+			ResourcePath: []string{tableName},
+			DeltaUpdates: res.DeltaUpdates,
+		})
+	}
+	var response = &pm.Response_Validated{Bindings: bindings}
+
+	return response, nil
+}
+
+func (d driver) Apply(ctx context.Context, req *pm.Request_Apply) (*pm.Response_Applied, error) {
+	if err := req.Validate(); err != nil {
+		return nil, fmt.Errorf("validating request: %w", err)
+	}
+
+	cfg, err := resolveEndpointConfig(req.Materialization.ConfigJson)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := cfg.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	actions := []string{}
+	doAction := func(desc string, fn func() error) error {
+		if !req.DryRun {
+			actions = append(actions, "- "+desc)
+			return fn()
+		}
+		actions = append(actions, "- "+desc+" (skipping due to dry-run)")
+		return nil
+	}
+
+	storedSpec, err := getSpec(ctx, client, req.Materialization.Name.String())
+	if err != nil {
+		if err == errMetaTableNotFound {
+			if err := doAction(fmt.Sprintf("create table '%s", metaTableName), func() error {
+				return createTable(ctx, req.DryRun, client, metaTableName, metaTableAttrs, metaTableSchema)
+			}); err != nil {
+				return nil, fmt.Errorf("creating metadata table: %w", err)
+			}
+		} else {
+			return nil, fmt.Errorf("getting stored spec: %w", err)
+		}
+	}
+
+	for _, b := range req.Materialization.Bindings {
+		if len(b.Collection.Key) > 2 {
+			return nil, fmt.Errorf(
+				"cannot materialize collection '%s' because it has more than 2 keys (has %d keys)'",
+				b.Collection.Name.String(),
+				len(b.Collection.Key),
+			)
+		}
+
+		if err := ddbValidator.ValidateSelectedFields(b, storedSpec); err != nil {
+			return nil, err
+		}
+
+		found, err := validate.FindExistingBinding(b.ResourcePath, b.Collection.Name, storedSpec)
+		if err != nil {
+			return nil, err
+		}
+
+		tableName := b.ResourcePath[0]
+		if found == nil {
+			// This table may not have been created yet, so create it now.
+			attrs, schema := tableConfigFromBinding(b.Collection.Projections)
+			if err := doAction(fmt.Sprintf("create table '%s'", tableName), func() error {
+				return createTable(ctx, req.DryRun, client, tableName, attrs, schema)
+			}); err != nil {
+				return nil, fmt.Errorf("creating table '%s': %w", tableName, err)
+			}
+		}
+	}
+
+	// Update the stored materialization spec now that all of the new tables have been created.
+	if err := doAction(fmt.Sprintf("update stored materialization spec and set version = %s", req.Version), func() error {
+		return putSpec(ctx, client, req.Materialization, req.Version)
+	}); err != nil {
+		return nil, fmt.Errorf("updating stored materialization spec: %w", err)
+	}
+
+	return &pm.Response_Applied{
+		ActionDescription: strings.Join(actions, "\n"),
+	}, nil
+}
+
+func (d driver) NewTransactor(ctx context.Context, open pm.Request_Open) (pm.Transactor, *pm.Response_Opened, error) {
+	var cfg, err = resolveEndpointConfig(open.Materialization.ConfigJson)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client, err := cfg.client(ctx)
+	if err != nil {
+		return nil, nil, fmt.Errorf("creating client: %w", err)
+	}
+
+	var bindings []binding
+	tablesToBindings := make(map[string]int)
+	for idx, b := range open.Materialization.Bindings {
+		tablesToBindings[b.ResourcePath[0]] = idx
+		bindings = append(bindings, binding{
+			tableName: b.ResourcePath[0],
+			fields:    mapFields(b),
+			docField:  b.FieldSelection.Document,
+		})
+	}
+
+	return &transactor{
+		client:           client,
+		bindings:         bindings,
+		tablesToBindings: tablesToBindings,
+	}, &pm.Response_Opened{}, nil
+}
+
+func resolveEndpointConfig(specJson json.RawMessage) (config, error) {
+	var cfg = config{}
+	if err := pf.UnmarshalStrict(specJson, &cfg); err != nil {
+		return cfg, fmt.Errorf("parsing endpoint config: %w", err)
+	}
+
+	return cfg, nil
+}
+
+func resolveResourceConfig(specJson json.RawMessage) (resource, error) {
+	var res = resource{}
+	if err := pf.UnmarshalStrict(specJson, &res); err != nil {
+		return res, fmt.Errorf("parsing resource config: %w", err)
+	}
+
+	return res, nil
+}
+
+func createTable(
+	ctx context.Context,
+	dryRun bool,
+	client *client,
+	name string,
+	attrs []types.AttributeDefinition,
+	keySchema []types.KeySchemaElement,
+) error {
+	if dryRun {
+		return nil
+	}
+
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: attrs,
+		KeySchema:            keySchema,
+		TableName:            aws.String(name),
+		BillingMode:          types.BillingModePayPerRequest,
+	}
+
+	_, err := client.db.CreateTable(ctx, input)
+	if err != nil {
+		var errInUse *types.ResourceInUseException
+		// Any error other than an "already exists" error is a more serious problem. Usually we
+		// should not be trying to create tables that already exist, so emit a warning log if that
+		// ever occurs.
+		if !errors.As(err, &errInUse) {
+			return fmt.Errorf("create table %s: %w", name, err)
+		}
+		log.WithField("table", name).Warn("table already exists")
+	}
+
+	// Wait for the table to be in an "active" state.
+	maxAttempts := 30
+	for attempt := 0; attempt < maxAttempts; attempt++ {
+		d, err := client.db.DescribeTable(ctx, &dynamodb.DescribeTableInput{
+			TableName: aws.String(name),
+		})
+		if err != nil {
+			return err
+		}
+
+		if d.Table.TableStatus == types.TableStatusActive {
+			return nil
+		}
+
+		log.WithFields(log.Fields{
+			"table":      name,
+			"lastStatus": d.Table.TableStatus,
+		}).Debug("waiting for table to become ready")
+		time.Sleep(1 * time.Second)
+	}
+
+	return fmt.Errorf("table %s was created but did not become ready in time", name)
+}
+
+func tableConfigFromBinding(projections []pf.Projection) ([]types.AttributeDefinition, []types.KeySchemaElement) {
+	mappedKeys := []mappedType{}
+	for _, p := range projections {
+		if p.IsPrimaryKey {
+			mappedKeys = append(mappedKeys, mapType(&p))
+		}
+	}
+
+	// The collection keys will be used as the partition key and sort key, respectively.
+	keyTypes := [2]types.KeyType{types.KeyTypeHash, types.KeyTypeRange}
+
+	attrs := []types.AttributeDefinition{}
+	schema := []types.KeySchemaElement{}
+
+	for idx, k := range mappedKeys {
+		attrs = append(attrs, types.AttributeDefinition{
+			AttributeName: aws.String(k.field),
+			AttributeType: k.ddbScalarType,
+		})
+		schema = append(schema, types.KeySchemaElement{
+			AttributeName: aws.String(k.field),
+			KeyType:       keyTypes[idx],
+		})
+	}
+
+	return attrs, schema
+}
---
+++ materialize-dynamodb/type_mapping.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"slices"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/estuary/connectors/materialize-boilerplate/validate"
+	"github.com/estuary/flow/go/protocols/fdb/tuple"
+	pf "github.com/estuary/flow/go/protocols/flow"
+	pm "github.com/estuary/flow/go/protocols/materialize"
+	log "github.com/sirupsen/logrus"
+)
+
+func mapFields(spec *pf.MaterializationSpec_Binding) []mappedType {
+	out := []mappedType{}
+
+	for _, f := range spec.FieldSelection.AllFields() {
+		p := spec.Collection.GetProjection(f)
+		out = append(out, mapType(p))
+	}
+
+	return out
+}
+
+type mappedType struct {
+	// Name of the field.
+	field string
+
+	// The equivalent DynamoDB scalar attribute type if this JSON type is able to be used as a
+	// DynamoDB partition key or sort key. Unset otherwise.
+	ddbScalarType types.ScalarAttributeType
+
+	// Converts tuple values into database-friendly values.
+	converter func(tuple.TupleElement) (any, error)
+}
+
+func mapType(p *pf.Projection) mappedType {
+	out := mappedType{
+		field: p.Field,
+	}
+
+	if _, ok := validate.AsFormattedNumeric(p); ok {
+		// A string field formatted as an integer or number, with a possible additional
+		// corresponding integer or number type.
+		out.converter = convertNumeric
+		return out
+	}
+
+	jsonTypes := slices.DeleteFunc(p.Inference.Types, func(t string) bool {
+		// DynamoDB has no requirements for nullability of non-key fields.
+		return t == pf.JsonTypeNull
+	})
+
+	if len(jsonTypes) != 1 {
+		// Multiple possible types, a single null type, or completely unconstrained types.
+		out.converter = passthrough
+		return out
+	}
+
+	// Single type. Map it to a DynamoDB scalar type if possible, and establish additional
+	// conversions for storing its data in the database.
+	switch t := jsonTypes[0]; t {
+	case pf.JsonTypeString:
+		if p.Inference.String_.ContentEncoding == "base64" {
+			out.ddbScalarType = types.ScalarAttributeTypeB
+			out.converter = convertBase64
+		} else {
+			out.ddbScalarType = types.ScalarAttributeTypeS
+			out.converter = passthrough
+		}
+	case pf.JsonTypeBoolean:
+		// For boolean key fields to be used as DynamoDB key fields, they must be converted to
+		// strings. Otherwise they can be directly as booleans.
+		if p.IsPrimaryKey {
+			out.ddbScalarType = types.ScalarAttributeTypeS
+			out.converter = stringify
+		} else {
+			out.converter = passthrough
+		}
+	case pf.JsonTypeInteger:
+		out.ddbScalarType = types.ScalarAttributeTypeN
+		out.converter = convertNumeric
+	case pf.JsonTypeNumber:
+		out.converter = convertNumeric
+	case pf.JsonTypeArray, pf.JsonTypeObject:
+		out.converter = convertObject
+	default:
+		panic(fmt.Errorf("invalid JSON type %s", t))
+	}
+
+	return out
+}
+
+func stringify(te tuple.TupleElement) (any, error) {
+	b, err := json.Marshal(te)
+	if err != nil {
+		return nil, err
+	}
+	return string(b), nil
+}
+
+func convertObject(te tuple.TupleElement) (any, error) {
+	var out any
+
+	switch b := te.(type) {
+	case []byte:
+		if err := json.Unmarshal(b, &out); err != nil {
+			return nil, err
+		}
+	case json.RawMessage:
+		if err := json.Unmarshal(b, &out); err != nil {
+			return nil, err
+		}
+	case nil:
+		return nil, nil
+	default:
+		return nil, fmt.Errorf("invalid type %T (%#v) for variant", te, te)
+	}
+
+	return out, nil
+}
+
+func passthrough(te tuple.TupleElement) (any, error) {
+	return te, nil
+}
+
+// wrappedNumeric provides handling for numeric values that can either be the actual numeric value
+// (integer or number), or a string with an applicable format.
+type wrappedNumeric struct {
+	innerNumeric string
+}
+
+var _ attributevalue.Marshaler = (*wrappedNumeric)(nil)
+
+func (w *wrappedNumeric) MarshalDynamoDBAttributeValue() (types.AttributeValue, error) {
+	return &types.AttributeValueMemberN{
+		Value: w.innerNumeric,
+	}, nil
+}
+
+func convertNumeric(te tuple.TupleElement) (any, error) {
+	out := &wrappedNumeric{}
+
+	switch tt := te.(type) {
+	case string:
+		out.innerNumeric = tt
+	case int:
+		out.innerNumeric = strconv.Itoa(tt)
+	case int64:
+		out.innerNumeric = strconv.FormatInt(tt, 10)
+	case float64:
+		out.innerNumeric = strconv.FormatFloat(tt, 'f', -1, 64)
+	case nil:
+		return nil, nil
+	default:
+		return out, fmt.Errorf("unsupported type %T (%#v)", te, te)
+	}
+
+	return out, nil
+}
+
+func convertBase64(te tuple.TupleElement) (any, error) {
+	bytes, err := base64.StdEncoding.DecodeString(te.(string))
+	if err != nil {
+		return nil, fmt.Errorf("decoding bytes from string: %w", err)
+	}
+
+	return bytes, nil
+}
+
+var ddbValidator = validate.NewValidator(constrainter{})
+
+type constrainter struct{}
+
+func (constrainter) NewConstraints(p *pf.Projection, deltaUpdates bool) *pm.Response_Validated_Constraint {
+	// By default only the collection key and root document fields are materialized, due to
+	// DyanmoDB's 400kb single item size limit. Additional fields are optional and may be selected
+	// to materialize as top-level properties with the applicable conversion applied, if desired.
+	var constraint = pm.Response_Validated_Constraint{}
+	switch {
+	case p.IsPrimaryKey:
+		constraint.Type = pm.Response_Validated_Constraint_LOCATION_REQUIRED
+		constraint.Reason = "Primary key locations are required"
+	case p.IsRootDocumentProjection() && !deltaUpdates:
+		constraint.Type = pm.Response_Validated_Constraint_LOCATION_REQUIRED
+		constraint.Reason = "The root document is required for a standard updates materialization"
+	case p.IsRootDocumentProjection():
+		constraint.Type = pm.Response_Validated_Constraint_LOCATION_RECOMMENDED
+		constraint.Reason = "The root document should usually be materialized"
+
+	default:
+		constraint.Type = pm.Response_Validated_Constraint_FIELD_OPTIONAL
+		constraint.Reason = "This field is able to be materialized"
+	}
+
+	return &constraint
+}
+
+func (constrainter) Compatible(existing *pf.Projection, proposed *pf.Projection) bool {
+	// Non-key fields have no compatibility restrictions and can be changed in any way at any time.
+	if !existing.IsPrimaryKey && !proposed.IsPrimaryKey {
+		return true
+	}
+
+	if existing.IsPrimaryKey != proposed.IsPrimaryKey {
+		// This should not be possible given our constraints.
+		log.WithFields(log.Fields{
+			"existing":             existing.Field,
+			"existingIsPrimaryKey": existing.IsPrimaryKey,
+			"proposed":             proposed.Field,
+			"proposedIsPrimaryKey": proposed.IsPrimaryKey,
+		}).Warn("constrainter asked to compare a collection key with a non-key field")
+	}
+
+	return mapType(existing).ddbScalarType == mapType(proposed).ddbScalarType
+}
+
+func (constrainter) DescriptionForType(p *pf.Projection) string {
+	out := ""
+	switch t := mapType(p).ddbScalarType; t {
+	case types.ScalarAttributeTypeS:
+		out = "string"
+	case types.ScalarAttributeTypeN:
+		out = "numeric"
+	case types.ScalarAttributeTypeB:
+		out = "binary"
+	}
+
+	return out
+}
