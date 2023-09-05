c039c7a205eae2d1cd65263100609f1be642282f
Will Baker <wbaker@gmail.com>
2023-09-05 09:15:53 -0400

    fixup

--- materialize-sql/type_mapping.go
+++ materialize-sql/type_mapping.go
@@ -254,8 +254,14 @@ func Compose(upper ElementConverter, lower ElementConverter) ElementConverter {
 func StdByteArrayToStr(te tuple.TupleElement) (interface{}, error) {
 	switch tt := te.(type) {
 	case []byte:
+		if tt == nil {
+			return nil, nil
+		}
 		return string(tt), nil
 	case json.RawMessage:
+		if tt == nil {
+			return nil, nil
+		}
 		return string(tt), nil
 	default:
 		return te, nil
