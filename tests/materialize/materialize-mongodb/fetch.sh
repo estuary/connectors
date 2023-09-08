5f56c43b23ea07f638f71f045036904aab78bf3d
Will Baker <wbaker@gmail.com>
2023-09-08 12:54:06 -0400

    materialize-mongodb: bulk load and store operations
    Use bulk operations for loads and stores, which is a significant performance improvement.
    
    Loads are done using the `find` operation with a reasonably sized list of IDs to retrieve. Stores
    are done using the `bulkWrite` operation.

--- tests/materialize/materialize-mongodb/fetch.sh
+++ tests/materialize/materialize-mongodb/fetch.sh
@@ -7,14 +7,14 @@ set -o nounset
 function exportToJsonl() {
 	projections=${2:-"{'_meta':0}"}
 	docker exec \
-    materialize-mongodb-mongo-1 mongosh test \
+		materialize-mongodb-mongo-1 mongosh test \
 		--username flow \
 		--password flow \
 		--json=canonical \
 		--quiet \
-		--eval="db.$1.find({}, $projections).toArray()"  | jq -S '{ index: "$1", rows: . }'
+		--eval="db.$1.find({}, $projections).toArray()" | jq -S "{ index: \"$1\", rows: . }"
 }
 
-exportToJsonl "Simple" 
-exportToJsonl "duplicate_keys_standard" 
+exportToJsonl "Simple"
+exportToJsonl "duplicate_keys_standard"
 exportToJsonl "duplicate_keys_delta" "{'_meta':0,'_id':0}"
