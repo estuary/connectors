4ee8e2632fbe7029afa96b3867d5389e288a6429
Will Baker <wbaker@gmail.com>
2023-09-13 10:15:46 -0400

    source-kinesis: move endpoint to advanced config & fix tests
    Moves the "endpoint" configuration to an advanced option, which is consistent to other AWS captures
    and materializations. This is not a field that a user is ever likely to use. There aren't any
    existing kinesis captures that use this field, so this is not a breaking change.
    
    Also cleans up and fixes the set of capture tests for Kinesis, which had gotten into quite a state
    of disarray.

--- source-kinesis/connection.go
+++ source-kinesis/connection.go
@@ -12,12 +12,16 @@ import (
 )
 
 // Config represents the fully merged endpoint configuration for Kinesis.
-// It matches the `KinesisConfig` struct in `crates/sources/src/specs.rs`
 type Config struct {
-	Endpoint           string `json:"endpoint,omitempty" jsonschema:"title=AWS Endpoint,description=The AWS endpoint URI to connect to (useful if you're capturing from a kinesis-compatible API that isn't provided by AWS)"`
 	Region             string `json:"region" jsonschema:"title=AWS Region,description=The name of the AWS region where the Kinesis stream is located"`
 	AWSAccessKeyID     string `json:"awsAccessKeyId" jsonschema:"title=AWS Access Key ID,description=Part of the AWS credentials that will be used to connect to Kinesis"`
 	AWSSecretAccessKey string `json:"awsSecretAccessKey" jsonschema:"title=AWS Secret Access Key,description=Part of the AWS credentials that will be used to connect to Kinesis"`
+
+	Advanced advancedConfig `json:"advanced,omitempty"`
+}
+
+type advancedConfig struct {
+	Endpoint string `json:"endpoint,omitempty" jsonschema:"title=AWS Endpoint,description=The AWS endpoint URI to connect to (useful if you're capturing from a kinesis-compatible API that isn't provided by AWS)"`
 }
 
 func (c *Config) Validate() error {
@@ -44,8 +48,8 @@ func connect(config *Config) (*kinesis.Kinesis, error) {
 	if config.Region != "" {
 		c = c.WithRegion(config.Region)
 	}
-	if config.Endpoint != "" {
-		c = c.WithEndpoint(config.Endpoint)
+	if config.Advanced.Endpoint != "" {
+		c = c.WithEndpoint(config.Advanced.Endpoint)
 	}
 
 	awsSession, err := session.NewSession(c)
