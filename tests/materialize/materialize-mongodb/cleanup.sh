3e6ba8884e24e397a9125845b3dec1ad316c2017
Will Baker <wbaker@gmail.com>
2023-09-15 09:20:39 -0400

    materialize-mongodb: simplify test setup
    Since it turns out we can't actually use MongoDB transactions, we don't need to use a MongoDB
    cluster with a replica set, and can instead use a single node for our test setup. This removes some
    flakiness from the automated tests, and should make it easier to get a local MongoDB running with
    the compose file.

--- tests/materialize/materialize-mongodb/cleanup.sh
+++ tests/materialize/materialize-mongodb/cleanup.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-docker compose --file  ./materialize-mongodb/docker-compose.yaml down || true
+docker compose --file ./materialize-mongodb/docker-compose.yaml down || true
