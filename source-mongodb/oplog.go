da84bf1f35d22228568c68476f34ef4f50a825aa
Mahdi Dibaiee <mdibaiee@pm.me>
2023-09-19 14:18:52 +0100

    source-mongodb: check backfill timestamp exists in oplog

--- source-mongodb/oplog.go
+++ source-mongodb/oplog.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -41,4 +42,27 @@ func oplogTimeDifference(ctx context.Context, client *mongo.Client) (uint32, err
 	}
 
 	return latest.Ts.T - oldest.Ts.T, nil
+}
+
+// Check whether the timestamp ts is included in the oplog. This check is
+// necessary to make sure that when we set StartAtOperationTime, that timestamp
+// is actually available in the oplog. If it is not available, the driver will
+// not error, and we will lose some events without knowing
+func oplogHasTimestamp(ctx context.Context, client *mongo.Client, ts time.Time) error {
+	var db = client.Database("local")
+	var oplog = db.Collection("oplog.rs")
+
+	// get oldest record
+	var opts = options.FindOne().SetSort(bson.D{{tsProperty, SortAscending}})
+	var oldest oplogRecord
+	if err := oplog.FindOne(ctx, bson.D{}, opts).Decode(&oldest); err != nil {
+		return fmt.Errorf("querying oldest oplog record: %w", err)
+	}
+	var oldestTs = time.Unix(int64(oldest.Ts.T), 0)
+
+	if oldestTs.After(ts) {
+		return fmt.Errorf("oplog's oldest record is for %s, but the last checkpoint requires reading changes since %s. This is usually due to a small oplog storage available. Please resize your oplog to be able to safely capture data from your database: https://go.estuary.dev/NurkrE. After resizing your uplog, you can remove the binding for this collection add it back to trigger a backfill.", oldestTs.String(), ts.String())
+	}
+
+	return nil
 }
