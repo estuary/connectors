6023ef18aa7aa32068fc1976e2e4442115220eeb
Mahdi Dibaiee <mdibaiee@pm.me>
2023-09-27 16:44:47 +0100

    source-mongodb: run a single change stream

--- source-mongodb/pull.go
+++ source-mongodb/pull.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"math"
 	"time"
 
@@ -13,7 +11,6 @@ import (
 	pc "github.com/estuary/flow/go/protocols/capture"
 	pf "github.com/estuary/flow/go/protocols/flow"
 	log "github.com/sirupsen/logrus"
-	"golang.org/x/sync/errgroup"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -80,8 +77,6 @@ func (d *driver) Pull(open *pc.Request_Open, stream *boilerplate.PullOutput) err
 		Output: stream,
 	}
 
-	eg, egCtx := errgroup.WithContext(ctx)
-
 	if err := c.Output.Ready(false); err != nil {
 		return err
 	}
@@ -124,6 +119,7 @@ func (d *driver) Pull(open *pc.Request_Open, stream *boilerplate.PullOutput) err
 		}
 	}
 
+	var resources = make([]resource, len(open.Capture.Bindings))
 	// Capture each binding.
 	for idx, binding := range open.Capture.Bindings {
 		idx := idx
@@ -132,6 +128,7 @@ func (d *driver) Pull(open *pc.Request_Open, stream *boilerplate.PullOutput) err
 		if err := pf.UnmarshalStrict(binding.ResourceConfigJson, &res); err != nil {
 			return fmt.Errorf("parsing resource config: %w", err)
 		}
+		resources[idx] = res
 		var resState, _ = prevState.Resources[resourceId(res)]
 
 		if resState.Status == StatusBackfill {
@@ -139,17 +136,9 @@ func (d *driver) Pull(open *pc.Request_Open, stream *boilerplate.PullOutput) err
 				return fmt.Errorf("backfill: %w", err)
 			}
 		}
-
-		eg.Go(func() error {
-			return c.ChangeStream(egCtx, client, idx, res, &resState)
-		})
 	}
 
-	if err := eg.Wait(); err != nil && !errors.Is(err, io.EOF) {
-		return err
-	}
-
-	return nil
+	return c.ChangeStream(ctx, client, resources, prevState)
 }
 
 type capture struct {
@@ -166,6 +155,7 @@ func (s *deprecatedCaptureState) Validate() error {
 
 type captureState struct {
 	Resources map[string]resourceState `json:"resources"`
+	StreamResumeToken bson.Raw `json:"stream_resume_token,omitempty"`
 }
 
 func (s *captureState) Validate() error {
@@ -190,46 +180,88 @@ type resourceState struct {
 type docKey struct {
 	Id interface{} `bson:"_id"`
 }
+type namespace struct {
+	Database string `bson:"db"`
+	Collection string `bson:"coll"`
+}
 type changeEvent struct {
-	DocumentKey   docKey `bson:"documentKey"`
-	OperationType string `bson:"operationType"`
-	FullDocument  bson.M `bson:"fullDocument"`
+	DocumentKey   docKey    `bson:"documentKey"`
+	OperationType string    `bson:"operationType"`
+	FullDocument  bson.M    `bson:"fullDocument"`
+	Ns            namespace `bson:"ns"`
 }
 
 const resumePointGoneErrorMessage = "the resume point may no longer be in the oplog"
 const resumeTokenNotFoundErrorMessage = "the resume token was not found"
 
-func (c *capture) ChangeStream(ctx context.Context, client *mongo.Client, binding int, res resource, state *resourceState) error {
-	var db = client.Database(res.Database)
+func (c *capture) ChangeStream(ctx context.Context, client *mongo.Client, resources []resource, state captureState) error {
+	// If we have a StreamResumeToken, we use that to resume our change stream
+	var streamResumeToken = state.StreamResumeToken
 
-	var collection = db.Collection(res.Collection)
+	// otherwise, we look at BackfillStartedAt of all resources and use the
+	// minimum value to start our change stream from
+	var streamStartAt time.Time
+	if len(streamResumeToken) < 1 {
+		for _, res := range resources {
+			var resState = state.Resources[resourceId(res)]
+
+			if !resState.BackfillStartedAt.IsZero() {
+				if streamStartAt.IsZero() {
+					streamStartAt = resState.BackfillStartedAt
+				} else if resState.BackfillStartedAt.Before(streamStartAt) {
+					streamStartAt = resState.BackfillStartedAt
+				}
+			}
+		}
+	}
+
+	var collectionFilters = make([]bson.D, len(resources))
+	var collectionBindingIndex = make(map[string]int)
+	for idx, res := range resources {
+		collectionBindingIndex[resourceId(res)] = idx
+
+		collectionFilters[idx] = bson.D{
+			{"ns.db", res.Database},
+			{"ns.coll", res.Collection},
+		}
+	}
 
 	log.WithFields(log.Fields{
-		"database":           res.Database,
-		"collection":         res.Collection,
-		"state":              state,
-	}).Info("listening on changes on collection")
-	var eventFilter = bson.D{{"$match", bson.D{{"$or",
-	bson.A{
-		bson.D{{"operationType", "delete"}},
-		bson.D{{"operationType", "insert"}},
-		bson.D{{"operationType", "update"}},
-		bson.D{{"operationType", "replace"}},
-	}}}}}
+		"state":             state,
+		"collectionFilters": collectionFilters,
+		"streamResumeToken": streamResumeToken,
+		"streamStartAt":     streamStartAt,
+	}).Info("listening on changes")
+
+	var eventFilter = bson.D{{"$match", bson.D{{
+		"$and", bson.A{
+			bson.D{{"$or",
+				bson.A{
+					bson.D{{"operationType", "delete"}},
+					bson.D{{"operationType", "insert"}},
+					bson.D{{"operationType", "update"}},
+					bson.D{{"operationType", "replace"}},
+				},
+			}},
+		  bson.D{{"$or", collectionFilters}},
+		}}}}}
 
 	var opts = options.ChangeStream().SetFullDocument(options.UpdateLookup)
-	if len(state.StreamResumeToken) > 0 {
+
+	if len(streamResumeToken) > 0 {
 		opts = opts.SetResumeAfter(state.StreamResumeToken)
-	} else if !state.BackfillStartedAt.IsZero() {
+	} else if !streamStartAt.IsZero() {
 		var oplogSafetyBuffer, _ = time.ParseDuration("-5m")
-		if err := oplogHasTimestamp(ctx, client, state.BackfillStartedAt.Add(oplogSafetyBuffer)); err != nil {
+
+		var startAtWithSafety = streamStartAt.Add(oplogSafetyBuffer)
+		if err := oplogHasTimestamp(ctx, client, startAtWithSafety); err != nil {
 			return err
 		}
-		var startAt = &primitive.Timestamp{T: uint32(state.BackfillStartedAt.Unix()) - 1, I: 0}
+		var startAt = &primitive.Timestamp{T: uint32(startAtWithSafety.Unix()) - 1, I: 0}
 		opts = opts.SetStartAtOperationTime(startAt)
 	}
 
-	cursor, err := collection.Watch(ctx, mongo.Pipeline{eventFilter}, opts)
+	cursor, err := client.Watch(ctx, mongo.Pipeline{eventFilter}, opts)
 	if err != nil {
 		// This error means events from the starting point are no longer available,
 		// which means we have hit a gap in between last run of the connector and
@@ -239,27 +271,27 @@ func (c *capture) ChangeStream(ctx context.Context, client *mongo.Client, bindin
 		// There is potential for optimisation here by only resetting the checkpoint
 		// of the collection which can't resume instead of resetting the whole
 		// connector's checkpoint and backfilling everything
+
+		// TODO: how to restart connector after oplog has been passed?
 		if e, ok := err.(mongo.ServerError); ok {
 			if e.HasErrorMessage(resumePointGoneErrorMessage) || e.HasErrorMessage(resumeTokenNotFoundErrorMessage) {
-				return fmt.Errorf("change stream on collection %s cannot resume capture, this is usually due to a small oplog storage available. Please resize your oplog to be able to safely capture data from your database: https://go.estuary.dev/NurkrE. After resizing your uplog, you can remove the binding for this collection add it back to trigger a backfill: %w", res.Collection, e)
+				return fmt.Errorf("change stream cannot resume capture, this is usually due to a small oplog storage available. Please resize your oplog to be able to safely capture data from your database: https://go.estuary.dev/NurkrE. After resizing your uplog, you can remove all bindings for this capture and add them back to trigger a backfill: %w", e)
 			}
 		}
 
-		return fmt.Errorf("change stream on collection %s: %w", res.Collection, err)
+		return fmt.Errorf("change stream: %w", err)
 	}
 
 	for cursor.Next(ctx) {
 		var ev changeEvent
 		if err = cursor.Decode(&ev); err != nil {
-			return fmt.Errorf("decoding document in collection %s: %w", res.Collection, err)
+			return fmt.Errorf("decoding document in collection %s.%s: %w", ev.Ns.Database, ev.Ns.Collection, err)
 		}
-
-		state.StreamResumeToken = cursor.ResumeToken()
+		var resId = eventResourceId(ev)
+		var binding = collectionBindingIndex[resId]
 
 		var checkpoint = captureState{
-			Resources: map[string]resourceState{
-				resourceId(res): *state,
-			},
+			StreamResumeToken: cursor.ResumeToken(),
 		}
 
 		checkpointJson, err := json.Marshal(checkpoint)
@@ -279,7 +311,7 @@ func (c *capture) ChangeStream(ctx context.Context, client *mongo.Client, bindin
 
 			js, err := json.Marshal(doc)
 			if err != nil {
-				return fmt.Errorf("encoding document %v in collection %s as json: %w", doc, res.Collection, err)
+				return fmt.Errorf("encoding document %v in collection %s.%s as json: %w", doc, ev.Ns.Database, ev.Ns.Collection, err)
 			}
 
 			if err = c.Output.DocumentsAndCheckpoint(checkpointJson, true, binding, js); err != nil {
@@ -306,7 +338,7 @@ func (c *capture) ChangeStream(ctx context.Context, client *mongo.Client, bindin
 
 			js, err := json.Marshal(doc)
 			if err != nil {
-				return fmt.Errorf("encoding document %v in collection %s as json: %w", doc, res.Collection, err)
+				return fmt.Errorf("encoding document %v in collection %s.%s as json: %w", doc, ev.Ns.Database, ev.Ns.Collection, err)
 			}
 
 			if err = c.Output.DocumentsAndCheckpoint(checkpointJson, true, binding, js); err != nil {
@@ -316,7 +348,7 @@ func (c *capture) ChangeStream(ctx context.Context, client *mongo.Client, bindin
 	}
 
 	if err := cursor.Err(); err != nil {
-		return fmt.Errorf("cursor error for change stream %s: %w", res.Collection, err)
+		return fmt.Errorf("cursor error for change stream: %w", err)
 	}
 
 	defer cursor.Close(ctx)
@@ -450,6 +482,10 @@ func (c *capture) BackfillCollection(ctx context.Context, client *mongo.Client, 
 
 func resourceId(res resource) string {
 	return fmt.Sprintf("%s.%s", res.Database, res.Collection)
+}
+
+func eventResourceId(ev changeEvent) string {
+	return fmt.Sprintf("%s.%s", ev.Ns.Database, ev.Ns.Collection)
 }
 
 func sanitizeDocument(doc map[string]interface{}) map[string]interface{} {
