c684d5a81deaaf36c936f9dd82fb55f0eb529677
Will Baker <wbaker@gmail.com>
2023-09-29 15:00:56 -0400

    source-dynamodb: don't try running a table scan on validate
    This won't work very well if capturing from 100's of tables, so don't try to do a small scan on each
    one during Validate.

--- source-dynamodb/driver.go
+++ source-dynamodb/driver.go
@@ -5,13 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	schemagen "github.com/estuary/connectors/go/schema-gen"
 	boilerplate "github.com/estuary/connectors/source-boilerplate"
 	pc "github.com/estuary/flow/go/protocols/capture"
 	pf "github.com/estuary/flow/go/protocols/flow"
-	log "github.com/sirupsen/logrus"
 )
 
 type driver struct{}
@@ -41,26 +38,12 @@ func (driver) Validate(ctx context.Context, req *pc.Request_Validate) (*pc.Respo
 		return nil, fmt.Errorf("parsing endpoint config: %w", err)
 	}
 
-	client, err := cfg.toClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("creating client: %w", err)
-	}
-
 	var out []*pc.Response_Validated_Binding
 	for _, binding := range req.Bindings {
 		var res resource
 		if err := pf.UnmarshalStrict(binding.ResourceConfigJson, &res); err != nil {
 			return nil, fmt.Errorf("parsing resource config: %w", err)
 		}
-
-		// Sanity-check: Can we run a scan on the table?
-		if _, err := client.db.Scan(ctx, &dynamodb.ScanInput{
-			TableName: aws.String(res.Table),
-			Limit:     aws.Int32(1),
-		}); err != nil {
-			return nil, fmt.Errorf("cannot read from table %s: %w", res.Table, err)
-		}
-		log.WithField("table", res.Table).Debug("validated table scan")
 
 		out = append(out, &pc.Response_Validated_Binding{
 			ResourcePath: []string{res.Table},
