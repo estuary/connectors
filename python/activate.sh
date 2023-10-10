3d38ec00997620728e68f049f2c3b3d40a0efd5f
Michael Bianco <iloveitaly@gmail.com>
2023-10-10 07:49:07 -0600

    python: document activate, notify when sops doesn't exist, gracefully handle missing poetry files

--- python/activate.sh
+++ python/activate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Description: switch which connector's python configuration is setup for development
 
 set -o errexit
 set -o pipefail
@@ -8,11 +9,17 @@ ROOTDIR="$(git rev-parse --show-toplevel)"
 CONNECTOR=$1
 
 # Unlink previous connector.
-rm ${ROOTDIR}/pyproject.toml || true
-rm ${ROOTDIR}/poetry.lock || true
+rm ${ROOTDIR}/pyproject.toml 2> /dev/null || true
+rm ${ROOTDIR}/poetry.lock 2> /dev/null || true
 
 # Link activated connector.
 ln ${ROOTDIR}/${CONNECTOR}/pyproject.toml ${ROOTDIR}/pyproject.toml
 ln ${ROOTDIR}/${CONNECTOR}/poetry.lock ${ROOTDIR}/poetry.lock
 
 cd ${ROOTDIR} && poetry install
+
+# check if other required tools are installed
+if ! command -v sops &> /dev/null; then
+  echo -e "\nsops could not be found, install with:\n\n   brew install sops"
+  exit 1
+fi
