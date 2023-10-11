7d46582466417f77308d89aad6f120a6ab9b97c7
Mahdi Dibaiee <mdibaiee@pm.me>
2023-10-11 14:43:12 +0100

    source-mongodb: filter out system collections from discovery

--- source-mongodb/discovery_test.go
+++ source-mongodb/discovery_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"testing"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/stretchr/testify/require"
 	st "github.com/estuary/connectors/source-boilerplate/testing"
 )
 
@@ -34,6 +36,13 @@ func TestDiscover(t *testing.T) {
 
 		return float64(idx) + 0.5
 	}
+
+	// Create a view, this leads to `system.views` being created
+	// but neither the view itself, nor the `system.views` collections must not
+	// show up in the discovered results
+	var db = client.Database(database)
+	err := db.CreateView(ctx, "testView", col1, mongo.Pipeline{})
+	require.NoError(t, err)
 
 	addTestTableData(ctx, t, client, database, col1, 5, 0, stringPkVals, "onlyColumn")
 	addTestTableData(ctx, t, client, database, col2, 5, 0, numberPkVals, "firstColumn", "secondColumn")
