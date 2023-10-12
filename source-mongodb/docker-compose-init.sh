f277bed7c75ab318bcfc2424529d9388086e41ac
Mahdi Dibaiee <mdibaiee@pm.me>
2023-10-12 11:37:25 +0100

    source-mongodb: run tests in CI

--- source-mongodb/docker-compose-init.sh
+++ source-mongodb/docker-compose-init.sh
@@ -1,8 +1,7 @@
 function mongosh() {
-	echo "$@" | docker exec -i source-mongodb-mongo-1 mongosh -u flow -p flow
+	echo "$@" | docker exec -i source-mongodb-db-1 mongosh -u flow -p flow admin
 }
 
 mongosh 'rs.initiate()'
-mongosh 'db.createUser({"user": "flow", "pwd": "flow", "roles": ["readAnyDatabase", "readWrite"]})'
 sleep 3
 mongosh 'cfg = rs.conf(); cfg.members[0].host="localhost:27017"; rs.reconfig(cfg);'
