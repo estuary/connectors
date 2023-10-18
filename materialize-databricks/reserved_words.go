341ac0137b951b9a69fc5cb40a1608efe39e983a
Mahdi Dibaiee <mdibaiee@pm.me>
2023-10-18 15:45:16 +0100

    materialize-databricks: new connector

---
+++ materialize-databricks/reserved_words.go
@@ -0,0 +1,27 @@
+package main
+
+// https://docs.databricks.com/en/sql/language-manual/sql-ref-reserved-words.html
+var DATABRICKS_RESERVED_WORDS = []string{
+"anti",
+"cross",
+"except",
+"full",
+"inner",
+"intersect",
+"join",
+"lateral",
+"left",
+"minus",
+"natural",
+"on",
+"right",
+"semi",
+"union",
+"using",
+"null",
+"default",
+"true",
+"false",
+"lateral",
+}
+
