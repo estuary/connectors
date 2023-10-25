af4a0cd5bbaa25059e6bf0ffd4d28ad5f8c6bbe9
Joseph Shearer <joseph@josephshearer.net>
2023-10-25 11:37:56 -0400

    Update build process to use single `python/Dockerfile`

--- build-local.sh
+++ build-local.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 if [[ $# < 1 ]]; then
     echo "This script builds \"<connector_name>:local\" images for testing."
-    echo "Usage: build-local.sh <connector_name>"
+    echo "Usage: build-local.sh <connector_name> <Dockerfile>"
     exit 1
 fi
+
+if [[ $1 == source-* ]]; 
+then
+  CONNECTOR_TYPE=capture
+elif [[ $1 == materialize-* ]];
+then
+  CONNECTOR_TYPE=materialization
+fi
+
+if [ -z "$2" ]
+then
+  DOCKERFILE="$1"/Dockerfile
+else
+  DOCKERFILE=$2
+fi
+
 docker buildx build \
   --cache-to=type=local,dest=.docker-cache \
   --cache-from=type=local,src=.docker-cache \
   --platform linux/amd64 \
+  --build-arg CONNECTOR_NAME="$1" \
+  --build-arg CONNECTOR_TYPE="$CONNECTOR_TYPE" \
   --load \
   -t ghcr.io/estuary/"$1":local \
-  -f "$1"/Dockerfile \
+  -f "$DOCKERFILE" \
   .
