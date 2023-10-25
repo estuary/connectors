f828c0131bf9892c4715367b183ab75faa818c6c
Joseph Shearer <joseph@josephshearer.net>
2023-10-25 12:35:20 -0400

    update `activate.sh` to use `poetry -C` instead of symlinking

--- python/activate.sh
+++ python/activate.sh
@@ -3,16 +3,10 @@
 set -o errexit
 set -o pipefail
 set -o nounset
+set -x
 
 ROOTDIR="$(git rev-parse --show-toplevel)"
 CONNECTOR=$1
 
-# Unlink previous connector.
-rm ${ROOTDIR}/pyproject.toml || true
-rm ${ROOTDIR}/poetry.lock || true
-
-# Link activated connector.
-ln ${ROOTDIR}/${CONNECTOR}/pyproject.toml ${ROOTDIR}/pyproject.toml
-ln ${ROOTDIR}/${CONNECTOR}/poetry.lock ${ROOTDIR}/poetry.lock
-
-cd ${ROOTDIR} && poetry install
+poetry -C ${ROOTDIR}/${CONNECTOR} install
+poetry -C ${ROOTDIR}/${CONNECTOR} shell
