8296eb10149f50df2ad934cc48131ed24efde031
Joseph Shearer <joseph@josephshearer.net>
2023-10-25 10:49:59 -0400

    Re-arrange folder structure to make python imports happier

--- python/common/logger.py
+++ python/common/logger.py
@@ -52,7 +52,7 @@ def init_logger():
         "disable_existing_loggers": False,
         "formatters": {
             "flow": {
-                "()": "python.logger.LogFormatter",
+                "()": "common.logger.LogFormatter",
                 "format": "",
             },
         },
