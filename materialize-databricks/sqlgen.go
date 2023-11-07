b72c0095dca7adc5396742a7d09e795ed2b70401
Mahdi Dibaiee <mdibaiee@pm.me>
2023-11-07 16:11:07 +0000

    materialize-databricks: embed shard range in temp table name

--- materialize-databricks/sqlgen.go
+++ materialize-databricks/sqlgen.go
@@ -68,20 +68,21 @@ var databricksDialect = func() sql.Dialect {
 
 
 // TODO: shard key in temporary table names
+// TODO: use create table USING location instead of copying data into temporary table
 var (
 	tplAll = sql.MustParseTemplate(databricksDialect, "root", `
 {{ define "temp_name_load" -}}
-flow_temp_load_table_{{ $.Binding }}
+flow_temp_load_table_{{ $.ShardRange }}_{{ $.Table.Binding }}
 {{- end }}
 
 {{ define "temp_name_store" -}}
-flow_temp_store_table_{{ $.Binding }}
+flow_temp_store_table_{{ $.ShardRange }}_{{ $.Table.Binding }}
 {{- end }}
 
 -- Idempotent creation of the load table for staging load keys.
 {{ define "createLoadTable" }}
 CREATE TABLE IF NOT EXISTS {{ template "temp_name_load" . }} (
-{{- range $ind, $key := $.Keys }}
+{{- range $ind, $key := $.Table.Keys }}
 	{{- if $ind }},{{ end }}
 	{{ $key.Identifier }} {{ $key.DDL }}
 {{- end }}
@@ -125,13 +126,13 @@ CREATE TABLE IF NOT EXISTS {{$.Identifier}} (
 -- deliberately skips the trailing semi-colon as these queries are composed with a UNION ALL.
 
 {{ define "loadQuery" }}
-{{ if $.Document -}}
-SELECT {{ $.Binding }}, {{ $.Identifier }}.{{ $.Document.Identifier }}
-	FROM {{ $.Identifier }}
+{{ if $.Table.Document -}}
+SELECT {{ $.Table.Binding }}, {{ $.Table.Identifier }}.{{ $.Table.Document.Identifier }}
+	FROM {{ $.Table.Identifier }}
 	JOIN {{ template "temp_name_load" . }} AS r
-	ON {{ range $ind, $key := $.Keys }}
+	ON {{ range $ind, $key := $.Table.Keys }}
 	{{- if $ind }} AND {{ end -}}
-	{{ $.Identifier }}.{{ $key.Identifier }} = r.{{ $key.Identifier }}
+	{{ $.Table.Identifier }}.{{ $key.Identifier }} = r.{{ $key.Identifier }}
 	{{- end }}
 {{ else -}}
 SELECT -1, ""
@@ -172,7 +173,7 @@ SELECT -1, ""
 
 -- Copy into temporary store table
 {{ define "copyIntoStore" }}
-	COPY INTO {{ template "temp_name_store" $.Table }} FROM (
+	COPY INTO {{ template "temp_name_store" . }} FROM (
     SELECT
 		_metadata.file_name as _metadata_file_name,
 		{{ range $ind, $key := $.Table.Columns }}
@@ -190,7 +191,7 @@ SELECT -1, ""
 
 -- Copy into temporary load table
 {{ define "copyIntoLoad" }}
-	COPY INTO {{ template "temp_name_load" $.Table }} FROM (
+	COPY INTO {{ template "temp_name_load" . }} FROM (
     SELECT
       {{ range $ind, $key := $.Table.Keys }}
         {{- if $ind }}, {{ end -}}
@@ -205,47 +206,38 @@ SELECT -1, ""
 {{ end }}
 
 {{ define "mergeInto" }}
-	MERGE INTO {{ $.Identifier }} AS l
+	MERGE INTO {{ $.Table.Identifier }} AS l
 	USING {{ template "temp_name_store" . }} AS r
-	ON {{ range $ind, $key := $.Keys }}
+	ON {{ range $ind, $key := $.Table.Keys }}
 		{{- if $ind }} AND {{ end -}}
 		l.{{ $key.Identifier }} = r.{{ $key.Identifier }}{{ template "cast" $key.DDL }}
 	{{- end }}
 	AND r._metadata_file_name IN (%s)
-	{{- if $.Document }}
-	WHEN MATCHED AND r.{{ $.Document.Identifier }} <=> NULL THEN
+	{{- if $.Table.Document }}
+	WHEN MATCHED AND r.{{ $.Table.Document.Identifier }} <=> NULL THEN
 		DELETE
 	{{- end }}
 	WHEN MATCHED THEN
-		UPDATE SET {{ range $ind, $key := $.Values }}
+		UPDATE SET {{ range $ind, $key := $.Table.Values }}
 		{{- if $ind }}, {{ end -}}
 		l.{{ $key.Identifier }} = r.{{ $key.Identifier }}{{ template "cast" $key.DDL }}
 	{{- end -}}
-	{{- if $.Document -}}
-	{{ if $.Values }}, {{ end }}l.{{ $.Document.Identifier}} = r.{{ $.Document.Identifier }}
+	{{- if $.Table.Document -}}
+	{{ if $.Table.Values }}, {{ end }}l.{{ $.Table.Document.Identifier}} = r.{{ $.Table.Document.Identifier }}
 	{{- end }}
 	WHEN NOT MATCHED THEN
 		INSERT (
-		{{- range $ind, $key := $.Columns }}
+		{{- range $ind, $key := $.Table.Columns }}
 			{{- if $ind }}, {{ end -}}
 			{{$key.Identifier -}}
 		{{- end -}}
 	)
 		VALUES (
-		{{- range $ind, $key := $.Columns }}
+		{{- range $ind, $key := $.Table.Columns }}
 			{{- if $ind }}, {{ end -}}
 			r.{{ $key.Identifier }}{{ template "cast" $key.DDL }}
 		{{- end -}}
 	);
-{{ end }}
-
-{{ define "updateFence" }}
-UPDATE {{ Identifier $.TablePath }}
-	SET   checkpoint = {{ Literal (Base64Std $.Checkpoint) }}
-	WHERE materialization = {{ Literal $.Materialization.String }}
-	AND   key_begin = {{ $.KeyBegin }}
-	AND   key_end   = {{ $.KeyEnd }}
-	AND   fence     = {{ $.Fence }};
 {{ end }}
   `)
 	tplCreateTargetTable = tplAll.Lookup("createTargetTable")
@@ -260,17 +252,17 @@ UPDATE {{ Identifier $.TablePath }}
 	tplCopyIntoLoad      = tplAll.Lookup("copyIntoLoad")
 	tplCopyIntoStore     = tplAll.Lookup("copyIntoStore")
 	tplMergeInto         = tplAll.Lookup("mergeInto")
-	tplUpdateFence       = tplAll.Lookup("updateFence")
 )
 
-type CopyTemplate struct {
+type Template struct {
   StagingPath string
+  ShardRange string
   Table *sql.Table
 }
 
-func RenderTableWithStagingPath(table sql.Table, stagingPath string, tpl *template.Template) (string, error) {
+func RenderTable(table sql.Table, stagingPath string, shardRange string, tpl *template.Template) (string, error) {
 	var w strings.Builder
-  if err := tpl.Execute(&w, &CopyTemplate{Table: &table, StagingPath: stagingPath}); err != nil {
+  if err := tpl.Execute(&w, &Template{Table: &table, StagingPath: stagingPath, ShardRange: shardRange}); err != nil {
 		return "", err
 	}
 	return w.String(), nil
