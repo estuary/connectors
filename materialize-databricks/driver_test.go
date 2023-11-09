9af8899972700f16cd9be3b0fb831b09a5ccded7
Mahdi Dibaiee <mdibaiee@pm.me>
2023-11-09 15:46:35 +0000

    materialize-databricks: cleanup leftovers

--- materialize-databricks/driver_test.go
+++ materialize-databricks/driver_test.go
@@ -3,43 +3,10 @@
 package main
 
 import (
-	/*"context"
-	stdsql "database/sql"
-	"fmt"
-	"os"
-	"strings"*/
 	"testing"
 
 	sql "github.com/estuary/connectors/materialize-sql"
 )
-
-/*func mustGetCfg(t *testing.T) config {
-	if os.Getenv("TESTDB") != "yes" {
-		t.Skipf("skipping %q: ${TESTDB} != \"yes\"", t.Name())
-		return config{}
-	}
-
-	out := config{}
-	out.Credentials.AuthType = "PAT"
-
-	for _, prop := range []struct {
-		key  string
-		dest *string
-	}{
-		{"DATABRICKS_HOST", &out.Address},
-		{"DATABRICKS_HTTP_PATH", &out.HTTPPath},
-		{"DATABRICKS_CATALOG", &out.CatalogName},
-		{"DATABRICKS_PAT", &out.Credentials.PersonalAccessToken},
-	} {
-		*prop.dest = os.Getenv(prop.key)
-	}
-
-	if err := out.Validate(); err != nil {
-		t.Fatal(err)
-	}
-
-	return out
-}*/
 
 func TestValidate(t *testing.T) {
 	sql.RunValidateTestCases(t, databricksDialect)
