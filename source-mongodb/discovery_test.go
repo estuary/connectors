cb70714c4b126fc24e2b69c93f69af9a89c5335f
Mahdi Dibaiee <mdibaiee@pm.me>
2023-11-21 14:19:20 +0000

    source-mongodb: allow discovery of all databases in an instance

--- source-mongodb/discovery_test.go
+++ source-mongodb/discovery_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/estuary/flow/go/protocols/flow"
 	"github.com/stretchr/testify/require"
 	st "github.com/estuary/connectors/source-boilerplate/testing"
 )
@@ -54,6 +55,122 @@ func TestDiscover(t *testing.T) {
 		Validator:  &st.SortedCaptureValidator{},
 		Sanitizers: commonSanitizers(),
 		Bindings:   bindings(t, database, col1),
+	}
+
+	cs.VerifyDiscover(ctx, t, nil...)
+}
+
+func TestDiscoverMultipleDatabases(t *testing.T) {
+	ctx := context.Background()
+	client, cfg := testClient(t)
+
+	var databases = []string{"foo", "bar", "baz"}
+	var col1 = "collection1"
+	var col2 = "collection2"
+
+	cleanup := func() {
+		for _, d := range databases {
+			dropCollection(ctx, t, client, d, col1)
+			dropCollection(ctx, t, client, d, col2)
+		}
+	}
+	cleanup()
+	t.Cleanup(cleanup)
+
+	stringPkVals := func(idx int) any {
+		return fmt.Sprintf("pk val %d", idx)
+	}
+
+	numberPkVals := func(idx int) any {
+		if idx%2 == 0 {
+			return idx
+		}
+
+		return float64(idx) + 0.5
+	}
+
+	// Create a view, this leads to `system.views` being created
+	// but neither the view itself, nor the `system.views` collections must not
+	// show up in the discovered results
+	var bs []*flow.CaptureSpec_Binding
+	for _, d := range databases {
+		var db = client.Database(d)
+		err := db.CreateView(ctx, "testView", col1, mongo.Pipeline{})
+		require.NoError(t, err)
+
+		addTestTableData(ctx, t, client, d, col1, 5, 0, stringPkVals, "onlyColumn")
+		addTestTableData(ctx, t, client, d, col2, 5, 0, numberPkVals, "firstColumn", "secondColumn")
+
+		bs = append(bs, bindings(t, d, col1)...)
+	}
+
+	cfg.Database = " foo, bar "
+
+	cs := &st.CaptureSpec{
+		Driver:       &driver{},
+		EndpointSpec: &cfg,
+		Checkpoint:   []byte("{}"),
+		Validator:  &st.SortedCaptureValidator{},
+		Sanitizers: commonSanitizers(),
+		Bindings:   bs,
+	}
+
+	cs.VerifyDiscover(ctx, t, nil...)
+}
+
+func TestDiscoverAllDatabases(t *testing.T) {
+	ctx := context.Background()
+	client, cfg := testClient(t)
+
+	var databases = []string{"foo", "bar", "baz"}
+	var col1 = "collection1"
+	var col2 = "collection2"
+
+	cleanup := func() {
+		for _, d := range databases {
+			dropCollection(ctx, t, client, d, col1)
+			dropCollection(ctx, t, client, d, col2)
+		}
+	}
+	cleanup()
+	t.Cleanup(cleanup)
+
+	stringPkVals := func(idx int) any {
+		return fmt.Sprintf("pk val %d", idx)
+	}
+
+	numberPkVals := func(idx int) any {
+		if idx%2 == 0 {
+			return idx
+		}
+
+		return float64(idx) + 0.5
+	}
+
+	// Create a view, this leads to `system.views` being created
+	// but neither the view itself, nor the `system.views` collections must not
+	// show up in the discovered results
+	var bs []*flow.CaptureSpec_Binding
+	for _, d := range databases {
+		var db = client.Database(d)
+		err := db.CreateView(ctx, "testView", col1, mongo.Pipeline{})
+		require.NoError(t, err)
+
+		addTestTableData(ctx, t, client, d, col1, 5, 0, stringPkVals, "onlyColumn")
+		addTestTableData(ctx, t, client, d, col2, 5, 0, numberPkVals, "firstColumn", "secondColumn")
+
+		bs = append(bs, bindings(t, d, col1)...)
+	}
+
+	cfg.Database = ""
+
+	cs := &st.CaptureSpec{
+		Driver:       &driver{},
+		EndpointSpec: &cfg,
+		Checkpoint:   []byte("{}"),
+		Validator:  &st.SortedCaptureValidator{},
+		Sanitizers: commonSanitizers(),
+		Bindings:   bs,
 	}
 
 	cs.VerifyDiscover(ctx, t, nil...)
