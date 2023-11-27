4b004a6dc935843b2ca716615dd24810acfe518a
Jon Wihl <jonwihl@jons-mbp.mynetworksettings.com>
2023-11-27 16:09:11 -0500

    Oauth2 support

--- source-asana/__main__.py
+++ source-asana/__main__.py
@@ -3,5 +3,27 @@ from source_asana import SourceAsana
 
 shim_airbyte_cdk.CaptureShim(
     delegate=SourceAsana(),
-    oauth2=None
+    oauth2={
+        "provider": "asana",
+        "authUrlTemplate": (
+            f"https://app.asana.com/-/oauth_authorize?"
+            f"client_id={client_id}&"
+            f"redirect_uri={redirect_uri}&"
+            f"response_type=code&"
+            f"state={urlencode_field('state')}&"
+            f"scope=default"
+            
+        ),
+        "accessTokenUrlTemplate": (
+            f"https://app.asana.com/-/oauth_token?"
+            f"grant_type=authorzation_code&"
+            f"client_id={client_id}&"
+            f"client_secret={client_secret}&"
+            f"redirect_uri={redirect_uri}&"
+            f"code={urlencode_field('code')}"
+        ),
+        "accessTokenReponseMap": {
+            "access_token": "/access_token"
+        }
+    }
 ).main()
