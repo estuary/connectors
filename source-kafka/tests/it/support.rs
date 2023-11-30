610ec78ea8f0c2ca027906e7bc66e26f4ad629d7
Will Baker <wbaker@gmail.com>
2023-11-30 13:05:34 -0500

    update flow version for rust connectors

--- source-kafka/tests/it/support.rs
+++ source-kafka/tests/it/support.rs
@@ -4,12 +4,6 @@ pub(crate) fn assert_valid_json(output: &[u8]) {
     serde_json::from_reader::<_, Value>(output).expect("output to be valid json");
 }
 
-/// Verifies the output was empty. If this check fails, you get useful output to
-/// help debug the error.
-pub(crate) fn assert_empty(output: &[u8]) {
-    assert_eq!("", std::str::from_utf8(output).unwrap());
-}
-
 pub(crate) fn mock_stdout() -> Vec<u8> {
     Vec::new()
 }
