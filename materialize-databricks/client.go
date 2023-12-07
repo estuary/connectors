cb97713bb8f26fc36a2472eacda55c2e7c95d397
Will Baker <wbaker@gmail.com>
2023-12-07 10:16:02 -0500

    materializations(refactor): move SQL materializations `client` into a separate file
    The materialize-sql `client` abstraction has picked up more responsibility over time, and with some
    coming changes to the `Apply` process it will start to encapsulate even more. This commit is a pure
    refactoring that moves the `client` for SQL materializations into its own separate file to make
    things a little more manageable.

---
+++ materialize-databricks/client.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"context"
+	stdsql "database/sql"
+	"errors"
+	"fmt"
+	"net"
+	"slices"
+	"strings"
+	"time"
+
+	dbsqlerr "github.com/databricks/databricks-sql-go/errors"
+	sql "github.com/estuary/connectors/materialize-sql"
+	pf "github.com/estuary/flow/go/protocols/flow"
+	pm "github.com/estuary/flow/go/protocols/materialize"
+	"golang.org/x/sync/errgroup"
+)
+
+type client struct {
+	uri string
+}
+
+func (c client) Apply(ctx context.Context, ep *sql.Endpoint, req *pm.Request_Apply, actions sql.ApplyActions, updateSpec sql.MetaSpecsUpdate) (string, error) {
+	cfg := ep.Config.(*config)
+
+	db, err := stdsql.Open("databricks", c.uri)
+	if err != nil {
+		return "", err
+	}
+	defer db.Close()
+
+	// Build the list of schemas included in this update.
+	var schemas []string
+	for _, t := range actions.CreateTables {
+		if !slices.Contains(schemas, t.InfoLocation.TableSchema) {
+			schemas = append(schemas, t.InfoLocation.TableSchema)
+		}
+	}
+	for _, t := range actions.AlterTables {
+		if !slices.Contains(schemas, t.InfoLocation.TableSchema) {
+			schemas = append(schemas, t.InfoLocation.TableSchema)
+		}
+	}
+
+	existing, err := fetchExistingColumns(ctx, db, databricksDialect, cfg.CatalogName, schemas)
+	if err != nil {
+		return "", fmt.Errorf("fetching columns: %w", err)
+	}
+
+	resolved, err := sql.FilterActions(actions, databricksDialect, existing)
+	if err != nil {
+		return "", err
+	}
+
+	// Build up the list of actions for logging. These won't be executed directly, since Databricks
+	// apparently doesn't support multi-statement queries or dropping nullability for multiple
+	// columns in a single statement, and throws errors on concurrent table updates.
+	actionList := []string{}
+	for _, tc := range resolved.CreateTables {
+		actionList = append(actionList, tc.TableCreateSql)
+	}
+	for _, ta := range resolved.AlterTables {
+		if len(ta.AddColumns) > 0 {
+			var addColumnsStmt strings.Builder
+			if err := tplAlterTableColumns.Execute(&addColumnsStmt, ta); err != nil {
+				return "", fmt.Errorf("rendering alter table columns statement: %w", err)
+			}
+			actionList = append(actionList, addColumnsStmt.String())
+		}
+		for _, dn := range ta.DropNotNulls {
+			actionList = append(actionList, fmt.Sprintf(
+				"ALTER TABLE %s ALTER COLUMN %s DROP NOT NULL;",
+				ta.Identifier,
+				dn.Identifier,
+			))
+		}
+	}
+	for _, tr := range resolved.ReplaceTables {
+		actionList = append(actionList, tr.TableReplaceSql)
+	}
+	action := strings.Join(append(actionList, updateSpec.QueryString), "\n")
+	if req.DryRun {
+		return action, nil
+	}
+
+	// Execute actions for each table involved separately. Concurrent actions can't be done on the
+	// same table without throwing errors, so each goroutine handles the actions only for that
+	// table, looping over them if needed until they are done. This is going to look pretty similar
+	// to building the actions list, except this time we are actually running the queries.
+	group, groupCtx := errgroup.WithContext(ctx)
+	group.SetLimit(5)
+
+	for _, tc := range resolved.CreateTables {
+		tc := tc
+		group.Go(func() error {
+			if _, err := db.ExecContext(groupCtx, tc.TableCreateSql); err != nil {
+				return fmt.Errorf("executing table create statement: %w", err)
+			}
+			return nil
+		})
+	}
+
+	for _, ta := range resolved.AlterTables {
+		ta := ta
+
+		group.Go(func() error {
+			if len(ta.AddColumns) > 0 {
+				var addColumnsStmt strings.Builder
+				if err := tplAlterTableColumns.Execute(&addColumnsStmt, ta); err != nil {
+					return fmt.Errorf("rendering alter table columns statement: %w", err)
+				}
+				if _, err := db.ExecContext(groupCtx, addColumnsStmt.String()); err != nil {
+					return fmt.Errorf("executing table add columns statement: %w", err)
+				}
+			}
+			for _, dn := range ta.DropNotNulls {
+				q := fmt.Sprintf(
+					"ALTER TABLE %s ALTER COLUMN %s DROP NOT NULL;",
+					ta.Identifier,
+					dn.Identifier,
+				)
+				if _, err := db.ExecContext(groupCtx, q); err != nil {
+					return fmt.Errorf("executing table drop not null statement: %w", err)
+				}
+			}
+			return nil
+		})
+	}
+
+	// TODO(whb): There's a chance that if a previous driver checkpoint was persisted before the
+	// actual data load completed, these table replacements will not be compatible with the data
+	// referenced by the persisted driver checkpoint. This is pretty unlikely to happen, and will be
+	// resolved when we incorporate the use of state keys, which incorporate the backfill counter.
+	for _, tr := range resolved.ReplaceTables {
+		tr := tr
+		group.Go(func() error {
+			if _, err := db.ExecContext(groupCtx, tr.TableReplaceSql); err != nil {
+				return fmt.Errorf("executing table create statement: %w", err)
+			}
+			return nil
+		})
+	}
+
+	if err := group.Wait(); err != nil {
+		return "", err
+	}
+
+	// Once all the table actions are done, we can update the stored spec.
+	if _, err := db.ExecContext(ctx, updateSpec.QueryString); err != nil {
+		return "", fmt.Errorf("executing spec update statement: %w", err)
+	}
+
+	return action, nil
+}
+
+func (c client) PreReqs(ctx context.Context, ep *sql.Endpoint) *sql.PrereqErr {
+	cfg := ep.Config.(*config)
+	errs := &sql.PrereqErr{}
+
+	// Use a reasonable timeout for this connection test. It is not uncommon for a misconfigured
+	// connection (wrong host, wrong port, etc.) to hang for several minutes on Ping and we want to
+	// bail out well before then. Note that this should be long enough to allow
+	// for an automatically shut down instance to be started up again
+	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+
+	if db, err := stdsql.Open("databricks", c.uri); err != nil {
+		errs.Err(err)
+	} else if err := db.PingContext(ctx); err != nil {
+		// Provide a more user-friendly representation of some common error causes.
+		var execErr dbsqlerr.DBExecutionError
+		var netConnErr *net.DNSError
+		var netOpErr *net.OpError
+
+		if errors.As(err, &execErr) {
+			// See https://pkg.go.dev/github.com/databricks/databricks-sql-go/errors#pkg-constants
+			// and https://docs.databricks.com/en/error-messages/index.html
+			switch execErr.SqlState() {
+			}
+		} else if errors.As(err, &netConnErr) {
+			if netConnErr.IsNotFound {
+				err = fmt.Errorf("host at address %q cannot be found", cfg.Address)
+			}
+		} else if errors.As(err, &netOpErr) {
+			if netOpErr.Timeout() {
+				err = fmt.Errorf("connection to host at address %q timed out (incorrect host or port?)", cfg.Address)
+			}
+		}
+
+		errs.Err(err)
+	} else {
+		db.Close()
+	}
+
+	return errs
+}
+
+func (c client) FetchSpecAndVersion(ctx context.Context, specs sql.Table, materialization pf.Materialization) (specB64, version string, err error) {
+	err = c.withDB(func(db *stdsql.DB) error {
+		// Fail-fast: surface a connection issue.
+		if err = db.PingContext(ctx); err != nil {
+			return fmt.Errorf("connecting to DB: %w", err)
+		}
+		err = db.QueryRowContext(
+			ctx,
+			fmt.Sprintf(
+				"SELECT version, spec FROM %s WHERE materialization = %s;",
+				specs.Identifier,
+				databricksDialect.Literal(materialization.String()),
+			),
+		).Scan(&version, &specB64)
+
+		return err
+	})
+	return
+}
+
+// ExecStatements is used for the DDL statements of ApplyUpsert and ApplyDelete.
+func (c client) ExecStatements(ctx context.Context, statements []string) error {
+	return c.withDB(func(db *stdsql.DB) error { return sql.StdSQLExecStatements(ctx, db, statements) })
+}
+
+func (c client) InstallFence(ctx context.Context, checkpoints sql.Table, fence sql.Fence) (sql.Fence, error) {
+	return sql.Fence{}, nil
+}
+
+func (c client) withDB(fn func(*stdsql.DB) error) error {
+	var db, err = stdsql.Open("databricks", c.uri)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	return fn(db)
+}
--- materialize-redshift/driver.go
+++ materialize-redshift/driver.go
@@ -3,35 +3,27 @@ package main
 import (
 	"context"
 	stdsql "database/sql"
-	"encoding/base64"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"net"
-	"net/http"
 	"net/url"
-	"path"
 	"slices"
 	"strings"
 	"text/template"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	awsHttp "github.com/aws/smithy-go/transport/http"
 	networkTunnel "github.com/estuary/connectors/go/network-tunnel"
 	sql "github.com/estuary/connectors/materialize-sql"
 	pf "github.com/estuary/flow/go/protocols/flow"
 	pm "github.com/estuary/flow/go/protocols/materialize"
-	"github.com/google/uuid"
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	log "github.com/sirupsen/logrus"
 	"go.gazette.dev/core/consumer/protocol"
-	"golang.org/x/sync/errgroup"
 
 	// Imported for side-effects (registers the required stdsql driver)
 	_ "github.com/jackc/pgx/v4/stdlib"
@@ -251,246 +243,6 @@ func newRedshiftDriver() *sql.Driver {
 			}, nil
 		},
 	}
-}
-
-type client struct {
-	uri string
-}
-
-func (c client) Apply(ctx context.Context, ep *sql.Endpoint, req *pm.Request_Apply, actions sql.ApplyActions, updateSpec sql.MetaSpecsUpdate) (string, error) {
-	cfg := ep.Config.(*config)
-
-	db, err := stdsql.Open("pgx", c.uri)
-	if err != nil {
-		return "", err
-	}
-	defer db.Close()
-
-	catalog := cfg.Database
-	if catalog == "" {
-		// An endpoint-level database configuration is not required, so query for the active
-		// database if that's the case.
-		if err := db.QueryRowContext(ctx, "select current_database();").Scan(&catalog); err != nil {
-			return "", fmt.Errorf("querying for connected database: %w", err)
-		}
-	}
-
-	resolved, err := sql.ResolveActions(ctx, db, actions, rsDialect, catalog)
-	if err != nil {
-		return "", fmt.Errorf("resolving apply actions: %w", err)
-	}
-
-	statements := []string{}
-	for _, tc := range resolved.CreateTables {
-		statements = append(statements, tc.TableCreateSql)
-	}
-
-	// Redshift only allows a single column to be added per ALTER TABLE statement. Also, we will
-	// never need to drop nullability constraints, since Redshift does not allow dropping
-	// nullability and we don't ever create columns as NOT NULL as a result.
-	for _, ta := range resolved.AlterTables {
-		if len(ta.DropNotNulls) != 0 { // sanity check
-			return "", fmt.Errorf("logic error: redshift cannot drop nullability constraints but got %d DropNotNulls", len(ta.DropNotNulls))
-		}
-		if len(ta.AddColumns) > 0 {
-			addColStmts := []string{}
-			for _, c := range ta.AddColumns {
-				addColStmts = append(addColStmts, fmt.Sprintf(
-					"ALTER TABLE %s ADD COLUMN %s %s;",
-					ta.Identifier,
-					c.Identifier,
-					c.NullableDDL,
-				))
-			}
-			// Each table column addition statement is a separate statement, but will be grouped
-			// together in a single multi-statement query.
-			statements = append(statements, strings.Join(addColStmts, "\n"))
-		}
-	}
-
-	for _, tr := range resolved.ReplaceTables {
-		statements = append(statements, tr.TableReplaceSql)
-	}
-
-	// The spec will get updated last, after all the other actions are complete, but include it in
-	// the description of actions.
-	action := strings.Join(append(statements, updateSpec.QueryString), "\n")
-	if req.DryRun {
-		return action, nil
-	}
-
-	if len(resolved.ReplaceTables) > 0 {
-		if err := sql.StdIncrementFence(ctx, db, ep, req.Materialization.Name.String()); err != nil {
-			return "", err
-		}
-	}
-
-	// Execute statements in parallel for efficiency. Each statement acts on a single table, and
-	// everything that needs to be done for a given table is contained in that statement.
-	group, groupCtx := errgroup.WithContext(ctx)
-	group.SetLimit(5)
-
-	for _, s := range statements {
-		s := s
-		group.Go(func() error {
-			if _, err := db.ExecContext(groupCtx, s); err != nil {
-				return fmt.Errorf("executing apply statement: %w", err)
-			}
-			return nil
-		})
-	}
-
-	if err := group.Wait(); err != nil {
-		return "", err
-	}
-
-	// Once all the table actions are done, we can update the stored spec.
-	if _, err := db.ExecContext(ctx, updateSpec.ParameterizedQuery, updateSpec.Parameters...); err != nil {
-		return "", fmt.Errorf("executing spec update statement: %w", err)
-	}
-
-	return action, nil
-}
-
-func (c client) PreReqs(ctx context.Context, ep *sql.Endpoint) *sql.PrereqErr {
-	cfg := ep.Config.(*config)
-	errs := &sql.PrereqErr{}
-
-	// Use a reasonable timeout for this connection test. It is not uncommon for a misconfigured
-	// connection (wrong host, wrong port, etc.) to hang for several minutes on Ping and we want to
-	// bail out well before then.
-	pingCtx, cancel := context.WithTimeout(ctx, 20*time.Second)
-	defer cancel()
-
-	if db, err := stdsql.Open("pgx", cfg.toURI()); err != nil {
-		errs.Err(err)
-	} else if err := db.PingContext(pingCtx); err != nil {
-		// Provide a more user-friendly representation of some common error causes.
-		var pgErr *pgconn.PgError
-		var netConnErr *net.DNSError
-		var netOpErr *net.OpError
-
-		if errors.As(err, &pgErr) {
-			switch pgErr.Code {
-			case "28000":
-				err = fmt.Errorf("incorrect username or password")
-			case "3D000":
-				err = fmt.Errorf("database %q does not exist", cfg.Database)
-			}
-		} else if errors.As(err, &netConnErr) {
-			if netConnErr.IsNotFound {
-				err = fmt.Errorf("host at address %q cannot be found", cfg.Address)
-			}
-		} else if errors.As(err, &netOpErr) {
-			if netOpErr.Timeout() {
-				errStr := `connection to host at address %q timed out, possible causes:
-	* Redshift endpoint is not set to be publicly accessible
-	* there is no inbound rule allowing Estuary's IP address to connect through the Redshift VPC security group
-	* the configured address is incorrect, possibly with an incorrect host or port
-	* if connecting through an SSH tunnel, the SSH bastion server may not be operational, or the connection details are incorrect`
-				err = fmt.Errorf(errStr, cfg.Address)
-			}
-		}
-
-		errs.Err(err)
-	} else {
-		db.Close()
-	}
-
-	s3client, err := cfg.toS3Client(ctx)
-	if err != nil {
-		// This is not caused by invalid S3 credentials, and would most likely be a logic error in
-		// the connector code.
-		errs.Err(err)
-		return errs
-	}
-
-	// Test creating, reading, and deleting an object from the configured bucket and bucket path.
-	testKey := path.Join(cfg.BucketPath, uuid.NewString())
-
-	var awsErr *awsHttp.ResponseError
-	if _, err := s3client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(cfg.Bucket),
-		Key:    aws.String(testKey),
-		Body:   strings.NewReader("testing"),
-	}); err != nil {
-		if errors.As(err, &awsErr) {
-			// Handling for the two most common cases: The bucket doesn't exist, or the bucket does
-			// exist but the configured credentials aren't authorized to write to it.
-			if awsErr.Response.Response.StatusCode == http.StatusNotFound {
-				err = fmt.Errorf("bucket %q does not exist", cfg.Bucket)
-			} else if awsErr.Response.Response.StatusCode == http.StatusForbidden {
-				err = fmt.Errorf("not authorized to write to %q", path.Join(cfg.Bucket, cfg.BucketPath))
-			}
-		}
-		errs.Err(err)
-	} else if _, err := s3client.GetObject(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(cfg.Bucket),
-		Key:    aws.String(testKey),
-	}); err != nil {
-		if errors.As(err, &awsErr) && awsErr.Response.Response.StatusCode == http.StatusForbidden {
-			err = fmt.Errorf("not authorized to read from %q", path.Join(cfg.Bucket, cfg.BucketPath))
-		}
-		errs.Err(err)
-	} else if _, err := s3client.DeleteObject(ctx, &s3.DeleteObjectInput{
-		Bucket: aws.String(cfg.Bucket),
-		Key:    aws.String(testKey),
-	}); err != nil {
-		if errors.As(err, &awsErr) && awsErr.Response.Response.StatusCode == http.StatusForbidden {
-			err = fmt.Errorf("not authorized to delete from %q", path.Join(cfg.Bucket, cfg.BucketPath))
-		}
-		errs.Err(err)
-	}
-
-	return errs
-}
-
-func (c client) FetchSpecAndVersion(ctx context.Context, specs sql.Table, materialization pf.Materialization) (specB64, version string, err error) {
-	err = c.withDB(func(db *stdsql.DB) error {
-		var specHex string
-		specHex, version, err = sql.StdFetchSpecAndVersion(ctx, db, specs, materialization)
-		if err != nil {
-			return err
-		}
-
-		specBytes, err := hex.DecodeString(specHex)
-		if err != nil {
-			return err
-		}
-
-		specB64 = string(specBytes)
-		return err
-	})
-	return
-}
-
-func (c client) ExecStatements(ctx context.Context, statements []string) error {
-	return c.withDB(func(db *stdsql.DB) error { return sql.StdSQLExecStatements(ctx, db, statements) })
-}
-
-func (c client) InstallFence(ctx context.Context, checkpoints sql.Table, fence sql.Fence) (sql.Fence, error) {
-	var err = c.withDB(func(db *stdsql.DB) error {
-		var err error
-		fence, err = sql.StdInstallFence(ctx, db, checkpoints, fence, func(fenceHex string) ([]byte, error) {
-			fenceHexBytes, err := hex.DecodeString(fenceHex)
-			if err != nil {
-				return nil, err
-			}
-
-			return base64.StdEncoding.DecodeString(string(fenceHexBytes))
-		})
-		return err
-	})
-	return fence, err
-}
-
-func (c client) withDB(fn func(*stdsql.DB) error) error {
-	var db, err = stdsql.Open("pgx", c.uri)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-	return fn(db)
 }
 
 type transactor struct {
