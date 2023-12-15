133458e7718d2e1ddcf5d5f40689e5b7b03542a3
Will Donnelly <will@estuary.dev>
2023-12-15 00:09:20 -0600

    source-snowflake: Basic datatype testing

--- source-snowflake/capture_test.go
+++ source-snowflake/capture_test.go
@@ -48,3 +48,20 @@ func TestLongTableNames(t *testing.T) {
 	tb.Insert(ctx, t, tableName, [][]interface{}{{6, "F"}, {7, "ggg"}, {8, "QRSTUVWXYZ"}, {9, "Ten"}, {10, "11"}})
 	t.Run("main", func(t *testing.T) { verifiedCapture(ctx, t, cs) })
 }
+
+func TestBasicDatatypes(t *testing.T) {
+	var ctx, tb = context.Background(), snowflakeTestBackend(t)
+	var uniqueID = "77528227"
+	var tableName = tb.CreateTable(ctx, t, uniqueID, "(id INTEGER, data TEXT, bdata VARBINARY, fdata FLOAT, bit BOOLEAN, ymd DATE, hms TIME, tstz TIMESTAMP_TZ, tsntz TIMESTAMP_NTZ)")
+	var cs = tb.CaptureSpec(ctx, t, regexp.MustCompile(uniqueID))
+
+	t.Run("discover", func(t *testing.T) { cs.VerifyDiscover(ctx, t, regexp.MustCompile(uniqueID)) })
+
+	var x = "1991-08-31T09:25:27Z"
+	tb.Insert(ctx, t, tableName, [][]interface{}{{0, "abcd", []byte{0xFF, 0xA5, 0x00, 0x5A}, 1.32, true, "1991-08-31", "09:25:27", x, x}})
+	t.Run("init", func(t *testing.T) { verifiedCapture(ctx, t, cs) })
+
+	var y = "1991-11-25T06:13:44Z"
+	tb.Insert(ctx, t, tableName, [][]interface{}{{1, "DCBA", []byte{0xEE, 0x5A, 0x11, 0xA5}, 2.64, false, "1991-11-25", "06:13:44", y, y}})
+	t.Run("main", func(t *testing.T) { verifiedCapture(ctx, t, cs) })
+}
