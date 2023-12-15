e8f1fdd1f7a813772e926c11a14b32a7d7faba3f
Joseph Shearer <joseph@josephshearer.net>
2023-12-15 18:28:13 +0100

    Add `pytest` tests for `source-airtable`

---
+++ source-airtable/tests/test_snapshots.py
@@ -0,0 +1,63 @@
+import json
+import subprocess
+
+
+def test_capture(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "preview",
+            "--source",
+            request.config.rootdir + "/source-airtable/test.flow.yaml",
+            "--sessions",
+            "1",
+            "--delay",
+            "10s"
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    for l in lines:
+        l[1]["ts"] = "redacted-timestamp"
+
+    assert snapshot("capture.stdout.json") == lines
+
+def test_discover(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "discover",
+            "--source",
+            request.config.rootdir + "/source-airtable/test.flow.yaml",
+            "-o",
+            "json",
+            "--emit-raw"
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    assert snapshot("capture.stdout.json") == lines
+
+def test_spec(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "spec",
+            "--source",
+            request.config.rootdir + "/source-airtable/test.flow.yaml"
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    assert snapshot("capture.stdout.json") == lines
