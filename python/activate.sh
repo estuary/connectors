24f2abe556cb1a662e14cf04ee2bbbbfeb0c444c
Joseph Shearer <joseph@josephshearer.net>
2023-12-18 12:39:23 +0100

    Fix weird poetry thing

--- python/activate.sh
+++ python/activate.sh
@@ -8,5 +8,5 @@ set -x
 ROOTDIR="$(git rev-parse --show-toplevel)"
 CONNECTOR=$1
 
-poetry --directory ${ROOTDIR}/${CONNECTOR} install
+poetry --directory ${ROOTDIR}/${CONNECTOR} install --no-root
 poetry --directory ${ROOTDIR}/${CONNECTOR} shell
