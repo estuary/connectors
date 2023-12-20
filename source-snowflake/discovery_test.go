644038c27b0269f933e73c4b193c0ef2c5ac05ec
Will Donnelly <will@estuary.dev>
2023-12-20 15:49:21 -0600

    source-snowflake: Fixes for PR feedback
    Most notably this commit fixes a bug which caused discovery to
    fail for tables with multiple-column primary keys in which the
    order of columns making up the key did not match their order in
    the source table.

--- source-snowflake/discovery_test.go
+++ source-snowflake/discovery_test.go
@@ -19,3 +19,10 @@ func TestDiscoveryWithoutPrimaryKey(t *testing.T) {
 	tb.CreateTable(ctx, t, uniqueID, "(a INTEGER, b TEXT, c REAL NOT NULL, d VARCHAR(255))")
 	tb.CaptureSpec(ctx, t).VerifyDiscover(ctx, t, regexp.MustCompile(uniqueID))
 }
+
+func TestDiscoveryMultiplePrimaryKeys(t *testing.T) {
+	var ctx, tb = context.Background(), snowflakeTestBackend(t)
+	var uniqueID = "83519234"
+	tb.CreateTable(ctx, t, uniqueID, "(a TEXT, b TEXT, c TEXT, d TEXT, PRIMARY KEY (c, a, b))")
+	tb.CaptureSpec(ctx, t).VerifyDiscover(ctx, t, regexp.MustCompile(uniqueID))
+}
