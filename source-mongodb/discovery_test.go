007e5bee39da1ad74b47ce7dbc465ac6841d4548
Will Baker <wbaker@gmail.com>
2023-12-22 14:20:12 -0500

    source-mongodb: use `stateKey` for tracking binding state
    Updates the connector to use `stateKey` for each binding to track its state.
    
    This includes a migration for existing captures. Once all existing captures have started up with
    this new connector and we are confident that no additional old captures will be re-enabled, we can
    remove the migration code.

--- source-mongodb/discovery_test.go
+++ source-mongodb/discovery_test.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"testing"
 
-	"go.mongodb.org/mongo-driver/mongo"
+	st "github.com/estuary/connectors/source-boilerplate/testing"
 	"github.com/estuary/flow/go/protocols/flow"
 	"github.com/stretchr/testify/require"
-	st "github.com/estuary/connectors/source-boilerplate/testing"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func TestDiscover(t *testing.T) {
@@ -18,10 +18,12 @@ func TestDiscover(t *testing.T) {
 	var database = "test"
 	var col1 = "collection1"
 	var col2 = "collection2"
+	var view = "testView"
 
 	cleanup := func() {
 		dropCollection(ctx, t, client, database, col1)
 		dropCollection(ctx, t, client, database, col2)
+		dropCollection(ctx, t, client, database, view)
 	}
 	cleanup()
 	t.Cleanup(cleanup)
@@ -42,7 +44,7 @@ func TestDiscover(t *testing.T) {
 	// but neither the view itself, nor the `system.views` collections must not
 	// show up in the discovered results
 	var db = client.Database(database)
-	err := db.CreateView(ctx, "testView", col1, mongo.Pipeline{})
+	err := db.CreateView(ctx, view, col1, mongo.Pipeline{})
 	require.NoError(t, err)
 
 	addTestTableData(ctx, t, client, database, col1, 5, 0, stringPkVals, "onlyColumn")
@@ -52,9 +54,9 @@ func TestDiscover(t *testing.T) {
 		Driver:       &driver{},
 		EndpointSpec: &cfg,
 		Checkpoint:   []byte("{}"),
-		Validator:  &st.SortedCaptureValidator{},
-		Sanitizers: commonSanitizers(),
-		Bindings:   bindings(t, database, col1),
+		Validator:    &st.SortedCaptureValidator{},
+		Sanitizers:   commonSanitizers(),
+		Bindings:     bindings(t, database, col1),
 	}
 
 	cs.VerifyDiscover(ctx, t, nil...)
@@ -67,11 +69,13 @@ func TestDiscoverMultipleDatabases(t *testing.T) {
 	var databases = []string{"foo", "bar", "baz"}
 	var col1 = "collection1"
 	var col2 = "collection2"
+	var view = "testView"
 
 	cleanup := func() {
 		for _, d := range databases {
 			dropCollection(ctx, t, client, d, col1)
 			dropCollection(ctx, t, client, d, col2)
+			dropCollection(ctx, t, client, d, view)
 		}
 	}
 	cleanup()
@@ -95,7 +99,7 @@ func TestDiscoverMultipleDatabases(t *testing.T) {
 	var bs []*flow.CaptureSpec_Binding
 	for _, d := range databases {
 		var db = client.Database(d)
-		err := db.CreateView(ctx, "testView", col1, mongo.Pipeline{})
+		err := db.CreateView(ctx, view, col1, mongo.Pipeline{})
 		require.NoError(t, err)
 
 		addTestTableData(ctx, t, client, d, col1, 5, 0, stringPkVals, "onlyColumn")
@@ -110,9 +114,9 @@ func TestDiscoverMultipleDatabases(t *testing.T) {
 		Driver:       &driver{},
 		EndpointSpec: &cfg,
 		Checkpoint:   []byte("{}"),
-		Validator:  &st.SortedCaptureValidator{},
-		Sanitizers: commonSanitizers(),
-		Bindings:   bs,
+		Validator:    &st.SortedCaptureValidator{},
+		Sanitizers:   commonSanitizers(),
+		Bindings:     bs,
 	}
 
 	cs.VerifyDiscover(ctx, t, nil...)
@@ -125,11 +129,13 @@ func TestDiscoverAllDatabases(t *testing.T) {
 	var databases = []string{"foo", "bar", "baz"}
 	var col1 = "collection1"
 	var col2 = "collection2"
+	var view = "testView"
 
 	cleanup := func() {
 		for _, d := range databases {
 			dropCollection(ctx, t, client, d, col1)
 			dropCollection(ctx, t, client, d, col2)
+			dropCollection(ctx, t, client, d, view)
 		}
 	}
 	cleanup()
@@ -153,7 +159,7 @@ func TestDiscoverAllDatabases(t *testing.T) {
 	var bs []*flow.CaptureSpec_Binding
 	for _, d := range databases {
 		var db = client.Database(d)
-		err := db.CreateView(ctx, "testView", col1, mongo.Pipeline{})
+		err := db.CreateView(ctx, view, col1, mongo.Pipeline{})
 		require.NoError(t, err)
 
 		addTestTableData(ctx, t, client, d, col1, 5, 0, stringPkVals, "onlyColumn")
@@ -168,9 +174,9 @@ func TestDiscoverAllDatabases(t *testing.T) {
 		Driver:       &driver{},
 		EndpointSpec: &cfg,
 		Checkpoint:   []byte("{}"),
-		Validator:  &st.SortedCaptureValidator{},
-		Sanitizers: commonSanitizers(),
-		Bindings:   bs,
+		Validator:    &st.SortedCaptureValidator{},
+		Sanitizers:   commonSanitizers(),
+		Bindings:     bs,
 	}
 
 	cs.VerifyDiscover(ctx, t, nil...)
