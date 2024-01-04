6d254f98ff38b9a7a3d16e95da4a58993cb54d25
Flavio Riper <flavioriper_andrade@hotmail.com>
2024-01-04 15:38:30 -0300

    feat: removing old source-bing-ads repo

--- source-bing-ads/__main__.py
+++ source-bing-ads/__main__.py
@@ -1,60 +1,36 @@
 from flow_sdk import shim_airbyte_cdk
 from source_bing_ads import SourceBingAds
 
-scopes = " ".join([
-    "openid",
-    "profile",
-    "https://ads.microsoft.com/msads.manage",
-    "offline_access"
-])
-
-def wrap_with_braces(body: str, count: int):
-    opening = '{'*count
-    closing = '}'*count
-    return f"{opening}{body}{closing}"
-
-def urlencode_field(field: str):
-    return f"{wrap_with_braces('#urlencode',2)}{wrap_with_braces(field,3)}{wrap_with_braces('/urlencode',2)}"
-
-def urlencode_raw(raw: str):
-    return f"{wrap_with_braces('#urlencode',2)}{raw}{wrap_with_braces('/urlencode',2)}"
-
 shim_airbyte_cdk.CaptureShim(
-    delegate=SourceBingAds(),
-    oauth2=
-    {
+    delegate = SourceBingAds(),
+    oauth2 = {
         "provider": "microsoft",
-        "accessTokenBody": (
-            f"client_id={urlencode_field('client_id')}&"
-            f"scope={urlencode_raw(scopes)}&"
-            f"code={urlencode_field('code')}&"
-            f"grant_type=authorization_code&"
-            f"redirect_uri={urlencode_field('redirect_uri')}&"
-            f"client_secret={urlencode_field('client_secret')}"
-        ),
         "authUrlTemplate": (
-            f"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?"
-            f"client_id={urlencode_field('client_id')}&"
-            f"scope={urlencode_raw(scopes)}&"
-            f"response_type=code&"
-            f"redirect_uri={urlencode_field('redirect_uri')}&"
-            f"state={urlencode_field('state')}&"
-            f"prompt=login"
+            "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?"
+            r"client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}"
+            r"&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}"
+            "&scope=openid%20offline_access%20https%3A%2F%2Fads.microsoft.com%2Fmsads.manage"
+            r"&state={{#urlencode}}{{{ state }}}{{/urlencode}}"
+            "&response_type=code"
+            "&prompt=login"
         ),
+        "accessTokenBody": (
+            r"client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}"
+            r"&scope=https%3A%2F%2Fads.microsoft.com%2Fmsads.manage"
+            r"&code={{#urlencode}}{{{ code }}}{{/urlencode}}"
+            r"&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}"
+            "&grant_type=authorization_code"
+            r"&state={{#urlencode}}{{{ state }}}{{/urlencode}}"
+            r"&client_secret={{#urlencode}}{{{ client_secret }}}{{/urlencode}}"
+        ),
+        "accessTokenUrlTemplate": "https://login.microsoftonline.com/common/oauth2/v2.0/token",
         "accessTokenHeaders": {
-            "content-type": "application/x-www-form-urlencoded"
+            "content-type": "application/x-www-form-urlencoded",
         },
         "accessTokenResponseMap": {
             "access_token": "/access_token",
             "refresh_token": "/refresh_token",
+            "token_expiry_date": r"{{#now_plus}}{{ expires_in }}{{/now_plus}}"
         },
-        "accessTokenUrlTemplate": (
-            f"https://login.microsoftonline.com/common/oauth2/v2.0/token?"
-            f"client_id={urlencode_field('client_id')}&"
-            f"scope={urlencode_field(scopes)}&"
-            f"code={urlencode_field('code')}&"
-            f"grant_type=authorization_code&"
-            f"redirect_uri={urlencode_field('redirect_uri')}&"
-        )
-    },
+    }
 ).main()
