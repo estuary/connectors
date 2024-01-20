c3a0f4bfa89479acd5423f6c3a712f47e7e5d68f
Flavio Riper <flavioriper_andrade@hotmail.com>
2024-01-20 11:01:30 -0300

    feat: add setup files

---
+++ source-microsoft-onedrive/__main__.py
@@ -0,0 +1,40 @@
+from flow_sdk import shim_airbyte_cdk
+from flow_sdk.sources.file_based import FileBasedSource
+
+shim_airbyte_cdk.CaptureShim(
+    delegate = FileBasedSource(
+        stream_reader = None,
+        spec_class = None,
+        catalog_path = None,
+    ),
+    oauth2 = {
+        "provider": "microsoft",
+        "authUrlTemplate": (
+            "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?"
+            r"client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}"
+            r"&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}"
+            "&scope=offline_access%20user.read%20mail.read"
+            r"&state={{#urlencode}}{{{ state }}}{{/urlencode}}"
+            "&response_type=code"
+            "&prompt=login"
+        ),
+        "accessTokenBody": (
+            r"client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}"
+            r"&scope=offline_access%20user.read%20mail.read"
+            r"&code={{#urlencode}}{{{ code }}}{{/urlencode}}"
+            r"&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}"
+            "&grant_type=authorization_code"
+            r"&state={{#urlencode}}{{{ state }}}{{/urlencode}}"
+            r"&client_secret={{#urlencode}}{{{ client_secret }}}{{/urlencode}}"
+        ),
+        "accessTokenUrlTemplate": "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+        "accessTokenHeaders": {
+            "content-type": "application/x-www-form-urlencoded",
+        },
+        "accessTokenResponseMap": {
+            "access_token": "/access_token",
+            "refresh_token": "/refresh_token",
+            "token_expiry_date": r"{{#now_plus}}{{ expires_in }}{{/now_plus}}"
+        },
+    }
+).main()
