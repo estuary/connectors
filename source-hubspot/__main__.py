92ca1199737954d1e704685a8b3e522435d7c812
Flavio Riper <flavioriper_andrade@hotmail.com>
2024-02-01 14:09:17 -0300

    feat: add test credentials

--- source-hubspot/__main__.py
+++ source-hubspot/__main__.py
@@ -9,7 +9,7 @@ shim_airbyte_cdk.CaptureShim(
             "https://app.hubspot.com/oauth/authorize?"
             r"client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}"
             r"&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}"
-            r"&scope=content%20automation%20forms%20tickets%20e-commerce%20sales-email-read%20crm.objects.contacts.read%20crm.objects.custom.read%20crm.schemas.contacts.read%20crm.objects.companies.read%20crm.objects.deals.read%20crm.schemas.companies.read%20crm.schemas.deals.read%20crm.objects.owners.read%20crm.objects.goals.read"
+            r"&scope=content%20crm.schemas.deals.read%20automation%20oauth%20crm.objects.owners.read%20forms%20files%20tickets%20e-commerce%20files.ui_hidden.read%20crm.schemas.custom.read%20crm.objects.custom.read%20sales-email-read%20forms-uploaded-files%20crm.objects.goals.read%20crm.objects.companies.read%20crm.lists.read%20crm.objects.deals.read%20crm.schemas.contacts.read%20crm.objects.contacts.read%20crm.schemas.companies.read"
             r"&state={{#urlencode}}{{{ state }}}{{/urlencode}}"
         ),
         "accessTokenUrlTemplate": "https://api.hubapi.com/oauth/v1/token",
