c29a62306234505698ae7919b2809e3434e789c4
Will Baker <wbaker@gmail.com>
2024-02-05 18:09:40 -0500

    materialize-sql: forbidden constraints for fields that are too long
    Rather than erroring during `Apply`, we will now produce "field forbidden" constraints for Flow
    collection fields that are longer than the destination system will allow.
    
    This is slightly complicated by that fact that we want to return an error if "required" fields
    cannot be materialized due to length limits, rather than successfully validate with a "forbidden"
    constraint. This requires tracking all projections of a location and making sure that at least one
    of them is not too long if the location is required.

--- materialize-postgres/driver.go
+++ materialize-postgres/driver.go
@@ -212,6 +212,7 @@ func newPostgresDriver() *sql.Driver {
 				NewTransactor:        newTransactor,
 				Tenant:               tenant,
 				ConcurrentApply:      false,
+				MaxFieldCharLen:      0, // Postgres automatically truncates column names that are too long
 			}, nil
 		},
 	}
