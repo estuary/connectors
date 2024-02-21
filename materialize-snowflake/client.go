c1af24651fef42ea0dc966b0e4fc84309504e7fb
Mahdi Dibaiee <mdibaiee@pm.me>
2024-02-21 16:03:50 +0000

    materialize-snowflake: createPipe after ack, copy_history

--- materialize-snowflake/client.go
+++ materialize-snowflake/client.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	stdsql "database/sql"
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"strings"
@@ -135,7 +134,7 @@ func (c *client) ExecStatements(ctx context.Context, statements []string) error 
 }
 
 func (c *client) InstallFence(ctx context.Context, checkpoints sql.Table, fence sql.Fence) (sql.Fence, error) {
-	return sql.StdInstallFence(ctx, c.db, checkpoints, fence, base64.StdEncoding.DecodeString)
+	return sql.Fence{}, nil
 }
 
 func (c *client) Close() {
