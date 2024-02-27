15909df9651b4b654a950d5b5a10ad894545eca1
Will Donnelly <will@estuary.dev>
2024-02-26 19:07:32 -0600

    source-redshift-batch: Test improvements

--- source-redshift-batch/main_test.go
+++ source-redshift-batch/main_test.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"regexp"
+	"strings"
+	"sync/atomic"
 	"testing"
+	"text/template"
 	"time"
 
 	"github.com/bradleyjkemp/cupaloy"
@@ -20,12 +25,12 @@ import (
 )
 
 var (
-	dbAddress     = flag.String("db_address", "localhost:5432", "The database server address to use for tests")
+	dbAddress     = flag.String("db_address", "default-workgroup.123456789012.us-east-1.redshift-serverless.amazonaws.com:5439", "The database server address to use for tests")
 	dbName        = flag.String("db_name", "dev", "Use the named database for tests")
-	dbControlUser = flag.String("db_control_user", "postgres", "The user for test setup/control operations")
-	dbControlPass = flag.String("db_control_pass", "postgres", "The password the the test setup/control user")
 	dbCaptureUser = flag.String("db_capture_user", "flow_capture", "The user to perform captures as")
 	dbCapturePass = flag.String("db_capture_pass", "secret1234", "The password for the capture user")
+	dbControlUser = flag.String("db_control_user", "", "The user for test setup/control operations, if different from the capture user")
+	dbControlPass = flag.String("db_control_pass", "", "The password the the test setup/control user, if different from the capture password")
 )
 
 func TestMain(m *testing.M) {
@@ -47,46 +52,124 @@ func TestSpec(t *testing.T) {
 	cupaloy.SnapshotT(t, string(formatted))
 }
 
+func TestQueryTemplate(t *testing.T) {
+	res, err := redshiftDriver.GenerateResource("foobar", "testdata", "foobar", "BASE TABLE")
+	require.NoError(t, err)
+
+	tmpl, err := template.New("query").Funcs(templateFuncs).Parse(res.Template)
+	require.NoError(t, err)
+
+	for _, tc := range []struct {
+		Name    string
+		IsFirst bool
+		Cursor  []string
+	}{
+		{Name: "FirstNoCursor", IsFirst: true, Cursor: nil},
+		{Name: "SubsequentNoCursor", IsFirst: false, Cursor: nil},
+		{Name: "FirstOneCursor", IsFirst: true, Cursor: []string{"`ka`"}},
+		{Name: "SubsequentOneCursor", IsFirst: false, Cursor: []string{"`ka`"}},
+		{Name: "FirstTwoCursor", IsFirst: true, Cursor: []string{"`ka`", "`kb`"}},
+		{Name: "SubsequentTwoCursor", IsFirst: false, Cursor: []string{"`ka`", "`kb`"}},
+		{Name: "FirstThreeCursor", IsFirst: true, Cursor: []string{"`ka`", "`kb`", "`kc`"}},
+		{Name: "SubsequentThreeCursor", IsFirst: false, Cursor: []string{"`ka`", "`kb`", "`kc`"}},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			var buf = new(strings.Builder)
+			require.NoError(t, tmpl.Execute(buf, map[string]any{
+				"IsFirstQuery": tc.IsFirst,
+				"CursorFields": tc.Cursor,
+			}))
+			cupaloy.SnapshotT(t, buf.String())
+		})
+	}
+}
+
 func TestBasicCapture(t *testing.T) {
 	var ctx, cs = context.Background(), testCaptureSpec(t)
+	var control = testControlClient(ctx, t)
 	var uniqueID = "826935"
 	var tableName = fmt.Sprintf("test.basic_capture_%s", uniqueID)
 
-	// Connect to the test database and create the test table
-	var controlURI = fmt.Sprintf(`postgres://%s:%s@%s/%s?sslmode=require`, *dbControlUser, *dbControlPass, *dbAddress, *dbName)
+	executeControlQuery(ctx, t, control, fmt.Sprintf("DROP TABLE IF EXISTS %s", tableName))
+	t.Cleanup(func() { executeControlQuery(ctx, t, control, fmt.Sprintf("DROP TABLE IF EXISTS %s", tableName)) })
+	executeControlQuery(ctx, t, control, fmt.Sprintf("CREATE TABLE %s(id INTEGER PRIMARY KEY, data TEXT)", tableName))
+
+	// Discover the table and verify discovery snapshot
+	cs.Bindings = discoverStreams(ctx, t, cs, regexp.MustCompile(uniqueID))
+	t.Run("Discovery", func(t *testing.T) {
+		var summary = new(strings.Builder)
+		for idx, binding := range cs.Bindings {
+			fmt.Fprintf(summary, "Binding %d:\n", idx)
+			bs, err := json.MarshalIndent(binding, "  ", "  ")
+			require.NoError(t, err)
+			io.Copy(summary, bytes.NewReader(bs))
+			fmt.Fprintf(summary, "\n")
+		}
+		if len(cs.Bindings) == 0 {
+			fmt.Fprintf(summary, "(no output)")
+		}
+		cupaloy.SnapshotT(t, summary.String())
+	})
+
+	// Edit the discovered binding to use the ID column as a cursor
+	var res Resource
+	require.NoError(t, json.Unmarshal(cs.Bindings[0].ResourceConfigJson, &res))
+	res.Cursor = []string{"id"}
+	resourceConfigBytes, err := json.Marshal(res)
+	require.NoError(t, err)
+	cs.Bindings[0].ResourceConfigJson = resourceConfigBytes
+
+	t.Run("Capture", func(t *testing.T) {
+		// Spawn a worker thread which will insert 50 rows of data in parallel with the capture.
+		var insertsDone atomic.Bool
+		go func() {
+			for i := 0; i < 50; i++ {
+				time.Sleep(500 * time.Millisecond)
+				executeControlQuery(ctx, t, control, fmt.Sprintf("INSERT INTO %s VALUES ($1, $2)", tableName), i, fmt.Sprintf("Value for row %d", i))
+				log.WithField("i", i).Debug("inserted row")
+			}
+			time.Sleep(1 * time.Second)
+			insertsDone.Store(true)
+		}()
+
+		// Run the capture over and over for 5 seconds each time until all inserts have finished, then verify results.
+		for !insertsDone.Load() {
+			var captureCtx, cancelCapture = context.WithCancel(ctx)
+			time.AfterFunc(5*time.Second, cancelCapture)
+			cs.Capture(captureCtx, t, nil)
+		}
+		cupaloy.SnapshotT(t, cs.Summary())
+	})
+}
+
+func testControlClient(ctx context.Context, t testing.TB) *sql.DB {
+	t.Helper()
+	if os.Getenv("TEST_DATABASE") != "yes" {
+		t.Skipf("skipping %q capture: ${TEST_DATABASE} != \"yes\"", t.Name())
+	}
+
+	var controlUser = *dbControlUser
+	if controlUser == "" {
+		controlUser = *dbCaptureUser
+	}
+	var controlPass = *dbControlPass
+	if controlPass == "" {
+		controlPass = *dbCapturePass
+	}
+	var controlURI = fmt.Sprintf(`postgres://%s:%s@%s/%s?sslmode=require`, controlUser, controlPass, *dbAddress, *dbName)
 	log.WithField("uri", controlURI).Debug("opening database control connection")
 	var conn, err = sql.Open("pgx", controlURI)
 	require.NoError(t, err)
 	t.Cleanup(func() { conn.Close() })
 	require.NoError(t, conn.PingContext(ctx))
+	return conn
+}
 
-	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP TABLE IF EXISTS %s", tableName))
+func executeControlQuery(ctx context.Context, t testing.TB, client *sql.DB, query string, args ...interface{}) {
+	t.Helper()
+	log.WithFields(log.Fields{"query": query, "args": args}).Debug("executing setup query")
+	var _, err = client.ExecContext(ctx, query, args...)
 	require.NoError(t, err)
-	t.Cleanup(func() { conn.ExecContext(ctx, fmt.Sprintf("DROP TABLE IF EXISTS %s", tableName)) })
-	_, err = conn.ExecContext(ctx, fmt.Sprintf("CREATE TABLE %s(id INTEGER PRIMARY KEY, data TEXT)", tableName))
-	require.NoError(t, err)
-
-	// Discover the table and accept the default configuration
-	cs.Bindings = discoverStreams(ctx, t, cs, regexp.MustCompile(uniqueID))
-
-	// Spawn a worker thread which will insert 250 rows of data over the course of 25 seconds.
-	go func() {
-		for i := 0; i < 250; i++ {
-			time.Sleep(100 * time.Millisecond)
-			_, err = conn.ExecContext(ctx, fmt.Sprintf("INSERT INTO %s VALUES ($1, $2)", tableName), i, fmt.Sprintf("Value for row %d", i))
-			require.NoError(t, err)
-			log.WithField("i", i).Debug("inserted row")
-		}
-	}()
-
-	// Perform six captures each running for 5 seconds, then verify that
-	// the resulting data is correct.
-	for i := 0; i < 6; i++ {
-		var captureCtx, cancelCapture = context.WithCancel(ctx)
-		time.AfterFunc(5*time.Second, cancelCapture)
-		cs.Capture(captureCtx, t, nil)
-	}
-	cupaloy.SnapshotT(t, cs.Summary())
 }
 
 func testCaptureSpec(t testing.TB) *st.CaptureSpec {
