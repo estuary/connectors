0e27b242d0ed7bd086729674bd2029948d883b2b
Johnny Graettinger <johnny@estuary.dev>
2024-02-28 04:54:04 +0000

    source-google-sheets-native: add low-latency native Google Sheets connector

---
+++ source-google-sheets-native/source_google_sheets_native/__init__.py
@@ -0,0 +1,63 @@
+from logging import Logger
+from typing import Callable, Awaitable
+
+from estuary_cdk.flow import (
+    ConnectorSpec,
+)
+from estuary_cdk.capture import (
+    BaseCaptureConnector,
+    Request,
+    Task,
+    common,
+    request,
+    response,
+)
+from estuary_cdk.http import HTTPMixin
+
+from .resources import all_resources
+from .models import (
+    ConnectorState,
+    EndpointConfig,
+    OAUTH2_SPEC,
+    ResourceConfig,
+)
+
+class Connector(
+    BaseCaptureConnector[EndpointConfig, ResourceConfig, ConnectorState],
+    HTTPMixin,
+):
+    def request_class(self):
+        return Request[EndpointConfig, ResourceConfig, ConnectorState]
+
+    async def spec(self, _: request.Spec, logger: Logger) -> ConnectorSpec:
+        return ConnectorSpec(
+            configSchema=EndpointConfig.model_json_schema(),
+            documentationUrl="https://docs.estuary.dev",
+            oauth2=OAUTH2_SPEC,
+            resourceConfigSchema=ResourceConfig.model_json_schema(),
+            resourcePathPointers=ResourceConfig.PATH_POINTERS,
+        )
+
+    async def discover(
+        self, discover: request.Discover[EndpointConfig], logger: Logger
+    ) -> response.Discovered[ResourceConfig]:
+        resources = await all_resources(self, discover.config, logger)
+        return common.discovered(resources)
+
+    async def validate(
+        self,
+        validate: request.Validate[EndpointConfig, ResourceConfig],
+        logger: Logger,
+    ) -> response.Validated:
+        resources = await all_resources(self, validate.config, logger)
+        resolved = common.resolve_bindings(validate.bindings, resources, resource_term="Sheet")
+        return common.validated(resolved)
+
+    async def open(
+        self,
+        open: request.Open[EndpointConfig, ResourceConfig, ConnectorState],
+        logger: Logger,
+    ) -> tuple[response.Opened, Callable[[Task], Awaitable[None]]]:
+        resources = await all_resources(self, open.capture.config, logger)
+        resolved = common.resolve_bindings(open.capture.bindings, resources, resource_term="Sheet")
+        return common.open(open, resolved)
