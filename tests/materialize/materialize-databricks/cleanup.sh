dc5244d4064942c938e52feedd7712ad43a66e8b
Mahdi Dibaiee <mdibaiee@pm.me>
2024-02-29 13:06:44 +0000

    materialize-databricks: quote schema and volume identifier

--- tests/materialize/materialize-databricks/cleanup.sh
+++ tests/materialize/materialize-databricks/cleanup.sh
@@ -9,12 +9,12 @@ function dropTable() {
 }
 
 # Remove materialized tables.
-dropTable "\`$DATABRICKS_CATALOG\`.default.simple"
-dropTable "\`$DATABRICKS_CATALOG\`.default.duplicate_keys_standard"
-dropTable "\`$DATABRICKS_CATALOG\`.default.duplicate_keys_delta"
-dropTable "\`$DATABRICKS_CATALOG\`.default.duplicate_keys_delta_exclude_flow_doc"
-dropTable "\`$DATABRICKS_CATALOG\`.default.multiple_types"
-dropTable "\`$DATABRICKS_CATALOG\`.default.formatted_strings"
+dropTable "\`$DATABRICKS_CATALOG\`.\`some-schema\`.simple"
+dropTable "\`$DATABRICKS_CATALOG\`.\`some-schema\`.duplicate_keys_standard"
+dropTable "\`$DATABRICKS_CATALOG\`.\`some-schema\`.duplicate_keys_delta"
+dropTable "\`$DATABRICKS_CATALOG\`.\`some-schema\`.duplicate_keys_delta_exclude_flow_doc"
+dropTable "\`$DATABRICKS_CATALOG\`.\`some-schema\`.multiple_types"
+dropTable "\`$DATABRICKS_CATALOG\`.\`some-schema\`.formatted_strings"
 
-yes | dbsqlcli -e "delete from \`$DATABRICKS_CATALOG\`.\`default\`.flow_materializations_v2 where MATERIALIZATION='tests/materialize-databricks/materialize';"
+yes | dbsqlcli -e "delete from \`$DATABRICKS_CATALOG\`.\`some-schema\`.flow_materializations_v2 where MATERIALIZATION='tests/materialize-databricks/materialize';"
 
