e15046ab5329220fa0dc361a13e70f2dfc101ebf
Luishfs <luish1980@hotmail.com>
2024-03-11 04:23:43 -0300

    Revert "The idea behind removing the amount of max retries is to allow the connector" Wrong branch, hubspot is playing tricks on me This reverts commit 5447ff22fb54516d95786a4cd9b9e1ae8c9fa35c.

--- source-hubspot/source_hubspot/streams.py
+++ source-hubspot/source_hubspot/streams.py
@@ -368,7 +368,7 @@ class Stream(HttpStream, ABC):
         return json_schema
 
 
-    @retry_401()
+    @retry_401(max_tries=20)
     def handle_request(
         self,
         stream_slice: Mapping[str, Any] = None,
