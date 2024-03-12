estuary/connectors

commit d103672fda867f99eddf363bb5f0847475f5d5a0

source-mysql: Cleaner handling of "" enum value
The string `""` is always accepted by a MySQL enum column and any
illegal inputs are mapped to this string, which is represented by
the integer 0.

We already supported all of this (modulo a small bug in `ALTER TABLE`
parsing which I accidentally uncovered as part of this commit), but
the logic becomes a bit simpler after this refactor, which moves the
`""` option to be first in the list so that its index 0 corresponds
more directly to MySQL representing it as the integer zero.

source-mysql/replication.go
@@ -676,7 +676,9 @@ func (rs *mysqlReplicationStream) handleAlterTable(ctx context.Context, stmt *sq
 
 func translateDataType(t sqlparser.ColumnType) any {
 	var typeName = strings.ToLower(t.Type)
-	if typeName == "enum" || typeName == "set" {
+	if typeName == "enum" {
+		return &mysqlColumnType{Type: typeName, EnumValues: append([]string{""}, t.EnumValues...)}
+	} else if typeName == "set" {
 		return &mysqlColumnType{Type: typeName, EnumValues: t.EnumValues}
 	}
 	return typeName

source-mysql/.snapshots/TestAlterTable_AddColumnSetEnum-enum-restart
@@ -6,5 +6,5 @@
 # ================================
 # Final State Checkpoint
 # ================================
-{"bindingStateV1":{"test%2FAlterTable_AddColumnSetEnum_enum_76927424":{"backfilled":2,"key_columns":["id"],"metadata":{"schema":{"columns":["id","data","enumCol"],"types":{"data":"text","enumCol":{"enum":["'someValue'","'anotherValue'"],"type":"enum"},"id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
+{"bindingStateV1":{"test%2FAlterTable_AddColumnSetEnum_enum_76927424":{"backfilled":2,"key_columns":["id"],"metadata":{"schema":{"columns":["id","data","enumCol"],"types":{"data":"text","enumCol":{"enum":["","'someValue'","'anotherValue'"],"type":"enum"},"id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
 

source-mysql/.snapshots/TestAlterTable_AddColumnSetEnum-enum-stream
@@ -6,5 +6,5 @@
 # ================================
 # Final State Checkpoint
 # ================================
-{"bindingStateV1":{"test%2FAlterTable_AddColumnSetEnum_enum_76927424":{"backfilled":2,"key_columns":["id"],"metadata":{"schema":{"columns":["id","data","enumCol"],"types":{"data":"text","enumCol":{"enum":["'someValue'","'anotherValue'"],"type":"enum"},"id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
+{"bindingStateV1":{"test%2FAlterTable_AddColumnSetEnum_enum_76927424":{"backfilled":2,"key_columns":["id"],"metadata":{"schema":{"columns":["id","data","enumCol"],"types":{"data":"text","enumCol":{"enum":["","'someValue'","'anotherValue'"],"type":"enum"},"id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
 

source-mysql/.snapshots/TestEnumPrimaryKey
@@ -24,5 +24,5 @@
 # ================================
 # Final State Checkpoint
 # ================================
-{"bindingStateV1":{"test%2FEnumPrimaryKey_18676708":{"backfilled":20,"key_columns":["category","id"],"metadata":{"schema":{"columns":["category","id","data"],"types":{"category":{"enum":["A","C","B","D",""],"type":"enum"},"data":"text","id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
+{"bindingStateV1":{"test%2FEnumPrimaryKey_18676708":{"backfilled":20,"key_columns":["category","id"],"metadata":{"schema":{"columns":["category","id","data"],"types":{"category":{"enum":["","A","C","B","D"],"type":"enum"},"data":"text","id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
 

source-mysql/datatype_test.go
@@ -80,10 +80,10 @@ func TestDatatypes(t *testing.T) {
 		{ColumnType: "mediumblob", ExpectType: `{"type":["string","null"],"contentEncoding":"base64"}`, InputValue: []byte{0x12, 0x34, 0x56, 0x78}, ExpectValue: `"EjRWeA=="`},
 		{ColumnType: "longblob", ExpectType: `{"type":["string","null"],"contentEncoding":"base64"}`, InputValue: []byte{0x12, 0x34, 0x56, 0x78}, ExpectValue: `"EjRWeA=="`},
 
-		{ColumnType: `enum('sm', 'med', 'lg')`, ExpectType: `{"type":["string","null"],"enum":["sm","med","lg","",null]}`, InputValue: nil, ExpectValue: `null`},
-		{ColumnType: `enum('sm', 'med', 'lg') not null`, ExpectType: `{"type":"string","enum":["sm","med","lg",""]}`, InputValue: "sm", ExpectValue: `"sm"`},
-		{ColumnType: `enum('s,m', 'med', '\'lg\'')`, ExpectType: `{"type":["string","null"],"enum":["s,m","med","'lg'","",null]}`, InputValue: `'lg'`, ExpectValue: `"'lg'"`},
-		{ColumnType: `enum('s,m', 'med', '\'lg\'')`, ExpectType: `{"type":["string","null"],"enum":["s,m","med","'lg'","",null]}`, InputValue: `invalid`, ExpectValue: `""`},
+		{ColumnType: `enum('sm', 'med', 'lg')`, ExpectType: `{"type":["string","null"],"enum":["","sm","med","lg",null]}`, InputValue: nil, ExpectValue: `null`},
+		{ColumnType: `enum('sm', 'med', 'lg') not null`, ExpectType: `{"type":"string","enum":["","sm","med","lg"]}`, InputValue: "sm", ExpectValue: `"sm"`},
+		{ColumnType: `enum('s,m', 'med', '\'lg\'')`, ExpectType: `{"type":["string","null"],"enum":["","s,m","med","'lg'",null]}`, InputValue: `'lg'`, ExpectValue: `"'lg'"`},
+		{ColumnType: `enum('s,m', 'med', '\'lg\'')`, ExpectType: `{"type":["string","null"],"enum":["","s,m","med","'lg'",null]}`, InputValue: `invalid`, ExpectValue: `""`},
 
 		{ColumnType: "set('a', 'b', 'c')", ExpectType: `{"type":["string","null"]}`, InputValue: "b", ExpectValue: `"b"`},
 		{ColumnType: "set('a', 'b', 'c')", ExpectType: `{"type":["string","null"]}`, InputValue: "a,c", ExpectValue: `"a,c"`},

source-mysql/discovery.go
@@ -381,7 +381,9 @@ func getColumns(ctx context.Context, conn *client.Conn) ([]sqlcapture.ColumnInfo
 
 func parseDataType(typeName, fullColumnType string) any {
 	if typeName == "enum" {
-		return &mysqlColumnType{Type: "enum", EnumValues: append(parseEnumValues(fullColumnType), "")}
+		// Illegal values are represented internally by MySQL as the integer 0. Adding
+		// this to the list as the zero-th element allows everything else to flow naturally.
+		return &mysqlColumnType{Type: "enum", EnumValues: append([]string{""}, parseEnumValues(fullColumnType)...)}
 	} else if typeName == "set" {
 		return &mysqlColumnType{Type: "set", EnumValues: parseEnumValues(fullColumnType)}
 	}
@@ -402,14 +404,8 @@ func (t *mysqlColumnType) translateRecordField(val interface{}) (interface{}, er
 	switch t.Type {
 	case "enum":
 		if index, ok := val.(int64); ok {
-			if 1 <= index && index <= int64(len(t.EnumValues)) {
-				return t.EnumValues[index-1], nil
-			} else if index == 0 {
-				// Illegal values are represented internally by MySQL as the integer 0.
-				// Backfill queries return this as the empty string, which is our chosen
-				// representation as well, but we have to handle the conversion of replicated
-				// values since they're just provided as the raw integer.
-				return "", nil
+			if 0 <= index && int(index) < len(t.EnumValues) {
+				return t.EnumValues[index], nil
 			}
 		} else if bs, ok := val.([]byte); ok {
 			return string(bs), nil
@@ -439,18 +435,10 @@ func (t *mysqlColumnType) encodeKeyFDB(val any) (tuple.TupleElement, error) {
 	switch t.Type {
 	case "enum":
 		if bs, ok := val.([]byte); ok {
-			if len(bs) == 0 {
-				// MySQL represents illegal enum values as the empty string or the integer
-				// zero in different contexts. We include the empty string in the EnumValues
-				// list for other reasons, so here we have to explicitly hard-code that the
-				// empty string will be translated back into the integer zero as a row key.
-				return 0, nil
-			} else if idx := slices.Index(t.EnumValues, string(bs)); idx >= 0 {
-				// Since zero is reserved, the first enum value corresponds to integer 1
-				return idx + 1, nil
-			} else {
-				return val, fmt.Errorf("internal error: failed to translate enum value %q back to integer index", string(bs))
+			if idx := slices.Index(t.EnumValues, string(bs)); idx >= 0 {
+				return idx, nil
 			}
+			return val, fmt.Errorf("internal error: failed to translate enum value %q to integer index", string(bs))
 		}
 		return val, nil
 	}

commit b161b4b2102c4f8d641b6c32554eff2e9ddf28d2

source-mysql: Migrate the old enum metadata ordering
This fixes the enum decoding off-by-one bug introduced in
https://github.com/estuary/connectors/pull/1336 by adding
some startup logic which will migrate any old metadata to
the new ordering (where `""` is the first element).

Once every capture in production has run this code and had
its metadata migrated it will be safe to remove all of the
extra migration logic and the associated test.

source-mysql/.snapshots/TestEnumDecodingFix-replication2
@@ -1,13 +1,13 @@
 # ================================
 # Collection "acmeCo/test/test_enumdecodingfix_32314857": 5 Documents
 # ================================
-{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"C","id":11}
-{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"D","id":12}
-{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"B","id":13}
-{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"","id":14}
-{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"A","id":15}
+{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"A","id":11}
+{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"B","id":12}
+{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"C","id":13}
+{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"D","id":14}
+{"_meta":{"op":"c","source":{"ts_ms":1111111111111,"schema":"test","table":"EnumDecodingFix_32314857","cursor":"binlog.000123:56789:123","txid":"11111111-1111-1111-1111-111111111111:111"}},"category":"","id":15}
 # ================================
 # Final State Checkpoint
 # ================================
-{"bindingStateV1":{"test%2FEnumDecodingFix_32314857":{"backfilled":5,"key_columns":["id"],"metadata":{"schema":{"columns":["id","category"],"types":{"category":{"enum":["A","C","B","D",""],"type":"enum"},"id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
+{"bindingStateV1":{"test%2FEnumDecodingFix_32314857":{"backfilled":5,"key_columns":["id"],"metadata":{"schema":{"columns":["id","category"],"types":{"category":{"enum":["","A","C","B","D"],"type":"enum"},"id":"int"}}},"mode":"Active"}},"cursor":"binlog.000123:56789"}
 

source-mysql/replication.go
@@ -769,6 +769,36 @@ func (rs *mysqlReplicationStream) ActivateTable(ctx context.Context, streamID st
 				}
 			}
 		}
+
+		// This is a temporary piece of migration logic added in March of 2024. In the PR
+		// https://github.com/estuary/connectors/pull/1336 the ordering of enum cases in this
+		// metadata was changed to simplify the decoding logic. Specifically the order of the
+		// cases was changed from ["A", "B", ""] to ["", "A", "B"] to more directly mirror how
+		// MySQL represents the illegal-enum value "" as integer 0. However this introduced a bug
+		// when the new indexing code was used with older metadata from before that change, and
+		// by the time this was discovered there were captures in production with the new ordering
+		// in their checkpointed metadata, so a simple revert was not an option.
+		//
+		// The solution is to push forward and add the missing migration step, so that upon table
+		// activation any metadata containing the old ordering will be updated to the new. Once all
+		// metadata in production has been so updated, it will be safe to remove this logic and the
+		// associated 'TestEnumDecodingFix' test case.
+		for column, columnType := range metadata.Schema.ColumnTypes {
+			if enumType, ok := columnType.(*mysqlColumnType); ok && enumType.Type == "enum" {
+				if len(enumType.EnumValues) > 0 && enumType.EnumValues[0] != "" {
+					var logEntry = logrus.WithField("column", column)
+					var vals = enumType.EnumValues
+					logEntry.WithField("vals", vals).Warn("old enum metadata ordering detected, will migrate")
+					if vals[len(vals)-1] == "" {
+						logEntry.Info("trimming empty-string case from the end")
+						vals = vals[:len(vals)-1]
+					}
+					vals = append([]string{""}, vals...)
+					logEntry.WithField("vals", vals).Info("migrated old enum metadata")
+					enumType.EnumValues = vals
+				}
+			}
+		}
 	} else if discovery != nil {
 		// If metadata JSON is not present, construct new default metadata based on the discovery info.
 		logrus.WithField("stream", streamID).Debug("initializing table metadata")

source-mysql/capture_test.go
@@ -183,6 +183,9 @@ func TestEnumPrimaryKey(t *testing.T) {
 }
 
 func TestEnumDecodingFix(t *testing.T) {
+	// This test is part of the fix for the enum decoding bug introduced by https://github.com/estuary/connectors/pull/1336
+	// and can be deleted after the metadata migration logic is also removed. The metadata migration logic can safely be
+	// removed after all live captures we care about have run the fix code and updated their checkpoint metadata.
 	var tb, ctx = mysqlTestBackend(t), context.Background()
 	var uniqueID = "32314857"
 	var tableName = tb.CreateTable(ctx, t, uniqueID, "(id INTEGER PRIMARY KEY, category ENUM('A', 'C', 'B', 'D'))")