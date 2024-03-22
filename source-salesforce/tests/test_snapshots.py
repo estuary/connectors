d98d980daade3a1a18305877fdc10c479d7b4608
Luishfs <luish1980@hotmail.com>
2024-03-22 00:38:17 -0300

    Added initial files

---
+++ source-salesforce/tests/test_snapshots.py
@@ -0,0 +1,59 @@
+import json
+import subprocess
+
+import pytest_insta.format as insta_format
+insta_format.FmtJson.dump = lambda _self, path, value: path.write_text(json.dumps(value, sort_keys=True, indent=2) + "\n", "utf-8")
+
+# def test_capture(request, snapshot):
+#     result = subprocess.run(
+#         [
+#             "flowctl",
+#             "preview",
+#             "--source",
+#             request.config.rootdir + "/source-hubspot/test.flow.yaml",
+#         ],
+#         stdout=subprocess.PIPE,
+#         text=True,
+#     )
+#     assert result.returncode == 0
+#     lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+#     assert snapshot("capture.stdout.json") == lines
+
+def test_discover(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "discover",
+            "--source",
+            request.config.rootdir + "/source-salesforce/test.flow.yaml",
+            "-o",
+            "json",
+            "--emit-raw"
+        ],
+       stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    assert snapshot("capture.stdout.json") == lines
+
+def test_spec(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "spec",
+            "--source",
+            request.config.rootdir + "/source-salesforce/test.flow.yaml"
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    assert snapshot("capture.stdout.json") == lines
+ 
