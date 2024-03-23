d1ff268812a641e895c41e25c1d011f311f76c6b
Pedro <pedro@z33dd.com>
2024-03-22 23:24:07 -0300

    source-jira: Update Python and airbyte-cdk versions in pyproject.toml
    Update poetry lock file for source-jira

--- source-jira/source_jira/__main__.py
+++ source-jira/source_jira/__main__.py
@@ -1,14 +1,21 @@
-import estuary_cdk.pydantic_polyfill  # Must be first.
+import estuary_cdk.pydantic_polyfill  # Must be first. # noqa
 
 import asyncio
-from estuary_cdk import shim_airbyte_cdk
+from estuary_cdk import shim_airbyte_cdk, flow
 from source_jira import SourceJira
 
 
 asyncio.run(
     shim_airbyte_cdk.CaptureShim(
         delegate=SourceJira(),
-        oauth2=None,
+        oauth2=flow.OAuth2Spec(
+            provider="",
+            authUrlTemplate="",
+            accessTokenUrlTemplate="",
+            accessTokenBody="",
+            accessTokenHeaders={},
+            accessTokenResponseMap={},
+        ),
         schema_inference=True,
     ).serve()
 )
