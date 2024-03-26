d3da6bb69a35daf1c25912bbafb9e286dc862bf6
Pedro <pedro@z33dd.com>
2024-03-26 15:12:20 -0300

    source-jira: snapshot tests for imported connector
    Update jira-source with fixes
    
    Fix schemas and snapshots in source-jira
    
    Add source-jira to test pipeline
    
    Update snapshots
    
    source-jira: update snapshots
    
    Fix tests
    
    source-jira: Set usage rate for connector testing
    
    source-jira: Add schema files necessary to the tests
    
    source-jira: Update snapshot location
    
    source-jira: Refactor test_snapshots.py to capture all stdout lines in the snapshot
    
    source-jira: original snapshots
    
    jira-source: Update tests snapshots

---
+++ source-jira/source_jira/__main__.py
@@ -0,0 +1,14 @@
+import estuary_cdk.pydantic_polyfill  # Must be first. # noqa
+
+import asyncio
+from estuary_cdk import shim_airbyte_cdk
+from source_jira import SourceJira
+
+
+asyncio.run(
+    shim_airbyte_cdk.CaptureShim(
+        delegate=SourceJira(),
+        oauth2=None,
+        schema_inference=True,
+    ).serve()
+)
