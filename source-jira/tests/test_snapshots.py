0e41544757cf11b3bcc1c0aca1b11924a2329883
Pedro <pedro@z33dd.com>
2024-03-25 21:53:11 -0300

    Add tests and snapshots for jira-source
    Update jira-source with fixes
    
    Fix schemas and snapshots in source-jira
    
    Add source-jira to test pipeline
    
    Update snapshots
    
    source-jira: update snapshots
    
    Fix tests

---
+++ source-jira/tests/test_snapshots.py
@@ -0,0 +1,85 @@
+import json
+import subprocess
+
+
+def test_capture(request, snapshot):
+    snapshot.snapshot_dir = "tests/snapshots"
+
+    result = subprocess.run(
+        [
+            "flowctl",
+            "preview",
+            "--source",
+            request.fspath.dirname + "/../test.flow.yaml",
+            "--sessions",
+            "1",
+            "--delay",
+            "10s",
+            "--output",
+            "json",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+
+    stdout = []
+    for line in result.stdout.splitlines()[:50]:
+        stdout.append(json.loads(line))
+
+    snapshot.assert_match(
+        json.dumps(stdout, indent=2),
+        "capture.stdout.json",
+    )
+
+
+def test_discover(request, snapshot):
+    snapshot.snapshot_dir = "tests/snapshots"
+
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "discover",
+            "--source",
+            request.fspath.dirname + "/../test.flow.yaml",
+            "--output",
+            "json",
+            "--emit-raw",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+
+    stdout = []
+    for line in result.stdout.splitlines():
+        stdout.append(json.loads(line))
+
+    snapshot.assert_match(
+        json.dumps(stdout, indent=2),
+        "discover.stdout.json",
+    )
+
+
+def test_spec(request, snapshot):
+    snapshot.snapshot_dir = "tests/snapshots"
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "spec",
+            "--output",
+            "json",
+            "--source",
+            request.fspath.dirname + "/../test.flow.yaml",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+
+    snapshot.assert_match(
+        json.dumps(json.loads(result.stdout), indent=2),
+        "spec.stdout.json",
+    )
