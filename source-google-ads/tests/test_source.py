166b8db06802e68b9bc4f540011f74f2b392ef4d
Pedro <pedro@z33dd.com>
2024-03-27 13:38:46 -0300

    Format source-google-ads with ruff and isort

--- source-google-ads/tests/test_source.py
+++ source-google-ads/tests/test_source.py
@@ -10,32 +10,59 @@ import pytest
 from airbyte_cdk import AirbyteLogger
 from freezegun import freeze_time
 from google.ads.googleads.errors import GoogleAdsException
-from google.ads.googleads.v15.errors.types.authorization_error import AuthorizationErrorEnum
+from google.ads.googleads.v15.errors.types.authorization_error import (
+    AuthorizationErrorEnum,
+)
 from pendulum import today
 from source_google_ads.custom_query_stream import IncrementalCustomQuery
 from source_google_ads.google_ads import GoogleAds
 from source_google_ads.source import SourceGoogleAds
-from source_google_ads.streams import AdGroupAdReport, AdGroupLabels, ServiceAccounts, chunk_date_range
+from source_google_ads.streams import (
+    AdGroupAdReport,
+    AdGroupLabels,
+    ServiceAccounts,
+    chunk_date_range,
+)
 from source_google_ads.utils import GAQL
 
-from .common import MockErroringGoogleAdsClient, MockGoogleAdsClient, make_google_ads_exception
+from .common import (
+    MockErroringGoogleAdsClient,
+    MockGoogleAdsClient,
+    make_google_ads_exception,
+)
 
 
 @pytest.fixture
 def mock_account_info(mocker):
     mocker.patch(
         "source_google_ads.source.SourceGoogleAds.get_account_info",
-        Mock(return_value=[[{"customer.manager": False, "customer.time_zone": "Europe/Berlin", "customer.id": "8765"}]]),
+        Mock(
+            return_value=[
+                [
+                    {
+                        "customer.manager": False,
+                        "customer.time_zone": "Europe/Berlin",
+                        "customer.id": "8765",
+                    }
+                ]
+            ]
+        ),
     )
 
 
 @pytest.fixture()
 def stream_mock(mocker, config, customers):
     def mock(latest_record):
-        mocker.patch("source_google_ads.streams.GoogleAdsStream.read_records", Mock(return_value=[latest_record]))
+        mocker.patch(
+            "source_google_ads.streams.GoogleAdsStream.read_records",
+            Mock(return_value=[latest_record]),
+        )
         google_api = GoogleAds(credentials=config["credentials"])
         client = AdGroupAdReport(
-            start_date=config["start_date"], api=google_api, conversion_window_days=config["conversion_window_days"], customers=customers
+            start_date=config["start_date"],
+            api=google_api,
+            conversion_window_days=config["conversion_window_days"],
+            customers=customers,
         )
         return client
 
@@ -46,7 +73,11 @@ def stream_mock(mocker, config, customers):
 def mocked_gads_api(mocker):
     def mock(response=None, failure_code=1, failure_msg="", error_type=""):
         def side_effect_func():
-            raise make_google_ads_exception(failure_code=failure_code, failure_msg=failure_msg, error_type=error_type)
+            raise make_google_ads_exception(
+                failure_code=failure_code,
+                failure_msg=failure_msg,
+                error_type=error_type,
+            )
             yield
 
         side_effect = []
@@ -54,7 +85,10 @@ def mocked_gads_api(mocker):
             side_effect.append(response)
         if failure_msg or failure_code or error_type:
             side_effect.append(side_effect_func())
-        mocker.patch("source_google_ads.google_ads.GoogleAds.send_request", side_effect=side_effect)
+        mocker.patch(
+            "source_google_ads.google_ads.GoogleAds.send_request",
+            side_effect=side_effect,
+        )
 
     return mock
 
@@ -64,7 +98,9 @@ def mock_fields_meta_data():
     DataType = namedtuple("DataType", ["name"])
     Node = namedtuple("Node", ["data_type", "name", "enum_values", "is_repeated"])
     nodes = (
-        Node(DataType("RESOURCE_NAME"), "campaign.accessible_bidding_strategy", [], False),
+        Node(
+            DataType("RESOURCE_NAME"), "campaign.accessible_bidding_strategy", [], False
+        ),
         Node(
             DataType("ENUM"),
             "segments.ad_destination_type",
@@ -91,18 +127,28 @@ def mock_fields_meta_data():
         Node(
             DataType("ENUM"),
             "accessible_bidding_strategy.target_impression_share.location",
-            ["ABSOLUTE_TOP_OF_PAGE", "ANYWHERE_ON_PAGE", "TOP_OF_PAGE", "UNKNOWN", "UNSPECIFIED"],
+            [
+                "ABSOLUTE_TOP_OF_PAGE",
+                "ANYWHERE_ON_PAGE",
+                "TOP_OF_PAGE",
+                "UNKNOWN",
+                "UNSPECIFIED",
+            ],
             False,
         ),
         Node(DataType("STRING"), "campaign.name", [], False),
         Node(DataType("DOUBLE"), "campaign.optimization_score", [], False),
         Node(DataType("RESOURCE_NAME"), "campaign.resource_name", [], False),
-        Node(DataType("INT32"), "campaign.shopping_setting.campaign_priority", [], False),
+        Node(
+            DataType("INT32"), "campaign.shopping_setting.campaign_priority", [], False
+        ),
         Node(DataType("INT64"), "campaign.shopping_setting.merchant_id", [], False),
         Node(DataType("BOOLEAN"), "campaign_budget.explicitly_shared", [], False),
         Node(DataType("MESSAGE"), "bidding_strategy.enhanced_cpc", [], False),
     )
-    return Mock(get_fields_metadata=Mock(return_value={node.name: node for node in nodes}))
+    return Mock(
+        get_fields_metadata=Mock(return_value={node.name: node for node in nodes})
+    )
 
 
 # Test chunk date range without end date
@@ -112,7 +158,12 @@ def test_chunk_date_range_without_end_date():
     conversion_window = 0
     field = "date"
     response = chunk_date_range(
-        start_date=start_date_str, conversion_window=conversion_window, field=field, end_date=None, days_of_data_storage=None, range_days=1
+        start_date=start_date_str,
+        conversion_window=conversion_window,
+        field=field,
+        end_date=None,
+        days_of_data_storage=None,
+        range_days=1,
     )
     expected_response = [
         {"start_date": "2022-01-25", "end_date": "2022-01-26"},
@@ -130,7 +181,9 @@ def test_chunk_date_range():
     end_date = "2021-05-04"
     conversion_window = 14
     field = "date"
-    response = chunk_date_range(start_date, conversion_window, field, end_date, range_days=10)
+    response = chunk_date_range(
+        start_date, conversion_window, field, end_date, range_days=10
+    )
     assert [
         {"start_date": "2021-02-19", "end_date": "2021-02-28"},
         {"start_date": "2021-03-01", "end_date": "2021-03-10"},
@@ -156,8 +209,14 @@ def test_streams_count(config, mock_account_info):
         "is_metrics_in_query",
     ),
     (
-        ("SELECT customer.id, metrics.conversions, campaign.start_date FROM campaign", True),
-        ("SELECT segments.ad_destination_type, campaign.start_date, campaign.end_date FROM campaign", False),
+        (
+            "SELECT customer.id, metrics.conversions, campaign.start_date FROM campaign",
+            True,
+        ),
+        (
+            "SELECT segments.ad_destination_type, campaign.start_date, campaign.end_date FROM campaign",
+            False,
+        ),
     ),
 )
 def test_metrics_in_custom_query(query, is_metrics_in_query):
@@ -169,14 +228,24 @@ def test_metrics_in_custom_query(query, is_metrics_in_query):
     ("latest_record", "current_state", "expected_state"),
     (
         ({"segments.date": "2020-01-01"}, {}, {"segments.date": "2020-01-01"}),
-        ({"segments.date": "2020-02-01"}, {"segments.date": "2020-01-01"}, {"segments.date": "2020-02-01"}),
-        ({"segments.date": "2021-03-03"}, {"1234567890": {"segments.date": "2020-02-01"}}, {"segments.date": "2021-03-03"}),
+        (
+            {"segments.date": "2020-02-01"},
+            {"segments.date": "2020-01-01"},
+            {"segments.date": "2020-02-01"},
+        ),
+        (
+            {"segments.date": "2021-03-03"},
+            {"1234567890": {"segments.date": "2020-02-01"}},
+            {"segments.date": "2021-03-03"},
+        ),
     ),
 )
 def test_updated_state(stream_mock, latest_record, current_state, expected_state):
     mocked_stream = stream_mock(latest_record=latest_record)
     mocked_stream.state = current_state
-    for _ in mocked_stream.read_records(sync_mode=Mock(), stream_slice={"customer_id": "1234567890"}):
+    for _ in mocked_stream.read_records(
+        sync_mode=Mock(), stream_slice={"customer_id": "1234567890"}
+    ):
         pass
     assert mocked_stream.state["1234567890"] == expected_state
 
@@ -295,7 +364,14 @@ WHERE segments.date BETWEEN '1980-01-01' AND '2000-01-01'
 )
 def test_insert_date(original_query, expected_query):
     expected_query = re.sub(r"\s+", " ", expected_query.strip())
-    assert str(IncrementalCustomQuery.insert_segments_date_expr(GAQL.parse(original_query), "1980-01-01", "2000-01-01")) == expected_query
+    assert (
+        str(
+            IncrementalCustomQuery.insert_segments_date_expr(
+                GAQL.parse(original_query), "1980-01-01", "2000-01-01"
+            )
+        )
+        == expected_query
+    )
 
 
 def test_get_json_schema_parse_query(mock_fields_meta_data, customers):
@@ -315,7 +391,9 @@ def test_get_json_schema_parse_query(mock_fields_meta_data, customers):
         "segments.date",
     ]
 
-    instance = stream_instance(query=query, api_mock=mock_fields_meta_data, customers=customers)
+    instance = stream_instance(
+        query=query, api_mock=mock_fields_meta_data, customers=customers
+    )
     final_schema = instance.get_json_schema()
     schema_keys = final_schema["properties"]
     assert set(schema_keys) == set(final_fields)  # test 1
@@ -339,7 +417,12 @@ def test_get_json_schema_parse_query_with_end_date(mock_fields_meta_data, custom
         "segments.date",
     ]
 
-    instance = stream_instance(query=query, api_mock=mock_fields_meta_data, end_date="2021-04-04", customers=customers)
+    instance = stream_instance(
+        query=query,
+        api_mock=mock_fields_meta_data,
+        end_date="2021-04-04",
+        customers=customers,
+    )
     final_schema = instance.get_json_schema()
     schema_keys = final_schema["properties"]
     assert set(schema_keys) == set(final_fields)  # test 1
@@ -378,7 +461,9 @@ def test_google_type_conversion(mock_fields_meta_data, customers):
             bidding_strategy.enhanced_cpc
         FROM campaign
         """
-    instance = stream_instance(query=query, api_mock=mock_fields_meta_data, customers=customers)
+    instance = stream_instance(
+        query=query, api_mock=mock_fields_meta_data, customers=customers
+    )
     final_schema = instance.get_json_schema()
     schema_properties = final_schema.get("properties")
     for prop, value in schema_properties.items():
@@ -466,13 +551,21 @@ def test_check_connection_should_fail_when_api_call_fails(mocker):
         },
     )
     assert not check_successful
-    assert message.startswith("Unable to connect to Google Ads API with the provided configuration")
+    assert message.startswith(
+        "Unable to connect to Google Ads API with the provided configuration"
+    )
 
 
 def test_end_date_is_not_in_the_future(customers):
     source = SourceGoogleAds()
     config = source.get_incremental_stream_config(
-        None, {"end_date": today().add(days=1).to_date_string(), "conversion_window_days": 14, "start_date": "2020-01-23"}, customers
+        None,
+        {
+            "end_date": today().add(days=1).to_date_string(),
+            "conversion_window_days": 14,
+            "start_date": "2020-01-23",
+        },
+        customers,
     )
     assert config.get("end_date") == today().to_date_string()
 
@@ -497,23 +590,34 @@ def test_invalid_custom_query_handled(mocked_gads_api, config):
 @pytest.mark.parametrize(
     ("cls", "error", "failure_code", "raise_expected"),
     (
-        (AdGroupLabels, "authorization_error", AuthorizationErrorEnum.AuthorizationError.CUSTOMER_NOT_ENABLED, False),
+        (
+            AdGroupLabels,
+            "authorization_error",
+            AuthorizationErrorEnum.AuthorizationError.CUSTOMER_NOT_ENABLED,
+            False,
+        ),
         (AdGroupLabels, "internal_error", 1, True),
         (ServiceAccounts, "authentication_error", 1, True),
         (ServiceAccounts, "internal_error", 1, True),
     ),
 )
-def test_read_record_error_handling(config, customers, mocked_gads_api, cls, error, failure_code, raise_expected):
+def test_read_record_error_handling(
+    config, customers, mocked_gads_api, cls, error, failure_code, raise_expected
+):
     error_msg = "Some unexpected error"
     mocked_gads_api(failure_code=failure_code, failure_msg=error_msg, error_type=error)
     google_api = GoogleAds(credentials=config["credentials"])
     stream = cls(api=google_api, customers=customers)
     if raise_expected:
         with pytest.raises(GoogleAdsException):
-            for _ in stream.read_records(sync_mode=Mock(), stream_slice={"customer_id": "1234567890"}):
+            for _ in stream.read_records(
+                sync_mode=Mock(), stream_slice={"customer_id": "1234567890"}
+            ):
                 pass
     else:
-        for _ in stream.read_records(sync_mode=Mock(), stream_slice={"customer_id": "1234567890"}):
+        for _ in stream.read_records(
+            sync_mode=Mock(), stream_slice={"customer_id": "1234567890"}
+        ):
             pass
 
 
