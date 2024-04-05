d415db835b6a02b752024ea067c77c7d76a3e939
Will Baker <wbaker@gmail.com>
2024-04-05 13:00:10 -0400

    source-mongodb: incremental backfills
    Implements incremental backfills for `source-mongodb`.
    
    The strategy is essentially this: If there are any MongoDB collections that need backfilled, first
    "catch up" all of the tracked change streams to make sure we don't lose our position, and then work
    on backfilling from the collections for a while. After some time of backfilling, catch up the change
    streams again. Repeat this process until all collections have been backfilled, and then read from
    the change streams forever. This is fairly similar to how incremental backfills work for SQL
    captures.
    
    An important part of this strategy is being able to know when we are caught up on a change stream.
    For this, the non-blocking `TryNext` method of the change stream cursor is used. If there are no
    documents available, `TryNext` returns right away, and we get a "post batch resume token" to use for
    resuming the stream. This token is not associated with any particular document ID, and represents
    the particular change stream's position in reading the global oplog. `TryNext` may return documents
    and indeed on a high-traffic streams it may never _not_ return documents. In this case, we establish
    a high watermark using the cluster operation time at the start of catching up the streams. If we
    encounter a document with a later operation time than this, we know that we are reasonably well
    caught up.
    
    Using this strategy, we no longer need access to read the `local.oplog.rs` collection for checking
    the boundaries of the oplog.

---
+++ source-mongodb/emitter.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	boilerplate "github.com/estuary/connectors/source-boilerplate"
+	"github.com/segmentio/encoding/json"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// emitter provides synchronization for emitted documents and checkpoints, as well as updating the
+// capture's internal state as it progresses through backfills and change streaming.
+type emitter struct {
+	events chan emitEvent
+	errors chan error
+	wg     sync.WaitGroup
+}
+
+// emitEvent is either a single document + checkpoint from a streamEvent, or potentially multiple
+// documents + a checkpoint from a backfillEvent.
+type emitEvent interface {
+	isEvent()
+}
+
+type streamEvent struct {
+	doc         map[string]any
+	stateUpdate captureState
+	binding     bindingInfo
+}
+
+func (streamEvent) isEvent() {}
+
+type backfillEvent struct {
+	docs        []primitive.M
+	stateUpdate resourceState
+	binding     bindingInfo
+}
+
+func (backfillEvent) isEvent() {}
+
+// startEmitter is called once to initialize the emitter.
+func (c *capture) startEmitter(ctx context.Context) {
+	c.emitter.events = make(chan emitEvent)
+	c.emitter.errors = make(chan error)
+
+	c.emitter.wg.Add(1)
+	go c.emitWorker(ctx)
+}
+
+// flushEmitter is called whenever the events channel should be completely emptied. This ensures
+// that there will not be any concurrent writes to the capture's internal state until more events
+// are sent to the emitter. A call to flushEmitter leaves the emitter reader to continue receiving
+// emitted events.
+func (c *capture) flushEmitter(ctx context.Context) error {
+	close(c.emitter.events)
+	c.emitter.wg.Wait()
+
+	select {
+	case err := <-c.emitter.errors:
+		return err
+	default:
+		c.emitter.events = make(chan emitEvent)
+
+		c.emitter.wg.Add(1)
+		go c.emitWorker(ctx)
+
+		return nil
+	}
+}
+
+func (c *capture) emitWorker(ctx context.Context) {
+	defer c.emitter.wg.Done()
+
+	for {
+		select {
+		case event, ok := <-c.emitter.events:
+			if !ok {
+				// Channel is closed because of flushEmitter.
+				return
+			}
+
+			switch ev := event.(type) {
+			case streamEvent:
+				if err := c.handleStreamEvent(ev); err != nil {
+					c.emitter.errors <- err
+					return
+				}
+			case backfillEvent:
+				if err := c.handleBackfillEvent(ev); err != nil {
+					c.emitter.errors <- err
+					return
+				}
+			}
+		case <-ctx.Done():
+			c.emitter.errors <- ctx.Err()
+			return
+		}
+	}
+}
+
+// emitEvent is a convenience method for sending events to the emitter events channel, while also
+// checking for emitter errors or cancellation signals.
+func (c *capture) emitEvent(ctx context.Context, e emitEvent) error {
+	select {
+	case err := <-c.emitter.errors:
+		return err
+	case c.emitter.events <- e:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func (c *capture) handleStreamEvent(event streamEvent) error {
+	if event.stateUpdate.GlobalResumeToken != nil && event.stateUpdate.DatabaseResumeTokens != nil {
+		return fmt.Errorf("application error: global resume token and database-specific resume token were both set")
+	} else if event.stateUpdate.GlobalResumeToken != nil {
+		c.state.GlobalResumeToken = event.stateUpdate.GlobalResumeToken
+	} else if event.stateUpdate.DatabaseResumeTokens != nil {
+		for db, tok := range event.stateUpdate.DatabaseResumeTokens {
+			c.state.DatabaseResumeTokens[db] = tok
+		}
+	} else {
+		return fmt.Errorf("application error: must set either GlobalResumeToken or DatabaseResumeTokens")
+	}
+
+	if event.doc != nil {
+		// There may not be a document if this is a checkpoint-only for a post-batch resume token.
+		if docJson, err := json.Marshal(event.doc); err != nil {
+			return fmt.Errorf("serializing stream document: %w", err)
+		} else if err := c.output.Documents(event.binding.index, docJson); err != nil {
+			return fmt.Errorf("outputting stream document: %w", err)
+		}
+	}
+
+	if checkpointJson, err := json.Marshal(event.stateUpdate); err != nil {
+		return fmt.Errorf("encoding stream checkpoint: %w", err)
+	} else if err := c.output.Checkpoint(checkpointJson, true); err != nil {
+		return fmt.Errorf("outputting stream checkpoint: %w", err)
+	}
+
+	return nil
+}
+
+func (c *capture) handleBackfillEvent(event backfillEvent) error {
+	var sk = event.binding.stateKey
+	var docs = make([]json.RawMessage, 0, len(event.docs))
+
+	checkpoint := captureState{
+		Resources: map[boilerplate.StateKey]resourceState{
+			sk: event.stateUpdate,
+		},
+	}
+
+	state := c.state.Resources[sk]
+	state.Backfill.LastId = event.stateUpdate.Backfill.LastId
+	state.Backfill.Done = event.stateUpdate.Backfill.Done
+	c.state.Resources[sk] = state
+
+	for _, doc := range event.docs {
+		raw, err := json.Marshal(doc)
+		if err != nil {
+			return fmt.Errorf("serializing backfill document: %w", err)
+		}
+
+		docs = append(docs, raw)
+	}
+
+	if len(docs) > 0 {
+		// There may be no documents on the final "backfill complete" checkpoint.
+		if err := c.output.Documents(event.binding.index, docs...); err != nil {
+			return fmt.Errorf("outputting backfill documents: %w", err)
+		}
+	}
+
+	if checkpointJson, err := json.Marshal(checkpoint); err != nil {
+		return fmt.Errorf("encoding backfill checkpoint: %w", err)
+	} else if err := c.output.Checkpoint(checkpointJson, true); err != nil {
+		return fmt.Errorf("outputting backfill checkpoint: %w", err)
+	}
+
+	return nil
+}
--- source-mongodb/pull_test.go
+++ source-mongodb/pull_test.go
@@ -3,72 +3,14 @@ package main
 import (
 	"math"
 	"testing"
-	"time"
 
-	boilerplate "github.com/estuary/connectors/source-boilerplate"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func TestCheckBackfillState(t *testing.T) {
-	// Starting fresh, with an empty oplog
-	var state = &captureState{}
-	var oldestOplogEntry time.Time
-
-	var updated, restartReason = checkBackfillState(state, oldestOplogEntry)
-	require.True(t, updated)
-	require.Equal(t, "", restartReason)
-	require.True(t, *state.OplogEmptyBeforeBackfill)
-
-	// Starting fresh with a non-empty oplog
-	state = &captureState{}
-	oldestOplogEntry = time.Unix(5000, 0).UTC()
-	updated, restartReason = checkBackfillState(state, oldestOplogEntry)
-	require.True(t, updated)
-	require.Equal(t, "", restartReason)
-	require.False(t, *state.OplogEmptyBeforeBackfill)
-
-	// Resuming an in progress backfill, which started after the oldest oplog
-	// event and had previously observed the non-empty oplog
-	state.Resources = map[boilerplate.StateKey]resourceState{
-		"testdb.testcollection": {
-			Backfill: backfillState{
-				Done:      false,
-				StartedAt: time.Unix(6000, 0),
-			},
-		},
-	}
-	updated, restartReason = checkBackfillState(state, oldestOplogEntry)
-	require.False(t, updated)
-	require.Equal(t, "", restartReason)
-	require.False(t, *state.OplogEmptyBeforeBackfill)
-
-	// Resuming an in progress backfill, which started before the oldest oplog event
-	oldestOplogEntry = time.Unix(8000, 0).UTC()
-	updated, restartReason = checkBackfillState(state, oldestOplogEntry)
-	require.False(t, updated)
-	require.Equal(t, "the oldest event in the mongodb oplog (1970-01-01 02:13:20 +0000 UTC) is more recent than the starting time of the backfill", restartReason)
-
-	// Resuming an in progress backfill, which had previously observed a non-empty oplog
-	// and now observes an empty oplog
-	oldestOplogEntry = time.Time{}
-	updated, restartReason = checkBackfillState(state, oldestOplogEntry)
-	require.False(t, updated)
-	require.Equal(t, "previously non-empty change stream is now empty", restartReason)
-
-	// Resuming an in progress backfill, which had previously observed an empty oplog
-	// and now again observes it being empty
-	var superHighTechTrue = true
-	state.OplogEmptyBeforeBackfill = &superHighTechTrue
-	updated, restartReason = checkBackfillState(state, oldestOplogEntry)
-	require.False(t, updated)
-	require.Equal(t, "", restartReason)
-	require.True(t, *state.OplogEmptyBeforeBackfill)
-}
-
 func TestSanitizeDocument(t *testing.T) {
 	var cases = map[string][]map[string]interface{}{
-		"primitive": []map[string]interface{}{
+		"primitive": {
 			map[string]interface{}{
 				"x":           math.NaN(),
 				"y":           "untouched",
@@ -88,7 +30,7 @@ func TestSanitizeDocument(t *testing.T) {
 				"lowDate":     primitive.DateTime(minTimeMilli),
 			},
 		},
-		"nested": []map[string]interface{}{
+		"nested": {
 			map[string]interface{}{
 				"obj": map[string]interface{}{
 					"x":           math.NaN(),
@@ -112,7 +54,7 @@ func TestSanitizeDocument(t *testing.T) {
 				},
 			},
 		},
-		"array": []map[string]interface{}{
+		"array": {
 			map[string]interface{}{
 				"arr": []interface{}{math.NaN(), "untouched", 1, math.Inf(1), math.Inf(-1), primitive.DateTime(maxTimeMilli + 1), primitive.DateTime(minTimeMilli - 1)},
 			},
