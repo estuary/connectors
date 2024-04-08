4f2c2d97f03d72e0615bc64feb40f0a29c106dbb
Pedro <pedro@z33dd.com>
2024-04-08 13:48:12 -0300

    source-azure-blob-storage:  Add tests
    Add unit test for AzureBlobStore_Read function
    
    Refactor tests and update dependencies
    
    Add tests for getConfigSchema function

---
+++ source-azure-blob-storage/snapshot_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+	"github.com/stretchr/testify/assert"
+)
+
+// How to update the snapshots:
+// UPDATE_SNAPS=true go test ./...
+
+func TestCapture(t *testing.T) {
+	t.Skip("Skipping test because flowctl is not supporting go connectors yet.")
+	command := "flowctl"
+
+	args := []string{
+		"preview",
+		"--source",
+		"test.flow.yaml",
+		"--sessions",
+		"1",
+		"--delay",
+		"10s",
+	}
+
+	cmd := exec.Command(command, args...)
+	stdout, err := cmd.Output()
+	assert.NoError(t, err)
+
+	snaps.WithConfig(
+		snaps.Dir("snapshots"),
+		snaps.Filename("config_schema"),
+	).MatchSnapshot(t, string(stdout))
+}
+
+func TestDiscover(t *testing.T) {
+	t.Skip("Skipping test because flowctl is not supporting go connectors yet.")
+	command := "flowctl"
+
+	args := []string{
+		"raw",
+		"discover",
+		"--source",
+		"test.flow.yaml",
+		"-o",
+		"json",
+		"--emit-raw",
+	}
+
+	cmd := exec.Command(command, args...)
+	stdout, err := cmd.Output()
+	assert.NoError(t, err)
+
+	snaps.WithConfig(
+		snaps.Dir("snapshots"),
+		snaps.Filename("config_schema"),
+	).MatchJSON(t, string(stdout))
+}
+
+func TestSpec(t *testing.T) {
+	t.Skip("Skipping test because flowctl is not supporting go connectors yet.")
+	command := "flowctl"
+
+	args := []string{
+		"raw",
+		"spec",
+		"--source",
+		"test.flow.yaml",
+	}
+
+	cmd := exec.Command(command, args...)
+	stdout, err := cmd.Output()
+	assert.NoError(t, err)
+
+	snaps.WithConfig(
+		snaps.Dir("snapshots"),
+		snaps.Filename("config_schema"),
+	).MatchJSON(t, string(stdout))
+}
