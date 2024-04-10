f72669f1ca8b429bbc2a51a42b609a354bad8c33
Pedro <pedro@z33dd.com>
2024-04-10 14:59:00 -0300

    Update Azure Blob Storage source configuration and tests

--- source-azure-blob-storage/main_test.go
+++ source-azure-blob-storage/main_test.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"os"
 	"testing"
 
+	"github.com/estuary/connectors/filesource"
 	"github.com/gkampitakis/go-snaps/snaps"
 	"github.com/stretchr/testify/assert"
 )
 
 const connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
 
-func TestAzureBlobStore_Read(t *testing.T) {
-	// Create a test instance of the azureBlobStore
+func getTestConfig(t *testing.T) config {
+	if os.Getenv("TEST_DATABASE") != "yes" {
+		t.Skipf("skipping %q: ${TEST_DATABASE} != \"yes\"", t.Name())
+		return config{}
+	}
 	cfg := config{
 		Credentials: &credentials{
 			ConnectionString: connectionString,
@@ -18,10 +26,17 @@ func TestAzureBlobStore_Read(t *testing.T) {
 		StorageAccountName: "devstoreaccount1",
 		ContainerName:      "example",
 	}
+	return cfg
+}
+
+func TestAzureBlobStore_newAzureBlobStore(t *testing.T) {
+	// Create a test instance of the azureBlobStore
+	cfg := getTestConfig(t)
 	az, err := newAzureBlobStore(cfg) // Pass cfg directly instead of its address
 	assert.NoError(t, err)
 	assert.NotNil(t, az)
 }
+
 func TestGetConfigSchema(t *testing.T) {
 	parserJsonSchema := []byte(`{"type": "object", "properties": {"name": {"type": "string"}}}`)
 
@@ -31,4 +46,60 @@ func TestGetConfigSchema(t *testing.T) {
 		snaps.Dir("snapshots"),
 		snaps.Filename("config_schema"),
 	).MatchJSON(t, string(result))
+}
+
+func TestAzureBlobStore_List(t *testing.T) {
+	// Create a test instance of the azureBlobStore
+	cfg := getTestConfig(t)
+
+	az, err := newAzureBlobStore(cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, az)
+
+	// Create a test context
+	ctx := context.TODO()
+
+	// Create a test query
+	query := filesource.Query{}
+	listing, err := az.List(ctx, query)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, listing)
+
+	file, err := listing.Next()
+	assert.NoError(t, err)
+	assert.NotNil(t, file)
+
+	fmt.Println(file.Path)
+}
+func TestAzureBlobStore_Read(t *testing.T) {
+	// Create a test instance of the azureBlobStore
+	cfg := getTestConfig(t)
+
+	az, err := newAzureBlobStore(cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, az)
+
+	// Create a test context
+	ctx := context.TODO()
+
+	// Create a test object
+	obj := filesource.ObjectInfo{
+		Path: "l00cmqdvppy01-4060447306.jpg",
+	}
+
+	// Call the Read function
+	reader, info, err := az.Read(ctx, obj)
+	assert.NoError(t, err)
+	assert.NotNil(t, reader)
+	assert.Equal(t, obj, info)
+
+	// Read from the reader
+	data, err := io.ReadAll(reader)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, data)
+
+	// Close the reader
+	err = reader.Close()
+	assert.NoError(t, err)
 }
