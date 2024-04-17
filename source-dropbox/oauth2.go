5dff110644d6a89d4a8ee71bea1a7cc1dbedc540
Pedro <pedro@z33dd.com>
2024-04-17 17:14:06 -0300

    Refactor OAuth2Spec function in oauth2.go and made the necessary changes to the connector schema.

--- source-dropbox/oauth2.go
+++ source-dropbox/oauth2.go
@@ -1,53 +1,34 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
+	"strings"
 
-	"github.com/dropbox/dropbox-sdk-go-unofficial/v6/dropbox"
-	"golang.org/x/oauth2"
+	pf "github.com/estuary/flow/go/protocols/flow"
 )
 
-// TODO: Replace the client ID, client secret and redirect URL with the real ones
-var (
-	dropboxConfig = &oauth2.Config{
-		Scopes: []string{
-			"files.metadata.read",
-			"files.content.read",
-			// "account_info.read",  // Not sure if we need this
-		},
-		Endpoint:     dropbox.OAuthEndpoint(""),
-		ClientID:     "client ID goes here",
-		ClientSecret: "client secret goes here",
-		RedirectURL:  "http://localhost:8080/oauth2/callback",
-	}
-)
+// Most of this code was converted from the Google Auth specification updating with
+// the info on https://developers.dropbox.com/oauth-guide
+func OAuth2Spec() *pf.OAuth2 {
+	// Docs: https://www.dropbox.com/developers/documentation/http/documentation#oauth2-authorize
+	AUTH_URL_TEMPLATE_FORMAT_STR := "https://www.dropbox.com/oauth2/authorize?client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}&response_type=code&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}&state={{{ state }}}&scope={{#urlencode}}%s{{/urlencode}}"
 
-func GetOAuth2HTTPClient(ctx context.Context, config *oauth2.Config) (*http.Client, error) {
-
-	// token := new(oauth2.Token)
-	// tokenSource := config.TokenSource(ctx, token)
-
-	// use PKCE to protect against CSRF attacks
-	// https://www.ietf.org/archive/id/draft-ietf-oauth-security-topics-22.html#name-countermeasures-6
-	verifier := oauth2.GenerateVerifier()
-
-	// TODO: Handle the code in the UI
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := config.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
-
-	var code string
-
-	tok, err := config.Exchange(ctx, code, oauth2.VerifierOption(verifier))
-	if err != nil {
-		log.Fatal(err)
+	scopes := []string{
+		"files.metadata.read,files.content.read",
+		"files.content.read",
+		// "account_info.read",  // Not sure if we need this
 	}
 
-	client := config.Client(ctx, tok)
+	spec := pf.OAuth2{
+		Provider:        "dropbox",
+		AuthUrlTemplate: fmt.Sprintf(AUTH_URL_TEMPLATE_FORMAT_STR, strings.Join(scopes, " ")),
+		// Dropbox Docs: you’ll need to include `token_access_type=offline`` as a parameter on your
+		// Authorization URL in order to return a refresh_token inside your access token payload.
+		AccessTokenUrlTemplate:     "https://api.dropboxapi.com/oauth2/token?token_access_type=offline",
+		AccessTokenBody:            `{"grant_type": "authorization_code", "client_id": "{{{ client_id }}}", "client_secret": "{{{ client_secret }}}", "redirect_uri": "{{{ redirect_uri }}}", "code": "{{{ code }}}"}`,
+		AccessTokenResponseJsonMap: map[string]json.RawMessage{"refresh_token": json.RawMessage(`"/refresh_token"`)},
+	}
 
-	return client, nil
+	return &spec
 }
