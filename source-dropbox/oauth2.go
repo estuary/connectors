30f42773b480a7eeecf0e30e8c146f58e2104fd6
Pedro <pedro@z33dd.com>
2024-04-18 14:24:08 -0300

    Refactor OAuth2Spec function and add Credentials struct for Dropbox API authentication

--- source-dropbox/oauth2.go
+++ source-dropbox/oauth2.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"strings"
+	"time"
 
 	pf "github.com/estuary/flow/go/protocols/flow"
+	"golang.org/x/oauth2"
 )
+
+func GetScopes() []string {
+	return []string{
+		"files.metadata.read,files.content.read",
+		"files.content.read",
+		// "account_info.read",  // Not sure if we need this
+	}
+}
 
 // Most of this code was converted from the Google Auth specification updating with
 // the info on https://developers.dropbox.com/oauth-guide
@@ -14,11 +26,7 @@ func OAuth2Spec() *pf.OAuth2 {
 	// Docs: https://www.dropbox.com/developers/documentation/http/documentation#oauth2-authorize
 	AUTH_URL_TEMPLATE_FORMAT_STR := "https://www.dropbox.com/oauth2/authorize?client_id={{#urlencode}}{{{ client_id }}}{{/urlencode}}&response_type=code&redirect_uri={{#urlencode}}{{{ redirect_uri }}}{{/urlencode}}&state={{{ state }}}&scope={{#urlencode}}%s{{/urlencode}}"
 
-	scopes := []string{
-		"files.metadata.read,files.content.read",
-		"files.content.read",
-		// "account_info.read",  // Not sure if we need this
-	}
+	scopes := GetScopes()
 
 	spec := pf.OAuth2{
 		Provider:        "dropbox",
@@ -31,4 +39,52 @@ func OAuth2Spec() *pf.OAuth2 {
 	}
 
 	return &spec
+}
+
+type Credentials struct {
+	AuthType     string `json:"auth_type"`
+	ClientID     string `json:"client_id,omitempty"`
+	ClientSecret string `json:"client_secret,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+}
+
+func (c *Credentials) Validate() error {
+	requiredProperties := [][]string{
+		{"AuthType", c.AuthType},
+	}
+	for _, req := range requiredProperties {
+		if req[1] == "" {
+			return fmt.Errorf("missing '%s'", req[0])
+		}
+	}
+	return nil
+}
+
+func (c *Credentials) GetClient(ctx context.Context) (*http.Client, error) {
+	if err := c.Validate(); err != nil {
+		return nil, err
+	}
+
+	oauthConfig := &oauth2.Config{
+		ClientID:     c.ClientID,
+		ClientSecret: c.ClientSecret,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://www.dropbox.com/oauth2/authorize",
+			TokenURL: "https://api.dropboxapi.com/oauth2/token",
+		},
+		Scopes: GetScopes(),
+	}
+
+	token := &oauth2.Token{
+		RefreshToken: c.RefreshToken,
+		Expiry:       time.Now(),
+	}
+	tokenSource := oauthConfig.TokenSource(ctx, token)
+	token, err := tokenSource.Token()
+	if err != nil {
+		return nil, err
+	}
+	client := oauthConfig.Client(ctx, token)
+
+	return client, nil
 }
