f5770096c1abd43d1a46ad71cc2ed098db5fb9f0
Mahdi Dibaiee <mdibaiee@pm.me>
2024-04-26 11:28:02 +0100

    source-oracle: capture tests, meta.source

--- source-oracle/tests/db_seeds/insert_test_all_types.sql
+++ source-oracle/tests/db_seeds/insert_test_all_types.sql
@@ -1,31 +1,3 @@
-CREATE TABLE test_all_types(
-	id integer PRIMARY KEY,
-	nvchar2 NVARCHAR2(4000),
-	vchar2 VARCHAR2(4000),
-	single_nchar NCHAR,
-	vchar VARCHAR(4000),
-	num NUMBER (38, 9),
-	
-	small_int SMALLINT,
-	
-	double_precision DOUBLE PRECISION,
-	float_126 FLOAT(126),
-	real_num REAL,
-	
-	datetime DATE,
-	ts TIMESTAMP,
-	ts_nine TIMESTAMP(9),
-	
-	ts_tz TIMESTAMP WITH TIME ZONE,
-	ts_tz_nine TIMESTAMP(9) WITH TIME ZONE,
-	
-	ts_local_tz TIMESTAMP WITH LOCAL TIME ZONE,
-	ts_local_tz_nine TIMESTAMP(9) WITH LOCAL TIME ZONE,
-
-	interval_year INTERVAL YEAR(4) TO MONTH,
-	interval_day INTERVAL DAY TO SECOND
-)
-
 INSERT INTO test_all_types(
 	id,
 	nvchar2,
