79b8fe70f5d6780a1b26e87be1941cb606a6c338
Mahdi Dibaiee <mdibaiee@pm.me>
2024-05-13 21:12:37 +0100

    use gsed on mac

--- tests/materialize/materialize-databricks/checks.sh
+++ tests/materialize/materialize-databricks/checks.sh
@@ -1,2 +1,6 @@
 # cleanup the snapshot by replacing uuids with a placeholder so that the test is reproducible
-sed -i='' 's/.\{36\}\.json/<uuid>/g' ${SNAPSHOT}
+SED_CMD="sed"
+if [ "$(uname -s)" = "Darwin" ]; then
+  SED_CMD="gsed"
+fi
+$SED_CMD -i'' 's/.\{36\}\.json/<uuid>/g' ${SNAPSHOT}
--- tests/materialize/materialize-snowflake/checks.sh
+++ tests/materialize/materialize-snowflake/checks.sh
@@ -1,4 +1,8 @@
 # cleanup the snapshot by replacing uuids with a placeholder so that the test is reproducible
-sed -i'' 's/@flow_v1\/.\{36\}/<uuid>/g' ${SNAPSHOT}
-sed -i'' 's/"Path": ".*"/"Path": "<uuid>"/g' ${SNAPSHOT}
-sed -i'' 's/"PipeStartTime": ".\{1,\}"/"PipeStartTime": "<timestamp>"/g' ${SNAPSHOT}
+SED_CMD="sed"
+if [ "$(uname -s)" = "Darwin" ]; then
+  SED_CMD="gsed"
+fi
+$SED_CMD -i'' 's/@flow_v1\/.\{36\}/<uuid>/g' ${SNAPSHOT}
+$SED_CMD -i'' 's/"Path": ".*"/"Path": "<uuid>"/g' ${SNAPSHOT}
+$SED_CMD -i'' 's/"PipeStartTime": ".\{1,\}"/"PipeStartTime": "<timestamp>"/g' ${SNAPSHOT}
