ecf246f809ed83463c4516fcc66737dfe35f4a0b
Will Donnelly <will@estuary.dev>
2024-05-23 12:25:39 -0500

    source-firestore: Implement the 'Restart Cursor Path' resource config
    This allows us to optimize backfills to no longer pull down the entire
    contents of a collection when consistency is lost, instead we will
    order the backfill according to the specified property path and with
    a `StartAfter` value which was recently observed via replication.

--- source-firestore/main.go
+++ source-firestore/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"strings"
 	"time"
 
 	schemagen "github.com/estuary/connectors/go/schema-gen"
@@ -21,10 +22,13 @@ const (
 )
 
 type resource struct {
-	Path          string       `json:"path" jsonschema:"title=Path to Collection,description=Supports parent/*/nested to capture all nested collections of parent's children"`
-	BackfillMode  backfillMode `json:"backfillMode" jsonschema:"title=Backfill Mode,description=Configures the handling of data already in the collection. Refer to go.estuary.dev/source-firestore for details or just stick with 'async'. Has no effect if changed after a binding is added.,enum=async,enum=none,enum=sync"`
-	InitTimestamp string       `json:"initTimestamp,omitempty" jsonschema:"Initial Replication Timestamp,description=Optionally overrides the initial replication timestamp (which is either Zero or Now depending on the backfill mode). Has no effect if changed after a binding is added."`
+	Path              string       `json:"path" jsonschema:"title=Path to Collection,description=Supports parent/*/nested to capture all nested collections of parent's children"`
+	BackfillMode      backfillMode `json:"backfillMode" jsonschema:"title=Backfill Mode,description=Configures the handling of data already in the collection. Refer to go.estuary.dev/source-firestore for details or just stick with 'async'. Has no effect if changed after a binding is added.,enum=async,enum=none,enum=sync"`
+	InitTimestamp     string       `json:"initTimestamp,omitempty" jsonschema:"Initial Replication Timestamp,description=Optionally overrides the initial replication timestamp (which is either Zero or Now depending on the backfill mode). Has no effect if changed after a binding is added."`
+	RestartCursorPath string       `json:"restartCursorPath,omitempty" jsonschema:"Restart Cursor Property,description=Optionally specifies a JSON pointer to some document property which increases monotonically and can be used as a restart cursor to optimize backfill behavior when streaming consistency is lost. Generally this only matters for collections with very high write volumes.,pattern=^(/([^/~]|~[01])+)*$"`
 }
+
+var jsonPointerRegexp = regexp.MustCompile(`^(/([^/~]|~[01])+)*$`)
 
 func (r resource) Validate() error {
 	if r.Path == "" {
@@ -38,7 +42,21 @@ func (r resource) Validate() error {
 			return fmt.Errorf("invalid initTimestamp value %q: %w", r.InitTimestamp, err)
 		}
 	}
+	if r.RestartCursorPath != "" {
+		if !jsonPointerRegexp.MatchString(r.RestartCursorPath) {
+			return fmt.Errorf("invalid restart cursor path %q: path must be a JSON pointer", r.RestartCursorPath)
+		}
+	}
 	return nil
+}
+
+func parseJSONPointer(ptr string) []string {
+	var path = strings.Split(strings.TrimPrefix(ptr, "/"), "/") // Split on slashes
+	for idx := range path {
+		path[idx] = strings.ReplaceAll(path[idx], "~1", "/") // Unescape encoded slashes
+		path[idx] = strings.ReplaceAll(path[idx], "~0", "~") // Unescape encoded tildes
+	}
+	return path
 }
 
 type config struct {
