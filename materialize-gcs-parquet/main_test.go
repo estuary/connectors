39ded7212e35e35e984c487aae78f5e7a18bec08
Will Baker <wbaker@gmail.com>
2024-05-31 15:22:24 -0400

    materialize-gcs-parquet: new connector
    Uses the filesink framework and streaming Parquet encoder to materialize Parquet files to GCS.

---
+++ materialize-gcs-parquet/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/bradleyjkemp/cupaloy"
+	pm "github.com/estuary/flow/go/protocols/materialize"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSpec(t *testing.T) {
+	var resp, err = driver.
+		Spec(context.Background(), &pm.Request_Spec{})
+	require.NoError(t, err)
+
+	formatted, err := json.MarshalIndent(resp, "", "  ")
+	require.NoError(t, err)
+
+	cupaloy.SnapshotT(t, formatted)
+}
