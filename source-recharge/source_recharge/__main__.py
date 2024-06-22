7ee73b45d2d27e4077af3bc7520eab9a1c4da619
Luishfs <luish1980@hotmail.com>
2024-06-21 21:19:35 -0300

    source-recharge: applying airbyte-to-flow patches & schema fixes
    also removing unused files

---
+++ source-recharge/source_recharge/__main__.py
@@ -0,0 +1,15 @@
+import estuary_cdk.pydantic_polyfill  # Must be first.
+
+import asyncio
+import urllib
+from estuary_cdk import shim_airbyte_cdk, flow
+from source_recharge import SourceRecharge
+
+
+asyncio.run(
+    shim_airbyte_cdk.CaptureShim(
+        delegate=SourceRecharge(),
+        oauth2=None,
+        schema_inference=True,
+    ).serve()
+)
