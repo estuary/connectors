2e193412a71c857b1933af5f23fbfdac37cf2de2
Will Baker <wbaker@gmail.com>
2024-07-08 17:10:23 -0400

    materialize-motherduck: support for standard update materializations
    Motherduck now supports multi-statement transactions, and is able to do bulk
    in-place updates using a combination of delete + insert operations, so we can do
    a standard updates materialization in that way.
    
    Querying loaded documents was a little more difficult than normal here: The most
    obvious way of running the query with the SQL driver and iterating through the
    results apparently loads the entire query result into memory, which won't work
    for large load queries that would have results taking up a lot of memory. Saving
    the results to an S3 bucket does work though, and we can read the resulting rows
    efficiently from there.

--- tests/materialize/materialize-motherduck/fetch.sh
+++ tests/materialize/materialize-motherduck/fetch.sh
@@ -8,8 +8,10 @@ function exportToJsonl() {
   duckdb md: "set timezone to UTC; copy(select * from ${MOTHERDUCK_DATABASE}.${MOTHERDUCK_SCHEMA}."$1" order by ID) to '/dev/stdout' (format json);" | jq "{ "_table": \"$1\", rows: . }"
 }
 
-exportToJsonl "simple_delta"
+exportToJsonl "simple"
+exportToJsonl "duplicate_keys_standard"
 exportToJsonl "duplicate_keys_delta"
 exportToJsonl "duplicate_keys_delta_exclude_flow_doc"
-exportToJsonl "multiple_types_delta"
-exportToJsonl "formatted_strings_delta"
+exportToJsonl "multiple_types"
+exportToJsonl "formatted_strings"
+exportToJsonl "deletions"
