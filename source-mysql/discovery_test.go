51c68a6f4093986d72fff5606ae5ea3f7b46ee60
Will Donnelly <will@estuary.dev>
2024-07-19 12:40:06 -0500

    source-mysql: Simplify secondary index discovery
    Previously the secondary-index discovery query had a bug in its
    join against `information_schema.table_constraints` which would
    crop up if there were multiple constraints on the same table,
    with the same constraint name, but different constraint types.
    For instance, a `UNIQUE` constraint and a `FOREIGN KEY` one.
    
    But after rereading the relevant discovery query I'm not sure
    why we're even looking at that table in the first place. All we
    used that join for was a `tco.constraint_type != 'PRIMARY KEY'`
    filter clause, but as we can see in the primary-key discovery
    query we can also just check whether `stat.index_name = 'PRIMARY'`
    so the join should be entirely unnecessary.
    
    This commit also adds a subtest to `TestSecondaryIndexDiscovery`
    which reproduces the scenario that previously failed. After the
    query fix in this commit the new subtest also passes.

--- source-mysql/discovery_test.go
+++ source-mysql/discovery_test.go
@@ -25,6 +25,19 @@ func TestSecondaryIndexDiscovery(t *testing.T) {
 		tb.Query(ctx, t, fmt.Sprintf(`CREATE UNIQUE INDEX %s_k23 ON %s (k2, k3)`, shortName, tableName))
 		tb.CaptureSpec(ctx, t).VerifyDiscover(ctx, t, regexp.MustCompile(regexp.QuoteMeta(uniqueString)))
 	})
+	t.Run("index_and_fk", func(t *testing.T) {
+		// It's possible to have multiple constraints on the same table with the same constraint
+		// name but different types. This shouldn't matter now that our secondary index discovery
+		// query no longer joins against `information_schema.table_constraints` at all, but this
+		// test case just makes sure that scenario doesn't break anything.
+		var uniqueA, uniqueB = "g26313", "g16025"
+		var tableA = tb.CreateTable(ctx, t, uniqueA, "(id INTEGER PRIMARY KEY, data TEXT)")
+		var shortNameA = tableA[strings.Index(tableA, ".")+1:]
+		var tableB = tb.CreateTable(ctx, t, uniqueB, fmt.Sprintf("(id INTEGER NOT NULL, parent_id INTEGER, CONSTRAINT FK_%[1]s_ParentID FOREIGN KEY (parent_id) REFERENCES %[1]s(id), CONSTRAINT FK_%[1]s_ParentID UNIQUE (parent_id))", shortNameA))
+		var shortNameB = tableB[strings.Index(tableB, ".")+1:]
+		tb.Query(ctx, t, fmt.Sprintf(`CREATE UNIQUE INDEX IX_%[1]s_ParentID ON %s(id)`, shortNameB, tableB))
+		tb.CaptureSpec(ctx, t).VerifyDiscover(ctx, t, regexp.MustCompile(regexp.QuoteMeta(uniqueB)))
+	})
 	t.Run("nullable_index", func(t *testing.T) {
 		var uniqueString = "g31990"
 		var tableName = tb.CreateTable(ctx, t, uniqueString, "(k1 INTEGER, k2 INTEGER, k3 INTEGER, data TEXT)")
