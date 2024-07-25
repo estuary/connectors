9aa3d04472af4b1c7fbeca8c61df56173ad68812
Will Donnelly <will@estuary.dev>
2024-07-25 13:53:51 -0500

    source-postgres: Fix dumb issue with 'MinimumBackfillXID'
    I got confused between XIDs and LSNs when writing
    https://github.com/estuary/connectors/pull/1753. This
    should fix it to work correctly with decimal integer XIDs.

--- source-postgres/main.go
+++ source-postgres/main.go
@@ -106,7 +106,7 @@ type advancedConfig struct {
 	BackfillChunkSize  int      `json:"backfill_chunk_size,omitempty" jsonschema:"title=Backfill Chunk Size,default=50000,description=The number of rows which should be fetched from the database in a single backfill query."`
 	SSLMode            string   `json:"sslmode,omitempty" jsonschema:"title=SSL Mode,description=Overrides SSL connection behavior by setting the 'sslmode' parameter.,enum=disable,enum=allow,enum=prefer,enum=require,enum=verify-ca,enum=verify-full"`
 	DiscoverSchemas    []string `json:"discover_schemas,omitempty" jsonschema:"title=Discovery Schema Selection,description=If this is specified only tables in the selected schema(s) will be automatically discovered. Omit all entries to discover tables from all schemas."`
-	MinimumBackfillXID string   `json:"min_backfill_xid,omitempty" jsonschema:"title=Minimum Backfill XID,description=Only backfill rows with XMIN values greater (in a 32-bit modular comparison) than the specified XID. Helpful for reducing re-backfill data volume in certain edge cases." jsonschema_extras:"pattern=^[0-9a-fA-F/]+$"`
+	MinimumBackfillXID string   `json:"min_backfill_xid,omitempty" jsonschema:"title=Minimum Backfill XID,description=Only backfill rows with XMIN values greater (in a 32-bit modular comparison) than the specified XID. Helpful for reducing re-backfill data volume in certain edge cases." jsonschema_extras:"pattern=^[0-9]+$"`
 }
 
 // Validate checks that the configuration possesses all required properties.
