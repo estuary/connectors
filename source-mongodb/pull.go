a1c2cf85bc9853a57a9c0d8043caebdddf01668a
Will Baker <wbaker@gmail.com>
2024-07-26 14:59:56 -0400

    source-mongodb: enhanced check for server support of pre-images
    Support for pre-images is based entirely on the MongoDB version, and we can't
    request them if the server is too old.
    
    We also don't want to request them if we aren't able to use the
    $changeStreamSplitLargeEvent pipeline, because requesting pre-images makes it
    possible for an individual change event to exceed the 16MiB BSON document size
    limit. Previously it was assumed that this pipeline stage was universally
    supported on recent enough versions of MongoDB, but it turns out that commonly
    used Atlas deployments don't support it even though they are running a recent
    enough version.
    
    There is a dedicated Atlas API that conceivably could be used to determine the
    Atlas details and if $changeStreamSplitLargeEvent was available or not, but that
    API uses an entirely different authentication token so it's not a viable option
    for the the connector to use. And no other MongoDB commands such as `hello`,
    `buildInfo`, `isMaster`, etc. say anything about Atlas being used or not.
    
    So the only thing we can do to see if $changeStreamSplitLargeEvent is supported
    or not is to try opening a change stream with it enabled and see if there is an
    error.

--- source-mongodb/pull.go
+++ source-mongodb/pull.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math"
+	"strings"
 	"sync"
 	"time"
 
@@ -79,20 +81,14 @@ func (d *driver) Pull(open *pc.Request_Open, stream *boilerplate.PullOutput) err
 
 	log.Info("connected to database")
 
-	// Log some basic information about the database we are connected to if possible. buildInfo is
-	// an administrator command so it may not be available on all databases / configurations.
-	requestPreImages := false
-	if info, err := getBuildInfo(ctx, client); err != nil {
-		log.WithError(err).Info("could not query buildInfo")
-	} else {
-		log.WithFields(log.Fields{
-			"version": info.Version,
-		}).Info("buildInfo")
-		requestPreImages = info.supportsPreImages()
+	// Log some basic information about the database we are connected to. Even
+	// though this is an "admin" command, it always works with valid
+	// credentials, regardless of the user's actual permissions.
+	info, err := getBuildInfo(ctx, client)
+	if err != nil {
+		return fmt.Errorf("could not query buildInfo: %w", err)
 	}
-	if !requestPreImages {
-		log.Info("pre-images will not be requested for change streams")
-	}
+	log.WithFields(log.Fields{"version": info.Version}).Info("buildInfo")
 
 	var prevState captureState
 	if err := pf.UnmarshalStrict(open.StateJson, &prevState); err != nil {
@@ -122,6 +118,16 @@ func (d *driver) Pull(open *pc.Request_Open, stream *boilerplate.PullOutput) err
 		return fmt.Errorf("marshalling prevState checkpoint: %w", err)
 	} else if err := c.output.Checkpoint(checkpointJson, false); err != nil {
 		return fmt.Errorf("outputting prevState checkpoint: %w", err)
+	}
+
+	if len(bindings) == 0 {
+		// No bindings to capture.
+		return nil
+	}
+
+	requestPreImages, err := supportsPreImages(ctx, info, bindings[0].resource.Database, client)
+	if err != nil {
+		return fmt.Errorf("checking if server supports pre-images: %w", err)
 	}
 
 	didBackfill := false
@@ -322,23 +328,6 @@ type buildInfo struct {
 	VersionArray []int  `bson:"versionArray"`
 }
 
-// supportsPreImages returns true if the server supports pre-images, which is
-// really a proxy for it supporting the $changeStreamSplitLargeEvent pipeline
-// staged which was introduced in 6.0.9 and 7.0.0+. Pre-images were technically
-// supported starting in 6.0, but since we may need to use
-// $changeStreamSplitLargeEvent to handle resulting event documents larger than
-// 16MB we only support them if we're on 6.0.9 or later.
-func (b buildInfo) supportsPreImages() bool {
-	if len(b.VersionArray) < 3 {
-		return false
-	}
-
-	major := b.VersionArray[0]
-	minor := b.VersionArray[1]
-	patch := b.VersionArray[2]
-	return major >= 7 || (major == 6 && patch >= 9) || (major == 6 && minor >= 1)
-}
-
 func getBuildInfo(ctx context.Context, client *mongo.Client) (buildInfo, error) {
 	var info buildInfo
 	if res := client.Database("admin").RunCommand(ctx, bson.D{{"buildInfo", 1}}); res.Err() != nil {
@@ -348,4 +337,56 @@ func getBuildInfo(ctx context.Context, client *mongo.Client) (buildInfo, error) 
 	} else {
 		return info, nil
 	}
+}
+
+// supportsPreImages returns true if the server supports pre-images. To support
+// pre-images, the server must be a sufficiently recent version, and it must
+// support the $changeStreamSplitLargeEvent pipeline stage that is required to
+// split large change event documents that may arise from including the
+// pre-image for updates to large documents.
+//
+// Support for the $changeStreamSplitLargeEvent pipeline stage was added in
+// version 6.0.9 and 7.0.0+, so that's the first thing we check. Some versions
+// of MongoDB Atlas also do not support the $changeStreamSplitLargeEvent
+// pipeline stage, and the only way to know if we are connecting to one of those
+// short of using the MongoDB Atlas admin API (which requires a separate API
+// key) is to just try opening a change stream with the option enabled and
+// seeing if it works or not.
+func supportsPreImages(ctx context.Context, b buildInfo, database string, client *mongo.Client) (bool, error) {
+	ll := log.WithField("version", b.Version)
+
+	if len(b.VersionArray) < 3 {
+		ll.WithField("VersionArray", b.VersionArray).Info("not requesting pre-images due to insufficient version information")
+		return false, nil
+	}
+
+	major := b.VersionArray[0]
+	minor := b.VersionArray[1]
+	patch := b.VersionArray[2]
+	sufficientVersion := major >= 7 || (major == 6 && patch >= 9) || (major == 6 && minor >= 1)
+
+	if !sufficientVersion {
+		ll.Info("not requesting pre-images because this MongoDB server version is too old")
+		return false, nil
+	}
+
+	// Try opening a change stream with the $changeStreamSplitLargeEvent
+	// pipeline stage.
+	cs, err := client.Database(database).Watch(ctx, mongo.Pipeline{bson.D{{Key: "$changeStreamSplitLargeEvent", Value: bson.D{}}}})
+	if err != nil {
+		var commandError mongo.CommandError
+		if errors.As(err, &commandError) {
+			if commandError.Name == "AtlasError" && strings.HasPrefix(commandError.Message, "$changeStreamSplitLargeEvent is not allowed") {
+				ll.WithField("atlasError", err).Info("not requesting pre-images because this MongoDB Atlas instance does not support the $changeStreamSplitLargeEvent stage")
+				return false, nil
+			}
+		}
+		return false, fmt.Errorf("opening change stream to test for $changeStreamSplitLargeEvent support: %w", err)
+	}
+
+	if err := cs.Close(ctx); err != nil {
+		return false, fmt.Errorf("closing change stream: %w", err)
+	}
+
+	return true, nil
 }
