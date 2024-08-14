e2288bc3b3ce5a45afd223ce3eb7076b988aabdd
Alex Bair <alexbair143@gmail.com>
2024-08-14 17:59:43 -0400

    source-zendesk-support: redact fields in snapshot
    Zendesk automatically archives some data, which alters the capture
    snapshot even though nothing in the connector has changed. Redacting
    these fields makes the capture snapshot test a little more robust.
    
    Additionally, the posts schema was made more permissive due to schema
    violations we've encountered.

--- source-zendesk-support/tests/test_snapshots.py
+++ source-zendesk-support/tests/test_snapshots.py
@@ -29,6 +29,41 @@ def test_capture(request, snapshot):
             unique_stream_lines.append(line)
             seen.add(stream)
 
+    streams_with_updated_at = [
+        "acmeCo/group_memberships",
+        "acmeCo/groups",
+        "acmeCo/macros",
+        "acmeCo/organizations",
+        "acmeCo/organization_memberships",
+        "acmeCo/posts",
+        "acmeCo/post_comments",
+        "acmeCo/post_comment_votes",
+        "acmeCo/post_votes",
+        "acmeCo/satisfaction_ratings",
+        "acmeCo/sla_policies",
+        "tacmeCo/icket_fields",
+        "acmeCo/ticket_metrics",
+        "acmeCo/ticket_skips",
+        "acmeCo/tickets",
+        "acmeCo/users",
+        "acmeCo/brands",
+        "acmeCo/custom_roles",
+        "acmeCo/schedules",
+        "acmeCo/ticket_forms",
+        "acmeCo/account_attributes",
+        "acmeCo/attribute_definitions"
+    ]
+
+    for l in unique_stream_lines:
+        type, rec = l[0], l[1]
+
+        rec['_meta']['row_id'] = 0
+        if type in streams_with_updated_at:
+            rec["updated_at"] = "redacted"
+        if type == "acmeCo/users":
+            rec["last_login_at"] = "redacted"
+
+
     assert snapshot("capture.stdout.json") == unique_stream_lines
 
 
