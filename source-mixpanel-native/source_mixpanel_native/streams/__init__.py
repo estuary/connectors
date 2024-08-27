d602bfaf759eeab55b7767a9e0203f8e48cc0576
Luishfs <luish1980@hotmail.com>
2024-08-27 16:57:16 -0300

    source-mixpanel: renaming and applying changes

---
+++ source-mixpanel-native/source_mixpanel_native/streams/__init__.py
@@ -0,0 +1,23 @@
+from .annotations import Annotations
+from .base import DateSlicesMixin, IncrementalMixpanelStream, MixpanelStream
+from .cohort_members import CohortMembers
+from .cohorts import Cohorts
+from .engage import Engage
+from .export import Export, ExportSchema
+from .funnels import Funnels, FunnelsList
+from .revenue import Revenue
+
+__all__ = [
+    "IncrementalMixpanelStream",
+    "MixpanelStream",
+    "DateSlicesMixin",
+    "Engage",
+    "Export",
+    "ExportSchema",
+    "CohortMembers",
+    "Cohorts",
+    "Annotations",
+    "Funnels",
+    "FunnelsList",
+    "Revenue",
+]
