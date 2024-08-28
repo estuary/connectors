071f19cbd998a20bc331e6d4678c51bcda89258d
Will Baker <wbaker@gmail.com>
2024-08-28 18:58:03 -0400

    source-hubspot-native: cache recently emitted change events and don't emit them again
    Use a cache for keeping track of change events that have been recently emitted
    by `FetchRecentFn`, and don't emit change events again for documents returned
    from `FetchDelayedFn` if there was one already emitted for that key more
    recently.
    
    It may be worthwhile in the future to implement this or some other caching in
    `fetch_changes_with_associations` also so that we aren't looking up associations
    for documents that we don't need to emit, but it wasn't immediately obvious to
    me that this was necessary right now and I want to make sure the changes work
    as-implemented before incorporating that additional complication.

---
+++ source-hubspot-native/tests/test_emitted_changes_cache.py
@@ -0,0 +1,38 @@
+
+
+from datetime import datetime, timedelta
+import source_hubspot_native.emitted_changes_cache as cache
+
+
+def test_emitted_changes_cache():
+    base_dt = datetime.fromisoformat("2024-08-29T12:00:00.000Z")
+
+    test_entries = [
+        ("first", "id1", base_dt),
+        ("second", "id1", base_dt),
+        ("first", "id2", base_dt + timedelta(seconds=1)),
+        ("first", "id3", base_dt + timedelta(seconds=2)),
+        ("first", "id4", base_dt + timedelta(seconds=3)),
+        ("first", "id5", base_dt + timedelta(seconds=4)),
+    ]
+
+    assert not cache.has_as_recent_as("first", "id1", base_dt)
+    cache.add_recent("first", "id1", base_dt)
+    assert not cache.has_as_recent_as("second", "id1", base_dt)
+
+    for object_name, id, dt in test_entries:
+        cache.add_recent(object_name, id, dt)
+        assert cache.has_as_recent_as(object_name, id, dt)
+        assert cache.has_as_recent_as(object_name, id, dt - timedelta(milliseconds=1))
+        assert not cache.has_as_recent_as(object_name, id, dt + timedelta(milliseconds=1))
+
+    assert cache.count_for("first") == 5
+    assert cache.count_for("second") == 1
+    assert cache.cleanup("first", base_dt) == 1
+    assert cache.cleanup("second", base_dt) == 1
+    assert cache.count_for("first") == 4
+    assert cache.count_for("second") == 0
+    assert not cache.has_as_recent_as("first", "id1", base_dt)
+    assert not cache.has_as_recent_as("second", "id1", base_dt)
+    assert cache.cleanup("first", base_dt + timedelta(seconds=4)) == 4
+    assert cache.count_for("first") == 0
