22a258747342fb6846dbe20338024b0155b76eb1
Will Baker <wbaker@gmail.com>
2024-09-11 14:30:09 -0400

    source-mongodb: unify stream processing and introduce batch/stream coordinator
    Further refactoring and streamlining of the change stream code path: There will
    be just one `streamChanges` function, and it can be configured to return or not
    once streams are caught up. Returning in this way will be done for intermittent
    streaming while backfilling incremental change stream collections, and the
    change streams will run forever after those collections are done backfilling.
    
    This is partially enabled by the new batch/stream coordinator apparatus, which
    will be utilized in later commits to support concurrent change streams & batch
    backfills of non-incremental change stream collections.

---
+++ source-mongodb/coordinator_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestBatchStreamCoordinator(t *testing.T) {
+	ctx := context.Background()
+
+	getOpTime := func(_ context.Context) (primitive.Timestamp, error) {
+		return primitive.Timestamp{T: 1000}, nil
+	}
+
+	bindings := []bindingInfo{
+		{resource: resource{Database: "first"}},
+		{resource: resource{Database: "second"}},
+		{resource: resource{Database: "third"}},
+	}
+
+	coord := newBatchStreamCoordinator(bindings, getOpTime)
+
+	select {
+	case <-coord.streamsCaughtUp():
+	default:
+		t.Error("expected streams to be caught up on initialization")
+	}
+
+	require.NoError(t, coord.startCatchingUp(ctx))
+
+	select {
+	case <-coord.streamsCaughtUp():
+		t.Error("expected streams to not be caught up after starting catch up")
+	default:
+	}
+
+	require.False(t, coord.gotCaughtUp("first", primitive.Timestamp{T: 999}))
+	require.True(t, coord.gotCaughtUp("first", primitive.Timestamp{T: 1000}))
+	require.False(t, coord.gotCaughtUp("first", primitive.Timestamp{T: 1000}))
+
+	select {
+	case <-coord.streamsCaughtUp():
+		t.Error("expected streams to not be caught up after only one stream got caught up")
+	default:
+	}
+
+	require.True(t, coord.gotCaughtUp("second", primitive.Timestamp{}))
+	require.True(t, coord.gotCaughtUp("third", primitive.Timestamp{T: 1001}))
+
+	select {
+	case <-coord.streamsCaughtUp():
+	default:
+		t.Error("expected streams to be caught up after all streams got caught up")
+	}
+
+	require.NoError(t, coord.startCatchingUp(ctx))
+
+	select {
+	case <-coord.streamsCaughtUp():
+		t.Error("expected streams to not be caught up after starting catch up for the second time")
+	default:
+	}
+}
