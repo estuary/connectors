6d30e025e92b0b3143b6475d6757e5de30e23f0a
Luishfs <luish1980@hotmail.com>
2024-09-13 10:29:29 -0300

    source-impact-native: adding the initial version of the connector
    as of now, this version only supports Brands catalog.
    all streams were tested and materialized, but Catalogs stream.

---
+++ source-impact-native/tests/test_snapshots.py
@@ -0,0 +1,41 @@
+import json
+import subprocess
+
+
+def test_discover(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "discover",
+            "--source",
+            request.fspath.dirname + "/../test.flow.yaml",
+            "-o",
+            "json",
+            "--emit-raw",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    assert snapshot("stdout.json") == lines
+
+
+def test_spec(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "raw",
+            "spec",
+            "--source",
+            request.fspath.dirname + "/../test.flow.yaml",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    assert snapshot("stdout.json") == lines
