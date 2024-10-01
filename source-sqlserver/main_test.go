e1e54b2f0a9692cfe408513a2c19ba810250d6b9
Will Donnelly <will@estuary.dev>
2024-10-01 14:43:32 -0500

    source-sqlserver: Add test exercising computed column behavior
    This test tries to demonstrate all relevant behavior of computed
    columns: discovery, backfills, and all three replicated changes.
    
    Note how the use of the `ISNULL()` operator causes the computed
    column type to be a non-nullable varchar, which is correct, but
    then the replicated values of this column are always null which
    violates the discovered schema.

--- source-sqlserver/main_test.go
+++ source-sqlserver/main_test.go
@@ -473,3 +473,35 @@ func TestDeletedTextColumn(t *testing.T) {
 	tb.Delete(ctx, t, tableName, "id", 2)
 	t.Run("main", func(t *testing.T) { tests.VerifiedCapture(ctx, t, cs) })
 }
+
+func TestComputedColumn(t *testing.T) {
+	var tb, ctx = sqlserverTestBackend(t), context.Background()
+	var uniqueID = "18851564"
+	var tableName = tb.CreateTable(ctx, t, uniqueID, "(id INTEGER PRIMARY KEY, a VARCHAR(32), b VARCHAR(32), computed AS ISNULL(a, ISNULL(b, 'default')))")
+
+	var cs = tb.CaptureSpec(ctx, t, regexp.MustCompile(uniqueID))
+	cs.Validator = &st.OrderedCaptureValidator{}
+	t.Cleanup(func() { sqlcapture.TestShutdownAfterCaughtUp = false })
+	sqlcapture.TestShutdownAfterCaughtUp = true
+
+	t.Run("discovery", func(t *testing.T) {
+		cs.VerifyDiscover(ctx, t, regexp.MustCompile(uniqueID))
+	})
+
+	t.Run("capture", func(t *testing.T) {
+		tb.Insert(ctx, t, tableName, [][]any{{0, "a0", "b0"}, {1, nil, "b1"}, {2, "a2", nil}, {3, nil, nil}})
+		cs.Capture(ctx, t, nil)
+		tb.Insert(ctx, t, tableName, [][]any{{4, "a4", "b4"}, {5, nil, "b5"}, {6, "a6", nil}, {7, nil, nil}})
+		cs.Capture(ctx, t, nil)
+		tb.Update(ctx, t, tableName, "id", 4, "a", "a4-modified")
+		tb.Update(ctx, t, tableName, "id", 5, "b", "b5-modified")
+		tb.Update(ctx, t, tableName, "id", 6, "a", "a6-modified")
+		cs.Capture(ctx, t, nil)
+		tb.Delete(ctx, t, tableName, "id", 4)
+		tb.Delete(ctx, t, tableName, "id", 5)
+		tb.Delete(ctx, t, tableName, "id", 6)
+		tb.Delete(ctx, t, tableName, "id", 7)
+		cs.Capture(ctx, t, nil)
+		cupaloy.SnapshotT(t, cs.Summary())
+	})
+}
