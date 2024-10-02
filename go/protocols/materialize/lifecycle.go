ee04dfd7b099fe5586d486c71d6a995e8483f6ea
Will Baker <wbaker@gmail.com>
2024-10-02 16:17:42 -0400

    materialize(refactor): simplify stream interface
    Narrows the materialize "stream" interface to be focused on sending and
    receiving protocol messages. This is mostly removing baggage from when
    materializations only used gRPC and used the gRPC server interface more
    directly. Now they also support a JSON protocol so the prior implementation
    doesn't make much sense anymore.
    
    The lifecycle tests were also almost entirely redundant so rather than
    refactoring them I have deleted them.

--- go/protocols/materialize/lifecycle.go
+++ go/protocols/materialize/lifecycle.go
@@ -9,144 +9,8 @@ import (
 	"github.com/estuary/flow/go/protocols/fdb/tuple"
 	pf "github.com/estuary/flow/go/protocols/flow"
 	pm "github.com/estuary/flow/go/protocols/materialize"
-	"github.com/gogo/protobuf/proto"
 	pc "go.gazette.dev/core/consumer/protocol"
 )
-
-// Protocol routines for sending Request follow:
-
-type RequestTx interface {
-	Send(*pm.Request) error
-}
-
-func WriteOpen(stream RequestTx, open *pm.Request_Open) (pm.Request, error) {
-	var request = pm.Request{
-		Open: open,
-	}
-	if err := stream.Send(&request); err != nil {
-		return pm.Request{}, fmt.Errorf("sending Open: %w", err)
-	}
-	return request, nil
-}
-
-func WriteAcknowledge(stream RequestTx, request *pm.Request) error {
-	if request.Open == nil && request.StartCommit == nil {
-		panic(fmt.Sprintf("expected prior request is Open or StartCommit, got %#v", request))
-	}
-	*request = pm.Request{
-		Acknowledge: &pm.Request_Acknowledge{},
-	}
-	if err := stream.Send(request); err != nil {
-		return fmt.Errorf("sending Acknowledge request: %w", err)
-	}
-	return nil
-}
-
-func WriteLoad(
-	stream RequestTx,
-	request *pm.Request,
-	binding int,
-	keyPacked []byte,
-	keyJSON json.RawMessage,
-) error {
-	if request.Acknowledge == nil && request.Load == nil {
-		panic(fmt.Sprintf("expected prior request is Acknowledge or Load, got %#v", request))
-	}
-	*request = pm.Request{
-		Load: &pm.Request_Load{
-			Binding:   uint32(binding),
-			KeyPacked: keyPacked,
-			KeyJson:   keyJSON,
-		},
-	}
-	if err := stream.Send(request); err != nil {
-		return fmt.Errorf("sending Load request: %w", err)
-	}
-	return nil
-}
-
-func WriteFlush(stream RequestTx, request *pm.Request) error {
-	if request.Acknowledge == nil && request.Load == nil {
-		panic(fmt.Sprintf("expected prior request is Acknowledge or Load, got %#v", request))
-	}
-	*request = pm.Request{
-		Flush: &pm.Request_Flush{},
-	}
-	if err := stream.Send(request); err != nil {
-		return fmt.Errorf("sending Flush request: %w", err)
-	}
-	return nil
-}
-
-func WriteStore(
-	stream RequestTx,
-	request *pm.Request,
-	binding int,
-	keyPacked []byte,
-	keyJSON json.RawMessage,
-	valuesPacked []byte,
-	valuesJSON json.RawMessage,
-	doc json.RawMessage,
-	exists bool,
-) error {
-	if request.Flush == nil && request.Store == nil {
-		panic(fmt.Sprintf("expected prior request is Flush or Store, got %#v", request))
-	}
-	*request = pm.Request{
-		Store: &pm.Request_Store{
-			Binding:      uint32(binding),
-			KeyPacked:    keyPacked,
-			KeyJson:      keyJSON,
-			ValuesPacked: valuesPacked,
-			ValuesJson:   valuesJSON,
-			DocJson:      doc,
-			Exists:       exists,
-		},
-	}
-	if err := stream.Send(request); err != nil {
-		return fmt.Errorf("sending Store request: %w", err)
-	}
-	return nil
-}
-
-func WriteStartCommit(
-	stream RequestTx,
-	request *pm.Request,
-	checkpoint *pc.Checkpoint,
-) error {
-	if request.Flush == nil && request.Store == nil {
-		panic(fmt.Sprintf("expected prior request is Flush or Store, got %#v", request))
-	}
-	*request = pm.Request{
-		StartCommit: &pm.Request_StartCommit{
-			RuntimeCheckpoint: checkpoint,
-		},
-	}
-	if err := stream.Send(request); err != nil {
-		return fmt.Errorf("sending StartCommit request: %w", err)
-	}
-	return nil
-}
-
-// Protocol routines for receiving Request follow:
-
-type RequestRx interface {
-	Context() context.Context
-	RecvMsg(interface{}) error
-}
-
-func ReadAcknowledge(stream RequestRx, request *pm.Request) error {
-	if request.Open == nil && request.StartCommit == nil {
-		panic(fmt.Sprintf("expected prior request is Open or StartCommit, got %#v", request))
-	} else if err := recv(stream, request); err != nil {
-		return fmt.Errorf("reading Acknowledge: %w", err)
-	} else if request.Acknowledge == nil {
-		return fmt.Errorf("protocol error (expected Acknowledge, got %#v)", request)
-	} else if err = request.Validate_(); err != nil {
-		return fmt.Errorf("validation failed: %w", err)
-	}
-	return nil
-}
 
 // LoadIterator is an iterator over Load requests.
 type LoadIterator struct {
@@ -155,7 +19,7 @@ type LoadIterator struct {
 	PackedKey []byte      // PackedKey of the document to load.
 	Total     int         // Total number of iterated keys.
 
-	stream      RequestRx
+	stream      MaterializeStream
 	request     *pm.Request     // Request read into.
 	awaitDoneCh <-chan struct{} // Signaled when last commit acknowledgment has completed.
 	err         error           // Terminal error.
@@ -232,15 +96,6 @@ func (it *LoadIterator) Err() error {
 	return it.err
 }
 
-func ReadFlush(request *pm.Request) error {
-	if request.Flush == nil {
-		return fmt.Errorf("protocol error (expected Flush, got %#v)", request)
-	} else if err := request.Validate_(); err != nil {
-		return fmt.Errorf("validation failed: %w", err)
-	}
-	return nil
-}
-
 // StoreIterator is an iterator over Store requests.
 type StoreIterator struct {
 	Binding      int             // Binding index of this stored document.
@@ -253,13 +108,14 @@ type StoreIterator struct {
 	RawJSON      json.RawMessage // Document to store.
 	Total        int             // Total number of iterated stores.
 
-	stream  RequestRx
+	stream  MaterializeStream
 	request *pm.Request // Request read into.
 	err     error       // Terminal error.
+	ctx     context.Context
 }
 
 // Context returns the Context of this StoreIterator.
-func (it *StoreIterator) Context() context.Context { return it.stream.Context() }
+func (it *StoreIterator) Context() context.Context { return it.ctx }
 
 // Next returns true if there is another Store and makes it available.
 // When no Stores remain, or if an error is encountered, it returns false
@@ -310,6 +166,29 @@ func (it *StoreIterator) Err() error {
 	return it.err
 }
 
+func ReadAcknowledge(stream MaterializeStream, request *pm.Request) error {
+	if request.Open == nil && request.StartCommit == nil {
+		panic(fmt.Sprintf("expected prior request is Open or StartCommit, got %#v", request))
+	} else if err := recv(stream, request); err != nil {
+		return fmt.Errorf("reading Acknowledge: %w", err)
+	} else if request.Acknowledge == nil {
+		return fmt.Errorf("protocol error (expected Acknowledge, got %#v)", request)
+	} else if err = request.Validate_(); err != nil {
+		return fmt.Errorf("validation failed: %w", err)
+	}
+
+	return nil
+}
+
+func ReadFlush(request *pm.Request) error {
+	if request.Flush == nil {
+		return fmt.Errorf("protocol error (expected Flush, got %#v)", request)
+	} else if err := request.Validate_(); err != nil {
+		return fmt.Errorf("validation failed: %w", err)
+	}
+	return nil
+}
+
 func ReadStartCommit(request *pm.Request) (*pc.Checkpoint, error) {
 	if request.StartCommit == nil {
 		return nil, fmt.Errorf("protocol error (expected StartCommit, got %#v)", request)
@@ -319,13 +198,7 @@ func ReadStartCommit(request *pm.Request) (*pc.Checkpoint, error) {
 	return request.StartCommit.RuntimeCheckpoint, nil
 }
 
-// Protocol routines for sending Response follow:
-
-type ResponseTx interface {
-	Send(*pm.Response) error
-}
-
-func WriteOpened(stream ResponseTx, opened *pm.Response_Opened) (pm.Response, error) {
+func WriteOpened(stream MaterializeStream, opened *pm.Response_Opened) (pm.Response, error) {
 	var response = pm.Response{Opened: opened}
 
 	if err := stream.Send(&response); err != nil {
@@ -334,7 +207,7 @@ func WriteOpened(stream ResponseTx, opened *pm.Response_Opened) (pm.Response, er
 	return response, nil
 }
 
-func WriteAcknowledged(stream ResponseTx, state *pf.ConnectorState, response *pm.Response) error {
+func WriteAcknowledged(stream MaterializeStream, state *pf.ConnectorState, response *pm.Response) error {
 	if response.Opened == nil && response.StartedCommit == nil {
 		panic(fmt.Sprintf("expected prior response is Opened or StartedCommit, got %#v", response))
 	}
@@ -350,7 +223,7 @@ func WriteAcknowledged(stream ResponseTx, state *pf.ConnectorState, response *pm
 }
 
 func WriteLoaded(
-	stream ResponseTx,
+	stream MaterializeStream,
 	response *pm.Response,
 	binding int,
 	document json.RawMessage,
@@ -370,7 +243,7 @@ func WriteLoaded(
 	return nil
 }
 
-func WriteFlushed(stream ResponseTx, response *pm.Response) error {
+func WriteFlushed(stream MaterializeStream, response *pm.Response) error {
 	if response.Acknowledged == nil && response.Loaded == nil {
 		panic(fmt.Sprintf("expected prior response is Acknowledged or Loaded, got %#v", response))
 	}
@@ -384,7 +257,7 @@ func WriteFlushed(stream ResponseTx, response *pm.Response) error {
 }
 
 func WriteStartedCommit(
-	stream ResponseTx,
+	stream MaterializeStream,
 	response *pm.Response,
 	checkpoint *pf.ConnectorState,
 ) error {
@@ -402,78 +275,9 @@ func WriteStartedCommit(
 	return nil
 }
 
-// Protocol routines for reading Response follow:
-
-type ResponseRx interface {
-	RecvMsg(interface{}) error
-}
-
-func ReadOpened(stream ResponseRx) (pm.Response, error) {
-	var response pm.Response
-
-	if err := recv(stream, &response); err != nil {
-		return pm.Response{}, fmt.Errorf("reading Opened: %w", err)
-	} else if response.Opened == nil {
-		return pm.Response{}, fmt.Errorf("protocol error (expected Opened, got %#v)", response)
-	} else if err = response.Validate(); err != nil {
-		return pm.Response{}, fmt.Errorf("validation failed: %w", err)
-	}
-	return response, nil
-}
-
-func ReadAcknowledged(stream ResponseRx, response *pm.Response) error {
-	if response.Opened == nil && response.StartedCommit == nil {
-		panic(fmt.Sprintf("expected prior response is Opened or StartedCommit, got %#v", response))
-	} else if err := recv(stream, response); err != nil {
-		return fmt.Errorf("reading Acknowledged: %w", err)
-	} else if response.Acknowledged == nil {
-		return fmt.Errorf("protocol error (expected Acknowledged, got %#v)", response)
-	} else if err = response.Validate(); err != nil {
-		return fmt.Errorf("validation failed: %w", err)
-	}
-	return nil
-}
-
-func ReadLoaded(stream ResponseRx, response *pm.Response) (*pm.Response_Loaded, error) {
-	if response.Acknowledged == nil && response.Loaded == nil {
-		panic(fmt.Sprintf("expected prior response is Acknowledged or Loaded, got %#v", response))
-	} else if err := recv(stream, response); err == io.EOF && response.Acknowledged != nil {
-		return nil, io.EOF // Clean EOF.
-	} else if err != nil {
-		return nil, fmt.Errorf("reading Loaded: %w", err)
-	} else if response.Loaded == nil {
-		return nil, nil // No loads remain.
-	} else if err = response.Validate(); err != nil {
-		return nil, fmt.Errorf("validation failed: %w", err)
-	}
-	return response.Loaded, nil
-}
-
-func ReadFlushed(response *pm.Response) error {
-	if response.Flushed == nil {
-		return fmt.Errorf("protocol error (expected Flushed, got %#v)", response)
-	} else if err := response.Validate(); err != nil {
-		return fmt.Errorf("validation failed: %w", err)
-	}
-	return nil
-}
-
-func ReadStartedCommit(stream ResponseRx, response *pm.Response) (*pf.ConnectorState, error) {
-	if response.Flushed == nil {
-		panic(fmt.Sprintf("expected prior response is Flushed, got %#v", response))
-	} else if err := recv(stream, response); err != nil {
-		return nil, fmt.Errorf("reading StartedCommit: %w", err)
-	} else if response.StartedCommit == nil {
-		return nil, fmt.Errorf("protocol error (expected StartedCommit, got %#v)", response)
-	} else if err = response.Validate(); err != nil {
-		return nil, fmt.Errorf("validation failed: %w", err)
-	}
-	return response.StartedCommit.State, nil
-}
-
 func recv(
-	stream interface{ RecvMsg(interface{}) error },
-	message proto.Message,
+	stream MaterializeStream,
+	message *pm.Request,
 ) error {
 	return pf.UnwrapGRPCError(stream.RecvMsg(message))
 }
