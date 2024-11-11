bf9c54e3b646b43f6aacd5a27864c46bc045ebd2
Alex Bair <alexbair143@gmail.com>
2024-11-11 13:01:38 -0500

    source-genesys: new connector
    This is a minimal connector for Genesys, containing just the `users`
    and `conversations` streams.
    
    `users` is a simple snapshot since there is no way to request updated
    users.
    
    `conversations` is incremental and leverage two different endpoints. A
    real-time endpoint is used to get created conversations incrementally.
    This real-time endpoint is not appropriate to get updates
    (we can only specify time ranges for created conversations), so we also
    attempt to use the delayed, asynchronous analytics job endpoint to get
    conversations that have ended in the last day. This is not meant to
    capture all ended conversations as there are scenarios where we would
    miss capturing these updates (ex: no conversations are created for a
    while but conversations are ended), and a backfill will be required to
    fully capture all ended conversations.

---
+++ source-genesys/source_genesys/models.py
@@ -0,0 +1,117 @@
+from datetime import datetime, timezone, timedelta
+from pydantic import AwareDatetime, BaseModel, Field
+from typing import Literal
+
+
+from estuary_cdk.capture.common import (
+    BaseDocument,
+    ConnectorState as GenericConnectorState,
+    ClientCredentialsOAuth2Credentials,
+    ClientCredentialsOAuth2Spec,
+    ResourceConfig,
+    ResourceState,
+)
+
+
+OAUTH2_SPEC = ClientCredentialsOAuth2Spec(
+    # The access token URL requires a cloud region dependent domain. REGION_DEPENDENT_DOMAIN is replaced
+    # at runtime with the genesys_cloud_domain configured by the user.
+    accessTokenUrlTemplate=f"https://login.REGION_DEPENDENT_DOMAIN/oauth/token",
+    accessTokenResponseMap={
+        "access_token": "/access_token"
+    }
+)
+
+OAuth2Credentials = ClientCredentialsOAuth2Credentials
+
+
+def default_start_date():
+    dt = datetime.now(timezone.utc) - timedelta(days=30)
+    return dt
+
+class EndpointConfig(BaseModel):
+    start_date: AwareDatetime = Field(
+        description="UTC date and time in the format YYYY-MM-DDTHH:MM:SSZ. Any event data generated before this date will not be replicated. If left blank, the start date will be set to 30 days before the present date.",
+        title="Start Date",
+        default_factory=default_start_date,
+    )
+    # Genesis Cloud URLs are listed at https://help.mypurecloud.com/articles/aws-regions-for-genesys-cloud-deployment/.
+    genesys_cloud_domain: Literal[
+        'aps1.pure.cloud',
+        'apne3.pure.cloud',
+        'apne2.pure.cloud',
+        'mypurecloud.com.au',
+        'mypurecloud.jp',
+        'cac1.pure.cloud',
+        'mypurecloud.de',
+        'mypurecloud.ie',
+        'euw2.pure.cloud',
+        'euc2.pure.cloud',
+        'mec1.pure.cloud',
+        'sae1.pure.cloud',
+        'mypurecloud.com',
+        'use2.us-gov-pure.cloud',
+        'usw2.pure.cloud',
+    ] = Field(
+        title="Genesys Cloud Domain"
+    )
+    credentials: OAuth2Credentials = Field(
+        title="Authentication",
+    )
+
+
+ConnectorState = GenericConnectorState[ResourceState]
+
+
+class User(BaseDocument, extra="allow"):
+    pass
+
+
+class UserResponse(BaseModel, extra="forbid"):
+    entities: list[User]
+    pageSize: int
+    pageNumber: int
+    total: int
+    pageCount: int
+    lastUri: str
+    firstUri: str
+    selfUri: str
+
+
+class Conversation(BaseDocument, extra="allow"):
+    conversationId: str
+    conversationStart: AwareDatetime
+
+
+class EndedConversation(Conversation):
+    conversationEnd: AwareDatetime
+
+
+class ConversationQueryResponse(BaseModel, extra="forbid"):
+    conversations: list[Conversation] | None = None # If there are no conversations in the requested range, the conversations field is omitted from the response.
+    totalHits: int
+
+
+class CreateJobResponse(BaseModel, extra="forbid"):
+    jobId: str
+
+
+class CheckJobStatusResponse(BaseModel, extra="forbid"):
+    state: Literal[
+        "QUEUED",
+        "PENDING",
+        "FAILED",
+        "CANCELLED",
+        "FULFILLED",
+        "EXPIRED",
+    ]
+    submissionDate: AwareDatetime
+    completionDate: AwareDatetime | None = None # Only included if the job completed.
+    expirationDate: AwareDatetime | None = None # Only included if the job has expired.
+    errorMessage: str | None = None # Only included if the job status is FAILED.
+
+
+class JobResultsResponse(BaseModel, extra="forbid"):
+    conversations: list[Conversation]
+    dataAvailabilityDate: AwareDatetime
+    cursor: str | None = None # Included if response does not contain the last page of results.
