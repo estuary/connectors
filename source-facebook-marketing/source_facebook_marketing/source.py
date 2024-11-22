beb2ff136a82bf00b5e4c97525039c0f79138e56
Alex Bair <alexbair143@gmail.com>
2024-11-21 22:46:34 -0500

    source-facebook-marketing: update docs url

--- source-facebook-marketing/source_facebook_marketing/source.py
+++ source-facebook-marketing/source_facebook_marketing/source.py
@@ -198,8 +198,7 @@ class SourceFacebookMarketing(AbstractSource):
         (e.g: username and password) required to run this integration.
         """
         return ConnectorSpecification(
-            documentationUrl="https://docs.airbyte.com/integrations/sources/facebook-marketing",
-            changelogUrl="https://docs.airbyte.com/integrations/sources/facebook-marketing",
+            documentationUrl="https://go.estuary.dev/facebook-marketing",
             supportsIncremental=True,
             supported_destination_sync_modes=[DestinationSyncMode.append],
             connectionSpecification=ConnectorConfig.schema(),
