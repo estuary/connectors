28f06a8d4bd1b5ee061e3eb9fe170daebbd1310b
Will Donnelly <will@estuary.dev>
2024-12-06 12:12:20 -0600

    source-boilerplate: ParseFeatureFlags helper function

---
+++ source-boilerplate/feature_flags.go
@@ -0,0 +1,25 @@
+package boilerplate
+
+import "strings"
+
+// ParseFeatureFlags parses a comma-separated list of flag names and combines that with a
+// map describing default flag settings in the absence of any flags. A flag name can be
+// prefixed with 'no_' to explicitly set it to a false value, in case the default is (or
+// might soon become) true.
+func ParseFeatureFlags(flags string, defaults map[string]bool) map[string]bool {
+	var settings = make(map[string]bool)
+	for k, v := range defaults {
+		settings[k] = v
+	}
+	for _, flagName := range strings.Split(flags, ",") {
+		var flagValue = true
+		if strings.HasPrefix(flagName, "no_") {
+			flagName = strings.TrimPrefix(flagName, "no_")
+			flagValue = false
+		}
+		if flagName != "" {
+			settings[flagName] = flagValue
+		}
+	}
+	return settings
+}
