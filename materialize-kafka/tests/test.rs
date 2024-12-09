f62ddc16712b381c94e43b143e33ad2586c410e1
Will Baker <wbaker@gmail.com>
2024-12-09 15:03:10 -0500

    materialize-kafka: add to CI & create integration test
    Adds the dockerfile and CI build steps for materialize-kafka.
    
    I ended up not trying to share code from source-kafka right now since it was
    only needed in the tests, and have instead copy/pasted the Avro to JSON
    translating logic directly into the materialize-kafka tests file.

--- materialize-kafka/tests/test.rs
+++ materialize-kafka/tests/test.rs
@@ -1,11 +1,17 @@
+use anyhow::{Context, Result};
+use apache_avro::{types::Value as AvroValue, Schema as AvroSchema};
+use base64::engine::general_purpose::STANDARD as base64;
+use base64::Engine;
+use bigdecimal::BigDecimal;
 use core::str;
-use std::collections::HashMap;
-
 use rdkafka::{
     admin::{AdminClient, AdminOptions},
     consumer::{BaseConsumer, Consumer},
     ClientConfig, Message, TopicPartitionList,
 };
+use serde_json::{json, Map};
+use std::collections::HashMap;
+use time::{format_description, OffsetDateTime};
 
 const BOOTSTRAP_SERVERS: &str = "localhost:9092";
 const SCHEMA_REGISTRY_ENDPOINT: &str = "http://localhost:8081";
@@ -171,9 +177,7 @@ async fn parse_datum(datum: &[u8], schema_cache: &mut HashMap<u32, apache_avro::
             let schema = schema_cache.get(&schema_id).unwrap();
             let avro_value = apache_avro::from_avro_datum(schema, &mut &datum[5..], None).unwrap();
 
-            source_kafka::pull::avro_to_json(avro_value, schema)
-                .unwrap()
-                .to_string()
+            avro_to_json(avro_value, schema).unwrap().to_string()
         }
         _ => str::from_utf8(datum).unwrap().to_owned(),
     }
@@ -192,4 +196,188 @@ async fn fetch_schema(id: u32) -> apache_avro::Schema {
     .unwrap();
 
     apache_avro::Schema::parse_str(&schema_string).unwrap()
+}
+
+// TODO(whb): This is copied entirely from source-kafka. Consider a better way
+// to share code between the two connectors.
+fn avro_to_json(value: AvroValue, schema: &AvroSchema) -> Result<serde_json::Value> {
+    Ok(match value {
+        AvroValue::Null => json!(null),
+        AvroValue::Boolean(v) => json!(v),
+        AvroValue::Int(v) => json!(v),
+        AvroValue::Long(v) => json!(v),
+        AvroValue::Float(v) => match v.is_nan() || v.is_infinite() {
+            true => json!(v.to_string()),
+            false => json!(v),
+        },
+        AvroValue::Double(v) => match v.is_nan() || v.is_infinite() {
+            true => json!(v.to_string()),
+            false => json!(v),
+        },
+        AvroValue::Bytes(v) => json!(base64.encode(v)),
+        AvroValue::String(v) => json!(v),
+        AvroValue::Fixed(_, v) => json!(base64.encode(v)),
+        AvroValue::Enum(_, v) => json!(v),
+        AvroValue::Union(idx, v) => match schema {
+            AvroSchema::Union(s) => avro_to_json(*v, &s.variants()[idx as usize])
+                .context("failed to decode union value")?,
+            _ => anyhow::bail!(
+                "expected a union schema for a union value but got {}",
+                schema
+            ),
+        },
+        AvroValue::Array(v) => match schema {
+            AvroSchema::Array(s) => json!(v
+                .into_iter()
+                .map(|v| avro_to_json(v, &s.items))
+                .collect::<Result<Vec<_>>>()?),
+            _ => anyhow::bail!(
+                "expected an array schema for an array value but got {}",
+                schema
+            ),
+        },
+        AvroValue::Map(v) => match schema {
+            AvroSchema::Map(s) => json!(v
+                .into_iter()
+                .map(|(k, v)| Ok((k, avro_to_json(v, &s.types)?)))
+                .collect::<Result<Map<_, _>>>()?),
+            _ => anyhow::bail!("expected a map schema for a map value but got {}", schema),
+        },
+        AvroValue::Record(v) => match schema {
+            AvroSchema::Record(s) => json!(v
+                .into_iter()
+                .zip(s.fields.iter())
+                .map(|((k, v), field)| {
+                    if k != field.name {
+                        anyhow::bail!(
+                            "expected record field value with name '{}' but schema had name '{}'",
+                            k,
+                            field.name,
+                        )
+                    }
+                    Ok((k, avro_to_json(v, &field.schema)?))
+                })
+                .collect::<Result<Map<_, _>>>()?),
+            _ => anyhow::bail!(
+                "expected a record schema for a record value but got {}",
+                schema
+            ),
+        },
+        AvroValue::Date(v) => {
+            let date = OffsetDateTime::UNIX_EPOCH + time::Duration::days(v.into());
+            json!(format!(
+                "{}-{:02}-{:02}",
+                date.year(),
+                date.month() as u8,
+                date.day()
+            ))
+        }
+        AvroValue::Decimal(v) => match schema {
+            AvroSchema::Decimal(s) => json!(BigDecimal::new(v.into(), s.scale as i64).to_string()),
+            _ => anyhow::bail!(
+                "expected a decimal schema for a decimal value but got {}",
+                schema
+            ),
+        },
+        AvroValue::BigDecimal(v) => json!(v.to_string()),
+        AvroValue::TimeMillis(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::milliseconds(v as i64);
+            json!(format!(
+                "{:02}:{:02}:{:02}.{:03}",
+                time.hour(),
+                time.minute(),
+                time.second(),
+                time.millisecond()
+            ))
+        }
+        AvroValue::TimeMicros(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::microseconds(v);
+            json!(format!(
+                "{:02}:{:02}:{:02}.{:06}",
+                time.hour(),
+                time.minute(),
+                time.second(),
+                time.microsecond()
+            ))
+        }
+        AvroValue::TimestampMillis(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::milliseconds(v);
+            json!(time
+                .format(&format_description::well_known::Rfc3339)
+                .unwrap())
+        }
+        AvroValue::TimestampMicros(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::microseconds(v);
+            json!(time
+                .format(&format_description::well_known::Rfc3339)
+                .unwrap())
+        }
+        AvroValue::TimestampNanos(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::nanoseconds(v);
+            json!(time
+                .format(&format_description::well_known::Rfc3339)
+                .unwrap())
+        }
+        AvroValue::LocalTimestampMillis(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::milliseconds(v);
+            json!(time
+                .format(&format_description::well_known::Rfc3339)
+                .unwrap())
+        }
+        AvroValue::LocalTimestampMicros(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::microseconds(v);
+            json!(time
+                .format(&format_description::well_known::Rfc3339)
+                .unwrap())
+        }
+        AvroValue::LocalTimestampNanos(v) => {
+            let time = OffsetDateTime::UNIX_EPOCH + time::Duration::nanoseconds(v);
+            json!(time
+                .format(&format_description::well_known::Rfc3339)
+                .unwrap())
+        }
+        AvroValue::Duration(v) => {
+            json!(duration_to_duration_string(
+                v.months().into(),
+                v.days().into(),
+                v.millis().into()
+            ))
+        }
+        AvroValue::Uuid(v) => json!(v.to_string()),
+    })
+}
+
+fn duration_to_duration_string(months: u32, days: u32, total_milliseconds: u32) -> String {
+    let total_seconds = total_milliseconds / 1000;
+    let hours = total_seconds / 3600;
+    let minutes = (total_seconds % 3600) / 60;
+    let seconds = total_seconds % 60;
+    let milliseconds = total_milliseconds % 1000;
+
+    let mut duration = String::from("P");
+    if months > 0 {
+        duration.push_str(&format!("{}M", months));
+    }
+    if days > 0 {
+        duration.push_str(&format!("{}D", days));
+    }
+
+    if hours > 0 || minutes > 0 || seconds > 0 || milliseconds > 0 {
+        duration.push('T');
+        if hours > 0 {
+            duration.push_str(&format!("{}H", hours));
+        }
+        if minutes > 0 {
+            duration.push_str(&format!("{}M", minutes));
+        }
+        if seconds > 0 || milliseconds > 0 {
+            if milliseconds > 0 {
+                duration.push_str(&format!("{}.{:03}S", seconds, milliseconds));
+            } else {
+                duration.push_str(&format!("{}S", seconds));
+            }
+        }
+    }
+
+    duration
 }
---
+++ tests/materialize/materialize-kafka/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
+config_json_template='{
+    "bootstrap_servers": "materialize-kafka-db-1.flow-test:9092",
+    "topic_partitions": 3,
+    "topic_replication_factor": 1,
+    "message_format": "JSON"
+}'
+
+resources_json_template='[
+  {
+    "resource": {
+      "topic": "simple"
+    },
+    "source": "${TEST_COLLECTION_SIMPLE}"
+  }
+]'
+
+export CONNECTOR_CONFIG="$(echo "$config_json_template" | envsubst | jq -c)"
+export RESOURCES_CONFIG="$(echo "$resources_json_template" | envsubst | jq -c)"
+
+LISTENER_HOST="materialize-kafka-db-1.flow-test"  docker compose -f materialize-kafka/docker-compose.yaml up --detach --wait
