98663a0c72e6f70d64c8da108c66e2f55b6fa2f4
Will Baker <wbaker@gmail.com>
2024-12-11 15:17:32 -0500

    materialize-kafka: fix ci test

--- tests/materialize/materialize-kafka/fetch.sh
+++ tests/materialize/materialize-kafka/fetch.sh
@@ -4,11 +4,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-docker run -it --rm --network flow-test \
+docker run --tty --rm --network flow-test \
         edenhill/kcat:1.7.1 \
-                -b materialize-kafka-db-1.flow-test:9092 \
-                -t simple \
-                -o beginning \
-                -J \
-                -q \
-                -e 
+        -b materialize-kafka-db-1.flow-test:9092 \
+        -t simple \
+        -o beginning \
+        -J \
+        -q \
+        -e | jq 'del(.ts)' | jq -s 'sort_by(.key)'
