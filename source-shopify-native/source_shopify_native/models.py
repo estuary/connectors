86bcba6c51297f63297f16d8e1e14e3098c1b757
Alex Bair <alexbair143@gmail.com>
2025-01-15 09:17:05 -0500

    source-shopify-native: new connector
    This is a native capture connector for Shopify leveraging their GraphQL
    API.
    
    Using the GraphQL API is motivated by Shopify stating that their REST
    API (specifically the `/products` and `/variants` endpoints in the
    near-term) will be inaccessible soon. There are multiple differences
    between the REST and GraphQL schemas for equivalent resources, including:
    - snake_case vs. camelCase field names
    - renamed fields
    - different nesting of subresources
    
    Due to these differences, compatibility with our existing `source-shopify`
    import was not a goal during development; we don't want our native
    connector to serve as a transformation layer to try and maintain
    compatibility with Shopify's REST API schemas.
    
    This initial version of the connector only contains the `product` stream
    since that's the first one to supposedly stop working on 01FEB2025.
    
    Important design decisions include:
    - Retrieving data via bulk query operations. This avoids the
      rate/complexity limits enforced on non-bulk queries & allows us to
      fetch significantly more fields and results than using non-bulk queries.
    - Introspection was not used to programmatically build queries since it
      would have been possible to get in an infinite loop due to circular
      GraphQL connections.
    - Abstracting bulk query execution into a `graphql` module within the
      connector. `BulkJobManager` handles executing bulk queries, and stream
      specific queries and result processing are located within individual
      files. This should make it a little easier to add more streams later;
      we'll just need to add the query and result processing logic for the
      specific stream, and `products.py` can serve as a template.
    
    I've tested out what edge cases I can to ensure `BulkJobManager` works
    well, but I've left some info level logging to make monitoring easier
    as the connector begins being used. The logging can be tamped down to
    debug later as we tune & improve `BulkJobManager`.

---
+++ source-shopify-native/source_shopify_native/models.py
@@ -0,0 +1,182 @@
+from datetime import datetime, UTC, timedelta
+from enum import StrEnum
+from typing import Annotated, TYPE_CHECKING
+
+from estuary_cdk.capture.common import (
+    AccessToken,
+    BaseDocument,
+    LongLivedClientCredentialsOAuth2Credentials,
+    OAuth2Spec,
+    ResourceConfig,
+    ResourceState,
+)
+from estuary_cdk.capture.common import (
+    ConnectorState as GenericConnectorState,
+)
+from pydantic import AwareDatetime, BaseModel, Field
+
+
+scopes = [
+    "read_locales",
+    "read_products",
+    "read_orders",
+    "read_checkouts",
+    "read_locations",
+    "read_inventory",
+    "read_fulfillments",
+    "read_customers",
+    "read_publications",
+]
+
+OAUTH2_SPEC = OAuth2Spec(
+    provider="shopify",
+    authUrlTemplate=(
+        r"https://{{{ config.store }}}.myshopify.com/admin/oauth/authorize"
+        r"?client_id={{{#urlencode}}}{{{ client_id }}}{{{/urlencode}}}"
+        "&scope=" + ",".join(scopes) +
+        r"&state={{{#urlencode}}}{{{ state }}}{{{/urlencode}}}"
+        r"&redirect_uri={{{#urlencode}}}{{{ redirect_uri }}}{{{/urlencode}}}"
+    ),
+    accessTokenUrlTemplate=(
+        r"https://{{{ config.store }}}.myshopify.com/admin/oauth/access_token"
+        r"?client_id={{{#urlencode}}}{{{ client_id }}}{{{/urlencode}}}"
+        r"&client_secret={{{#urlencode}}}{{{ client_secret }}}{{{/urlencode}}}"
+        r"&code={{{#urlencode}}}{{{ code }}}{{{/urlencode}}}"
+    ),
+    accessTokenHeaders={"content-type": "application/x-www-form-urlencoded"},
+    accessTokenBody=(
+        r"client_id={{{#urlencode}}}{{{ client_id }}}{{{/urlencode}}}"
+        r"&client_secret={{{#urlencode}}}{{{ client_secret }}}{{{/urlencode}}}"
+        r"&code={{{#urlencode}}}{{{ code }}}{{{/urlencode}}}"
+    ),
+    accessTokenResponseMap={
+        "access_token": "/access_token",
+    },
+)
+
+
+if TYPE_CHECKING:
+    OAuth2Credentials = LongLivedClientCredentialsOAuth2Credentials
+else:
+    OAuth2Credentials = LongLivedClientCredentialsOAuth2Credentials.for_provider(OAUTH2_SPEC.provider)
+
+
+def default_start_date():
+    dt = datetime.now(tz=UTC) - timedelta(days=30)
+    return dt
+
+
+class EndpointConfig(BaseModel):
+    store: str = Field(
+        title="Shopify Store",
+        description="Shopify store ID. Use the prefix of your admin URL e.g. https://{YOUR_STORE}.myshopify.com/admin"
+    )
+    credentials: OAuth2Credentials | AccessToken = Field(
+        discriminator="credentials_title",
+        title="Authentication",
+    )
+    start_date: AwareDatetime = Field(
+        description="UTC date and time in the format YYYY-MM-DDTHH:MM:SSZ. Any data generated before this date will not be replicated. If left blank, the start date will be set to 30 days before the present.",
+        title="Start Date",
+        default_factory=default_start_date,
+    )
+    class Advanced(BaseModel):
+        window_size: Annotated[int, Field(
+            description="Window size in days for incremental streams.",
+            title="Window Size",
+            default=30,
+            gt=0,
+        )]
+
+    advanced: Advanced = Field(
+        default_factory=Advanced, #type: ignore
+        title="Advanced Config",
+        description="Advanced settings for the connector.",
+        json_schema_extra={"advanced": True},
+    )
+
+
+ConnectorState = GenericConnectorState[ResourceState]
+
+
+class BulkOperationTypes(StrEnum):
+    MUTATION = "MUTATION"
+    QUERY = "QUERY"
+
+
+class BulkOperationStatuses(StrEnum):
+    CANCELED = "CANCELED"
+    CANCELING = "CANCELING"
+    COMPLETED = "COMPLETED"
+    CREATED = "CREATED"
+    EXPIRED = "EXPIRED"
+    FAILED = "FAILED"
+    RUNNING = "RUNNING"
+
+
+class BulkOperationErrorCodes(StrEnum):
+    ACCESS_DENIED = "ACCESS_DENIED"
+    INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR"
+    TIMEOUT = "TIMEOUT"
+
+
+class BulkOperationUserErrorCodes(StrEnum):
+    INVALID = "INVALID"
+    OPERATION_IN_PROGRESS = "OPERATION_IN_PROGRESS"
+
+
+class BulkOperationDetails(BaseModel, extra="allow"):
+    type: BulkOperationTypes
+    id: str
+    status: BulkOperationStatuses
+    createdAt: str
+    completedAt: str | None
+    url: str | None
+    errorCode: BulkOperationErrorCodes | None
+
+
+class UserErrors(BaseModel, extra="forbid"):
+    field: str | None
+    message: str
+    code: BulkOperationUserErrorCodes
+
+
+class BulkJobCancelResponse(BaseModel, extra="allow"):
+    class Data(BaseModel, extra="forbid"):
+        class BulkOperationCancel(BaseModel, extra="forbid"):
+            bulkOperation: BulkOperationDetails
+            userErrors: list[UserErrors]
+
+        bulkOperationCancel: BulkOperationCancel
+
+    data: Data
+
+
+class BulkCurrentJobResponse(BaseModel, extra="allow"):
+    class Data(BaseModel, extra="forbid"):
+        currentBulkOperation: BulkOperationDetails | None
+
+    data: Data
+
+
+class BulkSpecificJobResponse(BaseModel, extra="allow"):
+    class Data(BaseModel, extra="forbid"):
+        node: BulkOperationDetails
+
+    data: Data
+
+
+class BulkJobSubmitResponse(BaseModel, extra="allow"):
+    class Data(BaseModel, extra="forbid"):
+        class BulkOperationRunQuery(BaseModel, extra="forbid"):
+            bulkOperation: BulkOperationDetails | None
+            userErrors: list[UserErrors]
+
+        bulkOperationRunQuery: BulkOperationRunQuery
+
+    data: Data
+
+
+class TimestampedResource(BaseDocument, extra="allow"):
+    id: str
+    updatedAt: AwareDatetime
