220aa19a2638ec5de40651c6d5830a0b8488915d
Will Baker <wbaker@gmail.com>
2025-01-23 13:09:40 -0500

    materialize-azure-fabric-warehouse: integration tests
    The integration tests for materialize-azure-fabric-warehouse. Adding these as a
    separate commit for ease of review. The actual connector code that can run them
    will be in a subsequent commit.

---
+++ tests/materialize/materialize-azure-fabric-warehouse/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+function dropTable() {
+    go run ${TEST_DIR}/materialize-azure-fabric-warehouse/fetch-data.go --delete "$1"
+}
+
+dropTable "simple"
+dropTable "duplicate_keys_standard"
+dropTable "duplicate_keys_delta"
+dropTable "multiple_types"
+dropTable "formatted_strings"
+dropTable "deletions"
+
+go run ${TEST_DIR}/materialize-azure-fabric-warehouse/fetch-data.go --delete-checkpoint notable
