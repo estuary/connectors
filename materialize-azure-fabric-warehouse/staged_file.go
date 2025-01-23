8420b2fea583ed2db7e5b7820725a451ee3812ef
Will Baker <wbaker@gmail.com>
2025-01-23 13:10:53 -0500

    materialize-azure-fabric-warehouse: initial working version
    Basic working version of the Azure Fabric Warehouse materialization.
    
    Fabric Warehouse is a relatively new product, and perhaps because of that it is
    missing some features typical of other SQL warehouse materializations:
    
    * No merge query support: This means we must delete rows from the target table
      that already exist, before inserting the updated rows. I don't think this is
      all that much different than what a true merge query would do, but it's a
      little more cumbersome to build queries for.
    * No ability to read from staged files directly; instead we must load them into
      "temporary" tables using a COPY INTO query.
    * Fabric Warehouse recently added support for temporary tables, but
      unfortunately COPY INTO does not work with them. An actual table must be
      created, COPY INTO'd, and then later dropped when it is not needed. Fabric
      Warehouse does at least support creating and dropping tables within a
      transaction, and this is utilized to ensure we don't leave tables hanging
      around.
    * No idempotent COPY INTO, which makes a post-commit apply strategy with
      exactly-once standard and delta updates impossible. Transactions are supported
      though, and there don't appear to be any strange limitations on them like
      number of tables in a single transaction, so the authoritative endpoint
      pattern works well.
    
    Other than these main limitations, a decent range of data types is supported,
    and the materialization seems to work pretty well.
    
    Later commits will add support for additional features like column migrations,
    and some additional optimizations.

---
+++ materialize-azure-fabric-warehouse/staged_file.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"path"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	boilerplate "github.com/estuary/connectors/materialize-boilerplate"
+	enc "github.com/estuary/connectors/materialize-boilerplate/stream-encode"
+	"golang.org/x/sync/errgroup"
+)
+
+// Multiple files are loaded faster by COPY INTO than a single large file.
+// Splitting files into 250MiB chunks (after compression) seems to work well
+// enough for larger transactions.
+const fileSizeLimit = 250 * 1024 * 1024
+
+type azureBlobObject struct {
+	directory string
+	name      string
+}
+
+func (o azureBlobObject) blobName() string {
+	return path.Join(o.directory, o.name)
+}
+
+func newFileClient(azClient *azblob.Client, container string, directory string) *stagedFileClient {
+	return &stagedFileClient{
+		container: container,
+		directory: directory,
+		azClient:  azClient,
+	}
+}
+
+type stagedFileClient struct {
+	container string
+	directory string
+	azClient  *azblob.Client
+}
+
+func (s *stagedFileClient) NewEncoder(w io.WriteCloser, fields []string) boilerplate.Encoder {
+	return enc.NewCsvEncoder(w, fields, enc.WithCsvSkipHeaders())
+}
+
+func (s *stagedFileClient) NewObject(uuid string) azureBlobObject {
+	return azureBlobObject{directory: s.directory, name: uuid}
+}
+
+func (s *stagedFileClient) URI(o azureBlobObject) string {
+	return s.azClient.URL() + path.Join(s.container, o.blobName())
+}
+
+func (s *stagedFileClient) UploadStream(ctx context.Context, o azureBlobObject, r io.Reader) error {
+	if _, err := s.azClient.UploadStream(ctx, s.container, o.blobName(), r, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *stagedFileClient) Delete(ctx context.Context, uris []string) error {
+	group, groupCtx := errgroup.WithContext(ctx)
+	group.SetLimit(5)
+
+	for _, uri := range uris {
+		parts := strings.TrimPrefix(uri, s.azClient.URL())
+		firstSlash := strings.Index(parts, "/")
+		container := parts[:firstSlash]
+		blobName := parts[firstSlash+1:]
+		group.Go(func() error {
+			if _, err := s.azClient.DeleteBlob(groupCtx, container, blobName, nil); err != nil {
+				return fmt.Errorf("deleting blob %q: %w", blobName, err)
+			}
+			return nil
+		})
+	}
+
+	return group.Wait()
+}
