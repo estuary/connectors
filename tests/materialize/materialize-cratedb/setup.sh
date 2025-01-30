498661adce2187755d16efc89a99d261b06bbd67
surister <surister98@gmail.com>
2025-01-30 21:07:34 +0100

    First draft of CrateDB connector

---
+++ tests/materialize/materialize-cratedb/setup.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Ensure canonical CrateDB environment variables are set.
+# It's ok to set 'crate' as password, even though the default user 'crate' doesn't have a password.
+export CRHOST="${CRHOST:=materialize-cratedb-cratedb-1.flow-test}"
+export CRPORT="${CRPORT:=5432}"
+export CRDATABASE="${CRDATABASE:=crate}"
+export CRPASSWORD="${CRPASSWORD:=crate}"
+export CRUSER="${CRUSER:=crate}"
+docker compose -f materialize-cratedb/docker-compose.yaml up --detach
+
+# Give it time to start.
+sleep 5
+config_json_template='{
+   "address":  "$CRHOST:$CRPORT",
+   "database": "$CRDATABASE",
+   "password": "$CRPASSWORD",
+   "user":     "$CRUSER",
+   "schema":   "doc",
+   "hardDelete": true
+}'
+resources_json_template='[
+  {
+    "resource": {
+      "table": "Simple"
+    },
+    "source": "${TEST_COLLECTION_SIMPLE}"
+  },
+  {
+    "resource": {
+      "table": "duplicate_keys_standard"
+    },
+    "source": "${TEST_COLLECTION_DUPLICATED_KEYS}"
+  },
+  {
+    "resource": {
+      "table": "duplicate_keys_delta",
+      "delta_updates": true
+    },
+    "source": "${TEST_COLLECTION_DUPLICATED_KEYS}"
+  },
+  {
+    "resource": {
+      "table": "duplicate_keys_delta_exclude_flow_doc",
+      "delta_updates": true
+    },
+    "source": "${TEST_COLLECTION_DUPLICATED_KEYS}",
+    "fields": {
+      "recommended": true,
+      "exclude": [
+        "flow_document"
+      ]
+    }
+  },
+  {
+    "resource": {
+      "table": "Multiple Types"
+    },
+    "source": "${TEST_COLLECTION_MULTIPLE_DATATYPES}",
+    "fields": {
+      "recommended": true,
+      "exclude": ["nested/id"],
+      "include": {
+        "nested": {},
+        "array_int": {},
+        "multiple": {}
+      }
+    }
+  },
+  {
+    "resource": {
+      "table": "Formatted Strings"
+    },
+    "source": "${TEST_COLLECTION_FORMATTED_STRINGS}",
+    "fields": {
+      "recommended": true
+    }
+  },
+  {
+    "resource": {
+      "table": "unsigned_bigint"
+    },
+    "source": "${TEST_COLLECTION_UNSIGNED_BIGINT}"
+  },
+  {
+    "resource": {
+      "table": "deletions"
+    },
+    "source": "${TEST_COLLECTION_DELETIONS}"
+  }
+]'
+export CONNECTOR_CONFIG="$(echo "$config_json_template" | envsubst | jq -c)"
+export RESOURCES_CONFIG="$(echo "$resources_json_template" | envsubst | jq -c)"
