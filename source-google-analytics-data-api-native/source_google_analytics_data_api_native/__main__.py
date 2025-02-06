8cc3933b453271ebe3020de95fdf7a3a5275f89b
Alex Bair <alexbair143@gmail.com>
2025-02-06 16:40:13 -0500

    source-google-analytics-data-api-native: new connector
    This is an initial version of a connector to capture reports from
    Google Analytics' Data API. Parity with the current ATF connector is
    achieved with this commit. Further features will be added in later
    commits.
    
    A few quirks about the API/connector:
    - The API uses the property's timezone, which is not always UTC. So we
      determine the appropriate timezone by hitting the API instead of
      having the user configure it. This assumes the timezones returned by
      Google are IANA compliant, which they should be according to the docs.
    - Similar to the ATF connector, I added the property id and the
      date of the report to each document. These are necessary as primary
      keys to differentiate the same report across different days & to
      pre-position for the likely feature request to support multiple
      property ids in a single connector.
    - There is no capture snapshot since the test credentials I created
      doesn't have much/any analytics data in it.

---
+++ source-google-analytics-data-api-native/source_google_analytics_data_api_native/__main__.py
@@ -0,0 +1,4 @@
+import asyncio
+import source_google_analytics_data_api_native
+
+asyncio.run(source_google_analytics_data_api_native.Connector().serve())
---
+++ source-google-analytics-data-api-native/source_google_analytics_data_api_native/models.py
@@ -0,0 +1,159 @@
+from annotated_types import MinLen
+from datetime import datetime, UTC, timedelta
+from enum import StrEnum
+import json
+from typing import Annotated, Any, Optional, TYPE_CHECKING, Union
+
+from estuary_cdk.capture.common import (
+    AccessToken,
+    BaseDocument,
+    BaseOAuth2Credentials,
+    OAuth2Spec,
+    ResourceConfig,
+    ResourceState,
+)
+from estuary_cdk.capture.common import (
+    ConnectorState as GenericConnectorState,
+    LogCursor,
+    Logger,
+)
+
+from pydantic import AwareDatetime, BaseModel, Field, create_model
+
+
+def urlencode_field(field: str):
+    return "{{#urlencode}}{{{ " + field + " }}}{{/urlencode}}"
+
+accessTokenBody = {
+    "grant_type": "authorization_code",
+    "code": "{{{ code }}}",
+    "client_id": "{{{ client_id }}}",
+    "client_secret": "{{{ client_secret }}}",
+    "redirect_uri": "{{{ redirect_uri }}}",
+}
+
+OAUTH2_SPEC = OAuth2Spec(
+    provider="google",
+    accessTokenBody=json.dumps(accessTokenBody),
+    authUrlTemplate=(
+        "https://accounts.google.com/o/oauth2/auth?"
+        "access_type=offline&"
+        "prompt=consent&"
+        f"response_type=code&"
+        f"client_id={urlencode_field('client_id')}&"
+        f"redirect_uri={urlencode_field('redirect_uri')}&"
+        f"scope=https://www.googleapis.com/auth/analytics.readonly&"
+        f"state={urlencode_field('state')}"
+    ),
+    accessTokenUrlTemplate="https://oauth2.googleapis.com/token",
+    accessTokenResponseMap={
+        "refresh_token": "/refresh_token",
+    },
+    accessTokenHeaders={
+        "Content-Type": "application/json",
+    },
+)
+
+
+if TYPE_CHECKING:
+    OAuth2Credentials = BaseOAuth2Credentials
+else:
+    OAuth2Credentials = BaseOAuth2Credentials.for_provider(OAUTH2_SPEC.provider)
+
+
+def default_start_date():
+    dt = datetime.now(tz=UTC) - timedelta(days=30)
+    return dt
+
+
+class EndpointConfig(BaseModel):
+    property_id: str = Field(
+        description="Your Google Analytics GA4 properity identifier whose events are tracked.",
+        title="Property ID"
+    )
+    custom_reports: str = Field(
+        description="A JSON array describing the custom reports you want to sync from the Google Analytics Data API. Reference this connector's documentation for information about how to fill out this field: https://go.estuary.dev/source-google-analytics-data-api-native",
+        title="Custom Reports",
+        default=""
+    )
+    start_date: AwareDatetime = Field(
+        description="UTC date and time in the format YYYY-MM-DDTHH:MM:SSZ. Any data generated before this date will not be replicated. If left blank, the start date will be set to 30 days before the present.",
+        title="Start Date",
+        default_factory=default_start_date,
+    )
+    credentials: OAuth2Credentials = Field(
+        discriminator="credentials_title",
+        title="Authentication",
+    )
+
+
+ConnectorState = GenericConnectorState[ResourceState]
+
+
+class ReportDocument(BaseDocument, extra="allow"):
+    pass
+
+
+# Report represents a valid configured report stream.
+class Report(BaseModel, extra="forbid"):
+    name: str
+    dimensions: Annotated[list[str], MinLen(1)]
+    metrics: Annotated[list[str], MinLen(1)]
+
+
+def create_report_doc_model(
+    report: Report,
+):
+    dimension_fields: dict[str, Any] = {
+        d_name: (str, ...) for d_name in report.dimensions
+    }
+
+    metric_fields: dict[str, Any] = {
+        m_name: (str, ...) for m_name in report.metrics
+    }
+
+    added_fields: dict[str, Any] = {
+        "property_id": (str, ...),
+        "report_date": (str, ...),
+    }
+
+    model = create_model(
+        report.name,
+        __base__ = ReportDocument,
+        **dimension_fields,
+        **metric_fields,
+        **added_fields,
+    )
+
+    return model
+
+
+class DimensionHeader(BaseModel, extra='forbid'):
+    name: str
+
+
+class MetricHeader(BaseModel, extra='forbid'):
+    name: str
+    type: str
+
+
+class Value(BaseModel, extra="forbid"):
+    value: str
+
+
+class Row(BaseModel, extra="forbid"):
+    dimensionValues: list[Value]
+    metricValues: list[Value]
+
+
+class RunReportResponse(BaseModel, extra="allow"):
+    dimensionHeaders: list[DimensionHeader]
+    metricHeaders: list[MetricHeader]
+    # If rows or rowCount is None, that means we requested data for days that haven't started yet.
+    rows: Optional[list[Row]] = None
+    rowCount: Optional[int] = None
+
+    class Metadata(BaseModel, extra="allow"):
+        timeZone: str
+
+    metadata: Metadata
