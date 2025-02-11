a15fdb5245e68e5980d13eb706d500b48b7f9ad2
Will Donnelly <will@estuary.dev>
2025-02-11 15:18:53 -0600

    source-postgres: Fix a missed bit of refactoring
    Will Baker recently merged a PR which moved the ParseFeatureFlags
    function from source-boilerplate to a new common package so it can
    be used in materializations as well, looks like somehow Postgres
    got missed and still has a call to the old location.

--- source-postgres/main.go
+++ source-postgres/main.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/estuary/connectors/go/common"
 	cerrors "github.com/estuary/connectors/go/connector-errors"
 	networkTunnel "github.com/estuary/connectors/go/network-tunnel"
 	schemagen "github.com/estuary/connectors/go/schema-gen"
@@ -76,7 +77,7 @@ func connectPostgres(ctx context.Context, name string, cfg json.RawMessage) (sql
 		}
 	}
 
-	var featureFlags = boilerplate.ParseFeatureFlags(config.Advanced.FeatureFlags, featureFlagDefaults)
+	var featureFlags = common.ParseFeatureFlags(config.Advanced.FeatureFlags, featureFlagDefaults)
 	if config.Advanced.FeatureFlags != "" {
 		logrus.WithField("flags", featureFlags).Info("parsed feature flags")
 	}
