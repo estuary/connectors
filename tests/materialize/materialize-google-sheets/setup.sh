b2a06818839f84d92f5ddef84bd787c0296235e3
Will Baker <wbaker@gmail.com>
2025-02-20 10:44:41 -0500

    materializations: integration test for merge-and-then-insert transactions
    Extends the materialization integration test fixture to include an additional
    transaction for the "duplicate keys" scenario where all of the keys are new. The
    progression is then 1) All keys are new for the initial transaction, 2) Updates
    to existing keys for the next transaction 3) (New) A transaction with all new
    keys.
    
    This exercises the behavior some materializations implement where merge types of
    queries are only run if some of the keys to store have updates, and otherwise a
    more efficient direct insert is run. It is reflected in the test snapshots for
    materializations that persist queries in their state, most notably for
    databricks, where a bug in this behavior was fixed in the prior commit.
    
    Since I had to update most of the test snapshots for this, I also included the
    following changes:
    
    * Updated the google sheets materialization test setup to use an encrypted
      config file like all the other materialization tests.
    * Did a bit of cleaned up the CI spec file mostly related to the above.
    * Added an empty transaction to the test fixture, which is run first. This
      simulates what happens if a materialization is configured with a "notBefore"
      setting, and processes some empty transactions at first due to skipping over
      data. This revealed that materialize-sqlserver still wasn't handling this
      correctly, so that is fixed as well.

--- tests/materialize/materialize-google-sheets/setup.sh
+++ tests/materialize/materialize-google-sheets/setup.sh
@@ -4,21 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# This spreadsheet lives under the Estuary org and is shared with our CI service account
-# and engineering@. If you're an engineer, feel free to add additional service accounts
-# as needed for your own testing:
-export SPREADSHEET_ID="1aki_PfFU-RCXCvm4-U0O4QoElZBIC7F9lfR-RBG0CTc"
-export SPREADSHEET_URL="https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/edit#gid=0"
-export GCP_SERVICE_ACCOUNT_KEY_QUOTED=$(echo ${GCP_SERVICE_ACCOUNT_KEY} | jq 'tojson')
-
-config_json_template='{
-    "spreadsheetUrl": "${SPREADSHEET_URL}",
-    "credentials": {
-      "auth_type": "Service",
-      "credentials_json": ${GCP_SERVICE_ACCOUNT_KEY_QUOTED}
-    }
-}'
-
 resources_json_template='[
   {
     "resource": {
@@ -40,8 +25,8 @@ resources_json_template='[
   }
 ]'
 
-CONNECTOR_CONFIG="$(echo "$config_json_template" | envsubst | jq -c)"
-export CONNECTOR_CONFIG
+export CONNECTOR_CONFIG="$(decrypt_config ${TEST_DIR}/${CONNECTOR}/config.yaml)"
+export SPREADSHEET_ID=$(echo $CONNECTOR_CONFIG | jq -r .spreadsheetUrl | sed -E 's#.*/d/([^/]+)/.*#\1#')
+export GCP_SERVICE_ACCOUNT_KEY=$(echo $CONNECTOR_CONFIG | jq -r .credentials.credentials_json)
 
-RESOURCES_CONFIG="$(echo "$resources_json_template" | envsubst | jq -c)"
-export RESOURCES_CONFIG
+export RESOURCES_CONFIG="$(echo "$resources_json_template" | envsubst | jq -c)"
