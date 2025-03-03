b2196bd8a618b054bc2369e04bfd5adfc850ed49
Will Donnelly <will@estuary.dev>
2025-03-03 12:48:52 -0600

    source-mysql-batch: Set keyless_row_id by default

--- source-mysql-batch/main.go
+++ source-mysql-batch/main.go
@@ -24,7 +24,7 @@ import (
 var featureFlagDefaults = map[string]bool{
 	// When true, the fallback collection key for keyless source tables will be
 	// ["/_meta/row_id"] instead of ["/_meta/polled", "/_meta/index"].
-	"keyless_row_id": false,
+	"keyless_row_id": true,
 }
 
 // Config tells the connector how to connect to and interact with the source database.
