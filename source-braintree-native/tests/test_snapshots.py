55012c65ed94be9effb93116501322d7110a8bba
Alex Bair <alexbair143@gmail.com>
2025-03-05 18:04:18 -0500

    source-braintree-native: update capture snapshot & redact frequently changing values
    A lot of fields frequently change in the capture snapshot. I'm guessing
    this is due to the recurring nature of subscriptions somehow. Redacting
    a lot of fields makes the test more resilient to these changes at the
    price of obscuring what the actual values of those fields should look
    like.
    
    I do find it sometimes helpful to know when certain fields are added or
    removed, either by the SaaS provider or changes we make to the connector.
    I made a helper function redact_nested_fields so we still snapshot the
    fields within lists & dictionaries while being able to redact nested
    primitive types.

--- source-braintree-native/tests/test_snapshots.py
+++ source-braintree-native/tests/test_snapshots.py
@@ -1,13 +1,48 @@
 import json
 import subprocess
 
+COMMON_FIELDS_TO_REDACT = [
+    'created_at',
+    'updated_at',
+    'id',
+    'global_id',
+    'graphql_id',
+]
+
+TRANSACTION_FIELDS_TO_REDACT = [
+    'authorization_expires_at',
+    'disbursement_details',
+    'network_transaction_id',
+    'payment_receipt',
+    'processor_authorization_code',
+    'settlement_batch_id',
+    'status_history',
+    'subscription',
+    'subscription_details',
+]
+
+
 SUBSCRIPTION_FIELDS_TO_REDACT = [
     'billing_period_end_date',
     'billing_period_start_date',
     'next_billing_date',
     'paid_through_date',
-    'updated_at',
 ]
+
+def redact_nested_fields(input: list | dict):
+    if isinstance(input, list):
+        for index, element in enumerate(input):
+            if isinstance(element, list) or isinstance(element, dict):
+                redact_nested_fields(element)
+            else:
+                input[index] = 'redacted'
+    elif isinstance(input, dict):
+        for key in list(input.keys()):
+            if isinstance(input[key], list) or isinstance(input[key], dict):
+                redact_nested_fields(input[key])
+            else:
+                input[key] = 'redacted'
+
 
 def test_capture(request, snapshot):
     result = subprocess.run(
@@ -39,6 +74,10 @@ def test_capture(request, snapshot):
     for l in unique_stream_lines:
         stream, rec = l[0], l[1]
 
+        for field in COMMON_FIELDS_TO_REDACT:
+            if field in rec:
+                rec[field] = 'redacted'
+
         if 'evidence' in rec:
             evidence: list = rec['evidence']
             for e in evidence:
@@ -51,6 +90,12 @@ def test_capture(request, snapshot):
 
             rec['status_history'] = [rec['status_history'][-1]]
             rec['transactions'] = [rec['transactions'][-1]]
+        if stream == 'acmeCo/transactions':
+            for field in TRANSACTION_FIELDS_TO_REDACT:
+                if isinstance(rec[field], list) or isinstance(rec[field], dict):
+                    redact_nested_fields(rec[field])
+                else:
+                    rec[field] = 'redacted'
 
     assert snapshot("capture.stdout.json") == unique_stream_lines
 
