2ff1ae567bdbd6b74e81db6e76f2cb2a3305d013
Will Baker <wbaker@gmail.com>
2025-03-05 14:00:15 -0500

    materialize-iceberg: add to CI
    Adds integration tests, a dockerfile, and entries in the Github actions file to
    build the connector.

---
+++ tests/materialize/materialize-iceberg/fetch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+function exportToJsonl() {
+  ${ICEBERG_HELPER_CMD} read ${NAMESPACE}."$1" | jq -s "sort_by([.id, .flow_published_at]) | { _table: \"$1\", rows: . }"
+}
+
+exportToJsonl "simple"
+exportToJsonl "duplicate_keys_standard"
+exportToJsonl "multiple_types"
+exportToJsonl "formatted_strings"
+exportToJsonl "deletions"
+exportToJsonl "binary_key"
