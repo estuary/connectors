52124ef41e8cbffb7983f15fe260be2840a5eb43
Mahdi Dibaiee <mdibaiee@pm.me>
2025-03-10 15:24:02 +0000

    materialize-databricks: use merge bounds in Load and Merge queries

--- materialize-databricks/sqlgen_test.go
+++ materialize-databricks/sqlgen_test.go
@@ -31,6 +31,42 @@ func TestSQLGeneration(t *testing.T) {
 	for _, tpl := range []*template.Template{
 		tplLoadQuery,
 		tplMergeInto,
+	} {
+		tbl := tables[0]
+		require.False(t, tbl.DeltaUpdates)
+		var testcase = tbl.Identifier + " " + tpl.Name()
+
+		bounds := []sql.MergeBound{
+			{
+				Column:       tbl.Keys[0],
+				LiteralLower: databricksDialect.Literal(int64(10)),
+				LiteralUpper: databricksDialect.Literal(int64(100)),
+			},
+			{
+				Column: tbl.Keys[1],
+				// No bounds - as would be the case for a boolean key, which
+				// would be a very weird key, but technically allowed.
+			},
+			{
+				Column:       tbl.Keys[2],
+				LiteralLower: databricksDialect.Literal("aGVsbG8K"),
+				LiteralUpper: databricksDialect.Literal("Z29vZGJ5ZQo="),
+			},
+		}
+
+		tf := tableWithFiles{
+			Table:       &tbl,
+			StagingPath: "test-staging-path",
+			Files:       []string{"file1", "file2"},
+			Bounds:      bounds,
+		}
+
+		snap.WriteString("--- Begin " + testcase + " ---")
+		require.NoError(t, tpl.Execute(snap, &tf))
+		snap.WriteString("--- End " + testcase + " ---\n\n")
+	}
+
+	for _, tpl := range []*template.Template{
 		tplCopyIntoDirect,
 	} {
 		tbl := tables[0]
