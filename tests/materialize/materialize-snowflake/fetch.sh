238bf8a99d05022bc74175ed4b03c9c1a57f8136
Mahdi Dibaiee <mdibaiee@pm.me>
2025-03-24 20:58:31 +0000

    materialize-snowflake: hash pipe names with special characters

--- tests/materialize/materialize-snowflake/fetch.sh
+++ tests/materialize/materialize-snowflake/fetch.sh
@@ -5,13 +5,15 @@ set -o pipefail
 set -o nounset
 
 function exportToJsonl() {
-  go run ${TEST_DIR}/materialize-snowflake/fetch-data.go "$1" | jq "{ "_table": \"$1\", rows: map(del(.FLOW_DOCUMENT)) }"
+  name=$(echo "$1" | tr -d '"')
+  go run ${TEST_DIR}/materialize-snowflake/fetch-data.go "$1" | jq "{ "_table": \"$name\", rows: map(del(.FLOW_DOCUMENT)) }"
 }
 
 exportToJsonl "simple"
 exportToJsonl "duplicate_keys_standard"
 exportToJsonl "duplicate_keys_delta"
 exportToJsonl "duplicate_keys_delta_exclude_flow_doc"
+exportToJsonl '"duplicate keys @ with spaces"'
 exportToJsonl "multiple_types"
 exportToJsonl "formatted_strings"
 exportToJsonl "symbols"
