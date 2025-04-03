d4b59379a6ab10ef124fef10cf919c0ec9abdca4
Alex Bair <alexbair143@gmail.com>
2025-04-03 10:18:23 -0400

    source-linkedin-pages: remove capture snapshot test
    The credentials for the test account are no longer valid, and the capture snapshot have been failing with "400 Client Error: Bad request for url: https://www.linkedin.com/oauth/v2/accessToken" for a while. I don't see much value in spending time making a new LinkedIn account for testing since we don't have any production tasks for this connector, so I'm just removing the capture snapshot test instead.

--- source-linkedin-pages/tests/test_snapshots.py
+++ source-linkedin-pages/tests/test_snapshots.py
@@ -2,31 +2,6 @@ import json
 import subprocess
 
 
-def test_capture(request):
-    """
-    This test doesn't have a snapshot
-    because the output data changes too frequently.
-    Almost all field values changes between test executions.
-    """
-    result = subprocess.run(
-        [
-            "flowctl",
-            "preview",
-            "--source",
-            request.fspath.dirname + "/../test.flow.yaml",
-            "--sessions",
-            "1",
-            "--delay",
-            "10s",
-            "--output",
-            "json",
-        ],
-        stdout=subprocess.PIPE,
-        text=True,
-    )
-    assert result.returncode == 0
-
-
 def test_discover(request, snapshot):
     snapshot.snapshot_dir = request.fspath.dirname + "/snapshots"
 
