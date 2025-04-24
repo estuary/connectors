04628090e2676ddd79cea3334af0bb2c61b2eeb3
Will Baker <wbaker@gmail.com>
2025-04-24 14:11:07 -0400

    blob: expand Bucket interface to include listing, deleting, and validating permissions
    Include the common operations of listing and deleting objects in the Bucket
    interface, as well as a helper for validating permissions of a bucket to do the
    things it needs to do.
    
    The `List` method is inspired the same one in the `filesource` package, but is
    at this point much simpler. It's not an immediate goal to rework the object
    storage filesource captures to use the `blob.Bucket` interface, but I did want
    to provide the opportunity to easily extend the capabilities of this `List`
    operation if future connectors need more features.

--- go/blob/bucket.go
+++ go/blob/bucket.go
@@ -8,8 +8,16 @@
 package blob
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"fmt"
 	"io"
+	"iter"
+	"path"
+
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
 )
 
 type writerConfig struct {
@@ -32,6 +40,23 @@ func getWriterConfig(opts []WriterOption) writerConfig {
 	return cfg
 }
 
+// CheckPermissionsConfig specifies which operations will be validated in
+// CheckPermissions.
+//
+// A typical materialization that writes staged files, reads from those files
+// with known key, and then deletes them by key should leave `ReadOnly` and
+// `Lister` unset to validate writing, reading, and deleting a key.
+//
+// A materialization requiring the ability to also list keys should set
+// `Lister`.
+//
+// A typical capture that only lists and reads from files should set `ReadOnly`.
+type CheckPermissionsConfig struct {
+	ReadOnly bool   // only verify that blobs can be read & listed; does not write or delete
+	Prefix   string // prefix for reading/writing a test blob, and listing
+	Lister   bool   // check that the list operation can succeed; implied by ReadOnly
+}
+
 // Bucket is a common interface for working with blob storage. Conceptually, a
 // Bucket is aligned with an S3 or GCS bucket, or an Azure Blob storage
 // "container".
@@ -46,6 +71,33 @@ type Bucket interface {
 
 	// Upload uploads an object as a stream.
 	Upload(ctx context.Context, key string, r io.Reader, opts ...WriterOption) error
+
+	// URI creates an identifier from a file key, usually by prepending the URI
+	// scheme, for example "s3://".
+	URI(key string) string
+
+	// Delete deletes the objects per the list of URIs.
+	Delete(ctx context.Context, uris []string) error
+
+	// CheckPermissions performs test operations to validate the bucket's access
+	// per `cfg`.
+	CheckPermissions(ctx context.Context, cfg CheckPermissionsConfig) error
+
+	// List lists the items in the bucket. It only yields actual objects; never
+	// "directory" or prefix items.
+	List(ctx context.Context, query Query) iter.Seq2[ObjectInfo, error]
+}
+
+// Query of objects to be returned by List.
+type Query struct {
+	// Prefix constrains the listing to paths which begin with the prefix.
+	Prefix string
+}
+
+// ObjectInfo is returned by List.
+type ObjectInfo struct {
+	// Key is the path of the object. It does not include the bucket name.
+	Key string
 }
 
 type uploadFn func(context.Context, string, io.Reader, ...WriterOption) error
@@ -83,4 +135,84 @@ func (w *blobWriteCloser) Close() error {
 
 func (w *blobWriteCloser) Write(p []byte) (int, error) {
 	return w.w.Write(p)
+}
+
+// Sanitized versions of user-facing errors for the two common cases of the
+// wrong bucket input, or incorrect / insufficient authorization to do various
+// actions on that bucket.
+var (
+	permissionsErrorNoSuchBucket = errors.New("no such bucket")
+	permissionsErrorUnauthorized = errors.New("unauthorized")
+)
+
+func checkPermissions[T error](
+	ctx context.Context,
+	bucket Bucket,
+	cfg CheckPermissionsConfig,
+	// checkReadOnlyFn is a system-specific function for reading from a key that
+	// we know doesn't exist to determine if we can read from any key. This is
+	// done instead of writing a file and then trying to read it, because we
+	// want to check reading permissions but not writing permissions. It will
+	// always be called after a successful `List`, and generally a "not found"
+	// response should be considered as success.
+	checkReadOnlyFn func(key string) error,
+	// handleErr adapts the error of type T into a more user-friendly error.
+	handleErr func(err T) error,
+) error {
+	adaptErr := func(err error) error {
+		var asErr T
+		if errors.As(err, &asErr) {
+			// Log the original error message, since it can sometimes contain
+			// useful (although potentially confusing) information.
+			log.WithField("err", err).Error("bucket permissions check failed")
+			err = handleErr(asErr)
+		}
+		return err
+	}
+
+	bucketAndPrefixURI := bucket.URI(cfg.Prefix)
+
+	if cfg.ReadOnly || cfg.Lister {
+		for _, err := range bucket.List(ctx, Query{Prefix: cfg.Prefix}) {
+			if err != nil {
+				return fmt.Errorf("unable to list %q: %w", bucketAndPrefixURI, adaptErr(err))
+			}
+			break
+		}
+	}
+
+	if cfg.ReadOnly {
+		if err := checkReadOnlyFn(path.Join(cfg.Prefix, uuid.NewString())); err != nil {
+			return fmt.Errorf("unable to read from %q: %w", bucketAndPrefixURI, adaptErr(err))
+		}
+
+		return nil
+	}
+
+	testData := []byte("test")
+	testKey := path.Join(cfg.Prefix, uuid.NewString())
+	testWriter := bucket.NewWriter(ctx, testKey)
+	got := make([]byte, len(testData))
+
+	if n, err := testWriter.Write(testData); err != nil {
+		return fmt.Errorf("unable to write to %q: %w", bucketAndPrefixURI, adaptErr(err))
+	} else if n != len(testData) {
+		return fmt.Errorf("short write: %d vs %d", n, len(testData))
+	} else if err := testWriter.Close(); err != nil {
+		return fmt.Errorf("unable to write to %q: %w", bucketAndPrefixURI, adaptErr(err))
+	} else if reader, err := bucket.NewReader(ctx, testKey); err != nil {
+		return fmt.Errorf("unable to read from %q: %w", bucketAndPrefixURI, adaptErr(err))
+	} else if n, err := reader.Read(got); err != nil && err != io.EOF {
+		return fmt.Errorf("unable to read from %q: %w", bucketAndPrefixURI, adaptErr(err))
+	} else if n != len(testData) {
+		return fmt.Errorf("short read: %d vs %d", n, len(testData))
+	} else if !bytes.Equal(testData, got) {
+		return fmt.Errorf("io error: read bytes do not match written bytes")
+	} else if err := reader.Close(); err != nil {
+		return fmt.Errorf("unexpected error when closing reader: %w", err)
+	} else if err := bucket.Delete(ctx, []string{bucket.URI(testKey)}); err != nil {
+		return fmt.Errorf("unable to delete from %q: %w", bucketAndPrefixURI, adaptErr(err))
+	}
+
+	return nil
 }
