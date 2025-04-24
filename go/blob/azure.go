e231c1a7c1d9e715ef24356a15a472222403a5f0
Will Baker <wbaker@gmail.com>
2025-04-24 14:11:07 -0400

    blob: expand Bucket interface to include listing, deleting, and validating permissions
    Include the common operations of listing and deleting objects in the Bucket
    interface, as well as a helper for validating permissions of a bucket to do the
    things it needs to do.
    
    The `List` method is inspired the same one in the `filesource` package, but is
    at this point much simpler. It's not an immediate goal to rework the object
    storage filesource captures to use the `blob.Bucket` interface, but I did want
    to provide the opportunity to easily extend the capabilities of this `List`
    operation if future connectors need more features.

--- go/blob/azure.go
+++ go/blob/azure.go
@@ -2,38 +2,44 @@ package blob
 
 import (
 	"context"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
+	"iter"
 	"net/url"
+	"path"
 	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/bloberror"
+	"golang.org/x/sync/errgroup"
 )
 
 const defaultAzureEndpoint = "blob.core.windows.net"
 
-type azureBlobStoreConfig struct {
+type azureBlobConfig struct {
 	storageAccountKey string
 	sasToken          string
 	endpoint          string
 }
 
-type AzureConfigOption func(*azureBlobStoreConfig)
+type AzureConfigOption func(*azureBlobConfig)
 
 func WithAzureStorageAccountKey(sak string) AzureConfigOption {
-	return func(cfg *azureBlobStoreConfig) {
+	return func(cfg *azureBlobConfig) {
 		cfg.storageAccountKey = sak
 	}
 }
 
 func WithAzureSasToken(token string) AzureConfigOption {
-	return func(cfg *azureBlobStoreConfig) {
+	return func(cfg *azureBlobConfig) {
 		cfg.sasToken = token
 	}
 }
 
 func WithAzureEndpoint(ep string) AzureConfigOption {
-	return func(cfg *azureBlobStoreConfig) {
+	return func(cfg *azureBlobConfig) {
 		cfg.endpoint = ep
 	}
 }
@@ -48,9 +54,9 @@ type AzureBlobBucket struct {
 // NewAzureBlobBucket creates an Azure Blob object storage bucket. At least one
 // AzureConfigOption must be provided to specify authentication; either storage
 // account key or SAS token.
-func NewAzureBlobBucket(ctx context.Context, container string, accountName string, opts []AzureConfigOption) (*AzureBlobBucket, error) {
-	cfg := azureBlobStoreConfig{endpoint: defaultAzureEndpoint}
-	for _, opt := range opts {
+func NewAzureBlobBucket(ctx context.Context, container string, accountName string, authOpt AzureConfigOption, otherOpts ...AzureConfigOption) (*AzureBlobBucket, error) {
+	cfg := azureBlobConfig{endpoint: defaultAzureEndpoint}
+	for _, opt := range append(otherOpts, authOpt) {
 		opt(&cfg)
 	}
 	if cfg.sasToken != "" && cfg.storageAccountKey != "" {
@@ -63,13 +69,15 @@ func NewAzureBlobBucket(ctx context.Context, container string, accountName strin
 	}
 
 	var client *azblob.Client
-	if cfg.sasToken != "" {
-		serviceUrl.RawQuery = strings.TrimPrefix(cfg.sasToken, "?")
+	if tok := cfg.sasToken; tok != "" {
+		serviceUrl.RawQuery = strings.TrimPrefix(tok, "?")
 		if client, err = azblob.NewClientWithNoCredential(serviceUrl.String(), nil); err != nil {
 			return nil, fmt.Errorf("failed to create client with SAS token: %w", err)
 		}
-	} else if cfg.storageAccountKey != "" {
-		if cred, err := azblob.NewSharedKeyCredential(accountName, cfg.storageAccountKey); err != nil {
+	} else if sak := cfg.storageAccountKey; sak != "" {
+		if _, err := base64.StdEncoding.DecodeString(sak); err != nil {
+			return nil, fmt.Errorf("invalid storage account key: must be base64-encoded")
+		} else if cred, err := azblob.NewSharedKeyCredential(accountName, sak); err != nil {
 			return nil, fmt.Errorf("failed to create storage client shared key credential: %w", err)
 		} else if client, err = azblob.NewClientWithSharedKeyCredential(serviceUrl.String(), cred, nil); err != nil {
 			return nil, fmt.Errorf("failed to create client with storage account key: %w", err)
@@ -84,8 +92,8 @@ func NewAzureBlobBucket(ctx context.Context, container string, accountName strin
 	}, nil
 }
 
-func (s *AzureBlobBucket) NewReader(ctx context.Context, key string) (io.ReadCloser, error) {
-	r, err := s.client.DownloadStream(ctx, s.container, key, nil)
+func (b *AzureBlobBucket) NewReader(ctx context.Context, key string) (io.ReadCloser, error) {
+	r, err := b.client.DownloadStream(ctx, b.container, key, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -93,11 +101,11 @@ func (s *AzureBlobBucket) NewReader(ctx context.Context, key string) (io.ReadClo
 	return r.Body, nil
 }
 
-func (s *AzureBlobBucket) NewWriter(ctx context.Context, key string, opts ...WriterOption) io.WriteCloser {
-	return newBlobWriteCloser(ctx, s.Upload, key, opts...)
+func (b *AzureBlobBucket) NewWriter(ctx context.Context, key string, opts ...WriterOption) io.WriteCloser {
+	return newBlobWriteCloser(ctx, b.Upload, key, opts...)
 }
 
-func (s *AzureBlobBucket) Upload(ctx context.Context, key string, r io.Reader, opts ...WriterOption) error {
+func (b *AzureBlobBucket) Upload(ctx context.Context, key string, r io.Reader, opts ...WriterOption) error {
 	cfg := getWriterConfig(opts)
 
 	var uploadOpts *azblob.UploadStreamOptions
@@ -110,7 +118,92 @@ func (s *AzureBlobBucket) Upload(ctx context.Context, key string, r io.Reader, o
 		uploadOpts.Metadata = meta
 	}
 
-	_, err := s.client.UploadStream(ctx, s.container, key, r, uploadOpts)
+	_, err := b.client.UploadStream(ctx, b.container, key, r, uploadOpts)
 
 	return err
+}
+
+func (b *AzureBlobBucket) URI(key string) string {
+	// This is not a scheme-specific URI like S3 and GCS use, where they start
+	// with `s3://` or `gs://`, but rather a full HTTPS URL, since that is all
+	// Azure officially supports.
+	return strings.TrimSuffix(b.client.URL(), "/") + "/" + path.Join(b.container, key)
+}
+
+func (b *AzureBlobBucket) Delete(ctx context.Context, uris []string) error {
+	group, groupCtx := errgroup.WithContext(ctx)
+	group.SetLimit(5)
+
+	for _, uri := range uris {
+		trimmed := strings.TrimPrefix(strings.TrimPrefix(uri, b.client.URL()), "/")
+		container, blobName, ok := strings.Cut(trimmed, "/")
+		if !ok {
+			return fmt.Errorf("invalid uri %q", uri)
+		}
+		group.Go(func() error {
+			if _, err := b.client.DeleteBlob(groupCtx, container, blobName, nil); err != nil {
+				return fmt.Errorf("deleting blob %q: %w", uri, err)
+			}
+			return nil
+		})
+	}
+
+	return group.Wait()
+}
+
+func (b *AzureBlobBucket) List(ctx context.Context, query Query) iter.Seq2[ObjectInfo, error] {
+	input := &azblob.ListBlobsFlatOptions{
+		Include: azblob.ListBlobsInclude{Metadata: true},
+	}
+	if query.Prefix != "" {
+		input.Prefix = &query.Prefix
+	}
+	pager := b.client.NewListBlobsFlatPager(b.container, input)
+
+	return func(yield func(ObjectInfo, error) bool) {
+		for pager.More() {
+			page, err := pager.NextPage(ctx)
+			if err != nil {
+				yield(ObjectInfo{}, err)
+				return
+			}
+
+			for _, obj := range page.Segment.BlobItems {
+				if isFolder, ok := obj.Metadata["hdi_isfolder"]; ok && *isFolder == "true" {
+					// Exclude directories.
+					continue
+				}
+
+				if !yield(ObjectInfo{
+					Key: *obj.Name,
+				}, nil) {
+					return
+				}
+
+			}
+		}
+	}
+}
+
+func (b *AzureBlobBucket) CheckPermissions(ctx context.Context, cfg CheckPermissionsConfig) error {
+	checkReadOnlyFn := func(key string) error {
+		_, err := b.client.DownloadBuffer(ctx, b.container, key, []byte{}, nil)
+		if !bloberror.HasCode(err, bloberror.BlobNotFound) {
+			return err
+		}
+
+		return nil
+	}
+
+	handleErr := func(err error) error {
+		if bloberror.HasCode(err, bloberror.ContainerNotFound) {
+			return errors.New("no such container") // azure calls them containers, not buckets
+		} else if bloberror.HasCode(err, bloberror.AuthorizationFailure) {
+			return permissionsErrorUnauthorized
+		}
+
+		return err
+	}
+
+	return checkPermissions(ctx, b, cfg, checkReadOnlyFn, handleErr)
 }
