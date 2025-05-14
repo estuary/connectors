fd4a68decca6fc10bf2579d1e06f62e5e7fe827f
Jon Wihl <jonathanwihl@gmail.com>
2025-05-14 11:26:56 -0400

    Updating tests, making articles incremental_cursor_paginated

--- source-zendesk-support-native/source_zendesk_support_native/models.py
+++ source-zendesk-support-native/source_zendesk_support_native/models.py
@@ -255,8 +255,7 @@ class FilterParam(StrEnum):
     START_TIME = "start_time"
     SINCE = "since"
 
-class ArticleResponse(IncrementalCursorPaginatedResponse):
-    resources: list[ZendeskResource] = Field(alias="articles")
+
 
 # Incremental resources that can be filtered by a start_time query param.
 # Tuples contain the name, path, filter param name, cursor field, and response model for each resource. 
@@ -264,7 +263,6 @@ INCREMENTAL_CURSOR_PAGINATED_RESOURCES: list[tuple[str, str, FilterParam, str, t
     ("ticket_skips", "skips", FilterParam.START_TIME, "updated_at", TicketSkipsResponse),
     ("ticket_metric_events", "incremental/ticket_metric_events", FilterParam.START_TIME, "time", TicketMetricEventsResponse),
     ("ticket_activities", "activities", FilterParam.START_TIME, "updated_at", TicketActivitiesResponse),
-    ("articles", "help_center/articles", FilterParam.START_TIME, "updated_at", ArticleResponse),
 ]
 
 
@@ -341,7 +339,12 @@ class MacrosResponse(ClientSideIncrementalCursorPaginatedResponse):
 class OrganizationMembershipsResponse(ClientSideIncrementalCursorPaginatedResponse):
     resources: list[TimestampedResource] = Field(alias="organization_memberships")
 
+class Article(TimestampedResource):
+    pass
 
+class ArticleResponse(IncrementalCursorPaginatedResponse):
+    resources: list[Article] = Field(alias="articles")
+    
 class Post(TimestampedResource):
     comment_count: int
     vote_count: int
@@ -367,6 +370,7 @@ CLIENT_SIDE_FILTERED_CURSOR_PAGINATED_RESOURCES: list[tuple[str, str, dict[str, 
     ("posts", "community/posts", None, PostsResponse),
     ("ticket_fields", "ticket_fields", None, TicketFieldsResponse),
     ("topics", "community/topics", None, TopicsResponse),
+    ("articles", "help_center/articles", None, ArticleResponse),
 ]
 
 
