32a4764c90c6a178a7a66c216fefc62ef046af5e
Will Baker <wbaker@gmail.com>
2025-05-14 11:41:25 -0400

    materialize-motherduck: add merge bounds to load and store queries
    Adds merge bounds to load and store queries in MotherDuck, as we have added for
    the other warehouse materializations.
    
    In the past I have mentioned a desire to have a more principled way to generally
    incorporate the "bounds" concept in materializations, since most of them will
    want access to those. My best idea for this so far is to incorporate them into
    the transactor, but that would also require more deeply incorporating the
    concept of "value converters" that materializations use to convert values to
    their specific needs. Probably a good idea, but a lot more work.
    
    But there is an immediate use case for performance optimization of MotherDuck
    load queries, and following this pattern that we have used before is relatively
    simple and realistically I don't know if I'm ever going to get around to doing
    it in a more elegant way.

--- materialize-motherduck/sqlgen_test.go
+++ materialize-motherduck/sqlgen_test.go
@@ -28,21 +28,57 @@ func TestSQLGeneration(t *testing.T) {
 		},
 	)
 
-	for _, tbl := range tables {
-		for _, tpl := range []*template.Template{
-			tplLoadQuery,
-			tplStoreDeleteQuery,
-			tplStoreQuery,
-		} {
-			var testcase = tbl.Identifier + " " + tpl.Name()
+	for _, tpl := range []*template.Template{
+		tplLoadQuery,
+		tplStoreDeleteQuery,
+		tplStoreQuery,
+	} {
+		// Standard updates cases, which use merge bounds for load and merge
+		// queries.
+		tbl := tables[0]
+		require.False(t, tbl.DeltaUpdates)
+		var testcase = tbl.Identifier + " " + tpl.Name()
 
-			snap.WriteString("--- Begin " + testcase + " ---")
-			require.NoError(t, tpl.Execute(snap, &queryParams{
-				Table: tbl,
-				Files: []string{"s3://bucket/file1", "s3://bucket/file2"},
-			}))
-			snap.WriteString("--- End " + testcase + " ---\n\n")
+		bounds := []sql.MergeBound{
+			{
+				Column:       tbl.Keys[0],
+				LiteralLower: duckDialect.Literal(int64(10)),
+				LiteralUpper: duckDialect.Literal(int64(100)),
+			},
+			{
+				Column: tbl.Keys[1],
+				// No bounds - as would be the case for a boolean key, which
+				// would be a very weird key, but technically allowed.
+			},
+			{
+				Column:       tbl.Keys[2],
+				LiteralLower: duckDialect.Literal("aGVsbG8K"),
+				LiteralUpper: duckDialect.Literal("Z29vZGJ5ZQo="),
+			},
 		}
+
+		snap.WriteString("--- Begin " + testcase + " ---")
+		require.NoError(t, tpl.Execute(snap, &queryParams{
+			Table:  tbl,
+			Bounds: bounds,
+			Files:  []string{"s3://bucket/file1", "s3://bucket/file2"},
+		}))
+		snap.WriteString("--- End " + testcase + " ---\n\n")
+	}
+
+	{
+		// Delta updates only run stores, never loads or merges.
+		tpl := tplStoreQuery
+		tbl := tables[1]
+		require.True(t, tbl.DeltaUpdates)
+		var testcase = tbl.Identifier + " " + tpl.Name()
+
+		snap.WriteString("--- Begin " + testcase + " ---")
+		require.NoError(t, tpl.Execute(snap, &queryParams{
+			Table: tbl,
+			Files: []string{"s3://bucket/file1", "s3://bucket/file2"},
+		}))
+		snap.WriteString("--- End " + testcase + " ---\n\n")
 	}
 
 	cupaloy.SnapshotT(t, snap.String())
