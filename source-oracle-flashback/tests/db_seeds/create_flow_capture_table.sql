f407b856db32e9edc5777773ae4ed39a340e471e
Mahdi Dibaiee <mdibaiee@pm.me>
2025-05-15 13:13:43 +0100

    remove source-oracle-flashback, deprecated connector

--- source-oracle-flashback/tests/db_seeds/create_flow_capture_table.sql
+++
@@ -1,3 +0,0 @@
-CREATE SCHEMA AUTHORIZATION flow_capture
-  CREATE TABLE flow_capture.test(id integer)
-  GRANT SELECT ON flow_capture.test TO ADMIN
--- source-oracle-flashback/tests/db_seeds/create_test_changes.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE flow_test.test_changes(
-  id int primary key,
-  str nvarchar2(2000)
-)
