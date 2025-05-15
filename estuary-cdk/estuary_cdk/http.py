5ffa918e04b807dd802ff66dee3bb2f359c4d003
Justin Smith <contact@justinsmith.sh>
2025-05-15 06:25:07 -0500

    estuary-cdk: increase aiohttp.ClientSession timeouts in HTTPMixin
    Increases timeout total to 30-minutes and sock_connect to 5-minutes.

--- estuary-cdk/estuary_cdk/http.py
+++ estuary-cdk/estuary_cdk/http.py
@@ -332,7 +332,12 @@ class HTTPMixin(Mixin, HTTPSession):
     token_source: TokenSource | None = None
 
     async def _mixin_enter(self, _: Logger):
-        self.inner = aiohttp.ClientSession()
+        self.inner = aiohttp.ClientSession(
+            timeout=aiohttp.ClientTimeout(
+                total=30 * 60,  # 30-minutes - default is 5-minutes
+                sock_connect=5 * 30, # 5-minutes - default is 30-seconds
+            ),
+        )
         self.rate_limiter = RateLimiter()
         return self
 
