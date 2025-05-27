64c23c5ec1d0d4afb3c75c111729df28331bed82
Will Donnelly <will@estuary.dev>
2025-05-27 12:18:20 -0500

    source-oracle: Enable emit_sourced_schemas by default

--- source-oracle/main.go
+++ source-oracle/main.go
@@ -30,11 +30,11 @@ import (
 var featureFlagDefaults = map[string]bool{
 	// When set, discovered collection schemas will request that schema inference be
 	// used _in addition to_ the full column/types discovery we already do.
-	"use_schema_inference": false,
+	"use_schema_inference": true,
 
 	// When set, discovered collection schemas will be emitted as SourcedSchema messages
 	// so that Flow can have access to 'official' schema information from the source DB.
-	"emit_sourced_schemas": false,
+	"emit_sourced_schemas": true,
 }
 
 type sshForwarding struct {
