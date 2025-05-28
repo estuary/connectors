58caa35b12503dc18c3ba3c7fcd928a14238a728
dyaffe <dyaffe1@gmail.com>
2025-05-28 10:26:21 -0400

    Update config.go
    Updating to use the mongodb+srv:// prefix in our description because a user tried it without that by copying the description and it didn't work.

--- materialize-mongodb/config.go
+++ materialize-mongodb/config.go
@@ -16,7 +16,7 @@ type tunnelConfig struct {
 }
 
 type config struct {
-	Address    string `json:"address" jsonschema:"title=Address" jsonschema_description:"The connection URI for your deployment without the username and password. For example mongodb://my-mongo.test?authSource=admin." jsonschema_extras:"order=0"`
+	Address    string `json:"address" jsonschema:"title=Address" jsonschema_description:"The connection URI for your deployment without the username and password. For example mongodb+srv://my-mongo.test?authSource=admin." jsonschema_extras:"order=0"`
 	User       string `json:"user" jsonschema:"title=User,description=Database user to connect as." jsonschema_extras:"order=1"`
 	Password   string `json:"password" jsonschema:"title=Password,description=Password for the specified database user." jsonschema_extras:"secret=true,order=2"`
 	Database   string `json:"database" jsonschema:"title=Database,description=Name of the database to materialize to." jsonschema_extras:"order=3"`
