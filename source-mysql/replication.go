4246cfc75cced2090c4a684423d9fa7b4de6715a
Will Donnelly <will@estuary.dev>
2025-06-13 18:38:33 -0500

    source-mysql: Fix MySQL enum discovery
    The previous MySQL enum discovery logic was pretty hacky and had
    several edge cases, one of which a production capture recently
    ran into. This commit rips that decoding logic out entirely and
    swaps it out for reusing the same Vitess SQL parser we already
    use for replication DDL query handling.
    
    There might be a more elegant way to ask it to parse an enum type,
    but I briefly checked and didn't see one so simply splatting the
    type definition into a dummy `CREATE TABLE` statement seemed like
    a decent enough workaround.

--- source-mysql/replication.go
+++ source-mysql/replication.go
@@ -1068,25 +1068,6 @@ func translateDataType(meta *mysqlTableMetadata, t *sqlparser.ColumnType) any {
 	}
 }
 
-// unquoteEnumValues applies MySQL single-quote-unescaping to a list of single-quoted
-// escaped string values such as the EnumValues list returned by the Vitess SQL Parser
-// package when parsing a DDL query involving enum/set values.
-//
-// The single-quote wrapping and escaping of these strings is clearly deliberate, as
-// under the hood the package actually tokenizes the strings to raw values and then
-// explicitly calls `encodeSQLString()` to re-wrap them when building the AST. The
-// actual reason for doing this is unknown however, and it makes very little sense.
-//
-// So whatever, here's a helper function to undo that escaping and get back down to
-// the raw strings again.
-func unquoteEnumValues(values []string) []string {
-	var unquoted []string
-	for _, qval := range values {
-		unquoted = append(unquoted, unquoteMySQLString(qval))
-	}
-	return unquoted
-}
-
 func resolveTableName(defaultSchema string, name sqlparser.TableName) sqlcapture.StreamID {
 	var schema, table = name.Qualifier.String(), name.Name.String()
 	if schema == "" {
