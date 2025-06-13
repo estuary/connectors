53ace2ff46bfbcbcdb3c1589fb1903b52196ee30
Will Donnelly <will@estuary.dev>
2025-06-13 13:05:54 -0500

    source-sqlserver: Add 'Discover Only Enabled' setting
    This is the SQL Server analogue to the PostgreSQL "Discover Only
    Published Tables" setting, which causes discovery to only return
    bindings for tables which are already CDC-enabled.
    
    This isn't the default behavior because there is still some value
    to having the default user setup flow list every table in the DB
    and let them select the ones they want and _then_ complain that
    they haven't enabled CDC on them, but it's a fairly common thing
    to want so we should have an option for it.

--- source-sqlserver/main.go
+++ source-sqlserver/main.go
@@ -81,6 +81,7 @@ type Config struct {
 }
 
 type advancedConfig struct {
+	DiscoverOnlyEnabled         bool   `json:"discover_only_enabled,omitempty" jsonschema:"title=Discover Only CDC-Enabled Tables,description=When set the connector will only discover tables which have already had CDC capture instances enabled."`
 	SkipBackfills               string `json:"skip_backfills,omitempty" jsonschema:"title=Skip Backfills,description=A comma-separated list of fully-qualified table names which should not be backfilled."`
 	BackfillChunkSize           int    `json:"backfill_chunk_size,omitempty" jsonschema:"title=Backfill Chunk Size,default=50000,description=The number of rows which should be fetched from the database in a single backfill query."`
 	AutomaticChangeTableCleanup bool   `json:"change_table_cleanup,omitempty" jsonschema:"title=Automatic Change Table Cleanup,default=false,description=When set the connector will delete CDC change table entries as soon as they are persisted into Flow. Requires DBO permissions to use."`
