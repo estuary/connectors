bc8091fa1a1da6981ce0a38811129690b7036240
Will Baker <wbaker@gmail.com>
2025-06-16 18:19:28 -0400

    materialize-sql: copy materialize-sql to materialize-sql-v2
    This is copying the entire materialize-sql folder to materialize-sql-v2. The
    diff is large but that's all it is.
    
    As a risk mitigation strategy, not all SQL materializations will be moved over
    to the updated materialize-sql that uses the `Materializer` interface and
    mechanisms at the same time.
    
    Instead, they will be gradually converted over to use materialize-sql-v2, which
    implements `Materializer`.

---
+++ materialize-sql-v2/meta_tables.go
@@ -0,0 +1,86 @@
+package sql
+
+import (
+	"github.com/estuary/flow/go/protocols/flow"
+)
+
+const (
+	// DefaultFlowCheckpoints is the default table for checkpoints.
+	DefaultFlowCheckpoints = "flow_checkpoints_v1"
+)
+
+// FlowCheckpointsTable returns the TableShape that (optionally) holds fenced
+// materialization checkpoints, where supported by the endpoint and connector.
+func FlowCheckpointsTable(base TablePath) *TableShape {
+	return &TableShape{
+		Path:         append(base, DefaultFlowCheckpoints),
+		Binding:      -1,
+		Source:       "",
+		Comment:      "This table holds Flow processing checkpoints used for exactly-once processing of materializations",
+		DeltaUpdates: false,
+		Keys: []Projection{
+			{
+				Projection: flow.Projection{
+					Field: "materialization",
+					Inference: flow.Inference{
+						Types:   []string{"string"},
+						Exists:  flow.Inference_MUST,
+						String_: &flow.Inference_String{},
+					},
+				},
+				RawFieldConfig: nil,
+				Comment:        "The name of the materialization.",
+			},
+			{
+				Projection: flow.Projection{
+					Field: "key_begin",
+					Inference: flow.Inference{
+						Types:  []string{"integer"},
+						Exists: flow.Inference_MUST,
+					},
+				},
+				RawFieldConfig: nil,
+				Comment:        "The inclusive lower-bound key hash covered by this checkpoint.",
+			},
+			{
+				Projection: flow.Projection{
+					Field: "key_end",
+					Inference: flow.Inference{
+						Types:  []string{"integer"},
+						Exists: flow.Inference_MUST,
+					},
+				},
+				RawFieldConfig: nil,
+				Comment:        "The inclusive upper-bound key hash covered by this checkpoint.",
+			},
+		},
+		Values: []Projection{
+			{
+				Projection: flow.Projection{
+					Field: "fence",
+					Inference: flow.Inference{
+						Types:  []string{"integer"},
+						Exists: flow.Inference_MUST,
+					},
+				},
+				RawFieldConfig: nil,
+				Comment:        "This nonce is used to uniquely identify unique process assignments of a shard and prevent them from conflicting.",
+			},
+			{
+				Projection: flow.Projection{
+					Field: "checkpoint",
+					Inference: flow.Inference{
+						Types:  []string{"string"},
+						Exists: flow.Inference_MUST,
+						String_: &flow.Inference_String{
+							ContentType: "application/x-protobuf; proto=consumer.Checkpoint",
+						},
+					},
+				},
+				RawFieldConfig: nil,
+				Comment:        "Checkpoint of the Flow consumer shard, encoded as base64 protobuf.",
+			},
+		},
+		Document: nil,
+	}
+}
