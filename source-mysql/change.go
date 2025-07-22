68d6ff2bae7caa54337b05851dac9905cba6fe55
Will Donnelly <will@estuary.dev>
2025-07-22 13:53:20 -0500

    source-mysql: Restructure change event

--- source-mysql/change.go
+++ source-mysql/change.go
@@ -25,30 +25,53 @@ func (evt *mysqlCommitEvent) AppendJSON(buf []byte) ([]byte, error) {
 }
 
 type mysqlChangeEvent struct {
-	Operation sqlcapture.ChangeOp
-	RowKey    []byte
-	Source    sqlcapture.SourceMetadata
-	Before    map[string]any
-	After     map[string]any
+	Info *mysqlChangeSharedInfo // Information about the source table structure which is shared across events.
+
+	Meta   mysqlChangeMetadata // Document metadata object representing the _meta property
+	RowKey []byte
+	Values map[string]any
+}
+
+type mysqlChangeSharedInfo struct {
+	StreamID sqlcapture.StreamID // StreamID of the table this change event came from.
+}
+
+type mysqlChangeMetadata struct {
+	Info *mysqlChangeSharedInfo `json:"-"` // Information about the source table structure which is shared across events. Copied here to simplify before-value serialization.
+
+	Operation sqlcapture.ChangeOp `json:"op"`
+	Source    mysqlSourceInfo     `json:"source"`
+	Before    map[string]any      `json:"before,omitempty"`
+}
+
+// mysqlSourceInfo is source metadata for data capture events.
+type mysqlSourceInfo struct {
+	sqlcapture.SourceCommon
+	EventCursor string `json:"cursor" jsonschema:"description=Cursor value representing the current position in the binlog."`
+	TxID        string `json:"txid,omitempty" jsonschema:"description=The global transaction identifier associated with a change by MySQL. Only set if GTIDs are enabled."`
+}
+
+func (s *mysqlSourceInfo) Common() sqlcapture.SourceCommon {
+	return s.SourceCommon
 }
 
 func (mysqlChangeEvent) IsDatabaseEvent() {}
 func (mysqlChangeEvent) IsChangeEvent()   {}
 
 func (e *mysqlChangeEvent) String() string {
-	switch e.Operation {
+	switch e.Meta.Operation {
 	case sqlcapture.InsertOp:
-		return fmt.Sprintf("Insert(%s)", e.Source.Common().StreamID())
+		return fmt.Sprintf("Insert(%s)", e.Info.StreamID)
 	case sqlcapture.UpdateOp:
-		return fmt.Sprintf("Update(%s)", e.Source.Common().StreamID())
+		return fmt.Sprintf("Update(%s)", e.Info.StreamID)
 	case sqlcapture.DeleteOp:
-		return fmt.Sprintf("Delete(%s)", e.Source.Common().StreamID())
+		return fmt.Sprintf("Delete(%s)", e.Info.StreamID)
 	}
-	return fmt.Sprintf("UnknownChange(%s)", e.Source.Common().StreamID())
+	return fmt.Sprintf("UnknownChange(%s)", e.Info.StreamID)
 }
 
 func (e *mysqlChangeEvent) StreamID() sqlcapture.StreamID {
-	return e.Source.Common().StreamID()
+	return e.Info.StreamID
 }
 
 func (e *mysqlChangeEvent) GetRowKey() []byte {
@@ -60,29 +83,7 @@ func (e *mysqlChangeEvent) GetRowKey() []byte {
 // Note that this implementation is destructive, but that's okay because
 // emitting the serialized JSON is the last thing we ever do with a change.
 func (e *mysqlChangeEvent) AppendJSON(buf []byte) ([]byte, error) {
-	var record map[string]any
-	var meta = struct {
-		Operation sqlcapture.ChangeOp       `json:"op"`
-		Source    sqlcapture.SourceMetadata `json:"source"`
-		Before    map[string]any            `json:"before,omitempty"`
-	}{
-		Operation: e.Operation,
-		Source:    e.Source,
-		Before:    nil,
-	}
-	switch e.Operation {
-	case sqlcapture.InsertOp:
-		record = e.After // Before is never used.
-	case sqlcapture.UpdateOp:
-		meta.Before, record = e.Before, e.After
-	case sqlcapture.DeleteOp:
-		record = e.Before // After is never used.
-	}
-	if record == nil {
-		record = make(map[string]any)
-	}
-	record["_meta"] = &meta
-
-	// Marshal to JSON and append to the provided buffer.
+	var record = e.Values
+	record["_meta"] = &e.Meta
 	return json.Append(buf, record, json.EscapeHTML|json.SortMapKeys)
 }
