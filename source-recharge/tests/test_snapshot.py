e26a59c2239243ff4f6360b1e5f46eb0a03d048c
Alex Bair <alexbair143@gmail.com>
2025-07-25 13:56:15 -0400

    source-recharge: add dummy credentials and update snapshots

--- source-recharge/tests/test_snapshot.py
+++ source-recharge/tests/test_snapshot.py
@@ -1,9 +1,6 @@
 import json
 import subprocess
 
-import pytest_insta.format as insta_format
-insta_format.FmtJson.dump = lambda _self, path, value: path.write_text(json.dumps(value, sort_keys=True, indent=2) + "\n", "utf-8")
-
 def test_discover(request, snapshot):
     result = subprocess.run(
         [
