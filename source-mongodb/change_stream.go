7c65aee61b05445990b5f415bb51c89bd9b0768c
Will Baker <wbaker@gmail.com>
2025-07-29 18:31:36 -0400

    Revert "source-mongodb: adjust catch-up stream retry logic"
    This reverts commit 1eb03c1d7e47fbbd60acaf4f6fcfbddf3bfbf385.

--- source-mongodb/change_stream.go
+++ source-mongodb/change_stream.go
@@ -169,22 +169,11 @@ func (c *capture) streamChanges(
 					log.WithFields(log.Fields{
 						"db":       s.db,
 						"attempts": catchUpAttempts,
-					}).Debug("change stream returned no documents during catch-up, retrying")
+					}).Info("change stream returned no documents during catch-up, retrying")
 					time.Sleep(1 * time.Second)
-					continue
 				} else if stopWhenCaughtUp && coordinator.gotCaughtUp(s.db, opTime) {
 					return nil
 				}
-				// Require 10 consecutive empty result sets to be considered
-				// caught-up when there is no event timestamp available for
-				// positive confirmation.
-				if catchUpAttempts > 1 {
-					log.WithFields(log.Fields{
-						"db":       s.db,
-						"attempts": catchUpAttempts,
-					}).Info("change stream catch-up retried and fetched documents")
-				}
-				catchUpAttempts = 0
 			}
 		})
 	}
