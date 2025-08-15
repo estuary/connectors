c7de0ceff46ba489011b0df768357940458f660b
Will Baker <wbaker@gmail.com>
2025-08-15 17:18:38 -0400

    filesink: add feature flag support
    Adds feature flag support to the filesink materializations.

--- materialize-azure-blob-parquet/main.go
+++ materialize-azure-blob-parquet/main.go
@@ -12,9 +12,16 @@ import (
 	"github.com/estuary/flow/go/protocols/materialize"
 )
 
+var featureFlagDefaults = map[string]bool{}
+
 type config struct {
 	filesink.AzureBlobConfig
 	ParquetConfig filesink.ParquetConfig `json:"parquetConfig,omitempty" jsonschema:"title=Parquet Configuration,description=Configuration specific to materializing parquet files."`
+	Advanced      advancedConfig         `json:"advanced,omitempty" jsonschema:"title=Advanced Options,description=Options for advanced users. You should not typically need to modify these." jsonschema_extra:"advanced=true"`
+}
+
+type advancedConfig struct {
+	FeatureFlags string `json:"feature_flags,omitempty" jsonschema:"title=Feature Flags,description=This property is intended for Estuary internal use. You should only modify this field as directed by Estuary support."`
 }
 
 func (c config) Validate() error {
@@ -25,6 +32,10 @@ func (c config) Validate() error {
 	}
 
 	return nil
+}
+
+func (c config) FeatureFlags() (string, map[string]bool) {
+	return c.Advanced.FeatureFlags, featureFlagDefaults
 }
 
 func (c config) CommonConfig() filesink.CommonConfig {
@@ -44,10 +55,10 @@ var driver = filesink.FileDriver{
 		}
 		return cfg, nil
 	},
-	NewStore: func(ctx context.Context, c filesink.Config) (filesink.Store, error) {
+	NewStore: func(ctx context.Context, c filesink.Config, featureFlags map[string]bool) (filesink.Store, error) {
 		return filesink.NewAzureBlob(ctx, c.(config).AzureBlobConfig)
 	},
-	NewWriter: func(c filesink.Config, b *pf.MaterializationSpec_Binding, w io.WriteCloser) (filesink.StreamWriter, error) {
+	NewWriter: func(c filesink.Config, featureFlags map[string]bool, b *pf.MaterializationSpec_Binding, w io.WriteCloser) (filesink.StreamWriter, error) {
 		return filesink.NewParquetWriter(c.(config).ParquetConfig, b, w)
 	},
 	NewConstraints: func(p *pf.Projection) *materialize.Response_Validated_Constraint {
