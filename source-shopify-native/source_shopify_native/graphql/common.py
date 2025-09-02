ad6dc22cdfbfe4aa2344274a243a67e39edbb616
Alex Bair <alexbair143@gmail.com>
2025-09-02 14:40:58 -0400

    source-shopify-native: fix queries' timestamps
    Not escaping timestamps in search query filters causes Shopify to ignore
    the time portion of the timestamp & fallback to using the user's
    timezone. Wrapping the timestamps in single quotes causes Shopify to
    correctly parse them & use the timezone we request (UTC).
    
    Since the timestamps in our search query filters are parsed correctly
    and Shopify no longer rounds these timestamps, the
    `round_to_latest_midnight` helper function is no longer needed.

--- source-shopify-native/source_shopify_native/graphql/common.py
+++ source-shopify-native/source_shopify_native/graphql/common.py
@@ -7,19 +7,6 @@ def dt_to_str(dt: datetime) -> str:
     return dt.strftime(DATETIME_STRING_FORMAT)
 
 
-# Shopify's bulk GraphQL queries round our "updated_at" datetime queries down to the earliest midnight.
-# Meaning, if our end datetime was 2025-01-16T12:00:00Z, then Shopify would round it down to 2025-01-16T00:00:00Z.
-# Effectively, we would miss all updates in the current day until midnight of the next day. To avoid this, the
-# round_to_latest_midnight function rounds queries' end datetimes to midnight of the next day.
-def round_to_latest_midnight(dt: datetime) -> datetime:
-    if dt.time() == datetime.min.time():
-        return dt
-    else:
-        return (dt + timedelta(days=1)).replace(
-            hour=0, minute=0, second=0, microsecond=0
-        )
-
-
 money_bag_fragment = """
 fragment _MoneyBagFields on MoneyBag {
     shopMoney {
