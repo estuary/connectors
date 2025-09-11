e926098ffb4bde2261012a303f9d0268edfa932e
Will Baker <wbaker@gmail.com>
2025-09-11 16:51:56 -0400

    wip

--- materialize-motherduck/driver_test.go
+++ materialize-motherduck/driver_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
 	"strings"
 	"testing"
 
@@ -15,39 +14,6 @@ import (
 
 	_ "github.com/marcboeker/go-duckdb/v2"
 )
-
-func mustGetCfg(t *testing.T) config {
-	if os.Getenv("TEST_DATABASE") != "yes" {
-		t.Skipf("skipping %q: ${TEST_DATABASE} != \"yes\"", t.Name())
-		return config{}
-	}
-
-	out := config{
-		StagingBucket: stagingBucketConfig{
-			StagingBucketType: stagingBucketTypeS3,
-		},
-	}
-	out.Advanced.FeatureFlags = "allow_existing_tables_for_new_bindings"
-
-	for _, prop := range []struct {
-		key  string
-		dest *string
-	}{
-		{"MOTHERDUCK_TOKEN", &out.Token},
-		{"MOTHERDUCK_DATABASE", &out.Database},
-		{"MOTHERDUCK_SCHEMA", &out.Schema},
-		{"MOTHERDUCK_BUCKET", &out.StagingBucket.BucketS3},
-		{"AWS_ACCESS_KEY_ID", &out.StagingBucket.AWSAccessKeyID},
-		{"AWS_SECRET_ACCESS_KEY", &out.StagingBucket.AWSSecretAccessKey},
-		{"AWS_REGION", &out.StagingBucket.Region},
-	} {
-		*prop.dest = os.Getenv(prop.key)
-	}
-
-	require.NoError(t, out.Validate())
-
-	return out
-}
 
 func TestIntegration(t *testing.T) {
 	makeResourceFn := func(table string, delta bool) tableConfig {
@@ -68,33 +34,23 @@ func TestIntegration(t *testing.T) {
 	t.Run("migrate", func(t *testing.T) {
 		sql.RunMigrationTest(t, newDuckDriver(), "testdata/migrate.flow.yaml", makeResourceFn)
 	})
-}
 
-func TestFencingCases(t *testing.T) {
-	var ctx = context.Background()
-
-	var cfg = mustGetCfg(t)
-
-	c, err := newClient(ctx, &sql.Endpoint[config]{Config: cfg})
-	require.NoError(t, err)
-	defer c.Close()
-
-	sql.RunFencingTest(t,
-		c,
-		[]string{cfg.Database, cfg.Schema, "temp_test_fencing_checkpoints"},
-		duckDialect,
-		tplCreateTargetTable,
-		func(table sql.Table, fence sql.Fence) error {
-			var fenceUpdate strings.Builder
-			if err := tplUpdateFence.Execute(&fenceUpdate, fence); err != nil {
-				return fmt.Errorf("evaluating fence template: %w", err)
-			}
-			return c.ExecStatements(ctx, []string{fenceUpdate.String()})
-		},
-		func(table sql.Table) (out string, err error) {
-			return sql.StdDumpTable(ctx, c.(*client).db, table)
-		},
-	)
+	t.Run("fence", func(t *testing.T) {
+		sql.RunFencingTest(
+			t,
+			newDuckDriver(),
+			"testdata/fence.flow.yaml",
+			makeResourceFn,
+			tplCreateTargetTable,
+			func(ctx context.Context, client sql.Client, fence sql.Fence) error {
+				var fenceUpdate strings.Builder
+				if err := tplUpdateFence.Execute(&fenceUpdate, fence); err != nil {
+					return fmt.Errorf("evaluating fence template: %w", err)
+				}
+				return client.ExecStatements(ctx, []string{fenceUpdate.String()})
+			},
+		)
+	})
 }
 
 func TestSpecification(t *testing.T) {
