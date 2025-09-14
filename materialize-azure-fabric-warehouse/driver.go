6b880801cf5761f68fdb092b5df690ba228f1549
Mahdi Dibaiee <mdibaiee@pm.me>
2025-09-14 09:41:09 +0100

    update no_flow_document description

--- materialize-azure-fabric-warehouse/driver.go
+++ materialize-azure-fabric-warehouse/driver.go
@@ -15,7 +15,7 @@ import (
 var featureFlagDefaults = map[string]bool{}
 
 type advancedConfig struct {
-	NoFlowDocument bool   `json:"no_flow_document,omitempty" jsonschema:"title=Exclude Flow Document,description=When enabled the flow_document column will not be materialized in destination tables.,default=false"`
+	NoFlowDocument bool   `json:"no_flow_document,omitempty" jsonschema:"title=Exclude Flow Document,description=When enabled the root document will not be required for standard updates.,default=false"`
 	FeatureFlags   string `json:"feature_flags,omitempty" jsonschema:"title=Feature Flags,description=This property is intended for Estuary internal use. You should only modify this field as directed by Estuary support."`
 }
 
