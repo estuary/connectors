02a59096993c1146542baf6d40dbf7764fe525ae
Mahdi Dibaiee <mdibaiee@pm.me>
2025-10-01 11:00:59 +0200

    materialize-%sql: add new tests for projected fields

--- tests/materialize/materialize-azure-fabric-warehouse/cleanup.sh
+++ tests/materialize/materialize-azure-fabric-warehouse/cleanup.sh
@@ -17,5 +17,6 @@ dropTable "string_escaped_key"
 dropTable "all_key_types_part_one"
 dropTable "all_key_types_part_two"
 dropTable "all_key_types_part_three"
+dropTable "fields_with_projections"
 
 go run ${TEST_DIR}/materialize-azure-fabric-warehouse/fetch-data.go --delete-checkpoint notable
--- tests/materialize/materialize-bigquery/cleanup.sh
+++ tests/materialize/materialize-bigquery/cleanup.sh
@@ -17,6 +17,7 @@ dropTable "string_escaped_key"
 dropTable "all_key_types_part_one"
 dropTable "all_key_types_part_two"
 dropTable "all_key_types_part_three"
+dropTable "fields_with_projections"
 
 # Remove the persisted materialization spec & checkpoint for this test materialization so subsequent
 # runs start from scratch.
