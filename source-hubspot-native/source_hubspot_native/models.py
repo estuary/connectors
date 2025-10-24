6f0c2c018b9e3df12c13fce627709d189a309e82
Alex Bair <alexbair143@gmail.com>
2025-10-24 13:44:22 -0400

    source-hubspot-native: add `goals` stream
    Note: there is no `goals` resource in the discovery snapshot because our
    test credentials in `config.yaml` don't have sufficient permissions to
    access the associated endpoints.

--- source-hubspot-native/source_hubspot_native/models.py
+++ source-hubspot-native/source_hubspot_native/models.py
@@ -161,6 +161,7 @@ class Names(StrEnum):
     contact_lists = auto()
     contact_list_memberships = auto()
     feedback_submissions = auto()
+    goals = auto()
 
 
 # A Property is a HubSpot or HubSpot-user defined attribute that's
@@ -369,6 +370,10 @@ class LineItem(BaseCRMObject):
 
 
 class FeedbackSubmission(BaseCRMObject):
+    ASSOCIATED_ENTITIES = []
+
+
+class Goals(BaseCRMObject):
     ASSOCIATED_ENTITIES = []
 
 
