b1e94d5c61d64f2ad7b1676c09375e8f63e09689
Alex Bair <alexbair143@gmail.com>
2025-11-14 09:36:46 -0500

    source-salesforce-native: add a capture snapshot test
    The capture snapshot test only includes two bindings, but that's ok.
    Some of the value of the capture snapshot test is ensuring the connector
    starts up & captures documents without error. That would have caught
    the SourcedSchema bug with Pydantic v2 in the previous commit.

--- source-salesforce-native/tests/test_snapshots.py
+++ source-salesforce-native/tests/test_snapshots.py
@@ -4,6 +4,60 @@ import subprocess
 from source_salesforce_native.supported_standard_objects import SUPPORTED_STANDARD_OBJECTS
 
 
+
+def test_capture(request, snapshot):
+    TIMESTAMP_FIELDS = [
+        "SystemModstamp",
+        "LastModifiedDate",
+        "CreatedDate",
+        "LoginTime",
+        "LastLoginDate",
+        "LastPasswordChangeDate",
+        "LastReferenceDate",
+        "LastViewedDate",
+        "PasswordExpirationDate",
+        "my_time_field__c",
+    ]
+
+    result = subprocess.run(
+        [
+            "flowctl",
+            "preview",
+            "--source",
+            request.fspath.dirname + "/../test.flow.yaml",
+            "--sessions",
+            "1",
+            "--delay",
+            "30s",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
+
+    unique_stream_lines = []
+    seen = set()
+
+    for line in lines:
+        stream = line[0]
+        if stream not in seen:
+            unique_stream_lines.append(line)
+            seen.add(stream)
+
+    for l in unique_stream_lines:
+        stream, rec = l[0], l[1]
+
+        for field in TIMESTAMP_FIELDS:
+            if field in rec:
+                rec[field] = "redacted"
+
+    # Sort lines to keep a consistent ordering of captured bindings.
+    sorted_unique_lines = sorted(unique_stream_lines, key=lambda l: l[0])
+
+    assert snapshot("capture.stdout.json") == sorted_unique_lines
+
+
 def test_discover(request, snapshot):
     enabled_bindings: list[str] = []
     for name in SUPPORTED_STANDARD_OBJECTS.keys():
