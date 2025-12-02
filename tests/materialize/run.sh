49437bfd95749ca3b5fb8cba02f2f0791f0b5771
Mahdi Dibaiee <mdibaiee@pm.me>
2025-12-02 16:43:28 +0100

    tests: switch to streaming fixture format

--- tests/materialize/run.sh
+++ tests/materialize/run.sh
@@ -104,8 +104,8 @@ function drive_connector {
     local fixture=$2;
 
     if [[ "${PERF_MODE}" == "true" ]]; then
-        # Drive the connector, writing its output into the snapshot.
-        RUST_LOG=info flowctl preview --source ${source} --fixture ${fixture} --streaming-fixture --output-state --output-apply --network flow-test \
+        # Drive the connector without writing output into the snapshot.
+        RUST_LOG=info flowctl preview --source ${source} --fixture ${fixture} --output-state --output-apply --network flow-test \
         || bail "connector invocation failed"
     else
         # Drive the connector, writing its output into the snapshot.
@@ -131,11 +131,7 @@ else
 fi
 
 # Drive it again to excercise any cleanup behavior when bindings are removed.
-if [[ "${PERF_MODE}" == "true" ]]; then
-    drive_connector ${TEMP_DIR}/empty.flow.json ${TEST_DIR}/empty.streaming_fixture.json
-else
-    drive_connector ${TEMP_DIR}/empty.flow.json ${TEST_DIR}/empty.fixture.json
-fi
+drive_connector ${TEMP_DIR}/empty.flow.json ${TEST_DIR}/empty.fixture.json
 
 if [[ -f "$CONNECTOR_TEST_DIR/checks.sh" ]]; then
   # Run connector-specific checks
