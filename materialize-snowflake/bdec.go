030a8fdca9f6781d58a863ed541771fa2d184ee5
Daniel Nelson <daniel@wavesofdawn.com>
2026-02-13 14:45:22 -0800

    materialize-snowflake: re-encrypt blob if needed (#3899)
    Adds the channel encryption key to the checkpoint.  If the channel
    encryption key has changed, recreate the blob with the new encryption
    key decrypting it with the key from the checkpoint.
    
    Consolidate the handling of "blob has wrong format or extension" into
    the recovery logic.

--- materialize-snowflake/bdec.go
+++ materialize-snowflake/bdec.go
@@ -416,22 +416,24 @@ func getCipherStream(encryptionKey string, fileName blobFileName) (cipher.Stream
 	return cipher.NewCTR(block, make([]byte, aes.BlockSize)), nil
 }
 
-// reencrypt reads encrypted blob data from r, decrypts it using the encryption
-// key and original file name, and then re-encrypts it using the new file name
-// and writes the output to w. The blob metadata is updated in place.
+// reencrypt reads encrypted blob data from r, decrypts it using the
+// decryptKey and original file name, and then re-encrypts it using the
+// new file name and writes the output to w. The blob metadata is updated in
+// place.
 func reencrypt(
 	r io.Reader,
 	w io.Writer,
 	blob *blobMetadata,
-	encryptionKey string,
+	decryptKey string,
+	channel *channel,
 	newFileName blobFileName,
 ) error {
-	decryptStream, err := getCipherStream(encryptionKey, blobFileName(blob.Path))
+	decryptStream, err := getCipherStream(decryptKey, blobFileName(blob.Path))
 	if err != nil {
 		return fmt.Errorf("getting decryptStream: %w", err)
 	}
 
-	encryptStream, err := getCipherStream(encryptionKey, newFileName)
+	encryptStream, err := getCipherStream(channel.EncryptionKey, newFileName)
 	if err != nil {
 		return fmt.Errorf("getting encryptStream: %w", err)
 	}
@@ -479,7 +481,6 @@ func reencrypt(
 		if err != nil && !errors.Is(err, io.EOF) {
 			return fmt.Errorf("reading from r: %w", err)
 		}
-		fmt.Println(n, err)
 	}
 
 	downHash := hex.EncodeToString(downMd5.Sum(nil))
@@ -491,6 +492,7 @@ func reencrypt(
 	blob.Path = string(newFileName)
 	blob.MD5 = upHash
 	blob.Chunks[0].ChunkMD5 = upHash
+	blob.Chunks[0].EncryptionKeyID = channel.EncryptionKeyId
 
 	return nil
 }
