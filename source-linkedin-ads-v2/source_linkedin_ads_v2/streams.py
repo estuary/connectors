b3b034536dfdccc0967c2ec81adb24a7741d77b4
Alex Bair <alexbair143@gmail.com>
2026-02-23 09:25:22 -0500

    source-linkedin-ads-v2: update API version to 202601
    The connector was using API version 202502, which was sunset on
    16FEB2026 and caused all existing captures to break. This commit updates
    the connector to use API version 202602.
    
    No notable breaking changes were seen in the [API changelog](https://learn.microsoft.com/en-us/linkedin/marketing/integrations/recent-changes?view=li-lms-2026-02#february-2026---version-202602-latest)
    for this API version bump.

--- source-linkedin-ads-v2/source_linkedin_ads_v2/streams.py
+++ source-linkedin-ads-v2/source_linkedin_ads_v2/streams.py
@@ -19,7 +19,7 @@ from .utils import get_parent_stream_values, transform_data
 
 logger = logging.getLogger("airbyte")
 
-LINKEDIN_VERSION_API = "202502"
+LINKEDIN_VERSION_API = "202602"
 
 
 class LinkedinAdsStream(HttpStream, ABC):
