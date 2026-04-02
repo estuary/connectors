7b11710835b44c4d7be908f6371f233e1ef41277
Mahdi Dibaiee <mdibaiee@pm.me>
2026-04-02 10:08:32 +0100

    fix mysql and databricks tests

--- materialize-databricks/driver_test.go
+++ materialize-databricks/driver_test.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"regexp"
 	"testing"
 
 	sql "github.com/estuary/connectors/materialize-sql"
@@ -20,13 +21,20 @@ func TestIntegration(t *testing.T) {
 		}
 	}
 
+	sanitizers := []func(string) string{
+		func(s string) string {
+			return regexp.MustCompile(`[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}`).
+				ReplaceAllString(s, "<uuid>")
+		},
+	}
+
 	t.Run("materialize", func(t *testing.T) {
-		sql.RunMaterializationTest(t, newDatabricksDriver(), "testdata/materialize.flow.yaml", makeResourceFn, nil)
+		sql.RunMaterializationTest(t, newDatabricksDriver(), "testdata/materialize.flow.yaml", makeResourceFn, sanitizers)
 	})
 	t.Run("apply", func(t *testing.T) {
 		sql.RunApplyTest(t, newDatabricksDriver(), "testdata/apply.flow.yaml", makeResourceFn)
 	})
 	t.Run("migrate", func(t *testing.T) {
-		sql.RunMigrationTest(t, newDatabricksDriver(), "testdata/migrate.flow.yaml", makeResourceFn, nil)
+		sql.RunMigrationTest(t, newDatabricksDriver(), "testdata/migrate.flow.yaml", makeResourceFn, sanitizers)
 	})
 }
