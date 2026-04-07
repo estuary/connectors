230acadefe56cc6897b6dc9e93b63fe758b6751f
Jacob Marble <jacobmarble@gmail.com>
2026-04-07 23:28:48 +0000

    properly order username/password

--- materialize-clickhouse/driver.go
+++ materialize-clickhouse/driver.go
@@ -30,8 +30,8 @@ const (
 )
 
 type usernamePassword struct {
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
+	Username string `json:"username,omitempty" jsonschema:"title=Username,description=Username for authentication." jsonschema_extras:"order=0"`
+	Password string `json:"password,omitempty" jsonschema:"title=Password,description=Password for authentication." jsonschema_extras:"order=1,secret=true"`
 }
 
 type credentialConfig struct {
@@ -63,9 +63,9 @@ func (credentialConfig) JSONSchema() *jsonschema.Schema {
 
 type config struct {
 	Address     string           `json:"address" jsonschema:"title=Address,description=Host and port of the database (in the form of host[:port]). Port 9000 is used as the default if no specific port is provided." jsonschema_extras:"order=0"`
-	Database    string           `json:"database" jsonschema:"title=Database,description=Name of the ClickHouse database to materialize to." jsonschema_extras:"order=2"`
-	HardDelete  bool             `json:"hardDelete,omitempty" jsonschema:"title=Hard Delete,description=If this option is enabled items deleted in the source will also be deleted from the destination. By default this is disabled and _meta/op in the destination will signify whether rows have been deleted (soft-delete).,default=false" jsonschema_extras:"order=3"`
-	Credentials credentialConfig `json:"credentials" jsonschema:"title=Authentication" jsonschema_extras:"order=1"`
+	Database    string           `json:"database" jsonschema:"title=Database,description=Name of the ClickHouse database to materialize to." jsonschema_extras:"order=1"`
+	HardDelete  bool             `json:"hardDelete,omitempty" jsonschema:"title=Hard Delete,description=If this option is enabled items deleted in the source will also be deleted from the destination. By default this is disabled and _meta/op in the destination will signify whether rows have been deleted (soft-delete).,default=false" jsonschema_extras:"order=2"`
+	Credentials credentialConfig `json:"credentials" jsonschema:"title=Authentication" jsonschema_extras:"order=3"`
 
 	Advanced advancedConfig `json:"advanced,omitempty" jsonschema:"title=Advanced Options,description=Options for advanced users. You should not typically need to modify these." jsonschema_extras:"advanced=true"`
 }
