f3edbd3792fdcb1db772ce8a79c21a150a9e62d8
Jacob Marble <jacobmarble@gmail.com>
2026-04-08 00:27:31 +0000

    export delta tables in integration test fetch script

--- tests/materialize/materialize-clickhouse/fetch.sh
+++ tests/materialize/materialize-clickhouse/fetch.sh
@@ -4,15 +4,23 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# All tables use ReplacingMergeTree to deduplicate and hard-delete delete rows asynchronously.
+# Standard tables use ReplacingMergeTree with _is_deleted for deduplication and hard deletes.
 # FINAL filters duplicate and deleted rows at query time.
 function exportTable() {
   query "SELECT * EXCEPT (_is_deleted) FROM \`$1\` FINAL ORDER BY flow_published_at, id FORMAT JSON;" \
     | jq -c "{ table: \"$1\", rows: .data | map(del(.flow_document)) }"
 }
 
+# Delta-update tables use plain MergeTree and have no _is_deleted column.
+function exportDeltaTable() {
+  query "SELECT * FROM \`$1\` FINAL ORDER BY flow_published_at FORMAT JSON;" \
+    | jq -c "{ table: \"$1\", rows: .data | map(del(.flow_document)) }"
+}
+
 exportTable "simple"
 exportTable "duplicate_keys_standard"
+exportDeltaTable "duplicate_keys_delta"
+exportDeltaTable "duplicate_keys_delta_exclude_flow_doc"
 exportTable "multiple_types"
 exportTable "formatted_strings"
 exportTable "long_string"
