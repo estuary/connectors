5b34fe2740a6d5727ff9ead41476ac6e501a8514
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2026-05-11 15:10:39 -0300

    source-quickbooks: miscellaneous QoL tweaks
    - Adds a "use sandbox environment" flag to let users try the connector
      using test data
    - Updates import paths to make the connector stay closer to current
      coding standards
    
    Signed-off-by: nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>

--- source-quickbooks/source_quickbooks/__init__.py
+++ source-quickbooks/source_quickbooks/__init__.py
@@ -1,9 +1,7 @@
+from collections.abc import Awaitable
 from logging import Logger
-from typing import Callable, Awaitable
+from typing import Callable
 
-from estuary_cdk.flow import (
-    ConnectorSpec,
-)
 from estuary_cdk.capture import (
     BaseCaptureConnector,
     Request,
@@ -12,41 +10,43 @@ from estuary_cdk.capture import (
     request,
     response,
 )
+from estuary_cdk.flow import (
+    ConnectorSpec,
+)
 
-from .resources import all_resources, validate_credentials
 from .models import (
     OAUTH2_SPEC,
     ConnectorState,
     EndpointConfig,
-    ResourceConfig,
 )
+from .resources import all_resources, validate_credentials
 
 
 class Connector(
-    BaseCaptureConnector[EndpointConfig, ResourceConfig, ConnectorState],
+    BaseCaptureConnector[EndpointConfig, common.ResourceConfig, ConnectorState],
 ):
     def request_class(self):
-        return Request[EndpointConfig, ResourceConfig, ConnectorState]
+        return Request[EndpointConfig, common.ResourceConfig, ConnectorState]
 
     async def spec(self, _: request.Spec, logger: Logger) -> ConnectorSpec:
         return ConnectorSpec(
             configSchema=EndpointConfig.model_json_schema(),
             oauth2=OAUTH2_SPEC,
             documentationUrl="https://go.estuary.dev/source-quickbooks",
-            resourceConfigSchema=ResourceConfig.model_json_schema(),
-            resourcePathPointers=ResourceConfig.PATH_POINTERS,
+            resourceConfigSchema=common.ResourceConfig.model_json_schema(),
+            resourcePathPointers=common.ResourceConfig.PATH_POINTERS,
         )
 
     async def discover(
         self, log: Logger, discover: request.Discover[EndpointConfig]
-    ) -> response.Discovered[ResourceConfig]:
+    ) -> response.Discovered[common.ResourceConfig]:
         resources = await all_resources(log, self, discover.config)
         return common.discovered(resources)
 
     async def validate(
         self,
         log: Logger,
-        validate: request.Validate[EndpointConfig, ResourceConfig],
+        validate: request.Validate[EndpointConfig, common.ResourceConfig],
     ) -> response.Validated:
         await validate_credentials(self, validate.config, log)
         resources = await all_resources(log, self, validate.config)
@@ -56,7 +56,7 @@ class Connector(
     async def open(
         self,
         log: Logger,
-        open: request.Open[EndpointConfig, ResourceConfig, ConnectorState],
+        open: request.Open[EndpointConfig, common.ResourceConfig, ConnectorState],
     ) -> tuple[response.Opened, Callable[[Task], Awaitable[None]]]:
         resources = await all_resources(log, self, open.capture.config)
         resolved = common.resolve_bindings(open.capture.bindings, resources)
--- source-quickbooks/source_quickbooks/resources.py
+++ source-quickbooks/source_quickbooks/resources.py
@@ -1,13 +1,22 @@
 import functools
 from datetime import UTC, datetime, timedelta
 from logging import Logger
-from typing import Type, TypeVar
+from typing import TypeVar
 
-from estuary_cdk.capture import Task, common
+from estuary_cdk.capture import Task
+from estuary_cdk.capture.common import (
+    BaseDocument,
+    Resource,
+    ResourceConfig,
+    ResourceState,
+    open_binding,
+)
 from estuary_cdk.flow import CaptureBinding, ValidationError
 from estuary_cdk.http import HTTPError, HTTPMixin, TokenSource
 
 from .api import (
+    PROD_API_BASE_URL,
+    SANDBOX_API_BASE_URL,
     backfill_entity,
     fetch_entity,
     query_entity,
@@ -19,8 +28,6 @@ from .models import (
     Account,
     EndpointConfig,
     QuickBooksEntity,
-    ResourceConfig,
-    ResourceState,
 )
 
 
@@ -29,10 +36,18 @@ async def validate_credentials(http: HTTPMixin, config: EndpointConfig, log: Log
         oauth_spec=OAUTH2_SPEC, credentials=config.credentials
     )
 
+    base_url = SANDBOX_API_BASE_URL if config.is_sandbox else PROD_API_BASE_URL
+
     try:
-        await anext(
+        _ = await anext(
             query_entity(
-                Account, EPOCH, datetime.now(tz=UTC), http, config.realm_id, log
+                Account,
+                EPOCH,
+                datetime.now(tz=UTC),
+                http,
+                base_url,
+                config.realm_id,
+                log,
             )
         )
     except HTTPError as err:
@@ -52,41 +67,44 @@ async def all_resources(
     _log: Logger,
     http: HTTPMixin,
     config: EndpointConfig,
-) -> list[common.Resource]:
+) -> list[Resource[BaseDocument, ResourceConfig, ResourceState]]:
     http.token_source = TokenSource(
         oauth_spec=OAUTH2_SPEC, credentials=config.credentials
     )
+
+    base_url = SANDBOX_API_BASE_URL if config.is_sandbox else PROD_API_BASE_URL
 
     # API works with second precision
     start_date = config.start_date.replace(microsecond=0)
     cutoff = datetime.now(tz=UTC).replace(microsecond=0)
 
     def open_all_bindings(
-        entity: Type[QuickBooksEntity],
+        entity: type[QuickBooksEntity],
         binding: CaptureBinding[ResourceConfig],
         binding_index: int,
         state: ResourceState,
         task: Task,
         _all_bindings,
     ):
-        common.open_binding(
+        open_binding(
             binding,
             binding_index,
             state,
             task,
             fetch_changes=functools.partial(
-                fetch_entity, entity, http, config.realm_id
+                fetch_entity, entity, http, base_url, config.realm_id
             ),
             fetch_page=functools.partial(
                 backfill_entity,
                 entity,
                 http,
+                base_url,
                 config.realm_id,
             ),
         )
 
     return [
-        common.Resource(
+        Resource(
             name=resource.resource_name,
             key=["/Id"],
             model=resource,
@@ -95,7 +113,7 @@ async def all_resources(
                 backfill=ResourceState.Backfill(
                     cutoff=cutoff, next_page=start_date.isoformat(timespec="seconds")
                 ),
-                inc=common.ResourceState.Incremental(cursor=cutoff),
+                inc=ResourceState.Incremental(cursor=cutoff),
             ),
             initial_config=ResourceConfig(
                 name=resource.resource_name, interval=timedelta(minutes=5)
