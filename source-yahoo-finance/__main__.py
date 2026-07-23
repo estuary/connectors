60cd2c14082fd783bee009668143311d4f54a08f
Alex Bair <alexbair143@gmail.com>
2026-07-23 09:21:15 -0400

    source-{python}: remove purely proof of concept connectors
    The `source-pokemon` and `source-yahoo-finance` connectors are early proof-of-concept connectors for our `python` library - our initial attempt at a CDK before the `estuary-cdk`. We've never published these two connectors, and we keep getting dependabot updates for them, so I'd like to remove them altogether.
    
    I'd also like to eventually remove the `python` library eventually since the `estuary-cdk` is the library we've been building out & using, but `python` contains a shim that we use to run a small handful of singer taps so we'd need to sort that out first before removing our `python` library.

--- source-yahoo-finance/__main__.py
+++
@@ -1,7 +0,0 @@
-from flow_sdk import shim_singer_sdk
-from tap_investing.tap import TapInvesting
-
-shim_singer_sdk.CaptureShim(
-    config_schema=TapInvesting.config_jsonschema,
-    delegate_factory=TapInvesting,
-).main()
